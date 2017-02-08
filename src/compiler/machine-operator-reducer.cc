b77777dbcb33ee1a0d1f5e6789b1c04d3bb06cdc
Andreas Haas <ahaas@chromium.org>
2017-02-08 16:19:43 +0100

    [turbofan] For Word32Shl optimizations only consider the last 5 bits of the shift
    One optimization in the machine-operator-reducer did not consider that
    that word32 shift left instructions only consider the last 5 bits of
    the shift input.
    
    The issue only occurs for WebAssembly because in JavaScript we always
    add a "& 0xf" on the shift value to the TurboFan graph.
    
    R=mstarzinger@chromium.org
    BUG=chromium:689450
    
    Change-Id: Id92f298ed6d7f1714b109b3f4fbcecd5ac6d30f7

--- src/compiler/machine-operator-reducer.cc
+++ src/compiler/machine-operator-reducer.cc
@@ -1180,7 +1180,8 @@ Reduction MachineOperatorReducer::ReduceWord32And(Node* node) {
     if (m.left().IsWord32Shl()) {
       Uint32BinopMatcher mleft(m.left().node());
       if (mleft.right().HasValue() &&
-          mleft.right().Value() >= base::bits::CountTrailingZeros32(mask)) {
+          (mleft.right().Value() & 0x1f) >=
+              base::bits::CountTrailingZeros32(mask)) {
         // (x << L) & (-1 << K) => x << L iff K >= L
         return Replace(mleft.node());
       }
