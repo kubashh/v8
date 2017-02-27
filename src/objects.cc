7086559611f7f3b29fa7a671ee87772a1a13d561
Georg Neis <neis@chromium.org>
2017-02-27 17:16:15 +0100

    [modules] Make Module::Evaluate return the completion value.
    This is more useful than always returning undefined.
    
    BUG=v8:1569,v8:5978
    
    Change-Id: Id10cf87f7865db1a85de412460eaead4e4bf3b62

--- src/objects.cc
+++ src/objects.cc
@@ -19937,7 +19937,16 @@ MaybeHandle<Object> Module::Evaluate(Handle<Module> module) {
   // Evaluation of module body.
   Handle<JSFunction> resume(
       isolate->native_context()->generator_next_internal(), isolate);
-  return Execution::Call(isolate, resume, generator, 0, nullptr);
+  Handle<Object> result;
+  ASSIGN_RETURN_ON_EXCEPTION(
+      isolate, result, Execution::Call(isolate, resume, generator, 0, nullptr),
+      Object);
+  DCHECK(static_cast<JSIteratorResult*>(JSObject::cast(*result))
+             ->done()
+             ->BooleanValue());
+  return handle(
+      static_cast<JSIteratorResult*>(JSObject::cast(*result))->value(),
+      isolate);
 }
 
 namespace {
