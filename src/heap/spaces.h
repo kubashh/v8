5adfdadc2690fee6fb658d4898327c77b7262e4e
Michael Lippautz <mlippautz@chromium.org>
2017-03-01 16:31:22 +0100

    [heap] Support for minor MC marking during incremental marking part 1
    First part of relanding d2c093bcafb0ec4795078424176e40ccb5f715aa.
    
    BUG=chromium:651354
    
    Change-Id: I34ebea331d482d5039626ccff48b11ad175793ee

--- src/heap/spaces.h
+++ src/heap/spaces.h
@@ -224,6 +224,10 @@ class FreeListCategory {
   friend class PagedSpace;
 };
 
+// MarkingMode determines which bitmaps and counters should be used when
+// accessing marking information on MemoryChunk.
+enum class MarkingMode { FULL, YOUNG_GENERATION };
+
 // MemoryChunk represents a memory region owned by a specific space.
 // It is divided into the header and the body. Chunk start is always
 // 1MB aligned. Start of the body is aligned so it can accommodate
@@ -370,6 +374,7 @@ class MemoryChunk {
 
   static const int kAllocatableMemory = kPageSize - kObjectStartOffset;
 
+  template <MarkingMode mode = MarkingMode::FULL>
   static inline void IncrementLiveBytes(HeapObject* object, int by);
 
   // Only works if the pointer is in the first kPageSize of the MemoryChunk.
@@ -395,7 +400,9 @@ class MemoryChunk {
 
   static bool IsValid(MemoryChunk* chunk) { return chunk != nullptr; }
 
-  Address address() { return reinterpret_cast<Address>(this); }
+  Address address() const {
+    return reinterpret_cast<Address>(const_cast<MemoryChunk*>(this));
+  }
 
   base::Mutex* mutex() { return mutex_; }
 
@@ -489,6 +496,7 @@ class MemoryChunk {
     }
   }
 
+  template <MarkingMode mode = MarkingMode::FULL>
   inline Bitmap* markbits() {
     return Bitmap::FromAddress(address() + kHeaderSize);
   }
