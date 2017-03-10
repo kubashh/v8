38bc0992f0720cb8f5c6e38864bc65aa9c478b04
Ross McIlroy <rmcilroy@chromium.org>
2017-03-10 14:18:01 +0000

    [Compiler] Remove CompileBaseline and three tier pipeline.
    Since we no longer support the ignition-staging configuration 
    any longer,  we can retire the three tier pipeline and the
    CompileBaseline functionallity.
    
    We still need support for JSFunction self healing due to
    liveedit (which for --no-turbo might end up replacing a
    forced Ignition function with a FCG function) - we can
    remove this once we remove --no-turbo support.
    
    BUG=v8:4280
    
    Change-Id: I5482abd17785324654e022affd6bdb555b19b181

--- src/objects-inl.h
+++ src/objects-inl.h
@@ -6121,7 +6121,6 @@ bool SharedFunctionInfo::is_compiled() const {
   Builtins* builtins = GetIsolate()->builtins();
   DCHECK(code() != builtins->builtin(Builtins::kCompileOptimizedConcurrent));
   DCHECK(code() != builtins->builtin(Builtins::kCompileOptimized));
-  DCHECK(code() != builtins->builtin(Builtins::kCompileBaseline));
   return code() != builtins->builtin(Builtins::kCompileLazy);
 }
 
@@ -6352,11 +6351,6 @@ bool JSFunction::IsInterpreted() {
   return code()->is_interpreter_trampoline_builtin();
 }
 
-bool JSFunction::IsMarkedForBaseline() {
-  return code() ==
-         GetIsolate()->builtins()->builtin(Builtins::kCompileBaseline);
-}
-
 bool JSFunction::IsMarkedForOptimization() {
   return code() == GetIsolate()->builtins()->builtin(
       Builtins::kCompileOptimized);
@@ -6539,7 +6533,6 @@ Object* JSFunction::prototype() {
 bool JSFunction::is_compiled() {
   Builtins* builtins = GetIsolate()->builtins();
   return code() != builtins->builtin(Builtins::kCompileLazy) &&
-         code() != builtins->builtin(Builtins::kCompileBaseline) &&
          code() != builtins->builtin(Builtins::kCompileOptimized) &&
          code() != builtins->builtin(Builtins::kCompileOptimizedConcurrent);
 }
