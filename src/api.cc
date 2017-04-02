578790ccb4edc1024e9510ef10fcf7b12eb58cce
Caitlin Potter <caitp@igalia.com>
2017-04-02 16:03:56 -0400

    [api] fix incorrect parameter end position in CompileFunctionInContext
    With --harmony-function-tostring enabled (now enabled by --harmony),
    CompileFunctionInContext would produce incorrect results whenever called
    with 1 or more argument parameters, due to specifying an incorrect end
    position for the parameters.
    
    BUG=v8:6190, v8:4958
    R=littledan@chromium.org, adamk@chromium.org, jwolfe@igalia.com
    
    Change-Id: Ied2bcba44116311ebcae3967963472b4e1058fd3

--- src/api.cc
+++ src/api.cc
@@ -2297,7 +2297,7 @@ MaybeLocal<Function> ScriptCompiler::CompileFunctionInContext(
     i::Handle<i::String> brackets;
     if (i::FLAG_harmony_function_tostring) {
       brackets = factory->NewStringFromStaticChars("\n) {");
-      parameters_end_pos = source_string->length() - 3;
+      parameters_end_pos = source_string->length() + 1;
     } else {
       brackets = factory->NewStringFromStaticChars("){");
     }
