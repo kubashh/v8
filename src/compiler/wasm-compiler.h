d0cb38786e5f7cb674efa5a241140fa6bf3aa2b9
Andreas Haas <ahaas@chromium.org>
2017-04-05 17:54:14 +0200

    Reland [wasm] Make WebAssembly.compile() asynchronous
    Change-Id: Ia42c46ec6bc73179cb1f458e36658414ff85cc23

--- src/compiler/wasm-compiler.h
+++ src/compiler/wasm-compiler.h
@@ -55,6 +55,7 @@ class WasmCompilationUnit final {
   Zone* graph_zone() { return graph_zone_.get(); }
   int func_index() const { return func_index_; }
 
+  void InitializeHandles();
   void ExecuteCompilation();
   Handle<Code> FinishCompilation(wasm::ErrorThrower* thrower);
 
