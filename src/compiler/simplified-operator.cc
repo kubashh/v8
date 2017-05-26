57d07fa7782e0c5f9575c5f7e9f4f532035c7bf6
Mythri <mythria@chromium.org>
2017-05-26 16:24:20 +0100

    [Interpreter] Introduce ThrowIfHole / ThrowIfNotHole bytecodes.
    Introduces ThrowIfHole / ThrowIfNotHole bytecodes to handle hole checks.
    In the bytecode-graph builder they are handled by introducing a deopt point
    instead of adding explicit control flow. JumpIfNotHole / JumpIfNotHoleConstant
    bytecodes are removed since they are no longer required.
    
    
    Bug:v8:4280, v8:6383
    Change-Id: I58b70c556b0ffa30e41a0cd44016874c3e9c5fe1

--- src/compiler/simplified-operator.cc
+++ src/compiler/simplified-operator.cc
@@ -525,6 +525,7 @@ UnicodeEncoding UnicodeEncodingOf(const Operator* op) {
   V(CheckString, 1, 1)                  \
   V(CheckSymbol, 1, 1)                  \
   V(CheckTaggedHole, 1, 1)              \
+  V(CheckNotTaggedHole, 1, 1)           \
   V(CheckedInt32Add, 2, 1)              \
   V(CheckedInt32Sub, 2, 1)              \
   V(CheckedInt32Div, 2, 1)              \
