96c77cc1bb4bb89f263cd290b007c3b1ada9c5b7
Mythri <mythria@chromium.org>
2017-06-01 10:27:27 +0100

    [Interpreter] Introduce ThrowIfHole / ThrowIfNotHole bytecodes.
    Introduces ThrowIfHole / ThrowIfNotHole bytecodes to handle hole checks.
    In the bytecode-graph builder they are handled by introducing a deopt point
    instead of adding explicit control flow. JumpIfNotHole / JumpIfNotHoleConstant
    bytecodes are removed since they are no longer required.
    
    
    Bug:v8:4280, v8:6383
    Change-Id: I58b70c556b0ffa30e41a0cd44016874c3e9c5fe1

--- src/compiler/bytecode-graph-builder.cc
+++ src/compiler/bytecode-graph-builder.cc
@@ -440,6 +440,7 @@ BytecodeGraphBuilder::BytecodeGraphBuilder(
     Zone* local_zone, Handle<SharedFunctionInfo> shared_info,
     Handle<FeedbackVector> feedback_vector, BailoutId osr_ast_id,
     JSGraph* jsgraph, CallFrequency invocation_frequency,
+    CompilationDependencies* dependencies,
     SourcePositionTable* source_positions, int inlining_id,
     JSTypeHintLowering::Flags flags)
     : local_zone_(local_zone),
@@ -458,6 +459,7 @@ BytecodeGraphBuilder::BytecodeGraphBuilder(
       bytecode_analysis_(nullptr),
       environment_(nullptr),
       osr_ast_id_(osr_ast_id),
+      dependencies_(dependencies),
       merge_environments_(local_zone),
       exception_handlers_(local_zone),
       current_exception_handler_(0),
@@ -1678,6 +1680,91 @@ void BytecodeGraphBuilder::VisitReThrow() {
   MergeControlToLeaveFunction(control);
 }
 
+void BytecodeGraphBuilder::BuildHoleCheckWithDeopt(
+    const Operator* hole_check_operator) {
+  DCHECK(jsgraph()->isolate()->IsHoleCheckProtectorIntact());
+  dependencies()->AssumePropertyCell(
+      jsgraph()->isolate()->factory()->hole_check_protector());
+  PrepareEagerCheckpoint();
+  Node* accumulator = environment()->LookupAccumulator();
+  NewNode(hole_check_operator, accumulator);
+  environment()->BindAccumulator(accumulator);
+}
+
+void BytecodeGraphBuilder::BuildHoleCheckAndThrow(
+    Node* condition, Runtime::FunctionId runtime_id, Node* name) {
+  Node* accumulator = environment()->LookupAccumulator();
+  NewBranch(condition);
+  {
+    SubEnvironment sub_environment(this);
+
+    NewIfTrue();
+    Node* node;
+    const Operator* op = javascript()->CallRuntime(runtime_id);
+    if (runtime_id == Runtime::kThrowReferenceError) {
+      DCHECK(name != nullptr);
+      node = NewNode(op, name);
+    } else {
+      DCHECK(runtime_id == Runtime::kThrowSuperAlreadyCalledError ||
+             runtime_id == Runtime::kThrowSuperNotCalled);
+      node = NewNode(op);
+    }
+    environment()->RecordAfterState(node, Environment::kAttachFrameState);
+    Node* control = NewNode(common()->Throw());
+    MergeControlToLeaveFunction(control);
+  }
+  NewIfFalse();
+  environment()->BindAccumulator(accumulator);
+}
+
+void BytecodeGraphBuilder::VisitThrowReferenceErrorIfHole() {
+  // To avoid deopt loops, we generate regular control flow, if we ever saw
+  // a throw because of the hole check.
+  if (jsgraph()->isolate()->IsHoleCheckProtectorIntact()) {
+    // Bailout if the value is the Hole.
+    BuildHoleCheckWithDeopt(simplified()->CheckNotTaggedHole());
+  } else {
+    // Generate the control flow to avoid deopt loop.
+    Node* accumulator = environment()->LookupAccumulator();
+    Node* check_for_hole = NewNode(simplified()->ReferenceEqual(), accumulator,
+                                   jsgraph()->TheHoleConstant());
+    Node* name =
+        jsgraph()->Constant(bytecode_iterator().GetConstantForIndexOperand(0));
+    BuildHoleCheckAndThrow(check_for_hole, Runtime::kThrowReferenceError, name);
+  }
+}
+
+void BytecodeGraphBuilder::VisitThrowSuperNotCalledIfHole() {
+  // To avoid deopt loops, we generate regular control flow, if we ever saw
+  // a throw because of the hole check.
+  if (jsgraph()->isolate()->IsHoleCheckProtectorIntact()) {
+    // Bailout if the value is the Hole.
+    BuildHoleCheckWithDeopt(simplified()->CheckNotTaggedHole());
+  } else {
+    Node* accumulator = environment()->LookupAccumulator();
+    Node* check_for_hole = NewNode(simplified()->ReferenceEqual(), accumulator,
+                                   jsgraph()->TheHoleConstant());
+    BuildHoleCheckAndThrow(check_for_hole, Runtime::kThrowSuperNotCalled);
+  }
+}
+
+void BytecodeGraphBuilder::VisitThrowSuperAlreadyCalledIfNotHole() {
+  // To avoid deopt loops, we generate regular control flow, if we ever saw
+  // a throw because of the hole check.
+  if (jsgraph()->isolate()->IsHoleCheckProtectorIntact()) {
+    // Bailout if the value is not the Hole.
+    BuildHoleCheckWithDeopt(simplified()->CheckTaggedHole());
+  } else {
+    Node* accumulator = environment()->LookupAccumulator();
+    Node* check_for_hole = NewNode(simplified()->ReferenceEqual(), accumulator,
+                                   jsgraph()->TheHoleConstant());
+    Node* check_for_not_hole =
+        NewNode(simplified()->BooleanNot(), check_for_hole);
+    BuildHoleCheckAndThrow(check_for_not_hole,
+                           Runtime::kThrowSuperAlreadyCalledError);
+  }
+}
+
 void BytecodeGraphBuilder::BuildBinaryOp(const Operator* op) {
   PrepareEagerCheckpoint();
   Node* left =
@@ -2125,12 +2212,6 @@ void BytecodeGraphBuilder::VisitJumpIfToBooleanFalse() {
 
 void BytecodeGraphBuilder::VisitJumpIfToBooleanFalseConstant() {
   BuildJumpIfToBooleanFalse();
-}
-
-void BytecodeGraphBuilder::VisitJumpIfNotHole() { BuildJumpIfNotHole(); }
-
-void BytecodeGraphBuilder::VisitJumpIfNotHoleConstant() {
-  BuildJumpIfNotHole();
 }
 
 void BytecodeGraphBuilder::VisitJumpIfJSReceiver() { BuildJumpIfJSReceiver(); }
