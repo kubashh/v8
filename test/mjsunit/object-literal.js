c053f5c584b12622a9712383660f9c991229b450
Camillo Bruni <cbruni@chromium.org>
2017-06-20 11:10:31 +0200

    [literals] Perform a deep boilerplate copy for MutableHeapNumber fields
    Bug: chromium:734162, chromium:734051, v8:6211
    Change-Id: I5c3e7578e9278b8f19ff16ad4d963f490dcc6c8c

--- test/mjsunit/object-literal.js
+++ test/mjsunit/object-literal.js
@@ -34,6 +34,8 @@ function runTest(fn) {
   fn();
   // The third run might copy literals directly in the stub.
   fn();
+  fn();
+  fn();
   // Make sure literals keep on workin in optimized code.
   %OptimizeFunctionOnNextCall(fn);
   fn();
@@ -321,7 +323,6 @@ function TestLiteralElementsKind() {
   assertTrue(%HasDictionaryElements({0xFFFFFF:true}));
 }
 runTest(TestLiteralElementsKind);
-
 
 function TestNonNumberElementValues() {
   var o = {
---
+++ test/mjsunit/regress/regress-crbug-734162.js
@@ -0,0 +1,23 @@
+// Copyright 2017 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+var __v_0 = {};
+function __f_0(constructor, closure) {
+  var __v_2 = { value: 0 };
+  __v_4 = closure(constructor, 1073741823, __v_0, __v_2);
+  assertEquals(1, __v_2.value);
+}
+function __f_1(constructor, val, deopt, __v_2) {
+  if (!new constructor(val, deopt, __v_2)) {
+  }
+}
+function __f_10(constructor) {
+  __f_0(constructor, __f_1);
+  __f_0(constructor, __f_1);
+  __f_0(constructor, __f_1);
+}
+function __f_12(val, deopt, __v_2) {
+  __v_2.value++;
+}
+__f_10(__f_12);
