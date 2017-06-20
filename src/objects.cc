a9b0b52e5f03a5cd30efedf5a197f83321626740
Camillo Bruni <cbruni@chromium.org>
2017-06-20 11:34:05 +0200

    [cleanup] Use AllocationSite::ShouldTrack instead of GetMode
    Change-Id: Ie91d29f2e2ec7dc6dc4f824212c17d70a57d8b08

--- src/objects.cc
+++ src/objects.cc
@@ -15475,13 +15475,9 @@ bool AllocationSite::DigestTransitionFeedback(Handle<AllocationSite> site,
   return result;
 }
 
-AllocationSiteMode AllocationSite::GetMode(ElementsKind from, ElementsKind to) {
-  if (IsFastSmiElementsKind(from) &&
-      IsMoreGeneralElementsKindTransition(from, to)) {
-    return TRACK_ALLOCATION_SITE;
-  }
-
-  return DONT_TRACK_ALLOCATION_SITE;
+bool AllocationSite::ShouldTrack(ElementsKind from, ElementsKind to) {
+  return IsFastSmiElementsKind(from) &&
+         IsMoreGeneralElementsKindTransition(from, to);
 }
 
 const char* AllocationSite::PretenureDecisionName(PretenureDecision decision) {
