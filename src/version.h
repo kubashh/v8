739b323938ab78963b5ad09f6f0608a2906bac65
Loo Rong Jie <loorongjie@gmail.com>
2017-06-21 15:24:01 +0800

    Remove unused Version class methods
    Bug:
    Change-Id: Iaa0f16809ad4dd6a742e5562f04352a7d76365d5

--- src/version.h
+++ src/version.h
@@ -5,6 +5,7 @@
 #ifndef V8_VERSION_H_
 #define V8_VERSION_H_
 
+#include "include/v8-version.h"
 #include "src/base/functional.h"
 #include "src/vector.h"
 
@@ -14,37 +15,24 @@ namespace internal {
 class Version {
  public:
   // Return the various version components.
-  static int GetMajor() { return major_; }
-  static int GetMinor() { return minor_; }
-  static int GetBuild() { return build_; }
-  static int GetPatch() { return patch_; }
-  static bool IsCandidate() { return candidate_; }
+  static constexpr int GetMajor() { return major_; }
+  static constexpr int GetMinor() { return minor_; }
+  static constexpr int GetBuild() { return build_; }
+  static constexpr int GetPatch() { return patch_; }
+  static constexpr bool IsCandidate() { return candidate_; }
   static uint32_t Hash() {
     return static_cast<uint32_t>(
         base::hash_combine(major_, minor_, build_, patch_));
   }
 
-  // Calculate the V8 version string.
-  static void GetString(Vector<char> str);
-
-  // Calculate the SONAME for the V8 shared library.
-  static void GetSONAME(Vector<char> str);
-
   static const char* GetVersion() { return version_string_; }
-
  private:
-  // NOTE: can't make these really const because of test-version.cc.
-  static int major_;
-  static int minor_;
-  static int build_;
-  static int patch_;
-  static bool candidate_;
-  static const char* soname_;
+  static constexpr int major_ = V8_MAJOR_VERSION;
+  static constexpr int minor_ = V8_MINOR_VERSION;
+  static constexpr int build_ = V8_BUILD_NUMBER;
+  static constexpr int patch_ = V8_PATCH_LEVEL;
+  static constexpr bool candidate_ = (V8_IS_CANDIDATE_VERSION != 0);
   static const char* version_string_;
-
-  // In test-version.cc.
-  friend void SetVersion(int major, int minor, int build, int patch,
-                         bool candidate, const char* soname);
 };
 
 }  // namespace internal
