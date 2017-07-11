1cfbb9a23216bc22a3729c5ce919e34e7011c2f4
Daniel Ehrenberg <littledan@chromium.org>
2017-07-11 21:10:36 +0200

    [parser] Disallow async functions as destructuring targets
    This patch teaches the parser that async functions are not valid
    destructuring targets so that it can cleanly exit with a SyntaxError.
    Previously, async functions used in the wrong position would lead
    to a check failure.
    
    Bug: chromium:740366
    Change-Id: Ie5b0cf50326c3f96174c6b29d0ccedb5da4f75a2

--- test/cctest/test-parsing.cc
+++ test/cctest/test-parsing.cc
@@ -7455,6 +7455,9 @@ TEST(DestructuringNegativeTests) {
         "{ x : y++ }",
         "[a++]",
         "(x => y)",
+        "(async x => y)",
+        "((x, z) => y)",
+        "(async (x, z) => y)",
         "a[i]", "a()",
         "a.b",
         "new a",
@@ -7469,6 +7472,8 @@ TEST(DestructuringNegativeTests) {
         "a <<< a",
         "a >>> a",
         "function a() {}",
+        "function* a() {}",
+        "async function a() {}",
         "a`bcd`",
         "this",
         "null",
@@ -7535,6 +7540,11 @@ TEST(DestructuringNegativeTests) {
       NULL
     };
 
+    const char* async_gen_data[] = {
+      "async function* a() {}",
+      NULL
+    };
+
     // clang-format on
     RunParserSyncTest(context_data, data, kError);
     RunParserSyncTest(context_data, rest_data, kError);
@@ -7543,6 +7553,9 @@ TEST(DestructuringNegativeTests) {
                       arraysize(flags));
     RunParserSyncTest(context_data, rest_data, kError, NULL, 0, flags,
                       arraysize(flags));
+    static const ParserFlag async_gen_flags[] = {kAllowHarmonyAsyncIteration};
+    RunParserSyncTest(context_data, async_gen_data, kError, NULL, 0,
+                      async_gen_flags, arraysize(async_gen_flags));
   }
 
   {  // All modes.
@@ -7921,6 +7934,8 @@ TEST(DestructuringAssignmentNegativeTests) {
     "[50]",
     "[(50)]",
     "[(function() {})]",
+    "[(async function() {})]",
+    "[(function*() {})]",
     "[(foo())]",
     "{ x: 50 }",
     "{ x: (50) }",
@@ -7929,10 +7944,17 @@ TEST(DestructuringAssignmentNegativeTests) {
     "{ x: ('str') }",
     "{ x: (foo()) }",
     "{ x: (function() {}) }",
+    "{ x: (async function() {}) }",
+    "{ x: (function*() {}) }",
     "{ x: y } = 'str'",
     "[x, y] = 'str'",
     "[(x,y) => z]",
+    "[async(x,y) => z]",
+    "[async x => z]",
     "{x: (y) => z}",
+    "{x: (y,w) => z}",
+    "{x: async (y) => z}",
+    "{x: async (y,w) => z}",
     "[x, ...y, z]",
     "[...x,]",
     "[x, y, ...z = 1]",
