68e9bef421179c4422bb11ff989b430e20f4e54f
Georg Neis <neis@chromium.org>
2017-07-13 13:26:21 +0200

    [compiler] Make --code-comments conditionally include inlining stacks.
    Bug:
    Change-Id: Ief7e52a4046cb74e02b1783dbace19534093c588

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -730,7 +730,9 @@ DEFINE_BOOL(fast_promotion_new_space, false,
 // assembler-ia32.cc / assembler-arm.cc / assembler-x64.cc
 DEFINE_BOOL(debug_code, DEBUG_BOOL,
             "generate extra code (assertions) for debugging")
-DEFINE_BOOL(code_comments, false, "emit comments in code disassembly")
+DEFINE_BOOL(code_comments, false,
+            "emit comments in code disassembly; "
+            "disable concurrent_recompilation to include inlining stacks")
 DEFINE_BOOL(enable_sse3, true, "enable use of SSE3 instructions if available")
 DEFINE_BOOL(enable_ssse3, true, "enable use of SSSE3 instructions if available")
 DEFINE_BOOL(enable_sse4_1, true,
