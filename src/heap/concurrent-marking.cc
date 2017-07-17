a147430a168e6c54a1f54dc33950bb9f16fa39a9
Ulan Degenbaev <ulan@chromium.org>
2017-07-17 19:32:27 +0200

    [heap] Fix handling of AllocationSites in concurrent marking.
    Now the concurrent marker skip the weak fields of AllocationSites.
    
    BUG=chromium:694255
    
    Change-Id: If920b6a14fa12c3a90ab0d283e22988ccbd48c0e

--- src/heap/concurrent-marking.cc
+++ src/heap/concurrent-marking.cc
@@ -99,7 +99,18 @@ class ConcurrentMarkingVisitor final
   }
 
   int VisitJSApiObject(Map* map, JSObject* object) {
-    return VisitJSObject(map, object);
+    if (ObjectMarking::IsGrey<AccessMode::ATOMIC>(object,
+                                                  marking_state(object))) {
+      int size = JSObject::BodyDescriptor::SizeOf(map, object);
+      VisitMapPointer(object, object->map_slot());
+      // It is OK to iterate body of JS API object here because they do not have
+      // unboxed double fields.
+      DCHECK(map->HasFastPointerLayout());
+      JSObject::BodyDescriptor::IterateBody(object, size, this);
+      // The main thread will do wrapper tracing in Blink.
+      bailout_.Push(object);
+    }
+    return 0;
   }
 
   // ===========================================================================
@@ -138,6 +149,14 @@ class ConcurrentMarkingVisitor final
       bailout_.Push(object);
     }
     return 0;
+  }
+
+  int VisitAllocationSite(Map* map, AllocationSite* object) {
+    if (!ShouldVisit(object)) return 0;
+    int size = AllocationSite::BodyDescriptorWeak::SizeOf(map, object);
+    VisitMapPointer(object, object->map_slot());
+    AllocationSite::BodyDescriptorWeak::IterateBody(object, size, this);
+    return size;
   }
 
   int VisitJSFunction(Map* map, JSFunction* object) {
