e16250e8a09fd713f89002586a96cb324042ba9a
Caitlin Potter <caitp@igalia.com>
2017-07-21 12:51:43 -0400

    [async-iteration] implement spec-change to `yield` in async generators
    Per https://github.com/tc39/proposal-async-iteration/pull/102/files:
    
    AsyncGeneratorResolve no longer unwraps a value component. Instead, the
    value is unwrapped before the builtin call via Await, allowing Promise
    rejections to affect the generator control flow.
    
    Thus, all `yield <expr>` implicitly become `yield await <expr>`.
    
    BUG=v8:5855
    R=littledan@chromium.org, neis@chromium.org, adamk@chromium.org
    
    Cq-Include-Trybots: master.tryserver.v8:v8_linux_noi18n_rel_ng
    Change-Id: Ife084076c3ed434b5467e6aeba14082f8b410ad5

--- src/builtins/builtins-definitions.h
+++ src/builtins/builtins-definitions.h
@@ -1046,6 +1046,7 @@ namespace internal {
                                                                                \
   TFS(AsyncGeneratorResolve, kGenerator, kValue, kDone)                        \
   TFS(AsyncGeneratorReject, kGenerator, kValue)                                \
+  TFS(AsyncGeneratorReturnProcessor, kGenerator)                               \
   TFS(AsyncGeneratorResumeNext, kGenerator)                                    \
                                                                                \
   /* AsyncGeneratorFunction( p1, p2, ... pn, body ) */                         \
@@ -1070,6 +1071,8 @@ namespace internal {
   TFJ(AsyncGeneratorAwaitUncaught, 1, kAwaited)                                \
   TFJ(AsyncGeneratorAwaitResolveClosure, 1, kValue)                            \
   TFJ(AsyncGeneratorAwaitRejectClosure, 1, kValue)                             \
+  TFJ(AsyncGeneratorReturnResolveClosure, 1, kValue)                           \
+  TFJ(AsyncGeneratorReturnRejectClosure, 1, kValue)                            \
                                                                                \
   /* Async-from-Sync Iterator */                                               \
                                                                                \
