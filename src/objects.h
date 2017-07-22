654865cbf851510f045ab395d7ea777830d0618d
Caitlin Potter <caitp@igalia.com>
2017-07-21 21:29:57 -0400

    [async-iteration] eliminate implicit Await when resuming with .return()l
    AsyncGenerators, when resumed with a "return" completion, Await the sent
    value to provide consistency with syntactic return statements. This
    moves the await to during AsyncGeneratorResumeNext, shrinking the number
    of bytecodes.
    
    There's a minor change to BytecodeGenerator which removes a
    %_GeneratorClose() call, since it's inserted implicitly by the parser.
    
    BUG=v8:5855
    
    Change-Id: I2965c610e5985ac24c713b481e62f6b97f96a3d8

--- src/objects.h
+++ src/objects.h
@@ -3156,7 +3156,6 @@ class HandlerTable : public FixedArray {
   void HandlerTableReturnPrint(std::ostream& os);  // NOLINT
 #endif
 
- private:
   // Layout description for handler table based on ranges.
   static const int kRangeStartIndex = 0;
   static const int kRangeEndIndex = 1;
