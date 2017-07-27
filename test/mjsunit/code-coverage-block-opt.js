136e925df1afca3c5513d3154135ac7771e5921b
jgruber <jgruber@chromium.org>
2017-07-27 17:24:24 +0200

    [coverage] Don't skip collection if invocation_count is zero
    Function-granularity coverage skips functions that are both uncovered
    and have an uncovered parent. This optimization needs to be tweaked once
    block coverage and incremental collection is in play, as it is possible
    to have a function with invocation_count == 0 (i.e. uncovered at
    function granularity) that still has relevant block-granularity
    coverage.
    
    Bug: v8:6000
    Change-Id: I4cc81b8a6935aa58e29d383ed4fa749cbfe69352

--- test/mjsunit/code-coverage-block-opt.js
+++ test/mjsunit/code-coverage-block-opt.js
@@ -22,4 +22,25 @@ f(); f(); f(); f(); f(); f();             // 0150
  {"start":50,"end":76,"count":2}]  // TODO(jgruber): Invocation count is off.
 );
 
+// This test is tricky: it requires a non-toplevel, optimized function.
+// After initial collection, counts are cleared. Further invocation_counts
+// are not collected for optimized functions, and on the next coverage
+// collection we and up with an uncovered function with an uncovered parent
+// but with non-trivial block coverage.
+TestCoverage("Partial coverage collection",
+`
+!function() {                             // 0000
+  function f(x) {                         // 0050
+    if (x) { nop(); } else { nop(); }     // 0100
+  }                                       // 0150
+  f(true); f(true);                       // 0200
+  %OptimizeFunctionOnNextCall(f);         // 0250
+  %DebugCollectCoverage();                // 0300
+  f(false);                               // 0350
+}();                                      // 0400
+`,
+[{"start":50,"end":151,"count":1},
+ {"start":115,"end":125,"count":0}]
+);
+
 %DebugToggleBlockCoverage(false);
