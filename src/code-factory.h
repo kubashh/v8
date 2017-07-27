d5d5a4f210e98d1fb4e36a3ada3584dfcb812240
Daniel Clifford <danno@chromium.org>
2017-07-27 18:59:41 +0200

    Add ArrayClone stub
    Change-Id: Ia0155bedcf39b4577605ff754f416c2af938efb7

--- src/code-factory.h
+++ src/code-factory.h
@@ -111,6 +111,7 @@ class V8_EXPORT_PRIVATE CodeFactory final {
   static Callable ArrayPop(Isolate* isolate);
   static Callable ArrayPush(Isolate* isolate);
   static Callable ArrayShift(Isolate* isolate);
+  static Callable ArraySlice(Isolate* isolate);
   static Callable FunctionPrototypeBind(Isolate* isolate);
   static Callable TransitionElementsKind(Isolate* isolate, ElementsKind from,
                                          ElementsKind to, bool is_jsarray);
