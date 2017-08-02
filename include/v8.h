08a3c9171a9e7a77cc6b244e23b2e0db033dd534
Peter Marshall <petermarshall@chromium.org>
2017-08-02 14:55:24 +0200

    [serializer] Enable TypedArrays and ArrayBuffers in the snapshot.
    Previously we could not support these due to their unique memory layout
    including off-heap backing store allocations. We now serialize these
    allocations and then fix-up references to them in the PostProcess step
    of deserialization.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_rel_ng
    Change-Id: Ic215049c06e6ee655bd17c11dfab0d8630568a84

--- include/v8.h
+++ include/v8.h
@@ -7803,7 +7803,10 @@ class V8_EXPORT V8 {
    * Returns { NULL, 0 } on failure.
    * The caller acquires ownership of the data array in the return value.
    */
-  static StartupData CreateSnapshotDataBlob(const char* embedded_source = NULL);
+  static StartupData CreateSnapshotDataBlob(
+      const char* embedded_source = NULL,
+      SerializeInternalFieldsCallback callback =
+          SerializeInternalFieldsCallback());
 
   /**
    * Bootstrap an isolate and a context from the cold startup blob, run the
@@ -8208,10 +8211,13 @@ class V8_EXPORT SnapshotCreator {
    * This must not be called from within a handle scope.
    * \param function_code_handling whether to include compiled function code
    *        in the snapshot.
+   // TODO(petermarshall): Document param.
    * \returns { nullptr, 0 } on failure, and a startup snapshot on success. The
    *        caller acquires ownership of the data array in the return value.
    */
-  StartupData CreateBlob(FunctionCodeHandling function_code_handling);
+  StartupData CreateBlob(FunctionCodeHandling function_code_handling,
+                         SerializeInternalFieldsCallback callback =
+                             SerializeInternalFieldsCallback());
 
   // Disallow copying and assigning.
   SnapshotCreator(const SnapshotCreator&) = delete;
