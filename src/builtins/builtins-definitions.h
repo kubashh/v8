896a37137ca9732bf6cd3626199af827f576b73c
Maya Lekova <mslekova@google.com>
2017-08-07 12:23:41 +0000

    Revert "Reland "[builtins] Port getting property from Proxy to CSA""
    This reverts commit 15ef03cbf3a439a99966a6e718e99f0617a9f604.
    
    Reason for revert: Found the following bugs
    
    Bug: chromium:752846, chromium:752712, chromium:752850
    
    Original change's description:
    > Reland "[builtins] Port getting property from Proxy to CSA"
    > 
    > This reland is after fix in [heap] Delete wrong DCHECK.
    > It includes moving ProxyGetProperty to its own stub to reduce
    > binary size.
    > 
    > This is a reland of 47a97aa53bcc0f44c1609633795b2c147a010ba5
    > Original change's description:
    > > [builtins] Port getting property from Proxy to CSA
    > > 
    > > Bug: v8:6559, v8:6557
    > > Change-Id: If6c51f5483adb73ddd2495cede5d85e887a3c298
    > > Reviewed-on: https://chromium-review.googlesource.com/589212
    > > Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    > > Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    > > Commit-Queue: Maya Lekova <mslekova@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#47113}
    > 
    > Bug: v8:6559, v8:6557
    > Change-Id: I76acd97ba1acb62b7e7983db1741441d997050f0
    > Reviewed-on: https://chromium-review.googlesource.com/600215
    > Commit-Queue: Maya Lekova <mslekova@google.com>
    > Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    > Reviewed-by: Franziska Hinkelmann <franzih@chromium.org>
    > Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#47159}
    
    TBR=jkummerow@chromium.org,mstarzinger@chromium.org,franzih@chromium.org,jgruber@chromium.org,ishell@chromium.org,bmeurer@chromium.org,mslekova@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: v8:6559, v8:6557
    Change-Id: I51bef25a031b02cf4deab11282473acae57f1ed3

--- src/builtins/builtins-definitions.h
+++ src/builtins/builtins-definitions.h
@@ -803,7 +803,6 @@ namespace internal {
   TFJ(ProxyConstructor, 0)                                                     \
   TFJ(ProxyConstructor_ConstructStub,                                          \
       SharedFunctionInfo::kDontAdaptArgumentsSentinel)                         \
-  TFS(ProxyGetProperty, kProxy, kName, kReceiverValue)                         \
                                                                                \
   /* Reflect */                                                                \
   ASM(ReflectApply)                                                            \
