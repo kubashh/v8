a78aa4ea05799e289f42c2227699e9df928c76df
Albert Mingkun Yang <albertnetymk@google.com>
2017-08-25 14:46:57 +0200

    Adjust SP delta for kArchSaveCallerRegisters case
    In kArchSaveCallerRegisters, PushCallerSaved alone alters stack pointer
    without informing `frame_access_state`. This commit compensate the
    inconsistency by manually increasing the delta after pushing registers.
    This affects systems (currently only ARM64) using stack pointer for
    accessing variables stored on stack.
    
    Bug: chromium:749486
    Change-Id: Ic6da3826323d4fb1c545ade3d4d2cd4d1fed1843

--- src/arm/macro-assembler-arm.h
+++ src/arm/macro-assembler-arm.h
@@ -407,9 +407,11 @@ class TurboAssembler : public Assembler {
   // values to location, restoring [d0..(d15|d31)].
   void RestoreFPRegs(Register location, Register scratch);
 
-  void PushCallerSaved(SaveFPRegsMode fp_mode, Register exclusion1 = no_reg,
-                       Register exclusion2 = no_reg,
-                       Register exclusion3 = no_reg);
+  // Push caller saved registers on the stack, and return the number of bytes
+  // stack pointer is adjusted.
+  int PushCallerSaved(SaveFPRegsMode fp_mode, Register exclusion1 = no_reg,
+                      Register exclusion2 = no_reg,
+                      Register exclusion3 = no_reg);
   void PopCallerSaved(SaveFPRegsMode fp_mode, Register exclusion1 = no_reg,
                       Register exclusion2 = no_reg,
                       Register exclusion3 = no_reg);
