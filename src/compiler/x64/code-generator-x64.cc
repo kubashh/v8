a1f6957e47f73f0dfa38ad5d91e5407954711590
Albert Mingkun Yang <albertnetymk@google.com>
2017-08-28 14:50:37 +0200

    Adjust SP delta for kArchSaveCallerRegisters case
    In kArchSaveCallerRegisters, PushCallerSaved alone alters stack pointer
    without informing `frame_access_state`. This commit compensate the
    inconsistency by manually increasing the delta after pushing registers.
    This affects systems (currently only ARM64) using stack pointer for
    accessing variables stored on stack.
    
    Bug: chromium:749486
    Change-Id: Ic6da3826323d4fb1c545ade3d4d2cd4d1fed1843

--- src/compiler/x64/code-generator-x64.cc
+++ src/compiler/x64/code-generator-x64.cc
@@ -944,12 +944,15 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
     }
     case kArchSaveCallerRegisters: {
       // kReturnRegister0 should have been saved before entering the stub.
-      __ PushCallerSaved(kSaveFPRegs, kReturnRegister0);
+      int bytes = __ PushCallerSaved(kSaveFPRegs, kReturnRegister0);
+      DCHECK(bytes % kPointerSize == 0);
+      frame_access_state()->IncreaseSPDelta(bytes / kPointerSize);
       break;
     }
     case kArchRestoreCallerRegisters: {
       // Don't overwrite the returned value.
       __ PopCallerSaved(kSaveFPRegs, kReturnRegister0);
+      DCHECK(frame_access_state()->sp_delta() == 0);
       break;
     }
     case kArchPrepareTailCall:
