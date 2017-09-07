bc1cb757383e05b44ec66a94c83b556cc8b4d1dc
jgruber <jgruber@chromium.org>
2017-09-07 15:27:31 +0200

    Eager-deserialize in test-debug/BuiltinsExceptionPrediction
    This test needs to iterate all builtin objects. Disable lazy
    deserialization to avoid replacing lazy builtins with DeserializeLazy.
    
    Bug: v8:6624
    Change-Id: Iab3f708380809b7486ef11a2816e9593ee7e65cd

--- test/cctest/test-debug.cc
+++ test/cctest/test-debug.cc
@@ -6618,6 +6618,7 @@ TEST(DebugCoverageWithScriptDataOutOfScope) {
 }
 
 TEST(BuiltinsExceptionPrediction) {
+  FLAG_lazy_deserialization = false;  // We need to inspect all builtins here.
   v8::Isolate* isolate = CcTest::isolate();
   v8::HandleScope handle_scope(isolate);
   v8::Context::New(isolate);
