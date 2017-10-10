4455cb594b303824a7a48880848a74381dd441de
Jan Krems <jan.krems@groupon.com>
2017-10-09 22:03:21 -0700

    [modules] Implement import.meta proposal
    Rewrites references to import.meta to a new GetImportMetaObject runtime
    call. Embedders can define a callback for creating the meta object using
    v8::Isolate::SetHostGetImportMetaObjectCallback. If no callback has been
    provided, an empty object with null prototype is created.
    
    This adds an example implementation to d8 that sets meta.url.
    
    Bug: v8:6693
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_rel_ng
    Change-Id: I6871eec79da45bba81bbbc84b1ffff48534c368d

--- src/isolate.h
+++ src/isolate.h
@@ -1256,6 +1256,10 @@ class Isolate {
   MaybeHandle<JSPromise> RunHostImportModuleDynamicallyCallback(
       Handle<Script> referrer, Handle<Object> specifier);
 
+  void SetHostGetImportMetaObjectCallback(
+      HostGetImportMetaObjectCallback callback);
+  Handle<Object> RunHostGetImportMetaObjectCallback(Handle<Module> module);
+
   void SetRAILMode(RAILMode rail_mode);
 
   RAILMode rail_mode() { return rail_mode_.Value(); }
@@ -1500,6 +1504,7 @@ class Isolate {
   bool promise_hook_or_debug_is_active_;
   PromiseHook promise_hook_;
   HostImportModuleDynamicallyCallback host_import_module_dynamically_callback_;
+  HostGetImportMetaObjectCallback host_get_import_meta_object_callback_;
   base::Mutex rail_mutex_;
   double load_start_time_ms_;
 
