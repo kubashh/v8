33dcd4cd7ee99a30d6f398371b296baa772ec7bf
Georg Neis <neis@chromium.org>
2017-10-10 13:49:42 +0200

    [bigint] Introduce ToNumeric conversion.
    This introduces a ToNumeric conversion to the runtime and interpreter.
    ToNumeric behaves like ToNumber, except that it also lets BigInts pass.
    
    Bug: v8:6791
    Change-Id: Idf9d0b5d283638459fe5893de41cc120356247a7

--- src/objects.h
+++ src/objects.h
@@ -913,6 +913,7 @@ enum class ComparisonResult {
   kUndefined     // at least one of x or y was undefined or NaN
 };
 
+enum class ConversionMode { kToNumber, kToNumeric };
 
 class AbstractCode;
 class AccessorPair;
@@ -1268,6 +1269,9 @@ class Object {
   MUST_USE_RESULT static inline MaybeHandle<Object> ToNumber(
       Handle<Object> input);
 
+  MUST_USE_RESULT static inline MaybeHandle<Object> ToNumeric(
+      Handle<Object> input);
+
   // ES6 section 7.1.4 ToInteger
   MUST_USE_RESULT static inline MaybeHandle<Object> ToInteger(
       Isolate* isolate, Handle<Object> input);
@@ -1537,8 +1541,8 @@ class Object {
       Isolate* isolate, Handle<Object> value);
   MUST_USE_RESULT static MaybeHandle<String> ConvertToString(
       Isolate* isolate, Handle<Object> input);
-  MUST_USE_RESULT static MaybeHandle<Object> ConvertToNumber(
-      Isolate* isolate, Handle<Object> input);
+  MUST_USE_RESULT static MaybeHandle<Object> ConvertToNumberOrNumeric(
+      Isolate* isolate, Handle<Object> input, ConversionMode mode);
   MUST_USE_RESULT static MaybeHandle<Object> ConvertToInteger(
       Isolate* isolate, Handle<Object> input);
   MUST_USE_RESULT static MaybeHandle<Object> ConvertToInt32(
