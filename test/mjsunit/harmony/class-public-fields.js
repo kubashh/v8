b5f7fc838e8d11135fb2af9faf730267d1acb81b
Sathya Gunasekaran <gsathya@chromium.org>
2017-10-11 22:27:49 -0700

    [class] Implement static public class fields
    This patch implements the runtime semantics of static public
    class fields.
    
    Bug: v8:5367
    Change-Id: I3574e4c685f1c039de42521c122e24f8d28e5d6c

---
+++ test/mjsunit/harmony/class-public-fields.js
@@ -0,0 +1,119 @@
+// Copyright 2016 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --harmony-class-fields --allow-natives-syntax
+
+{
+  class C {
+    static c = 1;
+  }
+
+  assertEquals(1, C.c);
+
+  let c = new C;
+  assertEquals(undefined, c.c);
+}
+
+{
+  this.c = 1;
+  class C {
+    static c = this.c;
+  }
+
+  assertEquals(1, C.c);
+
+  let c = new C;
+  assertEquals(undefined, c.c);
+}
+
+{
+  this.c = 1;
+  class C {
+    static c = () => this.c;
+  }
+
+  assertEquals(1, C.c());
+
+  let c = new C;
+  assertEquals(undefined, c.c);
+}
+
+{
+  this.c = 1;
+  class C {
+    static c = () => this.c;
+  }
+
+  let a = {c : 2 };
+  assertEquals(1, C.c.call(a));
+
+  let c = new C;
+  assertEquals(undefined, c.c);
+}
+
+{
+  this.foo = 1;
+  class C {
+    static c = function() { return this.foo; };
+    static d = function() { return this.bar; };
+  }
+
+  assertEquals(undefined, C.c());
+
+  C.bar = 1;
+  assertEquals(1, C.d());
+
+  let a = {foo : 2 };
+  assertEquals(2, C.c.call(a));
+
+  assertThrows(C.c.bind(undefined));
+  let c = new C;
+  assertEquals(undefined, c.c);
+}
+
+{
+  class C {
+    static c = function() { return 1 };
+  }
+
+  assertEquals('c', C.c.name);
+}
+
+{
+  d = function() { return new.target; }
+  class C {
+    static c = d;
+  }
+
+  assertEquals(undefined, C.c());
+  assertEquals(new d, new C.c());
+}
+
+// {
+
+//   class C {
+//     static c = new.target;
+//   }
+
+//   assertEquals(undefined, C.c());
+// }
+
+// {
+//   class A {
+//     constructor() {
+//       this.foo = 1;
+//     }
+//   }
+
+//   class B extends A {};
+
+//   class C {
+//     static c = function() { return super.foo; }
+//   }
+
+//   assertEquals(undefined, C.c());
+
+//   let b = new B;
+//   assertEquals(undefined, C.c.call(b));
+// }
