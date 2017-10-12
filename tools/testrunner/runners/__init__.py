fc0da6d2b9161871cd746faabea70cafb74c827f
Michal Majewski <majeski@google.com>
2017-10-12 11:36:13 +0200

    TestRunner initial refactor
    Preparation for sharing more code between deopt fuzzer and normal test runner.
    
    Bug: v8:6917
    Change-Id: Id8022c7597544ffddf5d1b0a0d73bb4a9d163fc3

--- tools/testrunner/runners/__init__.py
+++ tools/testrunner/runners/__init__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-#
 # Copyright 2012 the V8 project authors. All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -26,13 +24,3 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-import sys
-
-from testrunner.runners import deopt_fuzzer
-
-
-if __name__ == "__main__":
-  runner = deopt_fuzzer.DeoptFuzzerRunner()
-  sys.exit(runner.run())
