2339fe4dfd1917ecaf72302492c8a855c4083c49
Ed Schouten <ed@nuxi.nl>
2017-10-29 10:49:13 +0100

    Initial set of fixes to add support for CloudABI.
    CloudABI is a sandboxed capability-based POSIX-like runtime environment
    for Linux, FreeBSD, macOS, etc. More details can be found here:
    
      https://github.com/NuxiNL/cloudlibc
    
    I am porting V8 to CloudABI, with the goal of getting Node.js up and
    running.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_rel_ng
    Change-Id: I0002ad8277f4c1a81ba188e488ab8182f20ba3b7

--- src/base/utils/random-number-generator.cc
+++ src/base/utils/random-number-generator.cc
@@ -49,6 +49,10 @@ RandomNumberGenerator::RandomNumberGenerator() {
   result = rand_s(&second_half);
   DCHECK_EQ(0, result);
   SetSeed((static_cast<int64_t>(first_half) << 32) + second_half);
+#elif V8_OS_CLOUDABI
+  int64_t seed;
+  arc4random_buf(&seed, sizeof(seed));
+  SetSeed(seed);
 #else
   // Gather entropy from /dev/urandom if available.
   FILE* fp = fopen("/dev/urandom", "rb");
