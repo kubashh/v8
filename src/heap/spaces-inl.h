3345501f20dfc1d93c60bfd272bfc516a5d8c166
Ali Ijaz Sheikh <ofrobots@google.com>
2017-12-04 17:09:41 -0800

    [heap] disable flaky sampling heap profiler assert
    BUG=chromium:791582
    
    Change-Id: Ife3acf35eeaa6fdebd5ea2fabc1678ec762b3ed3

--- src/heap/spaces-inl.h
+++ src/heap/spaces-inl.h
@@ -464,8 +464,10 @@ AllocationResult NewSpace::AllocateRaw(int size_in_bytes,
                                        AllocationAlignment alignment) {
   if (top() < top_on_previous_step_) {
     // Generated code decreased the top() pointer to do folded allocations
-    DCHECK_EQ(Page::FromAddress(top()),
-              Page::FromAddress(top_on_previous_step_));
+    // TODO(ofrobots): There are edge cases this DCHECK is still running into.
+    // Reenable once it is fully understood.
+    // DCHECK_EQ(Page::FromAllocationAreaAddress(top()),
+    //           Page::FromAllocationAreaAddress(top_on_previous_step_));
     top_on_previous_step_ = top();
   }
 #ifdef V8_HOST_ARCH_32_BIT
