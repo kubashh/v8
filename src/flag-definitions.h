d9aeb29e7d87e6d06196943539553c2b7ae291d6
Michal Majewski <majeski@google.com>
2017-12-07 21:36:04 +0100

    [test] Introduce flag to stress atomic gc.
    --fuzzer-gc-analysis prints allocations count without need for
    predictable mode.
    
    --stress-atomic-gc is similar to --gc-interval with 2 differences:
    1) Causes gc every random(0, 2X) allocations instead of constant X.
    2) Doesn't update the allocation timeout when there is a GC for
    different reason than timeout equals to 0.
    
    Bug: v8:6972
    Change-Id: I07ad935fc264a61069255c7358b4fcbe42bfb17a

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -586,6 +586,9 @@ DEFINE_BOOL(experimental_new_space_growth_heuristic, false,
 DEFINE_INT(max_old_space_size, 0, "max size of the old space (in Mbytes)")
 DEFINE_INT(initial_old_space_size, 0, "initial old space size (in Mbytes)")
 DEFINE_BOOL(gc_global, false, "always perform global GCs")
+DEFINE_INT(stress_atomic_gc, 0,
+           "Collect garbage after random(0, 2X) allocations. It overrides "
+           "gc_interval.")
 DEFINE_INT(gc_interval, -1, "garbage collect after <n> allocations")
 DEFINE_INT(retain_maps_for_n_gc, 2,
            "keeps maps alive for <n> old space garbage collections")
@@ -692,8 +695,8 @@ DEFINE_BOOL(stress_incremental_marking, false,
             "force incremental marking for small heaps and run it more often")
 
 DEFINE_BOOL(fuzzer_gc_analysis, false,
-            "enables analysis mode for gc fuzz testing, e.g. --stress-marking, "
-            "--stress-scavenge")
+            "prints number of allocations and enables analysis mode for gc "
+            "fuzz testing, e.g. --stress-marking, --stress-scavenge")
 DEFINE_INT(stress_marking, 0,
            "force marking at random points between 0 and X (inclusive) percent "
            "of the regular marking start limit")
