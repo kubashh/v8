cabe07d6044c47a7a06311908895a7cf0f546236
Clemens Hammacher <clemensh@chromium.org>
2018-01-08 09:49:30 +0100

    [Liftoff] Store protected instruction info
    Store protected instruction info needed for the wasm trap handler.
    This code will be used and covered by existing tests once we add support
    for if-constructs to Liftoff.
    
    R=titzer@chromium.org
    CC=eholk@chromium.org
    
    Bug: v8:6600, v8:5277
    Change-Id: I508b0ab8572ef8b5c7943564714fcec7e9eb8e3b

--- src/wasm/baseline/s390/liftoff-assembler-s390.h
+++ src/wasm/baseline/s390/liftoff-assembler-s390.h
@@ -28,13 +28,15 @@ void LiftoffAssembler::FillContextInto(Register dst) { UNIMPLEMENTED(); }
 
 void LiftoffAssembler::Load(LiftoffRegister dst, Register src_addr,
                             Register offset_reg, uint32_t offset_imm,
-                            LoadType type, LiftoffRegList pinned) {
+                            LoadType type, LiftoffRegList pinned,
+                            uint32_t* protected_load_pc) {
   UNIMPLEMENTED();
 }
 
 void LiftoffAssembler::Store(Register dst_addr, Register offset_reg,
                              uint32_t offset_imm, LiftoffRegister src,
-                             StoreType type, LiftoffRegList pinned) {
+                             StoreType type, LiftoffRegList pinned,
+                             uint32_t* protected_store_pc) {
   UNIMPLEMENTED();
 }
 
