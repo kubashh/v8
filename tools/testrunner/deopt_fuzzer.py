29999927622997bcc57b8d92506aa1582fcfe0d4
Predrag Rudic <predrag.rudic@mips.com>
2018-01-16 11:30:45 +0100

    [cctest] Skip test-run-wasm-simd if SIMD is not supported
    For mips, if 'mips_arch_variant=="r6"' and if 'mips_use_msa' flag is set
    to 'true', then test-run-wasm-simd tests won't be skipped for mips. It
    will also force 'MIPS_SIMD' bit in CpuFeatures to be set.
    ARM processors are assumed to support SIMD.
    
    Bug:
    Change-Id: Ib3eb7e71bf5674c2f32c6e0a54a14604da831eb4

--- tools/testrunner/deopt_fuzzer.py
+++ tools/testrunner/deopt_fuzzer.py
@@ -287,6 +287,7 @@ class DeoptFuzzer(base_runner.BaseTestRunner):
       "no_snap": self.build_config.no_snap,
       "novfp3": False,
       "predictable": self.build_config.predictable,
+      "simd": True,
       "simulator": utils.UseSimulator(self.build_config.arch),
       "simulator_run": False,
       "system": utils.GuessOS(),
