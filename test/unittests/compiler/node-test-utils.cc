3d89da1064da5f7bac3a2bd27e89ea38fcea4174
Sigurd Schneider <sigurds@chromium.org>
2018-01-24 15:06:48 +0100

    [turbofan] Move Math.* builtins to JSCallReducer
    This CL also adds speculation to all Math builtins,
    and refactors the JSCallReducer.
    
    
    Bug: v8:7250
    Change-Id: Icdaddb767e875bb191939d907f65c7a8dcf79b8b

--- test/unittests/compiler/node-test-utils.cc
+++ test/unittests/compiler/node-test-utils.cc
@@ -2187,6 +2187,7 @@ IS_UNOP_MATCHER(Word32Clz)
 IS_UNOP_MATCHER(Word32Ctz)
 IS_UNOP_MATCHER(Word32Popcnt)
 IS_UNOP_MATCHER(Word32ReverseBytes)
+IS_UNOP_MATCHER(SpeculativeToNumber)
 #undef IS_UNOP_MATCHER
 
 }  // namespace compiler
