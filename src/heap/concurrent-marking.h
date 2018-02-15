5232f3f520efc7b4db8c10fcb468a768a7e53f00
Gabriel Charette <gab@chromium.org>
2018-02-15 18:40:29 +0100

    Relieve worker threads when pausing concurrent marking.
    This will allow using more workers for concurrent marking instead of
    having to keep a few workers behind for a potential future main thread
    task.
    Task count will be increased in a follow-up: https://chromium-review.googlesource.com/c/v8/v8/+/918663
    
    Nit: cleaned up some initialization using C++11isms :).
    
    Note: this makes it such that previous callers of ConcurrentMarking::EnsureCompleted()
    now wait for all tasks instead of trying to cancel unstarted tasks. I think this
    makes sense as if you're going to wait on one task you might as well keep
    up the parallelism to finish ASAP.
    
    The two pieces of code affected by this are:
    
        if (FLAG_parallel_marking) {
          DCHECK(FLAG_concurrent_marking);
          heap_->concurrent_marking()->RescheduleTasksIfNeeded();
        }
        ProcessMarkingWorklist();
    
        FinishConcurrentMarking();
    
    (clearly this didn't intend for FinishConcurrentMarking() to immediately cancel tasks it
    just started)
    
    and
    
      if (was_marked_incrementally_ && heap_->ShouldAbortIncrementalMarking()) {
        heap()->incremental_marking()->Stop();
        heap()->incremental_marking()->AbortBlackAllocation();
        FinishConcurrentMarking();
        heap()->incremental_marking()->Deactivate();
        (...)
    
    I will follow-up with another change to make this use PREEMPT_TASKS (not doing it
    in this CL since it may have unknown side-effects).
    
    
    Bug: chromium:812178
    Change-Id: I16ef90295fadaac82e031f204b0a7a8cd0931524

--- src/heap/concurrent-marking.h
+++ src/heap/concurrent-marking.h
@@ -5,7 +5,11 @@
 #ifndef V8_HEAP_CONCURRENT_MARKING_H_
 #define V8_HEAP_CONCURRENT_MARKING_H_
 
+#include "include/v8-platform.h"
 #include "src/allocation.h"
+#include "src/base/atomic-utils.h"
+#include "src/base/platform/condition-variable.h"
+#include "src/base/platform/mutex.h"
 #include "src/cancelable-task.h"
 #include "src/heap/spaces.h"
 #include "src/heap/worklist.h"
@@ -36,16 +40,26 @@ class ConcurrentMarking {
     ConcurrentMarking* concurrent_marking_;
   };
 
-  static const int kMaxTasks = 4;
+  static constexpr int kMaxTasks = 4;
   using MarkingWorklist = Worklist<HeapObject*, 64 /* segment size */>;
 
   ConcurrentMarking(Heap* heap, MarkingWorklist* shared,
                     MarkingWorklist* bailout, MarkingWorklist* on_hold,
                     WeakObjects* weak_objects);
 
+  // Schedules asynchronous tasks to perform concurrent marking. Objects in the
+  // heap should not be moved while these are active (can be stopped safely via
+  // Stop()).
   void ScheduleTasks();
-  void WaitForTasks();
-  void EnsureCompleted();
+
+  enum class StopRequest {
+    // Preempt ongoing tasks ASAP (and cancel unstarted tasks).
+    PREEMPT_TASKS,
+    // Wait for all scheduled tasks to complete.
+    COMPLETE_TASKS,
+  };
+  void Stop(StopRequest stop_request);
+
   void RescheduleTasksIfNeeded();
   // Flushes the local live bytes into the given marking state.
   void FlushLiveBytes(MajorNonAtomicMarkingState* marking_state);
@@ -60,33 +74,33 @@ class ConcurrentMarking {
  private:
   struct TaskState {
     // When the concurrent marking task has this lock, then objects in the
-    // heap are guaranteed to not move.
+    // heap are guaranteed not to move.
     base::Mutex lock;
-    // The main thread sets this flag to true, when it wants the concurrent
-    // maker to give up the lock.
-    base::AtomicValue<bool> interrupt_request;
-    // The concurrent marker waits on this condition until the request
-    // flag is cleared by the main thread.
-    base::ConditionVariable interrupt_condition;
+
+    // The main thread sets this flag to true when it wants the concurrent
+    // marker to give up the worker thread.
+    base::AtomicValue<bool> preemption_request;
+
     LiveBytesMap live_bytes;
-    size_t marked_bytes;
+    size_t marked_bytes = 0;
     char cache_line_padding[64];
   };
   class Task;
   void Run(int task_id, TaskState* task_state);
+
   Heap* heap_;
   MarkingWorklist* shared_;
   MarkingWorklist* bailout_;
   MarkingWorklist* on_hold_;
   WeakObjects* weak_objects_;
   TaskState task_state_[kMaxTasks + 1];
-  base::AtomicNumber<size_t> total_marked_bytes_;
+  base::AtomicNumber<size_t> total_marked_bytes_{0};
   base::Mutex pending_lock_;
   base::ConditionVariable pending_condition_;
-  int pending_task_count_;
-  bool is_pending_[kMaxTasks + 1];
+  int pending_task_count_ = 0;
+  bool is_pending_[kMaxTasks + 1] = {};
   CancelableTaskManager::Id cancelable_id_[kMaxTasks + 1];
-  int task_count_;
+  int task_count_ = 0;
 };
 
 }  // namespace internal
