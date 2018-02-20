696849fcf2679e5725784d9a0bcacd58de9e1de8
Clemens Hammacher <clemensh@chromium.org>
2018-02-20 21:08:51 +0100

    [Liftoff] Allow f64 parameters and arguments
    This allows liftoff compiled functions to receive f64 values as
    parameters and pass f64 values as arguments to called functions.
    
    R=titzer@chromium.org
    
    Bug: v8:6600
    Change-Id: Ie7467b424ccdf1ec8b7f7625d9439ab4ea427022

--- src/wasm/baseline/arm/liftoff-assembler-arm.h
+++ src/wasm/baseline/arm/liftoff-assembler-arm.h
@@ -167,7 +167,8 @@ void LiftoffAssembler::PushCallerFrameSlot(const VarState& src,
   BAILOUT("PushCallerFrameSlot");
 }
 
-void LiftoffAssembler::PushCallerFrameSlot(LiftoffRegister reg) {
+void LiftoffAssembler::PushCallerFrameSlot(LiftoffRegister reg,
+                                           ValueType type) {
   BAILOUT("PushCallerFrameSlot reg");
 }
 
--- src/wasm/baseline/ppc/liftoff-assembler-ppc.h
+++ src/wasm/baseline/ppc/liftoff-assembler-ppc.h
@@ -168,7 +168,8 @@ void LiftoffAssembler::PushCallerFrameSlot(const VarState& src,
   BAILOUT("PushCallerFrameSlot");
 }
 
-void LiftoffAssembler::PushCallerFrameSlot(LiftoffRegister reg) {
+void LiftoffAssembler::PushCallerFrameSlot(LiftoffRegister reg,
+                                           ValueType type) {
   BAILOUT("PushCallerFrameSlot reg");
 }
 
