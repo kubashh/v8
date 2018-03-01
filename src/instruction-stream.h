c1e0aa02f7e81c1b07233c0f6f2ef2ebf53e6351
jgruber <jgruber@chromium.org>
2018-03-01 14:58:37 +0100

    DO NOT LAND: Embedded builtins
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    Change-Id: I41adafb1a47fefc4620ce5572311a85898792baa

--- src/instruction-stream.h
+++ src/instruction-stream.h
@@ -14,19 +14,16 @@ namespace internal {
 class Code;
 class Isolate;
 
-// Wraps an mmap'ed off-heap instruction stream. This class will likely become
-// unneeded once --stress-off-heap-code is removed.
+// Wraps an off-heap instruction stream.
 class InstructionStream final {
  public:
-  explicit InstructionStream(Code* code);
-  ~InstructionStream();
+  InstructionStream(uint8_t* bytes, size_t byte_length, int builtin_index);
+
+  // Returns true, iff the given pc points into an off-heap instruction stream.
+  static bool PcIsOffHeap(Isolate* isolate, Address pc);
 
   // Returns the corresponding Code object if it exists, and nullptr otherwise.
   static Code* TryLookupCode(Isolate* isolate, Address address);
-
-  // Returns the corresponding stream if it exists, and nullptr otherwise.
-  static InstructionStream* TryLookupInstructionStream(Isolate* isolate,
-                                                       Code* code);
 
   bool Contains(Address address) const;
 
