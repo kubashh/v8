86977c6cb2984a5585524a90758983d7d69cb284
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-03-21 12:24:41 -0700

    [debug] introduced runtime side effect check
    This CL demonstrates minimum valuable addition to existing debug evaluate
    without side effects mechanism.
    With this CL user can evaluate expressions like:
    [a,b] // create any kind of temporary array literals
    [a,b].reduce((x,y) => x + y, 0); // use reduce method
    [1,2,3].fill(2); // change temporary arrays
    
    The core idea: any change of the object created during evaluation without
    side effects is side effect free. As soon as we try to store this temporary
    object to object existed before evaluation we will terminate execution.
    
    Implementation:
    - instrument all bytecodes which creates objects,
    - add check to all bytecodes which change objects.
    
    A little more details (including performance analysis): [1].
    
    [1] https://docs.google.com/document/d/10qqAtZADspPnpYa6SEdYRxrddfKIZJIzbLtGpsZQkRo/edit#
    
    Bug:none
    Change-Id: I69f7b96e1ebd7ad0022219e8213211c7be72a111

--- src/js/array.js
+++ src/js/array.js
@@ -134,6 +134,8 @@ function StackHas(stack, v) {
 // Global list of arrays visited during toString, toLocaleString and
 // join invocations.
 var visited_arrays = new Stack();
+%DebugMarkAsSideEffectFree(visited_arrays);
+%DebugMarkAsSideEffectFree(visited_arrays.values);
 
 function DoJoin(array, length, is_array, separator, use_locale) {
   if (UseSparseVariant(array, length, is_array, length)) {
