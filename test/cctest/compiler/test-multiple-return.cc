f5ee8e9ace0b249fb5714290801660a9ca2f01e8
Kim-Anh Tran <kimanh@google.com>
2018-04-19 09:19:14 +0200

    [wasm] Basic wasm tier-up
    Wasm tier-up first compiles the whole module using Liftoff, and then
    using Turbofan. The idea is to achieve fast start-up times by first
    running Liftoff-compiled code. In the meantime we finish compilation
    with Turbofan, and replace the Liftoff-compiled code as soon
    as Turbofan finished compilation, thus achieving high performance.
    Tier-up is enabled through the flag FLAG_wasm_tier_up.
    
    Change-Id: I70552969c53d909a591666a1e7ce1ee1419b2f34

--- test/cctest/compiler/test-multiple-return.cc
+++ test/cctest/compiler/test-multiple-return.cc
@@ -123,12 +123,16 @@ Node* ToInt32(RawMachineAssembler& m, MachineType type, Node* a) {
 
 std::unique_ptr<wasm::NativeModule> AllocateNativeModule(Isolate* isolate,
                                                          size_t code_size) {
+  std::unique_ptr<compiler::ModuleEnv> module_env =
+      base::make_unique<compiler::ModuleEnv>(
+          nullptr, trap_handler::IsTrapHandlerEnabled());
+
   // We have to add the code object to a NativeModule, because the
   // WasmCallDescriptor assumes that code is on the native heap and not
   // within a code object.
   std::unique_ptr<wasm::NativeModule> module =
-      isolate->wasm_engine()->code_manager()->NewNativeModule(code_size, 1, 0,
-                                                              false);
+      isolate->wasm_engine()->code_manager()->NewNativeModule(
+          code_size, 1, 0, false, std::move(module_env));
   return module;
 }
 
