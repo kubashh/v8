05ec3884c49b6a64f8fb7e27d87902570533e5d1
Marja Hölttä <marja@chromium.org>
2018-04-23 14:05:12 +0200

    [csa] Add TNode<MaybeObject>.
    LoadMaybeWeakField returns a TNode<MaybeObject>, and the only way to extract
    values from it is through explicit functions.
    
    BUG=v8:7308
    
    Change-Id: I98da6b715527bb9976b4c78ed9caaa39fbcb3ed5

--- src/compiler/code-assembler.h
+++ src/compiler/code-assembler.h
@@ -19,6 +19,7 @@
 #include "src/machine-type.h"
 #include "src/objects/data-handler.h"
 #include "src/objects/map.h"
+#include "src/objects/maybe-object.h"
 #include "src/runtime/runtime.h"
 #include "src/zone/zone-containers.h"
 
@@ -156,8 +157,10 @@ template <class T>
 struct is_valid_type_tag {
   static const bool value = std::is_base_of<Object, T>::value ||
                             std::is_base_of<UntaggedT, T>::value ||
+                            std::is_base_of<MaybeObject, T>::value ||
                             std::is_same<ExternalReference, T>::value;
-  static const bool is_tagged = std::is_base_of<Object, T>::value;
+  static const bool is_tagged = std::is_base_of<Object, T>::value ||
+                                std::is_base_of<MaybeObject, T>::value;
 };
 
 template <class T1, class T2>
@@ -342,6 +345,14 @@ struct types_have_common_values<UnionT<T1, T2>, UnionT<U1, U2>> {
                             types_have_common_values<T2, U1>::value ||
                             types_have_common_values<T2, U2>::value;
 };
+template <>
+struct types_have_common_values<MaybeObject, Object> {
+  static const bool value = true;
+};
+template <>
+struct types_have_common_values<Object, MaybeObject> {
+  static const bool value = true;
+};
 
 // TNode<T> is an SSA value with the static type tag T, which is one of the
 // following:
@@ -485,6 +496,7 @@ TNode<Float64T> Float64Add(TNode<Float64T> a, TNode<Float64T> b);
   V(Float64ExtractLowWord32, Word32T, Float64T)                \
   V(Float64ExtractHighWord32, Word32T, Float64T)               \
   V(BitcastTaggedToWord, IntPtrT, Object)                      \
+  V(BitcastMaybeObjectToWord, IntPtrT, MaybeObject)            \
   V(BitcastWordToTagged, Object, WordT)                        \
   V(BitcastWordToTaggedSigned, Smi, WordT)                     \
   V(TruncateFloat64ToFloat32, Float32T, Float64T)              \
