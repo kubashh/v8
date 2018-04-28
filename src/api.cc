9c0b9a1f47e8f425c70479f92c1abd3b55c71109
Erik Luo <luoe@chromium.org>
2018-04-27 18:48:12 -0700

    [prototype, wip] add ability to do side-effect-free call to NewInstance
    Bug: 829571
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    Change-Id: Ia4e410d487e2847bc511cb96f0be30a3563991f6

--- src/api.cc
+++ src/api.cc
@@ -5106,9 +5106,9 @@ Local<Function> Function::New(Isolate* v8_isolate, FunctionCallback callback,
       .FromMaybe(Local<Function>());
 }
 
-
-MaybeLocal<Object> Function::NewInstance(Local<Context> context, int argc,
-                                         v8::Local<v8::Value> argv[]) const {
+MaybeLocal<Object> Function::NewInstance(
+    Local<Context> context, int argc, v8::Local<v8::Value> argv[],
+    v8::SideEffectType side_effect_type) const {
   auto isolate = reinterpret_cast<i::Isolate*>(context->GetIsolate());
   TRACE_EVENT_CALL_STATS_SCOPED(isolate, "v8", "V8.Execute");
   ENTER_V8(isolate, context, Function, NewInstance, MaybeLocal<Object>(),
@@ -5118,8 +5118,17 @@ MaybeLocal<Object> Function::NewInstance(Local<Context> context, int argc,
   STATIC_ASSERT(sizeof(v8::Local<v8::Value>) == sizeof(i::Object**));
   i::Handle<i::Object>* args = reinterpret_cast<i::Handle<i::Object>*>(argv);
   Local<Object> result;
+  bool allow_next_side_effect =
+      side_effect_type == v8::SideEffectType::kHasNoSideEffect &&
+      isolate->debug_execution_mode() == i::DebugInfo::kSideEffects;
+  if (allow_next_side_effect) {
+    isolate->debug()->SkipNextSideEffectFromApi();
+  }
   has_pending_exception = !ToLocal<Object>(
       i::Execution::New(isolate, self, self, argc, args), &result);
+  if (allow_next_side_effect) {
+    isolate->debug()->CancelSkipNextSideEffectFromApi();
+  }
   RETURN_ON_FAILED_EXECUTION(Object);
   RETURN_ESCAPED(result);
 }
@@ -6361,12 +6370,22 @@ i::Object** Context::GetDataFromSnapshotOnce(size_t index) {
   return GetSerializedDataFromFixedArray(i_isolate, list, index);
 }
 
-MaybeLocal<v8::Object> ObjectTemplate::NewInstance(Local<Context> context) {
+MaybeLocal<v8::Object> ObjectTemplate::NewInstance(
+    Local<Context> context, SideEffectType side_effect_type) {
   PREPARE_FOR_EXECUTION(context, ObjectTemplate, NewInstance, Object);
   auto self = Utils::OpenHandle(this);
   Local<Object> result;
+  bool allow_next_side_effect =
+      side_effect_type == v8::SideEffectType::kHasNoSideEffect &&
+      isolate->debug_execution_mode() == i::DebugInfo::kSideEffects;
+  if (allow_next_side_effect) {
+    isolate->debug()->SkipNextSideEffectFromApi();
+  }
   has_pending_exception =
       !ToLocal<Object>(i::ApiNatives::InstantiateObject(self), &result);
+  if (allow_next_side_effect) {
+    isolate->debug()->CancelSkipNextSideEffectFromApi();
+  }
   RETURN_ON_FAILED_EXECUTION(Object);
   RETURN_ESCAPED(result);
 }
