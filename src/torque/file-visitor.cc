7b2135dc3b5a139875ac813e8216c6e74f1012e2
Daniel Clifford <danno@chromium.org>
2018-05-01 23:58:06 +0200

    Refactor/cleanup various Torque classes, inclduing making Type a Declarable
    This is a preparatory step for implementing generics. Along the way, clean up
    and encapsulate a bunch of code, including:
    
    * Fully encapsulate Scope by adding the new class ScopeChain that provide an
      abstraction for creating and activating scopes.
    * Untangle Modules and Scopes.
    * Unify scope activation so that it is always associated with an AST node
      and triggered by a RAII helper class.
    * Unify (somewhat) how builtins and macros are created, fixing a few
      inconsistencies with when and how parameters and their types are declared.
    * Create a new Declarations class that brokers between the visitor classes and
      the ScopeChain. This moves handling of declaration-related errors out of the
      visitors but also makes it possible to do so without polluting Scope and
      ScopeChain with details about resolving SourcePositions in error cases.
    
    Change-Id: I180017d4cf39ccf5ef1d20b84f53284c252f8d87

--- src/torque/file-visitor.cc
+++ src/torque/file-visitor.cc
@@ -10,19 +10,37 @@ namespace v8 {
 namespace internal {
 namespace torque {
 
-Signature FileVisitor::MakeSignature(const ParameterList& parameters,
+Signature FileVisitor::MakeSignature(SourcePosition pos,
+                                     const ParameterList& parameters,
                                      const std::string& return_type,
                                      const LabelAndTypesVector& labels) {
   LabelDeclarationVector definition_vector;
   for (auto label : labels) {
-    LabelDeclaration def = {label.name, GetTypeVector(label.types)};
+    LabelDeclaration def = {label.name, GetTypeVector(pos, label.types)};
     definition_vector.push_back(def);
   }
-  Signature result{parameters.names,
-                   {GetTypeVector(parameters.types), parameters.has_varargs},
-                   GetType(return_type),
-                   definition_vector};
+  Signature result{
+      parameters.names,
+      {GetTypeVector(pos, parameters.types), parameters.has_varargs},
+      LookupType(pos, return_type),
+      definition_vector};
   return result;
+}
+
+Type FileVisitor::LookupType(SourcePosition pos, const std::string& name) {
+  Declarable* raw = declarations()->Lookup(name);
+  if (raw == nullptr) {
+    std::stringstream s;
+    s << "definition of type \"" << name << "\" not found at "
+      << PositionAsString(pos);
+    ReportError(s.str());
+  }
+  if (!raw->IsTypeImpl()) {
+    std::stringstream s;
+    s << "\"" << name << "\" is not a type at " << PositionAsString(pos);
+    ReportError(s.str());
+  }
+  return Type(TypeImpl::cast(raw));
 }
 
 }  // namespace torque
