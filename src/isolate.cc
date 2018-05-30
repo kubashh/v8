4ecb81793e44aef7dcf0aa464208c21d2b429941
Ben L. Titzer <titzer@chromium.org>
2018-05-30 11:24:45 +0200

    [wasm] Remove the isolate_ field from WasmCodeManager
    The isolate is mainly used for accounting purposes. As such, it
    doesn't need a field in the WasmCodeManager, and cannot have one
    if it is to be made isolate independent. Instead, pass the isolate
    explicitly in the appropriate cases.
    
    R=mstarzinger@chromium.org
    BUG=v8:7424
    
    Change-Id: I539c2b33692e57605a280530bd704ef25269ad0f

--- src/isolate.cc
+++ src/isolate.cc
@@ -3008,9 +3008,9 @@ bool Isolate::Init(StartupDeserializer* des) {
   }
 
   // Setup the wasm engine. Currently, there's one per Isolate.
-  wasm_engine_.reset(new wasm::WasmEngine(
-      std::unique_ptr<wasm::WasmCodeManager>(new wasm::WasmCodeManager(
-          reinterpret_cast<v8::Isolate*>(this), kMaxWasmCodeMemory))));
+  wasm_engine_.reset(
+      new wasm::WasmEngine(std::unique_ptr<wasm::WasmCodeManager>(
+          new wasm::WasmCodeManager(kMaxWasmCodeMemory))));
   wasm_engine_->memory_tracker()->SetAllocationResultHistogram(
       counters()->wasm_memory_allocation_result());
   wasm_engine_->memory_tracker()->SetAddressSpaceUsageHistogram(
