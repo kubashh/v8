b7d7714475607fe72dd91f0c0ea7e039a2e26dd8
Dan Elphick <delphick@chromium.org>
2018-07-04 13:37:07 +0100

    Add ReadOnlyRoots containing the STRONG_ROOTS
    This creates a new ReadOnlyRoots object which contains accessors for all
    or the roots in STRONG_ROOT_LIST. Also adds an accessor for
    ReadOnlyRoots to HeapObject, which for now will use pointer magic to get
    hold of it. Also moves the ROOT_LIST macros into src/roots.h.
    
    Converts one GetHeap()->undefined_value() to use
    GetReadOnlyRoots().undefined_value().
    
    Bug: v8:7786
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng;luci.v8.try:v8_linux_noi18n_rel_ng
    Change-Id: Ie5aa41a71da100bc9d1ee44588b1189ae964f70a

--- src/snapshot/serializer.cc
+++ src/snapshot/serializer.cc
@@ -500,7 +500,7 @@ void Serializer<AllocatorT>::ObjectSerializer::SerializeExternalString() {
   // for native native source code strings, we replace the resource field
   // with the native source id.
   // For the rest we serialize them to look like ordinary sequential strings.
-  if (object_->map() != heap->native_source_string_map()) {
+  if (object_->map() != ReadOnlyRoots(heap).native_source_string_map()) {
     ExternalString* string = ExternalString::cast(object_);
     Address resource = string->resource_as_address();
     ExternalReferenceEncoder::Value reference;
@@ -534,7 +534,7 @@ void Serializer<
   // an imaginary sequential string with the same content.
   Isolate* isolate = serializer_->isolate();
   DCHECK(object_->IsExternalString());
-  DCHECK(object_->map() != isolate->heap()->native_source_string_map());
+  DCHECK(object_->map() != ReadOnlyRoots(isolate).native_source_string_map());
   ExternalString* string = ExternalString::cast(object_);
   int length = string->length();
   Map* map;
@@ -544,15 +544,16 @@ void Serializer<
   // Find the map and size for the imaginary sequential string.
   bool internalized = object_->IsInternalizedString();
   if (object_->IsExternalOneByteString()) {
-    map = internalized ? isolate->heap()->one_byte_internalized_string_map()
-                       : isolate->heap()->one_byte_string_map();
+    map = internalized
+              ? ReadOnlyRoots(isolate).one_byte_internalized_string_map()
+              : ReadOnlyRoots(isolate).one_byte_string_map();
     allocation_size = SeqOneByteString::SizeFor(length);
     content_size = length * kCharSize;
     resource = reinterpret_cast<const byte*>(
         ExternalOneByteString::cast(string)->resource()->data());
   } else {
-    map = internalized ? isolate->heap()->internalized_string_map()
-                       : isolate->heap()->string_map();
+    map = internalized ? ReadOnlyRoots(isolate).internalized_string_map()
+                       : ReadOnlyRoots(isolate).string_map();
     allocation_size = SeqTwoByteString::SizeFor(length);
     content_size = length * kShortSize;
     resource = reinterpret_cast<const byte*>(
@@ -595,7 +596,8 @@ class UnlinkWeakNextScope {
     if (object->IsAllocationSite()) {
       object_ = object;
       next_ = AllocationSite::cast(object)->weak_next();
-      AllocationSite::cast(object)->set_weak_next(heap->undefined_value());
+      AllocationSite::cast(object)->set_weak_next(
+          ReadOnlyRoots(heap).undefined_value());
     }
   }
 
@@ -648,7 +650,7 @@ void Serializer<AllocatorT>::ObjectSerializer::Serialize() {
 
   if (object_->IsScript()) {
     // Clear cached line ends.
-    Object* undefined = serializer_->isolate()->heap()->undefined_value();
+    Object* undefined = ReadOnlyRoots(serializer_->isolate()).undefined_value();
     Script::cast(object_)->set_line_ends(undefined);
   }
 
