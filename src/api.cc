b0d2e04cfc16cbd92c5e83174bef2f0fd67bfe48
Marja Hölttä <marja@chromium.org>
2018-07-16 15:07:31 +0200

    [in-place weak refs] Add & use PrototypeUsers (a specialized weak data structure)
    It's a growing weak array which also has an API for marking slots empty (those
    will then be filled before growing the array again).
    
    This is a more efficient implementation than the corresponding feature in
    FixedArrayOfWeakCells, because we chain the empty slots together.
    
    BUG=v8:7308
    
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    Change-Id: I335cd3f9cc7838c7f6ca350735b1503b2f5b8eed

--- src/api.cc
+++ src/api.cc
@@ -738,7 +738,10 @@ StartupData SnapshotCreator::CreateBlob(
   // context even after we have disposed of the context.
   isolate->heap()->CollectAllAvailableGarbage(
       i::GarbageCollectionReason::kSnapshotCreator);
-  isolate->heap()->CompactFixedArraysOfWeakCells();
+  {
+    i::HandleScope scope(isolate);
+    isolate->heap()->CompactFixedArraysOfWeakCells();
+  }
 
   isolate->heap()->read_only_space()->ClearStringPaddingIfNeeded();
 
