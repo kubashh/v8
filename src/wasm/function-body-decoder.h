be0b6b9e7686a613653fc571c973e5dabac69019
Ben L. Titzer <titzer@chromium.org>
2018-07-31 18:11:46 +0200

    [wasm] Inline VerifyWasmCodeWithStats
    This removes the need to pass Counters into the function body decoder
    by making timing happen in the callers.
    
    R=mstarzinger@chromium.org
    
    Change-Id: I05c8d2c85b1c315150cbf9e9b3f68efa03114b75

--- src/wasm/function-body-decoder.h
+++ src/wasm/function-body-decoder.h
@@ -46,13 +46,6 @@ V8_EXPORT_PRIVATE DecodeResult VerifyWasmCode(AccountingAllocator* allocator,
                                               const wasm::WasmModule* module,
                                               FunctionBody& body);
 
-// Note: If run in the background thread, must follow protocol using
-// isolate::async_counters() to guarantee usability of counters argument.
-DecodeResult VerifyWasmCodeWithStats(AccountingAllocator* allocator,
-                                     const wasm::WasmModule* module,
-                                     FunctionBody& body, ModuleOrigin origin,
-                                     Counters* counters);
-
 DecodeResult BuildTFGraph(AccountingAllocator* allocator, TFBuilder* builder,
                           FunctionBody& body,
                           compiler::NodeOriginTable* node_origins);
