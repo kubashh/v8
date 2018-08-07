200e6b0aa4493b977267251e69064c4f361e5ce0
Andreas Haas <ahaas@chromium.org>
2018-08-07 15:18:42 +0200

    [api] Add API callback setter for the wasm thread origin trial
    With the callback we can check if the origin trial is turned on for a
    given context.
    
    I will not land the other CL which added a flag to the isolate. The
    information if the origin trial is on is context-specific and not
    isolate-specific, and it's hard on the embedder side to track all
    creations of a context.
    
    With the API proposed in this CL we will ask the embedder every time we
    start compilation whether the origin trial is on or off.
    
    R=yangguo@chromium.org
    
    Bug:868844
    
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    Change-Id: I8822f40ab12582a5b0bd6640790a269107fc085a

--- src/isolate.h
+++ src/isolate.h
@@ -402,7 +402,6 @@ class ThreadLocalTop BASE_EMBEDDED {
   v8::TryCatch* try_catch_handler_ = nullptr;
 };
 
-
 #ifdef DEBUG
 
 #define ISOLATE_INIT_DEBUG_ARRAY_LIST(V)               \
@@ -436,6 +435,7 @@ typedef std::vector<HeapObject*> DebugObjectCache;
   V(ExtensionCallback, wasm_instance_callback, &NoExtension)                  \
   V(ApiImplementationCallback, wasm_compile_streaming_callback, nullptr)      \
   V(WasmStreamingCallback, wasm_streaming_callback, nullptr)                  \
+  V(WasmThreadsEnabledCallback, wasm_threads_enabled_callback, nullptr)       \
   /* State for Relocatable. */                                                \
   V(Relocatable*, relocatable_top, nullptr)                                   \
   V(DebugObjectCache*, string_stream_debug_object_cache, nullptr)             \
@@ -632,6 +632,8 @@ class Isolate : private HiddenFactory {
   inline Object* get_wasm_caught_exception();
   inline void set_wasm_caught_exception(Object* exception);
   inline void clear_wasm_caught_exception();
+
+  bool AreWasmThreadsEnabled(Handle<Context> context);
 
   THREAD_LOCAL_TOP_ADDRESS(Object*, pending_exception)
 
