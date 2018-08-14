ddcedf9fbbcd7e604f5507d9135c356fee995c22
Mathias Bynens <mathias@chromium.org>
2018-08-14 16:46:30 +0200

    Implement global proposal
    The proposal is currently at Stage 3 of the TC39 process.
    Repository: https://github.com/tc39/proposal-global
    
    Bug: v8:5537
    Change-Id: I36c39fdab049497f50685c6672655b67ec4d8ce9

---
+++ test/mjsunit/harmony/global.js
@@ -0,0 +1,21 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --harmony-global
+
+assertEquals(globalThis, this);
+assertEquals(globalThis.globalThis, this);
+assertEquals(globalThis.globalThis.globalThis, this);
+assertEquals(globalThis.globalThis.globalThis.globalThis, this);
+
+{
+  const descriptor = Object.getOwnPropertyDescriptor(
+    this,
+    'globalThis'
+  );
+  assertEquals(descriptor.value, this);
+  assertEquals(descriptor.writable, true);
+  assertEquals(descriptor.enumerable, false);
+  assertEquals(descriptor.configurable, true);
+}
