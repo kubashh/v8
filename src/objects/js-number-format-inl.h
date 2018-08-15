8d5aa47b584e184b1e2b286fe1a8b13c197a12f6
Frank Tang <ftang@chromium.org>
2018-08-15 10:38:30 -0700

    [Intl] Move NumberFormat to JSNumberFormat
    Cq-Include-Trybots: luci.v8.try:v8_linux_noi18n_rel_ng
    Change-Id: I9d5181c15b56de3bc5288d11bd83e55157c7a610

---
+++ src/objects/js-number-format-inl.h
@@ -0,0 +1,67 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_INTL_SUPPORT
+#error Internationalization is expected to be enabled.
+#endif  // V8_INTL_SUPPORT
+
+#ifndef V8_OBJECTS_JS_NUMBER_FORMAT_INL_H_
+#define V8_OBJECTS_JS_NUMBER_FORMAT_INL_H_
+
+#include "src/objects-inl.h"
+#include "src/objects/js-number-format.h"
+
+// Has to be the last include (doesn't have include guards):
+#include "src/objects/object-macros.h"
+
+namespace v8 {
+namespace internal {
+
+ACCESSORS(JSNumberFormat, icu_number_format, Managed<icu::NumberFormat>,
+          kIcuNumberFormatOffset)
+ACCESSORS(JSNumberFormat, bound_format, Object, kBoundFormatOffset)
+SMI_ACCESSORS(JSNumberFormat, flags, kFlagsOffset)
+
+inline void JSNumberFormat::set_style(Style style) {
+  DCHECK_LT(style, Style::COUNT);
+  int hints = flags();
+  hints = StyleBits::update(hints, style);
+  set_flags(hints);
+}
+
+inline JSNumberFormat::Style JSNumberFormat::style() const {
+  return StyleBits::decode(flags());
+}
+
+inline void JSNumberFormat::set_currency_display(
+    CurrencyDisplay currency_display) {
+  DCHECK_LT(currency_display, CurrencyDisplay::COUNT);
+  int hints = flags();
+  hints = CurrencyDisplayBits::update(hints, currency_display);
+  set_flags(hints);
+}
+
+inline JSNumberFormat::CurrencyDisplay JSNumberFormat::currency_display()
+    const {
+  return CurrencyDisplayBits::decode(flags());
+}
+
+inline void JSNumberFormat::set_significant_digits_are_set() {
+  int hints = flags();
+  hints = SignificantDigitsAreSetBits::update(hints, true);
+  set_flags(hints);
+}
+
+inline bool JSNumberFormat::significant_digits_are_set() const {
+  return SignificantDigitsAreSetBits::decode(flags());
+}
+
+CAST_ACCESSOR(JSNumberFormat);
+
+}  // namespace internal
+}  // namespace v8
+
+#include "src/objects/object-macros-undef.h"
+
+#endif  // V8_OBJECTS_JS_NUMBER_FORMAT_INL_H_
