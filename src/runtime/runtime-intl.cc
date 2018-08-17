9aa0c98a21571b46cc822c34dece2cd8120d6d9f
Frank Tang <ftang@chromium.org>
2018-08-17 08:11:06 -0700

    [Intl] Move NumberFormat to JSNumberFormat
    Bug: v8:7979
    
    Cq-Include-Trybots: luci.v8.try:v8_linux_noi18n_rel_ng
    Change-Id: I9d5181c15b56de3bc5288d11bd83e55157c7a610

--- src/runtime/runtime-intl.cc
+++ src/runtime/runtime-intl.cc
@@ -24,6 +24,7 @@
 #include "src/objects/js-collator-inl.h"
 #include "src/objects/js-list-format-inl.h"
 #include "src/objects/js-list-format.h"
+#include "src/objects/js-number-format-inl.h"
 #include "src/objects/js-plural-rules-inl.h"
 #include "src/objects/managed.h"
 #include "src/runtime/runtime-utils.h"
@@ -143,14 +144,6 @@ RUNTIME_FUNCTION(Runtime_GetDefaultICULocale) {
       Intl::DefaultLocale(isolate).c_str());
 }
 
-RUNTIME_FUNCTION(Runtime_IsWellFormedCurrencyCode) {
-  HandleScope scope(isolate);
-  DCHECK_EQ(1, args.length());
-  CONVERT_ARG_HANDLE_CHECKED(String, currency, 0);
-  return *(isolate->factory()->ToBoolean(
-      Intl::IsWellFormedCurrencyCode(isolate, currency)));
-}
-
 RUNTIME_FUNCTION(Runtime_DefineWEProperty) {
   HandleScope scope(isolate);
 
@@ -240,23 +233,31 @@ RUNTIME_FUNCTION(Runtime_FormatDate) {
       isolate, DateFormat::DateTimeFormat(isolate, date_format_holder, date));
 }
 
-RUNTIME_FUNCTION(Runtime_CreateNumberFormat) {
+RUNTIME_FUNCTION(Runtime_NumberFormatResolvedOptions) {
   HandleScope scope(isolate);
 
-  DCHECK_EQ(3, args.length());
-
-  CONVERT_ARG_HANDLE_CHECKED(String, locale, 0);
-  CONVERT_ARG_HANDLE_CHECKED(JSObject, options, 1);
-  CONVERT_ARG_HANDLE_CHECKED(JSObject, resolved, 2);
-  RETURN_RESULT_OR_FAILURE(
-      isolate, Intl::CreateNumberFormat(isolate, locale, options, resolved));
-}
-
-RUNTIME_FUNCTION(Runtime_CurrencyDigits) {
-  HandleScope scope(isolate);
   DCHECK_EQ(1, args.length());
-  CONVERT_ARG_HANDLE_CHECKED(String, currency, 0);
-  return *Intl::CurrencyDigits(isolate, currency);
+  CONVERT_ARG_HANDLE_CHECKED(Object, number_format_obj, 0);
+
+  // 2. If Type(nf) is not Object, throw a TypeError exception
+  if (!number_format_obj->IsJSReceiver()) {
+    Handle<String> method_str = isolate->factory()->NewStringFromStaticChars(
+        "Intl.NumberFormat.prototype.resolvedOptions");
+    THROW_NEW_ERROR_RETURN_FAILURE(
+        isolate, NewTypeError(MessageTemplate::kIncompatibleMethodReceiver,
+                              method_str, number_format_obj));
+  }
+
+  // 3. Let nf be ? UnwrapNumberFormat(nf).
+  Handle<JSReceiver> format_holder =
+      Handle<JSReceiver>::cast(number_format_obj);
+
+  Handle<JSNumberFormat> number_format;
+  ASSIGN_RETURN_FAILURE_ON_EXCEPTION(
+      isolate, number_format,
+      JSNumberFormat::UnwrapNumberFormat(isolate, format_holder));
+
+  return *JSNumberFormat::ResolvedOptions(isolate, number_format);
 }
 
 RUNTIME_FUNCTION(Runtime_CollatorResolvedOptions) {
