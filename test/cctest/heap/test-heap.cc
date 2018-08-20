3cac93c6c127dd82ad929880464e219a268ddff8
Rodrigo Bruno <rfbpb@google.com>
2018-08-20 10:30:46 +0200

    Reland "[heap] Attempt to incorporate backing store counters into heap sizing and GC trigger stragery."
    This is a reland of ba735dde20869a2ae4781c880184dcf8f1a1a5fd
    
    Original change's description:
    > [heap] Attempt to incorporate backing store counters into heap sizing and GC trigger stragery.
    > 
    > Bug: chromium:845409
    > Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    > Change-Id: Ic62a4339110e3dd2a6b1961a246e2bee0c07c03b
    > Reviewed-on: https://chromium-review.googlesource.com/1160162
    > Commit-Queue: Rodrigo Bruno <rfbpb@google.com>
    > Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#55128}
    
    Bug: chromium:845409
    Change-Id: Iaff177f7bebbc073460fab0ae4e5cd9e632e1921
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng

--- test/cctest/heap/test-heap.cc
+++ test/cctest/heap/test-heap.cc
@@ -1587,6 +1587,7 @@ HEAP_TEST(TestSizeOfObjects) {
   Isolate* isolate = CcTest::i_isolate();
   Heap* heap = CcTest::heap();
   MarkCompactCollector* collector = heap->mark_compact_collector();
+  ManualGCScope manual_gc_scope;
 
   // Get initial heap size after several full GCs, which will stabilize
   // the heap size and return with sweeping finished completely.
