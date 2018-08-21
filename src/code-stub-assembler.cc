3c1278b6220b1aab83d390c6619ccbb0efb953fa
Benedikt Meurer <bmeurer@chromium.org>
2018-08-21 10:56:33 +0200

    [es2015] Use [[NextIndex]] to indicate exhausted array iterators.
    Instead of changing the [[IteratedObject]] field to undefined to mark an
    array iterator as exhausted, store the appropriate maximum value into
    the [[NextIndex]] field such that the iterator will never produce any
    values again.
    
    Without this change the map check and the "length" access on the
    [[IteratedObject]] cannot be eliminated inside the loop, since the
    object can either be the array or undefined. Even with this change
    it's still not possible immediately due to missing aliasing
    information in the LoadElimination, but it paves the way for follow
    up improvements. Eventually the goal is to have `for..of` as fast as
    a traditional `for` loop even for really tight loops.
    
    Bug: v8:7510, v8:7514, v8:8070
    Change-Id: Ib46604fadad1a0f80e77fe71a1f47b0ca31ab841

--- src/code-stub-assembler.cc
+++ src/code-stub-assembler.cc
@@ -2224,6 +2224,47 @@ Node* CodeStubAssembler::LoadFixedTypedArrayElementAsTagged(
   }
 }
 
+TNode<Number> CodeStubAssembler::LoadFixedTypedArrayElementAsTagged(
+    TNode<WordT> data_pointer, TNode<Smi> index, TNode<Int32T> elements_kind) {
+  TVARIABLE(Number, var_result);
+  Label done(this), if_unknown_type(this, Label::kDeferred);
+  int32_t elements_kinds[] = {
+#define TYPED_ARRAY_CASE(Type, type, TYPE, ctype) TYPE##_ELEMENTS,
+      TYPED_ARRAYS(TYPED_ARRAY_CASE)
+#undef TYPED_ARRAY_CASE
+  };
+
+#define TYPED_ARRAY_CASE(Type, type, TYPE, ctype) Label if_##type##array(this);
+  TYPED_ARRAYS(TYPED_ARRAY_CASE)
+#undef TYPED_ARRAY_CASE
+
+  Label* elements_kind_labels[] = {
+#define TYPED_ARRAY_CASE(Type, type, TYPE, ctype) &if_##type##array,
+      TYPED_ARRAYS(TYPED_ARRAY_CASE)
+#undef TYPED_ARRAY_CASE
+  };
+  STATIC_ASSERT(arraysize(elements_kinds) == arraysize(elements_kind_labels));
+
+  Switch(elements_kind, &if_unknown_type, elements_kinds, elements_kind_labels,
+         arraysize(elements_kinds));
+
+  BIND(&if_unknown_type);
+  Unreachable();
+
+#define TYPED_ARRAY_CASE(Type, type, TYPE, ctype)               \
+  BIND(&if_##type##array);                                      \
+  {                                                             \
+    var_result = CAST(LoadFixedTypedArrayElementAsTagged(       \
+        data_pointer, index, TYPE##_ELEMENTS, SMI_PARAMETERS)); \
+    Goto(&done);                                                \
+  }
+  TYPED_ARRAYS(TYPED_ARRAY_CASE)
+#undef TYPED_ARRAY_CASE
+
+  BIND(&done);
+  return var_result.value();
+}
+
 void CodeStubAssembler::StoreFixedTypedArrayElementFromTagged(
     TNode<Context> context, TNode<FixedTypedArrayBase> elements,
     TNode<Object> index_node, TNode<Object> value, ElementsKind elements_kind,
