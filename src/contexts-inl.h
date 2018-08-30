c88b0f60173486328d5ef8cbc7c40e6efe36a10b
tzik <tzik@chromium.org>
2018-08-30 16:36:33 +0900

    Add v8::internal::MicrotaskQueue
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    Change-Id: Ie3d86ca49d46749660ec1316e4ae051528076e97

--- src/contexts-inl.h
+++ src/contexts-inl.h
@@ -10,6 +10,7 @@
 #include "src/objects-inl.h"
 #include "src/objects/dictionary.h"
 #include "src/objects/map-inl.h"
+#include "src/objects/microtask-queue-inl.h"
 #include "src/objects/regexp-match-info.h"
 #include "src/objects/scope-info.h"
 #include "src/objects/shared-function-info-inl.h"
---
+++ src/objects/microtask-queue.cc
@@ -0,0 +1,17 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/objects/microtask-queue.h"
+
+namespace v8 {
+namespace internal {
+
+void MicrotaskQueue::EnqueueMicrotask(Handle<Microtask> microtask) {
+  UNIMPLEMENTED();
+}
+
+void MicrotaskQueue::RunMicrotasks() { UNIMPLEMENTED(); }
+
+}  // namespace internal
+}  // namespace v8
