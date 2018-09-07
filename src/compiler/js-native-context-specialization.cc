710a0b248c80007e55e6e2691df2a82f40619d61
Maya Lekova <mslekova@chromium.org>
2018-09-07 10:01:51 +0200

    [turbofan] Optimize JSToString on strings early on
    This optimization addresses a case triggered by the Templates/Untagged
    js-perf-test, in which template literals get compiled to a cascade of
    JSAdd(JSAdd, JSToString) nodes.
    
    Before the optimization in https://chromium-review.googlesource.com/c/v8/v8/+/1193342
    JSToString no-ops used to get optimized away during typed lowering together
    with constant folding of string concatenation. This change allows us to get
    rid of a no-op JSToString call during native context specialization, thus
    allowing for constant folding of JSAdd to kick in and CheckStringAdd nodes to
    not be generated at all.
    
    This change also removes the NumberToString optimization from typed
    lowering, as it's being executed during earlier stage.
    
    Bug: chromium:879083
    Change-Id: I1d8155ed969b6959fbb86fca21e4714b88a2695a

--- src/compiler/js-native-context-specialization.cc
+++ src/compiler/js-native-context-specialization.cc
@@ -113,9 +113,36 @@ Reduction JSNativeContextSpecialization::Reduce(Node* node) {
       return ReduceJSStoreInArrayLiteral(node);
     case IrOpcode::kJSToObject:
       return ReduceJSToObject(node);
+    case IrOpcode::kJSToString:
+      return ReduceJSToString(node);
     default:
       break;
   }
+  return NoChange();
+}
+
+Reduction JSNativeContextSpecialization::ReduceJSToString(Node* node) {
+  DCHECK_EQ(IrOpcode::kJSToString, node->opcode());
+  // Try to reduce the input first.
+  Node* const input = node->InputAt(0);
+
+  Reduction reduction;
+
+  HeapObjectMatcher matcher(input);
+  if (matcher.HasValue() && matcher.Value()->IsString()) {
+    reduction = Changed(input);  // JSToString(x:string) => x
+    ReplaceWithValue(node, reduction.replacement());
+    return reduction;
+  }
+
+  NumberMatcher number_matcher(input);
+  if (number_matcher.HasValue()) {
+    reduction = Replace(jsgraph()->HeapConstant(factory()->NumberToString(
+        factory()->NewNumber(number_matcher.Value()))));
+    ReplaceWithValue(node, reduction.replacement());
+    return reduction;
+  }
+
   return NoChange();
 }
 
