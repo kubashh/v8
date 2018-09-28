8b0c127134b78dcbfc37b7bcb8541019733add11
Hai Dang <dhai@google.com>
2018-09-28 11:46:17 +0200

    Add string iterator protector.
    The protector is useful for follow-up optimizations on string iterator.
    Tests are also added.
    
    Change-Id: I416037c742628c4d4d3b878d0df727a9ae7162f7

---
+++ test/mjsunit/es6/string-iterator2.js
@@ -0,0 +1,19 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --allow-natives-syntax
+
+// Tests for primitive strings.
+
+var str = 'ott';
+assertTrue(%StringIteratorProtector());
+assertEquals(['o', 't', 't'], [...str]);
+
+// This changes the String Iterator prototype. No more tests should be run after
+// this in the same instance.
+var iterator = str[Symbol.iterator]();
+iterator.__proto__.next = () => ({ value : undefined, done : true });
+
+assertFalse(%StringIteratorProtector());
+assertEquals([], [...str]);
