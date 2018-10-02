a64f5047033f9e6cfd0bde50dca19f2e3f66c865
Mike Stanton <mvstanton@chromium.org>
2018-10-02 13:40:14 +0200

    [Builtins] Array.prototype.splice performance improvements
    a) The current size of the backing store for the array under splice
        wasn't considered. Additionally, allocate the array with the
        normal growing strategy.
    
    b) Use primitives memcpy, memmove, and Heap::MoveElements when
       appropriate.
    
    Bug:chromium:880780
    Change-Id: I39a917c71036f52250c68f2cced77a1c24f97b67

--- src/code-stub-assembler.h
+++ src/code-stub-assembler.h
@@ -1567,6 +1567,16 @@ class V8_EXPORT_PRIVATE CodeStubAssembler : public compiler::CodeAssembler {
                            SMI_PARAMETERS);
   }
 
+  void HeapMoveElements(TNode<FixedArray> elements, TNode<Smi> dst_index,
+                        TNode<Smi> src_index, TNode<Smi> length);
+
+  void MemMove(TNode<FixedDoubleArray> elements, TNode<Smi> src_index,
+               TNode<Smi> dst_index, TNode<Smi> length);
+
+  void MemCopy(TNode<FixedDoubleArray> src_elements, TNode<Smi> src_index,
+               TNode<FixedDoubleArray> dst_Elements, TNode<Smi> dst_index,
+               TNode<Smi> length);
+
   TNode<FixedArray> HeapObjectToFixedArray(TNode<HeapObject> base,
                                            Label* cast_fail);
 
@@ -1739,6 +1749,10 @@ class V8_EXPORT_PRIVATE CodeStubAssembler : public compiler::CodeAssembler {
 
   Node* CalculateNewElementsCapacity(Node* old_capacity,
                                      ParameterMode mode = INTPTR_PARAMETERS);
+
+  TNode<Smi> CalculateNewElementsCapacity(TNode<Smi> old_capacity) {
+    return CAST(CalculateNewElementsCapacity(old_capacity, SMI_PARAMETERS));
+  }
 
   // Tries to grow the |elements| array of given |object| to store the |key|
   // or bails out if the growing gap is too big. Returns new elements.
