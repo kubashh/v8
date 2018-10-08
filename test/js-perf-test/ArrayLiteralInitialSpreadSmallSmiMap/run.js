f27f8584432f68dea95fee29a1e59870c7262611
Hai Dang <dhai@google.com>
2018-10-08 12:39:54 +0200

    [benchmarks] Add micro-benchmarks for spreading maps and sets.
    Change-Id: I640119fc9a9af66370c47f4d5b16244a1cc3f716

---
+++ test/js-perf-test/ArrayLiteralInitialSpreadSmallSmiMap/run.js
@@ -0,0 +1,101 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Comparing different iterating schemes against spread initial literals.
+// Benchmarks for small smi maps.
+
+var keys = Array.from(Array(50).keys());
+var keyValuePairs = keys.map((value) => [value, value + 1] );
+var map = new Map(keyValuePairs);
+
+// ----------------------------------------------------------------------------
+// Benchmark: Spread
+// ----------------------------------------------------------------------------
+
+function Spread() {
+  var newArr = [...map];
+  // basic sanity check
+  if (newArr.length != map.size) throw 666;
+  return newArr;
+}
+
+// ----------------------------------------------------------------------------
+// Benchmark: IterateWithLength
+// ----------------------------------------------------------------------------
+
+function IterateWithLength() {
+  var iterator = map[Symbol.iterator]();
+  var length = map.size;
+  var newArr = new Array(length);
+  for (var i = 0; i < length; i++) {
+    newArr[i] = iterator.next().value;
+  }
+  return newArr;
+}
+
+
+// ----------------------------------------------------------------------------
+// Benchmark: Entries
+// ----------------------------------------------------------------------------
+
+function Entries() {
+  var iterator = map.entries();
+  var newArr = new Array(map.size);
+  for (var i = 0; ; i++) {
+    var iteratorResult = iterator.next();
+    if (iteratorResult.done) break;
+    newArr[i] = iteratorResult.value;
+  }
+  if (newArr.length != map.size) throw 666;
+  return newArr;
+}
+
+
+// ----------------------------------------------------------------------------
+// Benchmark: Keys
+// ----------------------------------------------------------------------------
+
+function Keys() {
+  var iterator = map.keys();
+  var newArr = new Array(map.size);
+  for (var i = 0; ; i++) {
+    var iterResult = iterator.next();
+    if (iterResult.done) break;
+    newArr[i] = [iterResult.value, map.get(iterResult.value)];
+  }
+  if (newArr.length != map.size) throw 666;
+  return newArr;
+}
+
+
+// ----------------------------------------------------------------------------
+// Setup and Run
+// ----------------------------------------------------------------------------
+
+load('../base.js');
+
+var success = true;
+
+function PrintResult(name, result) {
+  print(name + '-ArrayLiteralInitialSpreadSmallSmiMap(Score): ' + result);
+}
+
+function PrintError(name, error) {
+  PrintResult('Error: ' + name, error);
+  success = false;
+}
+
+function CreateBenchmark(name, f) {
+  new BenchmarkSuite(name, [1000], [ new Benchmark(name, false, false, 0, f) ]);
+}
+
+CreateBenchmark('Entries', Entries);
+CreateBenchmark('Keys', Keys);
+CreateBenchmark('IterateWithLength', IterateWithLength);
+CreateBenchmark('Spread', Spread);
+
+
+BenchmarkSuite.config.doWarmup = true;
+BenchmarkSuite.config.doDeterministic = false;
+BenchmarkSuite.RunSuites({NotifyResult: PrintResult, NotifyError: PrintError});
