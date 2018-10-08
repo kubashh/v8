7e02771fabe02b1577c8819360815230641bf09a
Hai Dang <dhai@google.com>
2018-10-08 11:27:08 +0200

    [test] Add benchmarks for spreading maps and sets.
    Change-Id: I640119fc9a9af66370c47f4d5b16244a1cc3f716

---
+++ test/js-perf-test/ArrayLiteralInitialSpreadSmallObjectSet/run.js
@@ -0,0 +1,82 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Comparing different iterating schemes against spread initial literals.
+// Benchmarks for small object sets.
+
+var keys = Array.from(Array(50).keys());
+var set = new Set(keys.map((v) => {key : v}));
+
+// ----------------------------------------------------------------------------
+// Benchmark: Spread
+// ----------------------------------------------------------------------------
+
+function Spread() {
+  var newArr = [...set];
+  // basic sanity check
+  if (newArr.length != set.size) throw 666;
+  return newArr;
+}
+
+// ----------------------------------------------------------------------------
+// Benchmark: Iterate
+// ----------------------------------------------------------------------------
+
+function Iterate() {
+  var iterator = set[Symbol.iterator]();
+  var length = set.size;
+  var newArr = new Array(length);
+  for (var i = 0; i < length; i++) {
+    newArr[i] = iterator.next().value;
+  }
+  return newArr;
+}
+
+
+// ----------------------------------------------------------------------------
+// Benchmark: Keys
+// ----------------------------------------------------------------------------
+
+function Keys() {
+  var iterator = set.keys();
+  var newArr = new Array(set.size);
+  for (var i = 0; ; i++) {
+    var iterResult = iterator.next();
+    if (iterResult.done) break;
+    newArr[i] = iterResult.value;
+  }
+  if (newArr.length != set.size) throw 666;
+  return newArr;
+}
+
+
+// ----------------------------------------------------------------------------
+// Setup and Run
+// ----------------------------------------------------------------------------
+
+load('../base.js');
+
+var success = true;
+
+function PrintResult(name, result) {
+  print(name + '-ArrayLiteralInitialSpreadSmallObjectSet(Score): ' + result);
+}
+
+function PrintError(name, error) {
+  PrintResult('Error: ' + name, error);
+  success = false;
+}
+
+function CreateBenchmark(name, f) {
+  new BenchmarkSuite(name, [1000], [ new Benchmark(name, false, false, 0, f) ]);
+}
+
+CreateBenchmark('Keys', Keys);
+CreateBenchmark('Iterate', Iterate);
+CreateBenchmark('Spread', Spread);
+
+
+BenchmarkSuite.config.doWarmup = true;
+BenchmarkSuite.config.doDeterministic = false;
+BenchmarkSuite.RunSuites({NotifyResult: PrintResult, NotifyError: PrintError});
