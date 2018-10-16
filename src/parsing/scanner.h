e48c36abfb164ebb3672a30c1703ec0dcff0de89
Florian Sattler <sattlerf@google.com>
2018-10-16 14:22:13 +0200

    Compress memory by reordering members and storing bits in pointers.
    Store the bits of a small type into the lower bits
    of a pointer type that are free due to alignment.
    Furthermore, reordering of members to reduce size of some classes.
    Change-Id: I3c619cb74053f64995ea7d0cb395e8edda604f18

--- src/parsing/scanner.h
+++ src/parsing/scanner.h
@@ -15,6 +15,7 @@
 #include "src/globals.h"
 #include "src/messages.h"
 #include "src/parsing/token.h"
+#include "src/pointer-with-payload.h"
 #include "src/unicode-decoder.h"
 #include "src/unicode.h"
 
@@ -501,8 +502,8 @@ class Scanner {
 
     Vector<byte> backing_store_;
     int position_;
-    bool is_one_byte_;
-    bool is_used_;
+    bool is_one_byte_ : 1;
+    bool is_used_ : 1;
 
     DISALLOW_COPY_AND_ASSIGN(LiteralBuffer);
   };
@@ -512,17 +513,18 @@ class Scanner {
   class LiteralScope {
    public:
     explicit LiteralScope(Scanner* scanner)
-        : buffer_(&scanner->next().literal_chars), complete_(false) {
-      buffer_->Start();
+        : buffer_and_complete_(&scanner->next().literal_chars) {
+      buffer_()->Start();
     }
     ~LiteralScope() {
-      if (!complete_) buffer_->Drop();
+      if (!buffer_and_complete_.GetPayload()) buffer_()->Drop();
     }
-    void Complete() { complete_ = true; }
+    void Complete() { buffer_and_complete_.SetPayload(true); }
 
    private:
-    LiteralBuffer* buffer_;
-    bool complete_;
+    LiteralBuffer* buffer_() const { return buffer_and_complete_.GetPointer(); }
+
+    PointerWithPayload<LiteralBuffer, bool, 1> buffer_and_complete_;
   };
 
   // The current and look-ahead token.
