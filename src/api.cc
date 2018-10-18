245101c87e55831bb43e1145bd07ae65067741a5
Andreas Haas <ahaas@chromium.org>
2018-10-18 15:25:03 +0200

    [wasm] Implement trap handler on Windows
    This is the V8 side of the implementation. You can take a look at a
    prototype of the Chrome side changes in https://crrev.com/c/1273043.
    
    Bug: v8:6743
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    Change-Id: I9bb3e717db17a4f30bbb8acfd80a1f6510d463ff

--- src/api.cc
+++ src/api.cc
@@ -89,6 +89,11 @@
 #include "src/trap-handler/handler-inside-posix.h"
 #endif
 
+#if V8_OS_WIN
+#include <windows.h>
+#include "src/trap-handler/handler-inside-win.h"
+#endif
+
 #include "src/trap-handler/trap-handler.h"
 #include "src/unicode-cache-inl.h"
 #include "src/unicode-inl.h"
@@ -5923,6 +5928,15 @@ bool TryHandleWebAssemblyTrapPosix(int sig_code, siginfo_t* info,
                                    void* context) {
 #if V8_OS_LINUX && V8_TARGET_ARCH_X64 && !V8_OS_ANDROID
   return i::trap_handler::TryHandleSignal(sig_code, info, context);
+#endif
+  return false;
+}
+#endif
+
+#if V8_OS_WIN
+bool TryHandleWebAssemblyTrapWindows(LPEXCEPTION_POINTERS exception) {
+#if V8_TARGET_ARCH_X64
+  return i::trap_handler::TryHandleWasmTrap(exception);
 #endif
   return false;
 }
