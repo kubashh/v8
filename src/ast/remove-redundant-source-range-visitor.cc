ef2f0126acbb599b1bf13d68964b933f39bdfbc9
Benjamin Coe <ben@npmjs.com>
2018-11-26 21:19:25 -0800

    collect coverage after return
    Introduces an AST visitor pass that removes the last source
    range from blocks, if it's a return, continue, throw, break.
    This prevents missing coverage immediately following a return.
    
    R=bmeurer@chromium.org, jgruber@chromium.org, yangguo@chromium.org
    
    Bug: v8:8381
    Change-Id: I6bcaea5060d8c481d7bae099f6db9f993cc30ee3

---
+++ src/ast/remove-redundant-source-range-visitor.cc
@@ -0,0 +1,53 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/ast/remove-redundant-source-range-visitor.h"
+
+namespace v8 {
+namespace internal {
+
+RemoveRedundantSourceRangeVisitor::RemoveRedundantSourceRangeVisitor(
+    uintptr_t stack_limit, Expression* root, SourceRangeMap* source_range_map)
+    : AstTraversalVisitor(stack_limit, root),
+      source_range_map_(source_range_map) {}
+
+void RemoveRedundantSourceRangeVisitor::VisitBlock(Block* stmt) {
+  AstTraversalVisitor::VisitBlock(stmt);
+  ZonePtrList<Statement>* stmts = stmt->statements();
+  AstNodeSourceRanges* enclosingSourceRanges = source_range_map_->Find(stmt);
+  if (enclosingSourceRanges != nullptr) {
+    MaybeRemoveLastRange(stmts);
+  }
+}
+
+void RemoveRedundantSourceRangeVisitor::VisitFunctionLiteral(
+    FunctionLiteral* expr) {
+  AstTraversalVisitor::VisitFunctionLiteral(expr);
+  ZonePtrList<Statement>* stmts = expr->body();
+  MaybeRemoveLastRange(stmts);
+}
+
+void RemoveRedundantSourceRangeVisitor::MaybeRemoveLastRange(
+    ZonePtrList<Statement>* stmts) {
+  if (stmts->length() == 0) return;
+  if (stmts->last()->IsIfStatement()) {
+    IfStatement* ifstmt = reinterpret_cast<IfStatement*>(stmts->last());
+    IfStatementSourceRanges* ranges =
+        reinterpret_cast<IfStatementSourceRanges*>(
+            source_range_map_->Find(stmts->last()));
+    if (ranges) {
+      source_range_map_->Erase(ifstmt->then_statement());
+      source_range_map_->Erase(ifstmt->else_statement());
+      ranges->DisableContinuation();
+    }
+  } else if (stmts->last()->IsJump()) {
+    AstNodeSourceRanges* ranges = source_range_map_->Find(stmts->last());
+    if (ranges != nullptr) {
+      source_range_map_->Erase(stmts->last());
+    }
+  }
+}
+
+}  // namespace internal
+}  // namespace v8
