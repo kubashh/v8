da39ca9db7c45b297800c9a4374c567c8ec79fde
Sven Sauleau <ssauleau@igalia.com>
2018-11-28 12:07:36 +0100

    [wasm] Add WebAssembly's i64<>JavaScript's BigInt conversions
    Uses the JavaScript BigInt Object to represent Wasm's 64bits integers.
    
    Bug: https://crbug.com/v8/7741
    Change-Id: I6589693dff5716a84776d25234ab12458a5d2032

---
+++ src/builtins/builtins-bigint-gen.cc
@@ -0,0 +1,95 @@
+// Copyright 2017 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/builtins/builtins-utils-gen.h"
+#include "src/builtins/builtins.h"
+#include "src/code-stub-assembler.h"
+
+namespace v8 {
+namespace internal {
+
+class BigIntBuiltinsAssembler : public CodeStubAssembler {
+ public:
+  explicit BigIntBuiltinsAssembler(compiler::CodeAssemblerState* state)
+      : CodeStubAssembler(state) {}
+};
+
+// https://tc39.github.io/proposal-bigint/#sec-to-big-int64
+TF_BUILTIN(ToBigInt64, BigIntBuiltinsAssembler) {
+  TVARIABLE(UintPtrT, var_low);
+  TVARIABLE(UintPtrT, var_high);
+
+  // input
+  Node* value = Parameter(Descriptor::kArgument);
+  Node* context = Parameter(Descriptor::kContext);
+
+  TNode<BigInt> bigint = ToBigInt(CAST(context), CAST(value));
+
+  // 2. Let int64bit be n modulo 2^64.
+  // 3. If int64bit ≥ 2^63, return int64bit - 2^64;
+  BigIntToRawBytes(bigint, &var_low, &var_high);
+
+  // otherwise return int64bit
+  if (Is64()) {
+    ReturnRaw(Signed(var_low.value()));
+  } else {
+    TNode<IntPtrT> heapObjectTag = IntPtrConstant(kHeapObjectTag);
+    TNode<IntPtrT> size = IntPtrConstant(sizeof(int32_t));
+    TNode<IntPtrT> two_times_size = IntPtrConstant(sizeof(int32_t) * 2);
+
+    /**
+     * Memory layout is:
+     * | 1 byte | 4 bytes | 4 bytes |
+     * | GC map |   low   |  high   |
+     */
+
+    TNode<HeapObject> ptr =
+        AllocateInNewSpace(IntPtrAdd(heapObjectTag, two_times_size));
+    StoreMapNoWriteBarrier(ptr, RootIndex::kHeapNumberMap);
+
+    StoreNoWriteBarrier(MachineRepresentation::kWord32, ptr, heapObjectTag,
+                        var_low.value());
+    StoreNoWriteBarrier(MachineRepresentation::kWord32, ptr,
+                        IntPtrAdd(heapObjectTag, size), var_high.value());
+
+    ReturnRaw(ptr);
+  }
+}
+
+// https://tc39.github.io/proposal-bigint/#sec-bigint-constructor-number-value
+TF_BUILTIN(NewBigInt, BigIntBuiltinsAssembler) {
+  if (Is64()) {
+    TVARIABLE(BigInt, value_bigint);
+
+    TNode<IntPtrT> argument =
+        UncheckedCast<IntPtrT>(Parameter(Descriptor::kArgument));
+
+    value_bigint = BigIntFromInt64(argument);
+    DCHECK(IsBigInt(value_bigint.value()));
+
+    Return(value_bigint.value());
+  } else {
+    Unreachable();
+  }
+}
+
+// https://tc39.github.io/proposal-bigint/#sec-bigint-constructor-number-value
+TF_BUILTIN(NewBigInt32, BigIntBuiltinsAssembler) {
+  if (Is32()) {
+    TVARIABLE(BigInt, value_bigint);
+
+    TNode<IntPtrT> low = UncheckedCast<IntPtrT>(Parameter(Descriptor::kLow));
+    TNode<IntPtrT> high = UncheckedCast<IntPtrT>(Parameter(Descriptor::kHigh));
+
+    value_bigint = BigIntFromInt32Pair(low, high);
+    DCHECK(IsBigInt(value_bigint.value()));
+
+    Return(value_bigint.value());
+  } else {
+    Unreachable();
+  }
+}
+
+}  // namespace internal
+}  // namespace v8
--- src/compiler/int64-lowering.cc
+++ src/compiler/int64-lowering.cc
@@ -1,6 +1,7 @@
 // Copyright 2014 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#include <string>
 
 #include "src/compiler/int64-lowering.h"
 
@@ -285,16 +286,18 @@ void Int64Lowering::LowerNode(Node* node) {
     }
     case IrOpcode::kParameter: {
       DCHECK_EQ(1, node->InputCount());
+
+      int paramCount = static_cast<int>(signature()->parameter_count());
+
       // Only exchange the node if the parameter count actually changed. We do
       // not even have to do the default lowering because the the start node,
       // the only input of a parameter node, only changes if the parameter count
       // changes.
-      if (GetParameterCountAfterLowering(signature()) !=
-          static_cast<int>(signature()->parameter_count())) {
+      if (GetParameterCountAfterLowering(signature()) != paramCount) {
         int old_index = ParameterIndexOf(node->op());
-        // TODO(wasm): Make this part not wasm specific.
-        // Prevent special lowering of the instance parameter.
-        if (old_index == wasm::kWasmInstanceParameterIndex) {
+        // Prevent special lowering of wasm's instance or JS
+        // context/closure parameters.
+        if (old_index < 0 || old_index >= paramCount) {
           DefaultLowering(node);
           break;
         }
@@ -342,13 +345,25 @@ void Int64Lowering::LowerNode(Node* node) {
     case IrOpcode::kCall: {
       auto call_descriptor =
           const_cast<CallDescriptor*>(CallDescriptorOf(node->op()));
+
       bool returns_require_lowering =
           GetReturnCountAfterLowering(call_descriptor) !=
           static_cast<int>(call_descriptor->ReturnCount());
       if (DefaultLowering(node) || returns_require_lowering) {
-        // We have to adjust the call descriptor.
-        NodeProperties::ChangeOp(node, common()->Call(GetI32WasmCallDescriptor(
-                                           zone(), call_descriptor)));
+        // Special case for BigInt builtins
+        // TODO(sven): is there a better way to detect the initial
+        // CallingDescriptor?
+        if (std::string(call_descriptor->debug_name()) ==
+            "WasmToJavaScriptTypeConversion Descriptor") {
+          NodeProperties::ChangeOp(
+              node, common()->Call(GetI32WasmCallDescriptorForBigInt(
+                        zone(), call_descriptor)));
+        } else {
+          // We have to adjust the call descriptor.
+          NodeProperties::ChangeOp(
+              node, common()->Call(
+                        GetI32WasmCallDescriptor(zone(), call_descriptor)));
+        }
       }
       if (returns_require_lowering) {
         size_t return_arity = call_descriptor->ReturnCount();
