99e3b9e1ec1a89dced9affb61c7cfe49ea511174
Aseem Garg <aseemgarg@chromium.org>
2018-11-28 21:39:18 -0800

    Reland "[wasm] Add I64AtomicWait implementation"
    This is a reland of 9436e8a817fde725425eb1dd95cc245205154569
    
    Original change's description:
    > [wasm] Add I64AtomicWait implementation
    >
    > Bug=v8:8075
    > R=adamk@chromium.org,binji@chromium.org
    >
    > Change-Id: I11ef5daccd043123b23e60c93ee0df79cabe9ccd
    > Reviewed-on: https://chromium-review.googlesource.com/c/1342948
    > Reviewed-by: Adam Klein <adamk@chromium.org>
    > Reviewed-by: Ben Smith <binji@chromium.org>
    > Commit-Queue: Aseem Garg <aseemgarg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#57917}
    
    Change-Id: Ifd26f1ecdb9fe24a1896162bb4d4285f9188a9ba

--- test/mjsunit/wasm/futex.js
+++ test/mjsunit/wasm/futex.js
@@ -47,6 +47,38 @@ function WasmI32AtomicWaitFunction(memory, offset, index, val, timeout) {
   return instance.exports.main(index, val, timeout);
 }
 
+function WasmI64AtomicWaitFunction(memory, offset, index, val_low,
+                                   val_high, timeout) {
+  let builder = new WasmModuleBuilder();
+  builder.addImportedMemory("m", "memory", 0, 20, "shared");
+  // Wrapper for I64AtomicWait that takes two I32 values and combines to into
+  // I64 for the instruction parameter.
+  builder.addFunction("main",
+    makeSig([kWasmI32, kWasmI32, kWasmI32, kWasmF64], [kWasmI32]))
+    .addLocals({i64_count: 1}) // local that is passed as value param to wait
+    .addBody([
+      kExprGetLocal, 1,
+      kExprI64UConvertI32,
+      kExprI64Const, 32,
+      kExprI64Shl,
+      kExprGetLocal, 2,
+      kExprI64UConvertI32,
+      kExprI64Ior,
+      kExprSetLocal, 4, // Store the created I64 value in local
+      kExprGetLocal, 0,
+      kExprGetLocal, 4,
+      kExprGetLocal, 3,
+      kExprI64SConvertF64,
+      kAtomicPrefix,
+      kExprI64AtomicWait, /* alignment */ 0, offset])
+      .exportAs("main");
+
+  // Instantiate module, get function exports
+  let module = new WebAssembly.Module(builder.toBuffer());
+  let instance = new WebAssembly.Instance(module, {m: {memory}});
+  return instance.exports.main(index, val_high, val_low, timeout);
+}
+
 (function TestInvalidIndex() {
   let memory = new WebAssembly.Memory({initial: 1, maximum: 1, shared: true});
 
@@ -59,10 +91,16 @@ function WasmI32AtomicWaitFunction(memory, offset, index, val, timeout) {
       WasmI32AtomicWaitFunction(memory, 0, invalidIndex, 0, -1);
     }, Error);
     assertThrows(function() {
+      WasmI64AtomicWaitFunction(memory, 0, invalidIndex, 0, 0, -1);
+    }, Error);
+    assertThrows(function() {
       WasmAtomicWakeFunction(memory, invalidIndex, 0, -1);
     }, Error);
     assertThrows(function() {
       WasmI32AtomicWaitFunction(memory, invalidIndex, 0, 0, -1);
+    }, Error);
+    assertThrows(function() {
+      WasmI64AtomicWaitFunction(memory, invalidIndex, 0, 0, 0, -1);
     }, Error);
     assertThrows(function() {
       WasmAtomicWakeFunction(memory, invalidIndex/2, invalidIndex/2, -1);
@@ -70,10 +108,13 @@ function WasmI32AtomicWaitFunction(memory, offset, index, val, timeout) {
     assertThrows(function() {
       WasmI32AtomicWaitFunction(memory, invalidIndex/2, invalidIndex/2, 0, -1);
     }, Error);
+    assertThrows(function() {
+      WasmI64AtomicWaitFunction(memory, invalidIndex/2, invalidIndex/2, 0, 0, -1);
+    }, Error);
   });
 })();
 
-(function TestWaitTimeout() {
+(function TestI32WaitTimeout() {
   let memory = new WebAssembly.Memory({initial: 1, maximum: 1, shared: true});
   var waitMs = 100;
   var startTime = new Date();
@@ -82,7 +123,16 @@ function WasmI32AtomicWaitFunction(memory, offset, index, val, timeout) {
   assertTrue(endTime - startTime >= waitMs);
 })();
 
-(function TestWaitNotEqual() {
+(function TestI64WaitTimeout() {
+  let memory = new WebAssembly.Memory({initial: 1, maximum: 1, shared: true});
+  var waitMs = 100;
+  var startTime = new Date();
+  assertEquals(2, WasmI64AtomicWaitFunction(memory, 0, 0, 0, 0, waitMs*1000000));
+  var endTime = new Date();
+  assertTrue(endTime - startTime >= waitMs);
+})();
+
+(function TestI32WaitNotEqual() {
   let memory = new WebAssembly.Memory({initial: 1, maximum: 1, shared: true});
   assertEquals(1, WasmI32AtomicWaitFunction(memory, 0, 0, 42, -1));
 
@@ -91,6 +141,20 @@ function WasmI32AtomicWaitFunction(memory, offset, index, val, timeout) {
   let i32a = new Int32Array(memory.buffer);
   i32a[0] = 1;
   assertEquals(1, WasmI32AtomicWaitFunction(memory, 0, 0, 0, -1));
+  assertEquals(2, WasmI32AtomicWaitFunction(memory, 0, 0, 1, 0));
+})();
+
+(function TestI64WaitNotEqual() {
+  let memory = new WebAssembly.Memory({initial: 1, maximum: 1, shared: true});
+  assertEquals(1, WasmI64AtomicWaitFunction(memory, 0, 0, 42, 0, -1));
+
+  assertEquals(2, WasmI64AtomicWaitFunction(memory, 0, 0, 0, 0, 0));
+
+  let i32a = new Int32Array(memory.buffer);
+  i32a[0] = 1;
+  i32a[1] = 2;
+  assertEquals(1, WasmI64AtomicWaitFunction(memory, 0, 0, 0, 0, -1));
+  assertEquals(2, WasmI64AtomicWaitFunction(memory, 0, 0, 1, 2, 0));
 })();
 
 (function TestWakeCounts() {
@@ -104,109 +168,104 @@ function WasmI32AtomicWaitFunction(memory, offset, index, val, timeout) {
 //// WORKER ONLY TESTS
 
 if (this.Worker) {
-  let wasm_wake_adapter = (memory, offset, index, count) => {
-    return WasmAtomicWakeFunction(memory, offset, index, count);
-  };
 
-  let js_wake_adapter = (memory, offset, index, count) => {
-    let i32a = new Int32Array(memory.buffer, offset);
-    return Atomics.wake(i32a, index>>>2, count);
-  };
+  let memory = new WebAssembly.Memory({initial: 1, maximum: 1, shared: true});
 
-  // Wait adapter strings that can be passed as a parameter to TestWaitWake to generate
-  // custom worker script
-  let js_wait_adapter = `(memory, offset, index, val) => {
-    let i32a = new Int32Array(memory.buffer, offset);
-    let res = Atomics.wait(i32a, index>>>2, val);
-    if (res == "ok") return 0;
-    if (res == "not-equal") return 1;
-    return 2;
-  }`
-
-  let wasm_wait_adapter = `(memory, offset, index, val) => {
+  let workerScript = `onmessage = function(msg) {
     load("test/mjsunit/wasm/wasm-constants.js");
     load("test/mjsunit/wasm/wasm-module-builder.js");
     ${WasmI32AtomicWaitFunction.toString()}
-    return WasmI32AtomicWaitFunction(memory, offset, index, val, -1);
-  }`
-
-  let TestWaitWake = function(wait_adapter, wake_adapter, num_workers, num_workers_wake) {
-    let memory = new WebAssembly.Memory({initial: 1, maximum: 1, shared: true});
+    ${WasmI64AtomicWaitFunction.toString()}
+    let id = msg.id;
+    let memory = msg.memory;
     let i32a = new Int32Array(memory.buffer);
-    // SAB values:
-    // memory[id*4], where id in range [0, num_workers]:
-    //   0 => Worker |id| is still waiting on the futex
-    //   1 => Worker |id| is not waiting on futex, but has not be reaped by the
-    //        main thread.
-    //   2 => Worker |id| has been reaped.
-    //
-    // memory[num_workers*4]:
-    //   always 0. Each worker is waiting on this index.
+    let result = Atomics.wait(i32a, 0>>>2, 0);                    // for wasm-wake 4 threads
+    postMessage(result);
+    result = Atomics.wait(i32a, 8>>>2, 0);                        // for wasm-wake 5 threads
+    postMessage(result);
+    result = Atomics.wait(i32a, 16>>>2, 0);                       // for wasm-wake 3 threads
+    postMessage(result);
+    result = WasmI32AtomicWaitFunction(memory, 0, 24, 0, -1);     // for js-wake 4 threads
+    postMessage(result);
+    result = WasmI32AtomicWaitFunction(memory, 0, 32, 0, -1);     // for js-wake 5 threads
+    postMessage(result);
+    result = WasmI32AtomicWaitFunction(memory, 0, 40, 0, -1);     // for js-wake 3 threads
+    postMessage(result);
+    result = WasmI32AtomicWaitFunction(memory, 0, 48, 0, -1);     // for wasm-wake 4 threads
+    postMessage(result);
+    result = WasmI32AtomicWaitFunction(memory, 0, 56, 0, -1);     // for wasm-wake 5 threads
+    postMessage(result);
+    result = WasmI32AtomicWaitFunction(memory, 0, 64, 0, -1);     // for wasm-wake 3 threads
+    postMessage(result);
+    result = WasmI64AtomicWaitFunction(memory, 0, 72, 0, 0, -1);  // for js-wake 4 threads
+    postMessage(result);
+    result = WasmI64AtomicWaitFunction(memory, 0, 80, 0, 0, -1);  // for js-wake 5 threads
+    postMessage(result);
+    result = WasmI64AtomicWaitFunction(memory, 0, 88, 0, 0, -1);  // for js-wake 3 threads
+    postMessage(result);
+    result = WasmI64AtomicWaitFunction(memory, 0, 96, 0, 0, -1);  // for wasm-wake 4 threads
+    postMessage(result);
+    result = WasmI64AtomicWaitFunction(memory, 0, 104, 0, 0, -1); // for wasm-wake 5 threads
+    postMessage(result);
+    result = WasmI64AtomicWaitFunction(memory, 0, 112, 0, 0, -1); // for wasm-wake 3 threads
+    postMessage(result);
+  };`;
 
-    let workerScript =
-      `onmessage = function(msg) {
-         let id = msg.id;
-         let memory = msg.memory;
-         let i32a = new Int32Array(memory.buffer);
-         let wait_adapter = eval(msg.wait_adapter);
-         let result = wait_adapter(memory, 0, 4*${num_workers}, 0);
-         // Set i32a[id] to 1 to notify the main thread which workers were
-         // woken up.
-         Atomics.store(i32a, id, 1);
-         postMessage(result);
-       };`;
-
-    let workers = [];
-    for (let id = 0; id < num_workers; id++) {
-      workers[id] = new Worker(workerScript, {type: 'string'});
-      workers[id].postMessage({id, memory, wait_adapter});
-    }
-
-    // Spin until all workers are waiting on the futex.
-    while (%AtomicsNumWaitersForTesting(i32a, num_workers) != num_workers) {}
-
-    if (num_workers_wake < num_workers) {
-      assertEquals(num_workers_wake, wake_adapter(memory, 0, 4*num_workers, num_workers_wake));
+  let js_wake_check = function(memory, index, num, workers, msg) {
+    let i32a = new Int32Array(memory.buffer);
+    while (%AtomicsNumWaitersForTesting(i32a, index>>>2) != 4) {}
+    if (num >= 4) {
+      assertEquals(4, Atomics.wake(i32a, index>>>2, num));
     } else {
-      assertEquals(num_workers, wake_adapter(memory, 0, 4*num_workers, num_workers_wake));
-      num_workers_wake = num_workers;
+      assertEquals(num, Atomics.wake(i32a, index>>>2, num));
+      assertEquals(4-num, Atomics.wake(i32a, index>>>2, 4));
     }
-
-    let wokenCount = 0;
-    while (wokenCount < num_workers_wake) {
-      for (let id = 0; id < num_workers; id++) {
-        // Look for workers that have not yet been reaped. Set i32a[id] to 2
-        // when they've been processed so we don't look at them again.
-        if (Atomics.compareExchange(i32a, id, 1, 2) == 1) {
-          assertEquals(0, workers[id].getMessage());
-          workers[id].terminate();
-          wokenCount++;
-        }
-      }
-    }
-
-    assertEquals(num_workers - num_workers_wake,
-                 %AtomicsNumWaitersForTesting(i32a, num_workers));
-
-    // Finally wake and kill all workers.
-    wake_adapter(memory, 0, 4*num_workers, num_workers)
-    for (let id = 0; id < num_workers; id++) {
-      workers[id].terminate();
+    for (let id = 0; id < 4; id++) {
+      assertEquals(msg, workers[id].getMessage());
     }
   };
 
-  TestWaitWake(js_wait_adapter, wasm_wake_adapter, 1, 1);
-  TestWaitWake(js_wait_adapter, wasm_wake_adapter, 4, 4);
-  TestWaitWake(js_wait_adapter, wasm_wake_adapter, 4, 3);
-  TestWaitWake(js_wait_adapter, wasm_wake_adapter, 3, 4);
+  let wasm_wake_check = function(memory, index, num, workers, msg) {
+    let i32a = new Int32Array(memory.buffer);
+    while (%AtomicsNumWaitersForTesting(i32a, index>>>2) != 4) {}
+    if (num >= 4) {
+      assertEquals(4, WasmAtomicWakeFunction(memory, 0, index, num));
+    } else {
+      assertEquals(num, WasmAtomicWakeFunction(memory, 0, index, num));
+      assertEquals(4-num, WasmAtomicWakeFunction(memory, 0, index, num));
+    }
+    for (let id = 0; id < 4; id++) {
+      assertEquals(msg, workers[id].getMessage());
+    }
+  };
 
-  TestWaitWake(wasm_wait_adapter, wasm_wake_adapter, 1, 1);
-  TestWaitWake(wasm_wait_adapter, wasm_wake_adapter, 4, 4);
-  TestWaitWake(wasm_wait_adapter, wasm_wake_adapter, 4, 3);
-  TestWaitWake(wasm_wait_adapter, wasm_wake_adapter, 3, 4);
+  let workers = [];
+  for (let id = 0; id < 4; id++) {
+    workers[id] = new Worker(workerScript, {type: 'string'});
+    workers[id].postMessage({id, memory});
+  }
 
-  TestWaitWake(wasm_wait_adapter, js_wake_adapter, 1, 1);
-  TestWaitWake(wasm_wait_adapter, js_wake_adapter, 4, 4);
-  TestWaitWake(wasm_wait_adapter, js_wake_adapter, 4, 3);
-  TestWaitWake(wasm_wait_adapter, js_wake_adapter, 3, 4);
+  wasm_wake_check(memory, 0, 4, workers, "ok");
+  wasm_wake_check(memory, 8, 5, workers, "ok");
+  wasm_wake_check(memory, 16, 3, workers, "ok");
+
+  js_wake_check(memory, 24, 4, workers, 0);
+  js_wake_check(memory, 32, 5, workers, 0);
+  js_wake_check(memory, 40, 3, workers, 0);
+
+  wasm_wake_check(memory, 48, 4, workers, 0);
+  wasm_wake_check(memory, 56, 5, workers, 0);
+  wasm_wake_check(memory, 64, 3, workers, 0);
+
+  js_wake_check(memory, 72, 4, workers, 0);
+  js_wake_check(memory, 80, 5, workers, 0);
+  js_wake_check(memory, 88, 3, workers, 0);
+
+  wasm_wake_check(memory, 96, 4, workers, 0);
+  wasm_wake_check(memory, 104, 5, workers, 0);
+  wasm_wake_check(memory, 112, 3, workers, 0);
+
+  for (let id = 0; id < 4; id++) {
+    workers[id].terminate();
+  }
 }
