f163d5897673de1ad819ae6c3ea948126fa26d39
Clemens Hammacher <clemensh@chromium.org>
2019-02-05 13:23:34 +0100

    [wasm] Reenable code logging
    Code logging was fixed in https://crrev.com/c/1454598. Reenable it.
    
    R=mstarzinger@chromium.org
    CC=herhut@chromium.org
    
    Bug: v8:8783
    Change-Id: I50773d4b42ce52e8d078e47a797ab517771c2756

--- src/wasm/module-compiler.cc
+++ src/wasm/module-compiler.cc
@@ -1587,8 +1587,7 @@ void CompilationStateImpl::OnFinishedUnit(ExecutionTier tier, WasmCode* code) {
     }
   }
 
-  // TODO(clemensh): Fix and reenable code logging (https://crbug.com/v8/8783).
-  // if (code != nullptr) engine_->LogCode(code);
+  if (code != nullptr) engine_->LogCode(code);
 }
 
 void CompilationStateImpl::RestartBackgroundCompileTask() {
