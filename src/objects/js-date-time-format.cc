84e12a8000ad1f30fc03d328d6556914702cb3df
Frank Tang <ftang@chromium.org>
2019-02-06 22:09:09 -0800

    [Intl] Speedup by cache DateTimePatternGenerator
    $ python -u tools/run_perf.py --binary-override-path   out/x64.release/d8 --filter "JSTests/Intl"   test/js-perf-test/JSTests.json
    ...
    NewIntlDateTimeFormat-Intl(Score): 92.9
    
    Compare to baseline in
    https://chromium-review.googlesource.com/c/v8/v8/+/1455727
    
    improve another x3.5
    
    Bug: chromium:928098
    Change-Id: Iab6b9e2d58231832c54bc626d01b25a3813537fa

--- src/objects/js-date-time-format.cc
+++ src/objects/js-date-time-format.cc
@@ -1003,6 +1003,33 @@ std::unique_ptr<icu::SimpleDateFormat> DateTimeStylePattern(
                              generator);
 }
 
+struct LocaleCmp {
+  bool operator()(const icu::Locale& lhs, const icu::Locale& rhs) const {
+    return strcmp(lhs.getName(), rhs.getName()) < 0;
+  }
+};
+
+class DateTimePatternGeneratorCache {
+ public:
+  // Return a clone copy that the caller have to free.
+  icu::DateTimePatternGenerator* CreateGenerator(const icu::Locale& locale) {
+    icu::Locale key(locale.getBaseName());
+    auto it = map.find(key);
+    if (it != map.end()) {
+      return it->second->clone();
+    }
+    UErrorCode status = U_ZERO_ERROR;
+    map[key].reset(icu::DateTimePatternGenerator::createInstance(key, status));
+    CHECK(U_SUCCESS(status));
+    return map[key]->clone();
+  }
+
+ private:
+  std::map<icu::Locale, std::unique_ptr<icu::DateTimePatternGenerator>,
+           LocaleCmp>
+      map;
+};
+
 }  // namespace
 
 enum FormatMatcherOption { kBestFit, kBasic };
@@ -1093,14 +1120,14 @@ MaybeHandle<JSDateTimeFormat> JSDateTimeFormat::Initialize(
                     JSDateTimeFormat);
   }
 
-  icu::Locale no_extension_locale(icu_locale.getBaseName());
-  UErrorCode status = U_ZERO_ERROR;
+  static base::LazyInstance<DateTimePatternGeneratorCache>::type
+      generator_cache = LAZY_INSTANCE_INITIALIZER;
+
   std::unique_ptr<icu::DateTimePatternGenerator> generator(
-      icu::DateTimePatternGenerator::createInstance(no_extension_locale,
-                                                    status));
-  CHECK(U_SUCCESS(status));
+      generator_cache.Pointer()->CreateGenerator(icu_locale));
 
   // 15.Let hcDefault be dataLocaleData.[[hourCycle]].
+  UErrorCode status = U_ZERO_ERROR;
   icu::UnicodeString hour_pattern = generator->getBestPattern("jjmm", status);
   CHECK(U_SUCCESS(status));
   Intl::HourCycle hc_default = HourCycleFromPattern(hour_pattern);
