06d3138abcc330a21eec0571c4d26d5f2383e640
Sigurd Schneider <sigurds@chromium.org>
2019-02-09 17:31:20 +0100

    Error if BUILDING_V8_SHARED and USING_V8_SHARED are both defined
    Bug: v8:8562
    Change-Id: I6dbf398f27108b6d9cb60ccf60178f53f2c0be9d

--- src/conversions.h
+++ src/conversions.h
@@ -94,7 +94,8 @@ double StringToDouble(Vector<const uint8_t> str, int flags,
 double StringToDouble(Vector<const uc16> str, int flags,
                       double empty_string_val = 0);
 // This version expects a zero-terminated character array.
-double StringToDouble(const char* str, int flags, double empty_string_val = 0);
+double V8_EXPORT_PRIVATE StringToDouble(const char* str, int flags,
+                                        double empty_string_val = 0);
 
 double StringToInt(Isolate* isolate, Handle<String> string, int radix);
 
@@ -126,7 +127,7 @@ V8_EXPORT_PRIVATE const char* IntToCString(int n, Vector<char> buffer);
 // The caller is responsible for calling free on the returned pointer.
 char* DoubleToFixedCString(double value, int f);
 char* DoubleToExponentialCString(double value, int f);
-char* DoubleToPrecisionCString(double value, int f);
+char* V8_EXPORT_PRIVATE DoubleToPrecisionCString(double value, int f);
 char* DoubleToRadixCString(double value, int radix);
 
 static inline bool IsMinusZero(double value) {
