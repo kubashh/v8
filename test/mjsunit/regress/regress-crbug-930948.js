e4b765c629d821fe17f55ae2468b5ac75c3a78ea
Simon Zünd <szuend@chromium.org>
2019-02-12 11:34:21 +0100

    [array] Fix Array#map storing signaling NaNs
    Bug: chromium:930948
    Change-Id: I7567fec06ec4bad11e8b8336ac13fdfc225b632c

---
+++ test/mjsunit/regress/regress-crbug-930948.js
@@ -0,0 +1,11 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function foo(x) {
+  return [x].map(x => Math.asin(x));
+}
+foo(undefined);
+foo(undefined);
+%OptimizeFunctionOnNextCall(foo);
+foo(undefined);
