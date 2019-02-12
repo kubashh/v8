ea751877a8449afb5cff970592a7969bfb3c3856
Junliang Yan <jyan@ca.ibm.com>
2019-02-12 10:36:05 -0500

    PPC: fix aix function descriptor issue on builtins
    Change-Id: I271f8cd4282f52fbcc573f6ccbe67b1111f62c7f

--- src/simulator.h
+++ src/simulator.h
@@ -121,7 +121,15 @@ class GeneratedCode {
 #else
   DISABLE_CFI_ICALL Return Call(Args... args) {
     // When running without a simulator we call the entry directly.
+#if V8_OS_AIX
+    // `volatile` is required for function_desc to be initialized correctly
+    volatile Address function_desc[] = {reinterpret_cast<Address>(fn_ptr_), 0,
+                                        0};
+    Signature* fn = reinterpret_cast<Signature*>(function_desc);
+    return fn(args...);
+#else
     return fn_ptr_(args...);
+#endif  // V8_OS_AIX
   }
 #endif
 
