5f7384fc49b813b8bc18756eba63ed0d613c78c1
Sathya Gunasekaran <gsathya@chromium.org>
2019-02-19 16:33:32 -0800

    [class] Throw error when private name occurs in top level function
    Bug: v8:7468
    Change-Id: I4e0d99b8ea4580b9cf7abb57f3d6974dd98041e5

--- src/parsing/parser-base.h
+++ src/parsing/parser-base.h
@@ -1557,6 +1557,21 @@ ParserBase<Impl>::ParsePropertyOrPrivatePropertyName() {
     name = impl()->GetSymbol();
     key = factory()->NewStringLiteral(name, pos);
   } else if (allow_harmony_private_fields() && next == Token::PRIVATE_NAME) {
+    // In the case of a top level function, we completely skip
+    // analysing it's scope, meaning, we don't have a chance to
+    // resolve private names and find that they are not enclosed in a
+    // class body.
+    //
+    // Here, we check if this is a new private name reference in a top
+    // level function and throw an error if so.
+    //
+    // Bug(v8:7468): This hack will go away once we refactor private
+    // name resolution to happen independently from scope resolution.
+    if (scope()->outer_scope() != nullptr &&
+        scope()->outer_scope()->scope_type() == SCRIPT_SCOPE) {
+      ReportMessage(MessageTemplate::kInvalidPrivateFieldResolution);
+    }
+
     name = impl()->GetSymbol();
     key = impl()->ExpressionFromIdentifier(name, pos, InferName::kNo);
   } else {
