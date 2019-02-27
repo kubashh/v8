a8aa75c14d550ecd5ed32332a415b52d34c417e2
Sven Sauleau <ssauleau@igalia.com>
2019-02-27 09:45:16 +0100

    [wasm] Add WebAssembly's i64<>JavaScript's BigInt conversions - macros
    Change-Id: I8bbbe88d643c566252945af62a47a93acd5ac600

--- src/builtins/builtins-wasm-gen.cc
+++ src/builtins/builtins-wasm-gen.cc
@@ -227,17 +227,23 @@ TF_BUILTIN(WasmMemoryGrow, WasmBuiltinsAssembler) {
 }
 
 TF_BUILTIN(BigIntToWasmI64, WasmBuiltinsAssembler) {
-  if (!Is64()) {
-    Unreachable();
-    return;
-  }
-
   TNode<Code> target = LoadBuiltinFromFrame(Builtins::kI64ToBigInt);
-  TNode<IntPtrT> argument =
-      UncheckedCast<IntPtrT>(Parameter(Descriptor::kArgument));
+
+  DebugBreak();
+
+#if V8_TARGET_ARCH_64_BIT
+  TNode<Int64T> argument =
+      UncheckedCast<Int64T>(Parameter(Descriptor::kArgument));
 
   TailCallStub(BigIntToWasmI64Descriptor(), target, NoContextConstant(),
                argument);
+#else
+  TNode<Uint32T> low = UncheckedCast<Uint32T>(Parameter(Descriptor::kLow));
+  TNode<Uint32T> high = UncheckedCast<Uint32T>(Parameter(Descriptor::kHigh));
+
+  TailCallStub(BigIntToWasmI64Descriptor(), target, NoContextConstant(), low,
+               high);
+#endif  // V8_TARGET_ARCH_64_BIT
 }
 
 TF_BUILTIN(WasmBigIntToI64, WasmBuiltinsAssembler) {
@@ -245,6 +251,8 @@ TF_BUILTIN(WasmBigIntToI64, WasmBuiltinsAssembler) {
     Unreachable();
     return;
   }
+
+  DebugBreak();
 
   TNode<Object> context =
       UncheckedCast<Object>(Parameter(Descriptor::kContext));
--- src/compiler/int64-lowering.cc
+++ src/compiler/int64-lowering.cc
@@ -83,6 +83,7 @@ int GetReturnIndexAfterLowering(CallDescriptor* call_descriptor,
   for (int i = 0; i < old_index; i++) {
     if (call_descriptor->GetReturnType(i).representation() ==
         MachineRepresentation::kWord64) {
+      PrintF("%d is a kWord64\n", i);
       result++;
     }
   }
@@ -284,16 +285,16 @@ void Int64Lowering::LowerNode(Node* node) {
     }
     case IrOpcode::kParameter: {
       DCHECK_EQ(1, node->InputCount());
+      int paramCount = static_cast<int>(signature()->parameter_count());
       // Only exchange the node if the parameter count actually changed. We do
       // not even have to do the default lowering because the the start node,
       // the only input of a parameter node, only changes if the parameter count
       // changes.
-      if (GetParameterCountAfterLowering(signature()) !=
-          static_cast<int>(signature()->parameter_count())) {
+      if (GetParameterCountAfterLowering(signature()) != paramCount) {
         int old_index = ParameterIndexOf(node->op());
-        // TODO(wasm): Make this part not wasm specific.
-        // Prevent special lowering of the instance parameter.
-        if (old_index == wasm::kWasmInstanceParameterIndex) {
+        // Prevent special lowering of wasm's instance or JS
+        // context/closure parameters.
+        if (old_index <= 0 || old_index > paramCount) {
           DefaultLowering(node);
           break;
         }
@@ -338,16 +339,26 @@ void Int64Lowering::LowerNode(Node* node) {
       }
       break;
     }
+    case IrOpcode::kCallUnverified:
     case IrOpcode::kCall: {
       auto call_descriptor =
           const_cast<CallDescriptor*>(CallDescriptorOf(node->op()));
+      PrintF("call_descriptor: %s\n", call_descriptor->debug_name());
       bool returns_require_lowering =
           GetReturnCountAfterLowering(call_descriptor) !=
           static_cast<int>(call_descriptor->ReturnCount());
+      PrintF("returns_require_lowering: %d\n", returns_require_lowering);
       if (DefaultLowering(node) || returns_require_lowering) {
         // We have to adjust the call descriptor.
-        NodeProperties::ChangeOp(node, common()->Call(GetI32WasmCallDescriptor(
-                                           zone(), call_descriptor)));
+        if (node->opcode() == IrOpcode::kCallUnverified) {
+          NodeProperties::ChangeOp(
+              node, common()->Call(
+                        GetI32WasmCallDescriptor(zone(), call_descriptor)));
+        } else {
+          NodeProperties::ChangeOp(
+              node, common()->CallUnverified(
+                        GetI32WasmCallDescriptor(zone(), call_descriptor)));
+        }
       }
       if (returns_require_lowering) {
         size_t return_arity = call_descriptor->ReturnCount();
