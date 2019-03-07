188356ba69ba0e8b1c57a60c9bc5d09c131faf8a
Bill Ticehurst <billti@microsoft.com>
2019-03-07 02:52:44 -0800

    Fix Wasm trap handler recursion on exceptions raised early
    Note: Putting this up for discussion, not a request to submit.
    
    __declspec(thread) is problematic when used in a vectored exception
    handler. The issue is that exceptions can get raised REALLY early in
    the thread's life, even before the thread local storage for
    "__declspec(thread)" variables is allocated. Should this occur, and the
    handler tries to access a thread local variable, it will attempt to
    access an offset from a null pointer, and raise another exception. The
    new exception will cause this handler to get called again, and an infinite loop ensues.
    
    In crash reports we were seeing recursion on the stack with the below:
    
     - msedge_child!v8::internal::trap_handler::HandleWasmTrap+0x27
     - ntdll!RtlpCallVectoredHandlers+0x106
     - ntdll!RtlCallVectoredExceptionHandlers+0xe
     - ntdll!RtlDispatchException+0x69
     - ntdll!KiUserExceptionDispatch+0x2e
     - msedge_child!v8::internal::trap_handler::HandleWasmTrap+0x27
    
    There is not currently a simple and supported way to check if the
    "implicit" thread-local variables, (i.e. those declared with
    "__declspec(thread)"), have been initialized for the thread.
    
    If thread local storage must be used in a vectored exception handler
    (as here), then using the Win32 Tls* APIs is the best option. The
    TlsGetValue API returns 0 on error, or if the slots haven't been
    allocated yet (as they are 0 initialized). In my looking through the
    code I could see this is already being done to store the current
    isolate on enter/exit. Check this first seems like a nice clean and
    simple approach before doing more advanced checks.
    
    I'm new to this code though, so my approach may be entirely naive.
    
    See the series of posts listed at http://www.nynaeve.net/?p=190 for
    more detail than you ever wanted to know about thread locale storage
    internals on Windows.
    
    Change-Id: Ifc6223b74999a55bfd0ed2d6ebf054bbffd7e809

--- src/trap-handler/handler-inside-win.cc
+++ src/trap-handler/handler-inside-win.cc
@@ -27,6 +27,7 @@
 
 #include <windows.h>
 
+#include "src/isolate.h"
 #include "src/trap-handler/trap-handler-internal.h"
 #include "src/trap-handler/trap-handler.h"
 
@@ -42,6 +43,7 @@ bool TryHandleWasmTrap(EXCEPTION_POINTERS* exception) {
 
   // Clear g_thread_in_wasm_code, primarily to protect against nested faults.
   g_thread_in_wasm_code = false;
+
 
   const EXCEPTION_RECORD* record = exception->ExceptionRecord;
 
@@ -66,9 +68,34 @@ bool TryHandleWasmTrap(EXCEPTION_POINTERS* exception) {
 }
 
 LONG HandleWasmTrap(EXCEPTION_POINTERS* exception) {
-  if (TryHandleWasmTrap(exception)) {
-    return EXCEPTION_CONTINUE_EXECUTION;
+  // VectoredExceptionHandlers need extreme caution. Do as little as
+  // possible to determine if the exception should be handled or not.
+  // Exceptions can be thrown very early in a threads life, before the
+  // thread has even completed initializing. As a demonstrative example,
+  // there was a bug where an exception would be raised before the thread
+  // local copy of the "__declspec(thread)" variables had been allocated,
+  // the handler tried to access the thread-local "g_thread_in_wasm_code",
+  // which would then raise another exception, and an infinite loop ensued.
+
+  // First ensure this is an exception type of interest
+  if (exception->ExceptionRecord->ExceptionCode == EXCEPTION_ACCESS_VIOLATION) {
+    // Then check that an isolate has been entered on this thread. If it hasn't
+    // then the exception can't be of interest.
+    //
+    // Note: The implementation of this uses the Windows Tls* APIs. These
+    // are safe to access as soon as the thread is created (i.e. even before
+    // thread-local variables are available), and just return NULL if not
+    // initialized yet. (As the Tls slots are just a block of zero-initialized
+    // memory inside the thread environment block).
+    if (Isolate::TryGetCurrent() != nullptr) {
+      // If the thread has entered an isolate, then it should be safe to do
+      // some of the more non-trivial checks.
+      if (TryHandleWasmTrap(exception)) {
+        return EXCEPTION_CONTINUE_EXECUTION;
+      }
+    }
   }
+
   return EXCEPTION_CONTINUE_SEARCH;
 }
 
