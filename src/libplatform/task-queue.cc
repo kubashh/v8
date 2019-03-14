eea96b033cdc6305859d36467afbdc8158dbb0e0
Peter Marshall <petermarshall@chromium.org>
2019-03-14 13:37:58 +0100

    [platform] Implement delayed tasks in the default worker runner
    Change-Id: I3629c5d6e15ced2fc47eb1b7519a2dbbf8461fce

--- src/libplatform/task-queue.cc
+++ src/libplatform/task-queue.cc
@@ -12,8 +12,7 @@
 namespace v8 {
 namespace platform {
 
-TaskQueue::TaskQueue() : process_queue_semaphore_(0), terminated_(false) {}
-
+TaskQueue::TaskQueue() : terminated_(false) {}
 
 TaskQueue::~TaskQueue() {
   base::MutexGuard guard(&lock_);
@@ -22,43 +21,91 @@ TaskQueue::~TaskQueue() {
 }
 
 void TaskQueue::Append(std::unique_ptr<Task> task) {
-  base::MutexGuard guard(&lock_);
-  DCHECK(!terminated_);
-  task_queue_.push(std::move(task));
-  process_queue_semaphore_.Signal();
+  {
+    base::MutexGuard guard(&lock_);
+    DCHECK(!terminated_);
+    task_queue_.push(std::move(task));
+    printf("append regular\n");
+  }
+  process_queue_condition_var_.NotifyOne();
+}
+
+void TaskQueue::AppendDelayed(std::unique_ptr<Task> task,
+                              double delay_in_seconds) {
+  base::TimeDelta delay_in_us =
+      base::TimeDelta::FromMicroseconds(static_cast<int64_t>(
+          delay_in_seconds * base::TimeConstants::kMicrosecondsPerSecond));
+  base::Time deadline = base::Time::NowFromSystemTime() + delay_in_us;
+
+  {
+    base::MutexGuard guard(&lock_);
+    DCHECK(!terminated_);
+    delayed_task_queue_.emplace(deadline, std::move(task));
+    printf("append delayed\n");
+  }
+  process_queue_condition_var_.NotifyOne();
 }
 
 std::unique_ptr<Task> TaskQueue::GetNext() {
   for (;;) {
-    {
-      base::MutexGuard guard(&lock_);
-      if (!task_queue_.empty()) {
-        std::unique_ptr<Task> result = std::move(task_queue_.front());
-        task_queue_.pop();
+    base::Time now = base::Time::NowFromSystemTime();
+    base::MutexGuard guard(&lock_);
+    // Note: This will starve non-delayed events if there are delayed events
+    // ready.
+    if (!delayed_task_queue_.empty()) {
+      printf("Delayed not empty\n");
+      auto it = delayed_task_queue_.begin();
+      if (now >= it->first) {
+        std::unique_ptr<Task> result = std::move(it->second);
+        delayed_task_queue_.erase(it);
+        printf("Send delayed event\n");
         return result;
       }
-      if (terminated_) {
-        process_queue_semaphore_.Signal();
-        return nullptr;
-      }
     }
-    process_queue_semaphore_.Wait();
+    if (!task_queue_.empty()) {
+      std::unique_ptr<Task> result = std::move(task_queue_.front());
+      task_queue_.pop();
+      printf("Send regular event\n");
+      return result;
+    }
+
+    if (terminated_) {
+      printf("terminate in loop notify all\n");
+      process_queue_condition_var_.NotifyAll();
+      printf("terminate in loop notify all done\n");
+      return nullptr;
+    }
+
+    if (task_queue_.empty() && !delayed_task_queue_.empty()) {
+      // Wait for the next delayed task or a newly posted task.
+      base::TimeDelta wait_time =
+          delayed_task_queue_.begin()->first - base::Time::NowFromSystemTime();
+      printf("WaitFor ms = %ld\n", wait_time.InMilliseconds());
+      bool notified = process_queue_condition_var_.WaitFor(&lock_, wait_time);
+      USE(notified);
+      printf("WaitFor notified: %d\n", notified);
+    } else {
+      printf("Wait in loop\n");
+      process_queue_condition_var_.Wait(&lock_);
+    }
   }
 }
 
 
 void TaskQueue::Terminate() {
-  base::MutexGuard guard(&lock_);
-  DCHECK(!terminated_);
-  terminated_ = true;
-  process_queue_semaphore_.Signal();
+  {
+    base::MutexGuard guard(&lock_);
+    DCHECK(!terminated_);
+    terminated_ = true;
+  }
+  process_queue_condition_var_.NotifyAll();
 }
 
 void TaskQueue::BlockUntilQueueEmptyForTesting() {
   for (;;) {
     {
       base::MutexGuard guard(&lock_);
-      if (task_queue_.empty()) return;
+      if (task_queue_.empty() && delayed_task_queue_.empty()) return;
     }
     base::OS::Sleep(base::TimeDelta::FromMilliseconds(5));
   }
