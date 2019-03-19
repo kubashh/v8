90f03baa810e668a8347ecf20905286803502252
Clemens Hammacher <clemensh@chromium.org>
2019-03-19 18:49:32 +0100

    [wasm] Split adding code from publishing it
    This prepares a refactoring to add and publish compilation results in
    batches. For this, we need to separate the two phases, so that we can
    first allocate all the code, then publish all of it, both under a lock,
    but with more work in-between.
    In particular, this CL does the following:
    1) It removes the {AddOwnedCode} method. The functionality of creating
       the {WasmCode} and memcpy'ing the instruction into that is done in
       the other {Add*Code} methods. Adding to {owned_code_} is done in
       {PublishCode}.
    2) {PublishInterpreterEntry} is now functionally equivalent to
       {PublishCode}, so it's removed.
    3) After {AddCode}, the caller has to call {PublishCode}. In a
       follow-up CL, this will be called in batches (first {AddCode} them
       all, then {PublishCode} them all).
    4) {AddCompiledCode} now assumes that the {WasmCompilationResult}
       succeeded. Otherwise, the caller should directly call {SetError} on
       the {CompilationState}.
    5) {PublishCode} is now the choke-point for installing code to the code
       table, the owned code vector, the jump table, and setting interpreter
       redirections. It replaces previous direct calls to {InstallCode} or
       explicitly adding to {owned_code_}.
    6) Increasing the {generated_code_size_} counter is now done in
       {AllocateForCode}, which is the choke point for allocating space for
       generated code. This way, we will only increase this counter once
       once we allocate in batches.
    
    R=titzer@chromium.org
    
    Bug: v8:8916
    Change-Id: I71e02e3a838f21797915cee3ebd373804fb12237

--- src/wasm/module-compiler.cc
+++ src/wasm/module-compiler.cc
@@ -437,8 +437,12 @@ bool FetchAndExecuteCompilationUnit(CompilationEnv* env,
   WasmCompilationResult result = unit->ExecuteCompilation(
       env, compilation_state->GetWireBytesStorage(), counters, detected);
 
-  WasmCode* code = native_module->AddCompiledCode(std::move(result));
-  compilation_state->OnFinishedUnit(result.requested_tier, code);
+  if (result.succeeded()) {
+    WasmCode* code = native_module->AddCompiledCode(std::move(result));
+    compilation_state->OnFinishedUnit(result.requested_tier, code);
+  } else {
+    compilation_state->SetError();
+  }
 
   return true;
 }
@@ -657,15 +661,17 @@ class BackgroundCompileTask : public CancelableTask {
       {
         BackgroundCompileScope compile_scope(token_);
         if (compile_scope.cancelled()) return;
-        WasmCode* code =
-            compile_scope.native_module()->AddCompiledCode(std::move(result));
-        if (code == nullptr) {
+        if (!result.succeeded()) {
           // Compile error.
+          compile_scope.compilation_state()->SetError();
           compile_scope.compilation_state()->OnBackgroundTaskStopped(
               detected_features);
           compilation_failed = true;
           break;
         }
+        WasmCode* code =
+            compile_scope.native_module()->AddCompiledCode(std::move(result));
+        DCHECK_NOT_NULL(code);
 
         // Successfully finished one unit.
         compile_scope.compilation_state()->OnFinishedUnit(result.requested_tier,
