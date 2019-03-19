83f27d09f564cc6d7a32f3f6e83b992303a4728f
Sigurd Schneider <sigurds@chromium.org>
2019-03-19 17:15:08 +0100

    Reland "[regalloc] Introduce deferred fixed ranges"
    This is a reland of b176931311c49597e312056109abfa45e31ea4a2
    
    Original change's description:
    > [regalloc] Introduce deferred fixed ranges
    > 
    > Fixed ranges are used to express register constraints in the
    > allocator. This change splits these fixed ranges into one for
    > normal code and deferred code. The former are handeled as before
    > whereas the latter are only made visible while allocating
    > registers for deferred code.
    > 
    > This prevents forward looking decisions in normal code to be
    > impacted by register constraints from deferred code.
    > 
    > Change-Id: I67d562bb41166194e62765d5ab051bc961054fc7
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1477742
    > Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    > Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    > Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#60322}
    
    Change-Id: I1a31150256eb5608db985b144aab7ea457169d0d

--- src/compiler/pipeline.cc
+++ src/compiler/pipeline.cc
@@ -401,11 +401,12 @@ class PipelineData {
   }
 
   void InitializeRegisterAllocationData(const RegisterConfiguration* config,
-                                        CallDescriptor* call_descriptor) {
+                                        CallDescriptor* call_descriptor,
+                                        RegisterAllocationFlags flags) {
     DCHECK_NULL(register_allocation_data_);
     register_allocation_data_ = new (register_allocation_zone())
         RegisterAllocationData(config, register_allocation_zone(), frame(),
-                               sequence(), debug_name());
+                               sequence(), flags, debug_name());
   }
 
   void InitializeOsrHelper() {
@@ -913,6 +914,12 @@ PipelineCompilationJob::Status PipelineCompilationJob::PrepareJobImpl(
   }
   if (FLAG_inline_accessors) {
     compilation_info()->MarkAsAccessorInliningEnabled();
+  }
+  if (FLAG_turbo_control_flow_aware_allocation) {
+    compilation_info()->MarkAsTurboControlFlowAwareAllocation();
+  }
+  if (FLAG_turbo_preprocess_ranges) {
+    compilation_info()->MarkAsTurboPreprocessRanges();
   }
 
   // This is the bottleneck for computing and setting poisoning level in the
@@ -2862,7 +2869,14 @@ void PipelineImpl::AllocateRegisters(const RegisterConfiguration* config,
   data_->sequence()->ValidateDeferredBlockExitPaths();
 #endif
 
-  data->InitializeRegisterAllocationData(config, call_descriptor);
+  RegisterAllocationFlags flags;
+  if (data->info()->is_turbo_control_flow_aware_allocation()) {
+    flags |= RegisterAllocationFlag::kTurboControlFlowAwareAllocation;
+  }
+  if (data->info()->is_turbo_preprocess_ranges()) {
+    flags |= RegisterAllocationFlag::kTurboPreprocessRanges;
+  }
+  data->InitializeRegisterAllocationData(config, call_descriptor, flags);
   if (info()->is_osr()) data->osr_helper()->SetupFrame(data->frame());
 
   Run<MeetRegisterConstraintsPhase>();
@@ -2883,7 +2897,7 @@ void PipelineImpl::AllocateRegisters(const RegisterConfiguration* config,
                                        data->register_allocation_data());
   }
 
-  if (FLAG_turbo_preprocess_ranges) {
+  if (info()->is_turbo_preprocess_ranges()) {
     Run<SplinterLiveRangesPhase>();
     if (info()->trace_turbo_json_enabled() &&
         !data->MayHaveUnverifiableGraph()) {
@@ -2899,7 +2913,7 @@ void PipelineImpl::AllocateRegisters(const RegisterConfiguration* config,
     Run<AllocateFPRegistersPhase<LinearScanAllocator>>();
   }
 
-  if (FLAG_turbo_preprocess_ranges) {
+  if (info()->is_turbo_preprocess_ranges()) {
     Run<MergeSplintersPhase>();
   }
 
