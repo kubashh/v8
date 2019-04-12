8f0ce2f7b9ad077c465cb799941c0eb196b3df41
Santiago Aboy Solanes <solanes@chromium.org>
2019-04-12 15:01:00 +0100

    [Turbofan][ptr-compr] Change native context specialisation for CompressedPointer
    Cq-Include-Trybots: luci.v8.try:v8_linux64_pointer_compression_rel_ng,v8_linux64_arm64_pointer_compression_rel_ng
    Bug: v8:8977, v8:7703
    Change-Id: I17811c64ad5cbc61042e0f22b7a46669667d9d35

--- src/compiler/access-info.cc
+++ src/compiler/access-info.cc
@@ -333,7 +333,11 @@ bool AccessInfoFactory::ComputeDataFieldAccessInfo(
   } else if (details_representation.IsHeapObject()) {
     // Extract the field type from the property details (make sure its
     // representation is TaggedPointer to reflect the heap object case).
+#ifdef V8_COMPRESS_POINTERS
+    field_representation = MachineRepresentation::kCompressedPointer;
+#else
     field_representation = MachineRepresentation::kTaggedPointer;
+#endif
     Handle<FieldType> descriptors_field_type(descriptors->GetFieldType(number),
                                              isolate());
     if (descriptors_field_type->IsNone()) {
@@ -703,6 +707,7 @@ bool AccessInfoFactory::LookupTransition(
                                                         details_representation);
   Type field_type = Type::NonInternal();
   MaybeHandle<Map> field_map;
+  // TODO(v8:8977): Should we add these three cases as well?
   MachineRepresentation field_representation = MachineRepresentation::kTagged;
   if (details_representation.IsSmi()) {
     field_type = Type::SignedSmall();
