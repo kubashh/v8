a2c6354d197b9d48269919af8c58b2046e1eed09
Frederik Gossen <frgossen@google.com>
2019-04-16 00:22:46 +0200

    [wasm-hints] Add Tests for Compilation Hints
    Add tests tiering and lazy compilation with compilation hints. The tests
    build modules and verify the {WasmCode}'s tier internally. The module
    builder now supports compilation hints in CCTests.
    
    Bug: v8:9003
    Change-Id: I18d926c3b1ef3508835a51a9d1d86bfadcb5216e

--- src/wasm/wasm-module-builder.h
+++ src/wasm/wasm-module-builder.h
@@ -8,10 +8,12 @@
 #include "src/signature.h"
 #include "src/zone/zone-containers.h"
 
+#include "src/base/optional.h"
 #include "src/v8memory.h"
 #include "src/vector.h"
 #include "src/wasm/leb-helper.h"
 #include "src/wasm/local-decl-encoder.h"
+#include "src/wasm/wasm-module.h"
 #include "src/wasm/wasm-opcodes.h"
 #include "src/wasm/wasm-result.h"
 
@@ -176,6 +178,9 @@ class V8_EXPORT_PRIVATE WasmFunctionBuilder : public ZoneObject {
   void SetName(Vector<const char> name);
   void AddAsmWasmOffset(size_t call_position, size_t to_number_position);
   void SetAsmFunctionStartPosition(size_t function_position);
+  void GiveCompilationHint(WasmCompilationHintStrategy strategy,
+                           WasmCompilationHintTier baseline,
+                           WasmCompilationHintTier top_tier);
 
   size_t GetPosition() const { return body_.size(); }
   void FixupByte(size_t position, byte value) {
@@ -217,6 +222,7 @@ class V8_EXPORT_PRIVATE WasmFunctionBuilder : public ZoneObject {
   uint32_t last_asm_byte_offset_ = 0;
   uint32_t last_asm_source_position_ = 0;
   uint32_t asm_func_start_source_position_ = 0;
+  base::Optional<uint8_t> hint_;
 };
 
 class V8_EXPORT_PRIVATE WasmModuleBuilder : public ZoneObject {
