9aa38d31415b67b5fbfe896f76e1a65785d8fcc1
Shiyu Zhang <shiyu.zhang@intel.com>
2019-04-19 21:25:20 +0800

    Port ToString to Torque
    Change-Id: I9480650b23da4f5aa38a0634c1a7662bf88189d7

--- src/code-stub-assembler.h
+++ src/code-stub-assembler.h
@@ -2400,8 +2400,6 @@ class V8_EXPORT_PRIVATE CodeStubAssembler
                          SloppyTNode<Object> input);
 
   // Convert any object to a String.
-  TNode<String> ToString(SloppyTNode<Context> context,
-                         SloppyTNode<Object> input);
   TNode<String> ToString_Inline(SloppyTNode<Context> context,
                                 SloppyTNode<Object> input);
 
