2bf10e07d869e8e99c6e4515f949cfa194f7d203
Sathya Gunasekaran <gsathya@chromium.org>
2019-04-19 11:46:24 -0700

    [promise] Lookup the resolve property only once
    In the PerformPromise{All, Race, AllSettled} operations, the resolve
    property of the constructor is looked up only once. For the fast path,
    where the constructor's resolve property is untainted, the resolve
    function is set to undefined. Since undefined can't be a valid value
    for the resolve function, we can switch on it (in CallResolve) to
    directly call the PromiseResolve builtin. If the resolve property is
    tainted, we do an observable property lookup, save this value, and
    call this property later (in CallResolve).
    
    Spec:
    - https://github.com/tc39/ecma262/pull/1506
    - https://github.com/tc39/proposal-promise-allSettled/pull/40
    
    Bug: v8:9152
    Change-Id: Icb36a90b5a244a67a729611c7b3315d2c29de6e9

--- src/code-stub-assembler.h
+++ src/code-stub-assembler.h
@@ -2114,6 +2114,7 @@ class V8_EXPORT_PRIVATE CodeStubAssembler
   Node* ThrowIfNotJSReceiver(Node* context, Node* value,
                              MessageTemplate msg_template,
                              const char* method_name = nullptr);
+  void ThrowIfNotCallable(Node* context, Node* value, const char* method_name);
 
   void ThrowRangeError(Node* context, MessageTemplate message,
                        Node* arg0 = nullptr, Node* arg1 = nullptr,
