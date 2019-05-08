c8ad46f3cc707c005fbeacb16c38ea332714a98b
Seth Brenith <seth.brenith@microsoft.com>
2019-05-08 15:01:40 -0700

    [torque] move Map layout definition to Torque
    This commit attempts to change as little behavior as possible, but it
    does require reordering the fields within Map to abide by Torque rules
    specifying that strong and weak fields must be in separate sections.
    
    Also includes some Torque compiler updates:
    - Allow enums (types extending from integral types) as class fields
    - Allows void as class field type, which emits a size-zero data
      declaration and no accessors
    - Avoid infinite recursion when generating human-readable type names
    
    Bug: v8:8952
    Change-Id: I1de6f34c1b15ed87d718666a05176980a218e97c

--- src/compiler/access-builder.cc
+++ src/compiler/access-builder.cc
@@ -598,7 +598,7 @@ FieldAccess AccessBuilder::ForMapBitField3() {
 // static
 FieldAccess AccessBuilder::ForMapDescriptors() {
   FieldAccess access = {
-      kTaggedBase,           Map::kDescriptorsOffset,
+      kTaggedBase,           Map::kInstanceDescriptorsOffset,
       Handle<Name>(),        MaybeHandle<Map>(),
       Type::OtherInternal(), MachineType::TypeCompressedTaggedPointer(),
       kPointerWriteBarrier};
--- src/torque/implementation-visitor.cc
+++ src/torque/implementation-visitor.cc
@@ -2890,16 +2890,14 @@ void ImplementationVisitor::GenerateClassDefinitions(std::string& file_name) {
                                 FieldSectionType::kStrongSection,
                                 &new_contents_stream);
         }
-      } else {
+      } else if (f.name_and_type.type != TypeOracle::GetVoidType()) {
         ProcessFieldInSection(&section, &completed_sections,
                               FieldSectionType::kScalarSection,
                               &new_contents_stream);
       }
       size_t field_size;
       std::string size_string;
-      std::string machine_type;
-      std::tie(field_size, size_string, machine_type) =
-          f.GetFieldSizeInformation();
+      std::tie(field_size, size_string) = f.GetFieldSizeInformation();
       new_contents_stream << "V(k" << CamelifyString(f.name_and_type.name)
                           << "Offset, " << size_string << ") \\\n";
     }
