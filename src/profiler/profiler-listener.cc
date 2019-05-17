2db0d500e4dac655fe876c8ddf4c97bdf7b4a17a
Andrew Comminos <acomminos@fb.com>
2019-05-16 17:24:16 -0700

    [cpu-profiler] Add parameter to filter profiles by v8::Context
    Adds support to the CPU profiler for scraping the incumbent contexts of
    V8 stack frames. While it is generally unsafe to access heap objects
    during a profiling interrupt, the native context is uniquely usable due
    to being guaranteed an alive root on the stack, as well as its slots
    being immutable after context creation.
    
    Change-Id: I2c3149c1302b74d2f13aa99d1fdd0cf006e0f9d1

--- src/profiler/profiler-listener.cc
+++ src/profiler/profiler-listener.cc
@@ -177,8 +177,7 @@ void ProfilerListener::CodeCreateEvent(CodeEventListener::LogEventsAndTags tag,
           CodeEntry* cached_entry = GetOrInsertCachedEntry(
               &cached_inline_entries, std::move(inline_entry));
 
-          inline_stack.push_back(
-              CodeEntryAndLineNumber{cached_entry, line_number});
+          inline_stack.push_back({cached_entry, line_number});
         }
         DCHECK(!inline_stack.empty());
         inline_stacks.emplace(inlining_id, std::move(inline_stack));
@@ -277,6 +276,14 @@ void ProfilerListener::SetterCallbackEvent(Name name, Address entry_point) {
   rec->entry =
       new CodeEntry(CodeEventListener::CALLBACK_TAG, GetConsName("set ", name));
   rec->instruction_size = 1;
+  DispatchCodeEvent(evt_rec);
+}
+
+void ProfilerListener::NativeContextMoveEvent(Address from, Address to) {
+  CodeEventsContainer evt_rec(CodeEventRecord::NATIVE_CONTEXT_MOVE);
+  NativeContextMoveEventRecord* rec = &evt_rec.NativeContextMoveEventRecord_;
+  rec->from_address = from;
+  rec->to_address = to;
   DispatchCodeEvent(evt_rec);
 }
 
