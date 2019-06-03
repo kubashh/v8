aebc886cd446f2ee86d1aaabb7601a13c8e747e7
Maciej Goszczycki <goszczycki@google.com>
2019-06-03 11:47:57 +0100

    [roheap] Set owner_ to nullptr
    As it stands read-only space is unusable once it has been sealed. Set
    owner_ to null to ensure nothing uses it.
    
    This also helps the future read-only space removal.
    
    Bug: v8:7464
    Change-Id: I3b24f20c644d6f5e23647bc1de4d256a20a0eb19

--- src/heap/scavenger-inl.h
+++ src/heap/scavenger-inl.h
@@ -214,8 +214,7 @@ bool Scavenger::HandleLargeObject(Map map, HeapObject object, int object_size,
   if (V8_UNLIKELY(
           FLAG_young_generation_large_objects &&
           MemoryChunk::FromHeapObject(object)->InNewLargeObjectSpace())) {
-    DCHECK_EQ(NEW_LO_SPACE,
-              MemoryChunk::FromHeapObject(object)->owner()->identity());
+    DCHECK_EQ(NEW_LO_SPACE, MemoryChunk::FromHeapObject(object)->identity());
     if (object.map_slot().Release_CompareAndSwap(
             map, MapWord::FromForwardingAddress(object).ToMap()) == map) {
       surviving_new_large_objects_.insert({object, map});
