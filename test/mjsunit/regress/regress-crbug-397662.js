374f454f319344027ebf786d970130f4d7a092f2
Toon Verwaest <verwaest@chromium.org>
2019-06-13 11:24:02 +0200

    [runtime] Throw RangeError if we try to get too many values or entries
    Bug: chromium:973363
    Change-Id: Id2e46702f73e901df5f26b764d98fb3d4f681a98

---
+++ test/mjsunit/regress/regress-crbug-397662.js
@@ -0,0 +1,7 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var a = new Uint8Array(0x70000000);
+a.x = 1;
+assertThrows(()=>Object.entries(a), RangeError);
