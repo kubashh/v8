4e898fb242fa39357ddbb7d28bb480f512555f4d
zhiguo <zhiguo.zhou@intel.com>
2019-06-13 21:49:33 +0800

    Add Base64 VLQ decoding support
    This CL adds the support for Base64 VLQ decoding, which is the base of
    parsing source map files (Version 3). With this support, the mappings of
    C/C++ source code and WASM bytecode could be built in V8 engine. The
    newly-added function is called VLQBase64Decode, which accepts two
    
    argument: the Base64-encoded VLQ string and the position of first
    character to be decoded. Upon its return, the position is updated with
    the next start position. The unittest of this support is also added in
    this CL.
    Change-Id: If0f32972ecd7488844478a7b93a0f10cc38b6a5d

---
+++ test/unittests/base/vlq-base64-unittest.cc
@@ -0,0 +1,289 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <limits>
+
+#include "src/base/vlq-base64.h"
+#include "testing/gtest-support.h"
+
+namespace v8 {
+namespace base {
+
+TEST(VLQBASE64, charToDigit) {
+  char syms[] =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  for (int i = 0; i < 64; ++i) {
+    EXPECT_EQ(i, charToDigitDecode(syms[i]));
+  }
+}
+
+TEST(VLQBASE64, DecodeOneSegment) {
+  size_t pos = 0;
+  EXPECT_EQ(0, VLQBase64Decode("A", pos));
+  EXPECT_EQ(1ul, pos);
+  pos = 0;
+  EXPECT_EQ(1, VLQBase64Decode("C", pos));
+  EXPECT_EQ(1ul, pos);
+  pos = 0;
+  EXPECT_EQ(12, VLQBase64Decode("Y", pos));
+  EXPECT_EQ(1ul, pos);
+  pos = 0;
+  EXPECT_EQ(123, VLQBase64Decode("2H", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(1234, VLQBase64Decode("ktC", pos));
+  EXPECT_EQ(3ul, pos);
+  pos = 0;
+  EXPECT_EQ(12345, VLQBase64Decode("yjY", pos));
+  EXPECT_EQ(3ul, pos);
+  pos = 0;
+  EXPECT_EQ(123456, VLQBase64Decode("gkxH", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(1234567, VLQBase64Decode("uorrC", pos));
+  EXPECT_EQ(5ul, pos);
+  pos = 0;
+  EXPECT_EQ(12345678, VLQBase64Decode("80wxX", pos));
+  EXPECT_EQ(5ul, pos);
+  pos = 0;
+  EXPECT_EQ(123456789, VLQBase64Decode("qxmvrH", pos));
+  EXPECT_EQ(6ul, pos);
+  pos = 0;
+  EXPECT_EQ(1234567890, VLQBase64Decode("kth4ypC", pos));
+  EXPECT_EQ(7ul, pos);
+  pos = 0;
+  EXPECT_EQ(std::numeric_limits<int32_t>::max(),
+            VLQBase64Decode("+/////D", pos));
+  EXPECT_EQ(7ul, pos);
+  pos = 0;
+  EXPECT_EQ(-1, VLQBase64Decode("D", pos));
+  EXPECT_EQ(1ul, pos);
+  pos = 0;
+  EXPECT_EQ(-12, VLQBase64Decode("Z", pos));
+  EXPECT_EQ(1ul, pos);
+  pos = 0;
+  EXPECT_EQ(-123, VLQBase64Decode("3H", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(-1234, VLQBase64Decode("ltC", pos));
+  EXPECT_EQ(3ul, pos);
+  pos = 0;
+  EXPECT_EQ(-12345, VLQBase64Decode("zjY", pos));
+  EXPECT_EQ(3ul, pos);
+  pos = 0;
+  EXPECT_EQ(-123456, VLQBase64Decode("hkxH", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(-1234567, VLQBase64Decode("vorrC", pos));
+  EXPECT_EQ(5ul, pos);
+  pos = 0;
+  EXPECT_EQ(-12345678, VLQBase64Decode("90wxX", pos));
+  EXPECT_EQ(5ul, pos);
+  pos = 0;
+  EXPECT_EQ(-123456789, VLQBase64Decode("rxmvrH", pos));
+  EXPECT_EQ(6ul, pos);
+  pos = 0;
+  EXPECT_EQ(-1234567890, VLQBase64Decode("lth4ypC", pos));
+  EXPECT_EQ(7ul, pos);
+  pos = 0;
+  EXPECT_EQ(std::numeric_limits<int32_t>::min() + 1,
+            VLQBase64Decode("//////D", pos));
+  EXPECT_EQ(7ul, pos);
+}
+
+TEST(VLQBASE64, DecodeTwoSegment) {
+  size_t pos = 0;
+  EXPECT_EQ(0, VLQBase64Decode("AA", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("AA", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(5, VLQBase64Decode("KA", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("KA", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(0, VLQBase64Decode("AQ", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(8, VLQBase64Decode("AQ", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(6, VLQBase64Decode("MG", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(3, VLQBase64Decode("MG", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(13, VLQBase64Decode("a4E", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(76, VLQBase64Decode("a4E", pos));
+  EXPECT_EQ(3ul, pos);
+  pos = 0;
+  EXPECT_EQ(108, VLQBase64Decode("4GyO", pos));
+  EXPECT_EQ(2ul, pos);
+  EXPECT_EQ(233, VLQBase64Decode("4GyO", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(2048, VLQBase64Decode("ggEqnD", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(1653, VLQBase64Decode("ggEqnD", pos));
+  EXPECT_EQ(6ul, pos);
+  pos = 0;
+  EXPECT_EQ(65376, VLQBase64Decode("g2/D0ilF", pos));
+  EXPECT_EQ(4ul, pos);
+  EXPECT_EQ(84522, VLQBase64Decode("g2/D0ilF", pos));
+  EXPECT_EQ(8ul, pos);
+  pos = 0;
+  EXPECT_EQ(537798, VLQBase64Decode("ss6gBy0m3B", pos));
+  EXPECT_EQ(5ul, pos);
+  EXPECT_EQ(904521, VLQBase64Decode("ss6gBy0m3B", pos));
+  EXPECT_EQ(10ul, pos);
+  pos = 0;
+  EXPECT_EQ(-5, VLQBase64Decode("LA", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("LA", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(0, VLQBase64Decode("AR", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(-8, VLQBase64Decode("AR", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(-6, VLQBase64Decode("NH", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(-3, VLQBase64Decode("NH", pos));
+  EXPECT_EQ(2ul, pos);
+  pos = 0;
+  EXPECT_EQ(-13, VLQBase64Decode("b5E", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(-76, VLQBase64Decode("b5E", pos));
+  EXPECT_EQ(3ul, pos);
+  pos = 0;
+  EXPECT_EQ(-108, VLQBase64Decode("5GzO", pos));
+  EXPECT_EQ(2ul, pos);
+  EXPECT_EQ(-233, VLQBase64Decode("5GzO", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(-2048, VLQBase64Decode("hgErnD", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(-1653, VLQBase64Decode("hgErnD", pos));
+  EXPECT_EQ(6ul, pos);
+  pos = 0;
+  EXPECT_EQ(-65376, VLQBase64Decode("h2/D1ilF", pos));
+  EXPECT_EQ(4ul, pos);
+  EXPECT_EQ(-84522, VLQBase64Decode("h2/D1ilF", pos));
+  EXPECT_EQ(8ul, pos);
+  pos = 0;
+  EXPECT_EQ(-537798, VLQBase64Decode("ts6gBz0m3B", pos));
+  EXPECT_EQ(5ul, pos);
+  EXPECT_EQ(-904521, VLQBase64Decode("ts6gBz0m3B", pos));
+  EXPECT_EQ(10ul, pos);
+  pos = 0;
+  EXPECT_EQ(108, VLQBase64Decode("4GzO", pos));
+  EXPECT_EQ(2ul, pos);
+  EXPECT_EQ(-233, VLQBase64Decode("4GzO", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(2048, VLQBase64Decode("ggErnD", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(-1653, VLQBase64Decode("ggErnD", pos));
+  EXPECT_EQ(6ul, pos);
+  pos = 0;
+  EXPECT_EQ(65376, VLQBase64Decode("g2/D1ilF", pos));
+  EXPECT_EQ(4ul, pos);
+  EXPECT_EQ(-84522, VLQBase64Decode("g2/D1ilF", pos));
+  EXPECT_EQ(8ul, pos);
+  pos = 0;
+  EXPECT_EQ(537798, VLQBase64Decode("ss6gBz0m3B", pos));
+  EXPECT_EQ(5ul, pos);
+  EXPECT_EQ(-904521, VLQBase64Decode("ss6gBz0m3B", pos));
+  EXPECT_EQ(10ul, pos);
+  pos = 0;
+  EXPECT_EQ(-108, VLQBase64Decode("5GyO", pos));
+  EXPECT_EQ(2ul, pos);
+  EXPECT_EQ(233, VLQBase64Decode("5GyO", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(-2048, VLQBase64Decode("hgEqnD", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(1653, VLQBase64Decode("hgEqnD", pos));
+  EXPECT_EQ(6ul, pos);
+  pos = 0;
+  EXPECT_EQ(-65376, VLQBase64Decode("h2/D0ilF", pos));
+  EXPECT_EQ(4ul, pos);
+  EXPECT_EQ(84522, VLQBase64Decode("h2/D0ilF", pos));
+  EXPECT_EQ(8ul, pos);
+  pos = 0;
+  EXPECT_EQ(-537798, VLQBase64Decode("ts6gBy0m3B", pos));
+  EXPECT_EQ(5ul, pos);
+  EXPECT_EQ(904521, VLQBase64Decode("ts6gBy0m3B", pos));
+  EXPECT_EQ(10ul, pos);
+}
+
+TEST(VLQBASE64, DecodeFourSegment) {
+  size_t pos = 0;
+  EXPECT_EQ(0, VLQBase64Decode("AAAA", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("AAAA", pos));
+  EXPECT_EQ(2ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("AAAA", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("AAAA", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(8, VLQBase64Decode("QADA", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("QADA", pos));
+  EXPECT_EQ(2ul, pos);
+  EXPECT_EQ(-1, VLQBase64Decode("QADA", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("QADA", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(2, VLQBase64Decode("ECQY", pos));
+  EXPECT_EQ(1ul, pos);
+  EXPECT_EQ(1, VLQBase64Decode("ECQY", pos));
+  EXPECT_EQ(2ul, pos);
+  EXPECT_EQ(8, VLQBase64Decode("ECQY", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(12, VLQBase64Decode("ECQY", pos));
+  EXPECT_EQ(4ul, pos);
+  pos = 0;
+  EXPECT_EQ(3200, VLQBase64Decode("goGguCioPk9I", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(1248, VLQBase64Decode("goGguCioPk9I", pos));
+  EXPECT_EQ(6ul, pos);
+  EXPECT_EQ(7809, VLQBase64Decode("goGguCioPk9I", pos));
+  EXPECT_EQ(9ul, pos);
+  EXPECT_EQ(4562, VLQBase64Decode("goGguCioPk9I", pos));
+  EXPECT_EQ(12ul, pos);
+  pos = 0;
+  EXPECT_EQ(1021, VLQBase64Decode("6/BACA", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("6/BACA", pos));
+  EXPECT_EQ(4ul, pos);
+  EXPECT_EQ(1, VLQBase64Decode("6/BACA", pos));
+  EXPECT_EQ(5ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("6/BACA", pos));
+  EXPECT_EQ(6ul, pos);
+  pos = 0;
+  EXPECT_EQ(1207, VLQBase64Decode("urCAQA", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("urCAQA", pos));
+  EXPECT_EQ(4ul, pos);
+  EXPECT_EQ(8, VLQBase64Decode("urCAQA", pos));
+  EXPECT_EQ(5ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("urCAQA", pos));
+  EXPECT_EQ(6ul, pos);
+  pos = 0;
+  EXPECT_EQ(54, VLQBase64Decode("sDACA", pos));
+  EXPECT_EQ(2ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("sDACA", pos));
+  EXPECT_EQ(3ul, pos);
+  EXPECT_EQ(1, VLQBase64Decode("sDACA", pos));
+  EXPECT_EQ(4ul, pos);
+  EXPECT_EQ(0, VLQBase64Decode("sDACA", pos));
+  EXPECT_EQ(5ul, pos);
+}
+}  // namespace base
+}  // namespace v8
