8b8f91baabe6a9a7622b6b15208b36049b24a73c
Seth Brenith <seth.brenith@microsoft.com>
2019-06-24 10:56:22 -0700

    WIP: Add postmortem debugging helper library
    This is an incomplete prototype, shared to provide a sketch of my
    current ideas for implementation of the "JavaScript object
    investigation" section of this design document:
    https://docs.google.com/document/d/1evHnb1uLlSbvHAAsmOXyc25x3uh1DjgNa8u1RHvwVhk/edit#
    
    The primary library entry point is the function GetObjectProperties:
    https://crrev.com/c/v8/v8/+/1628012/2/tools/debug_helper/debug-helper.h#161
    This function takes the object pointer to inspect, a callback function
    for reading memory, and some other optional data that might be helpful,
    and returns a struct which contains both a brief string description of
    the object and an array of properties the object contains.
    
    The test file demonstrates a few simple cases of fetching properties:
    https://crrev.com/c/v8/v8/+/1628012/2/test/cctest/test-debug-helper.cc
    
    This change currently uses plain C structs on the library boundary, to
    make linking/embedding easier from debugger extensions built with
    alternative toolchains. The library uses proper ownership semantics and
    standard collections everywhere up to the boundary function where it
    must release and return a raw pointer. I'm not sure this is worth the
    trouble though, since V8 embedders are already accustomed to linking
    against a C++ API.
    
    Bug: v8:9376
    
    Change-Id: Iebf3cc2dea3133c7811bcefcdf38d9458b02fded

---
+++ test/cctest/test-debug-helper.cc
@@ -0,0 +1,91 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/api/api-inl.h"
+#include "test/cctest/cctest.h"
+#include "tools/debug_helper/debug-helper.h"
+
+namespace v8 {
+namespace internal {
+
+namespace {
+
+namespace d = v8::debug_helper;
+
+// Implement the memory-reading callback. This one just fetches memory from the
+// current process, but a real implementation for a debugging extension would
+// fetch memory from the debuggee process or crash dump.
+d::MemoryAccessResult ReadMemory(uintptr_t address, uint8_t* destination,
+                                 size_t byte_count) {
+  memcpy(destination, reinterpret_cast<void*>(address), byte_count);
+  return d::MemoryAccessResult::kOk;
+}
+
+// Another memory-reading callback that simulates having no accessible memory in
+// the dump.
+d::MemoryAccessResult ReadMemoryFail(uintptr_t address, uint8_t* destination,
+                                     size_t byte_count) {
+  return d::MemoryAccessResult::kAddressValidButInaccessible;
+}
+
+void CheckProp(const d::ObjectProperty& property, const char* expected_type,
+               const char* expected_name) {
+  CHECK_EQ(property.num_values, 1);
+  CHECK(property.type_name == std::string(expected_type));
+  CHECK(property.kind == d::PropertyKind::kSingle);
+  CHECK(property.name == std::string(expected_name));
+}
+
+template <typename TValue>
+void CheckProp(const d::ObjectProperty& property, const char* expected_type,
+               const char* expected_name, TValue expected_value) {
+  CheckProp(property, expected_type, expected_name);
+  CHECK(*reinterpret_cast<TValue*>(property.address) == expected_value);
+}
+
+}  // namespace
+
+TEST(GetObjectProperties) {
+  CcTest::InitializeVM();
+  v8::HandleScope scope(CcTest::isolate());
+  LocalContext context;
+  d::Roots roots{0, 0, 0, 0};  // We don't know the heap roots.
+
+  v8::Local<v8::Value> v = CompileRun("42");
+  Handle<Object> o = v8::Utils::OpenHandle(*v);
+  d::ObjectPropertiesResultPtr props =
+      d::GetObjectProperties(o->ptr(), &ReadMemory, roots);
+  CHECK(props->type_check_result == d::TypeCheckResult::kSmi);
+  CHECK(props->brief == std::string("42 (0x2a)"));
+  CHECK(props->type == std::string("v8::internal::Smi"));
+  CHECK_EQ(props->num_properties, 0);
+
+  v = CompileRun("[\"a\", \"b\"]");
+  o = v8::Utils::OpenHandle(*v);
+  props = d::GetObjectProperties(o->ptr(), &ReadMemory, roots);
+  CHECK(props->type_check_result == d::TypeCheckResult::kUsedMap);
+  CHECK(props->type == std::string("v8::internal::JsArray"));
+  CHECK_EQ(props->num_properties, 4);
+  CheckProp(*props->properties[0], "v8::internal::Map", "map");
+  CheckProp(*props->properties[1], "v8::internal::Object",
+            "properties_or_hash");
+  CheckProp(*props->properties[2], "v8::internal::FixedArrayBase", "elements");
+  CheckProp(*props->properties[3], "v8::internal::Object", "length",
+            IntToSmi(2));
+
+  // The properties_or_hash_code field should be an empty fixed array. Since
+  // that is at a known offset, we should be able to detect it even without
+  // any ability to read memory.
+  props = d::GetObjectProperties(
+      *reinterpret_cast<uintptr_t*>(props->properties[1]->address),
+      &ReadMemoryFail, roots);
+  CHECK(props->type_check_result ==
+        d::TypeCheckResult::kObjectPointerValidButInaccessible);
+  CHECK(props->type == std::string("v8::internal::Object"));
+  CHECK_EQ(props->num_properties, 0);
+  CHECK(props->brief == std::string("maybe EmptyFixedArray"));
+}
+
+}  // namespace internal
+}  // namespace v8
---
+++ tools/debug_helper/debug-helper.h
@@ -0,0 +1,202 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file defines the public interface to v8_debug_helper.
+
+#ifndef V8_TOOLS_DEBUG_HELPER_DEBUG_HELPER_H_
+#define V8_TOOLS_DEBUG_HELPER_DEBUG_HELPER_H_
+
+#include <cstdint>
+#include <memory>
+
+#if defined(_WIN32)
+
+#ifdef BUILDING_V8_DEBUG_HELPER
+#define V8_DEBUG_HELPER_EXPORT __declspec(dllexport)
+#elif USING_V8_DEBUG_HELPER
+#define V8_DEBUG_HELPER_EXPORT __declspec(dllimport)
+#else
+#define V8_DEBUG_HELPER_EXPORT
+#endif
+
+#else  // defined(_WIN32)
+
+#ifdef BUILDING_V8_DEBUG_HELPER
+#define V8_DEBUG_HELPER_EXPORT __attribute__((visibility("default")))
+#else
+#define V8_DEBUG_HELPER_EXPORT
+#endif
+
+#endif  // defined(_WIN32)
+
+namespace v8 {
+namespace debug_helper {
+
+// Possible results when attempting to fetch memory from the debuggee.
+enum class MemoryAccessResult {
+  kOk,
+  kAddressNotValid,
+  kAddressValidButInaccessible,  // Possible in incomplete dump.
+};
+
+enum class SymbolicLookupResult {
+  kOk,
+  kSymbolNotFound,
+};
+
+// Information about how this tool discovered the type of the object.
+enum class TypeCheckResult {
+  // Success cases (description will be non-null):
+  kSmi,
+  kWeakRef,
+  kUsedMap,
+  kUsedTypeHint,
+
+  // Failure cases (description will be null):
+  kUnableToDecompress,  // Caller must provide the heap range somehow.
+  kObjectPointerInvalid,
+  kObjectPointerValidButInaccessible,  // Possible in incomplete dump.
+  kMapPointerInvalid,
+  kMapPointerValidButInaccessible,  // Possible in incomplete dump.
+  kUnknownInstanceType,
+};
+
+enum class PropertyKind {
+  kSingle,
+  kArrayOfKnownSize,
+  kArrayOfUnknownSizeDueToInvalidMemory,
+  kArrayOfUnknownSizeDueToValidButInaccessibleMemory,
+};
+
+enum class PropertyType {
+  // The type_name string is telling the truth.
+  kNormal,
+
+  // A 4-byte representation of an 8-byte tagged pointer. When making a
+  // subsequent request for data about this object, be sure to include its
+  // address as Roots::any_heap_pointer so we can decompress the data.
+  kCompressedTaggedObject,
+};
+
+struct ObjectProperty {
+  const char* name;
+
+  // Statically-determined type, such as from .tq definition. Be sure to also
+  // check the PropertyType, which can disambiguate between compressed and
+  // uncompressed objects.
+  const char* type_name;
+
+  // The address where the property value can be found in the debuggee's address
+  // space, or the address of the first value for an array.
+  uintptr_t address;
+
+  // If kind indicates an array of unknown size, num_values will be 0 and debug
+  // tools should display this property as a raw pointer. Note that there is a
+  // semantic difference between num_values=1 and kind=kSingle (normal property)
+  // versus num_values=1 and kind=kArrayOfKnownSize (one-element array).
+  size_t num_values;
+
+  PropertyKind kind;
+  PropertyType type;
+};
+
+struct ObjectPropertiesResult {
+  TypeCheckResult type_check_result;
+  const char* brief;
+  const char* type;  // Runtime type of the object.
+  size_t num_properties;
+  ObjectProperty** properties;
+};
+
+// Copies byte_count bytes of memory from the given address in the debuggee to
+// the destination buffer.
+typedef MemoryAccessResult (*MemoryAccessor)(uintptr_t address,
+                                             uint8_t* destination,
+                                             size_t byte_count);
+
+// Looks up an item in the debuggee's thread-local storage and writes it to the
+// destination.
+typedef MemoryAccessResult (*TlsAccessor)(uintptr_t tls_key,
+                                          uintptr_t* destination);
+
+// Looks up a global or class-static piece of data in the debuggee by fully-
+// qualified name, and writes its address to the destination (does not
+// dereference the memory).
+typedef SymbolicLookupResult (*GlobalFinder)(const char* name,
+                                             uintptr_t* destination);
+
+// Additional data that can help the debugger to be more accurate. Debuggers
+// that have access to thread-local storage can call FindRoots to fill this out.
+// Any fields you don't know can be set to zero and the debugger will do the
+// best it can with the information available.
+struct Roots {
+  // Beginning of allocated space for various kinds of data. These can help us
+  // to detect certain common objects that are placed in memory during startup.
+  // These values might be provided via name-value pairs in CrashPad dumps.
+  uintptr_t map_space;
+  uintptr_t old_space;
+  uintptr_t read_only_space;
+
+  // Any valid heap pointer address. On platforms where pointer compression is
+  // enabled, this can allow us to get data from compressed pointers even if the
+  // other data above is not provided.
+  uintptr_t any_heap_pointer;
+};
+
+}  // namespace debug_helper
+}  // namespace v8
+
+extern "C" {
+// Raw library interface. If possible, use functions in v8::debug_helper
+// namespace instead because they use smart pointers to prevent leaks.
+V8_DEBUG_HELPER_EXPORT v8::debug_helper::ObjectPropertiesResult*
+_v8_debug_helper_GetObjectProperties(
+    uintptr_t object, v8::debug_helper::MemoryAccessor memory_accessor,
+    const v8::debug_helper::Roots& heap_roots, const char* type_hint);
+V8_DEBUG_HELPER_EXPORT void _v8_debug_helper_Free_ObjectPropertiesResult(
+    v8::debug_helper::ObjectPropertiesResult* result);
+V8_DEBUG_HELPER_EXPORT void _v8_debug_helper_FindRoots(
+    v8::debug_helper::MemoryAccessor memory_accessor,
+    v8::debug_helper::TlsAccessor tls_accessor,
+    v8::debug_helper::GlobalFinder global_finder,
+    v8::debug_helper::Roots* roots);
+}
+
+namespace v8 {
+namespace debug_helper {
+
+struct DebugHelperObjectPropertiesResultDeleter {
+  void operator()(v8::debug_helper::ObjectPropertiesResult* ptr) {
+    _v8_debug_helper_Free_ObjectPropertiesResult(ptr);
+  }
+};
+using ObjectPropertiesResultPtr =
+    std::unique_ptr<ObjectPropertiesResult,
+                    DebugHelperObjectPropertiesResultDeleter>;
+
+// Get information about the given object pointer (either a tagged pointer
+// (compressed or uncompressed), or a SMI). The type hint is only used if the
+// object's Map is missing or corrupt. It should be the fully-qualified name of
+// a class that inherits from v8::internal::Object.
+inline ObjectPropertiesResultPtr GetObjectProperties(
+    uintptr_t object, v8::debug_helper::MemoryAccessor memory_accessor,
+    const Roots& heap_roots, const char* type_hint = nullptr) {
+  return ObjectPropertiesResultPtr(_v8_debug_helper_GetObjectProperties(
+      object, memory_accessor, heap_roots, type_hint));
+}
+
+// Attempt to find the heap roots by using the Isolate that the current thread's
+// local storage points to. Writes the result to the location pointed to by the
+// "roots" parameter. Passing this result to future GetObjectProperties calls
+// may improve the results.
+inline void FindRoots(MemoryAccessor memory_accessor, TlsAccessor tls_accessor,
+                      GlobalFinder global_finder, Roots* roots) {
+  _v8_debug_helper_FindRoots(memory_accessor, tls_accessor, global_finder,
+                             roots);
+}
+
+}  // namespace debug_helper
+}  // namespace v8
+
+#endif
