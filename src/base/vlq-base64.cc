dd5a4e1fe26ff6bd57395a714f03eebb5fe50ad2
zhiguo <zhiguo.zhou@intel.com>
2019-07-09 00:06:39 +0800

    Add Base64 VLQ decoding support
    This CL adds the support for Base64 VLQ decoding, which is the base of
    parsing source map files (Version 3). With this support, the mappings of
    C/C++ source code and WASM bytecode could be built in V8 engine. The
    newly-added function is called VLQBase64Decode, which accepts two
    argument: the Base64-encoded VLQ string and the position of first
    character to be decoded. Upon its return, the position is updated with
    the next start position. The unittest of this support is also added in
    this CL.
    Change-Id: If0f32972ecd7488844478a7b93a0f10cc38b6a5d

---
+++ src/base/vlq-base64.cc
@@ -0,0 +1,58 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <climits>
+#include <limits>
+
+#include "src/base/logging.h"
+#include "src/base/vlq-base64.h"
+
+namespace v8 {
+namespace base {
+static int16_t CharToDigit[128] = {
+    -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
+    -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
+    -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
+    -1,   -1,   -1,   -1,   -1,   -1,   -1,   0x3e, -1,   -1,   -1,   0x3f,
+    0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, -1,   -1,
+    -1,   -1,   -1,   -1,   -1,   0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+    0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12,
+    0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, -1,   -1,   -1,   -1,   -1,
+    -1,   0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24,
+    0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30,
+    0x31, 0x32, 0x33, -1,   -1,   -1,   -1,   -1};
+
+static int16_t charToDigitDecode(unsigned char c) { return CharToDigit[c]; }
+
+int16_t charToDigitDecodeForTesting(unsigned char c) {
+  return charToDigitDecode(c);
+}
+
+int32_t VLQBase64Decode(const std::string& s, size_t& pos) {
+  unsigned int CONTINUE_SHIFT = 5;
+  unsigned int CONTINUE_MASK = 1 << CONTINUE_SHIFT;
+  unsigned int DATA_MASK = CONTINUE_MASK - 1;
+  unsigned int res = 0;
+  uint64_t shift = 0;
+  bool toContinue = true;
+
+  while (toContinue) {
+    if (pos >= s.size()) {
+      return std::numeric_limits<int32_t>::min();
+    }
+    int digit = static_cast<int>(charToDigitDecode(s[pos]));
+    if (digit == -1 ||
+        (shift + CONTINUE_SHIFT >= CHAR_BIT * sizeof(int32_t) && digit >> 2)) {
+      return std::numeric_limits<int32_t>::min();
+    }
+    toContinue = !!(digit & CONTINUE_MASK);
+    res += (digit & DATA_MASK) << shift;
+    shift += CONTINUE_SHIFT;
+    pos++;
+  }
+
+  return (res & 1) ? -static_cast<int>(res >> 1) : (res >> 1);
+}
+}  // namespace base
+}  // namespace v8
