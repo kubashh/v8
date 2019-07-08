20aba311bad64f4fcbd64e8c9b1e9d8cae0a3521
Michael Starzinger <mstarzinger@chromium.org>
2019-07-08 10:37:40 +0200

    [wasm] Test WebAssembly.Function in non-zero tables.
    This adds a test case for using constructed {WebAssembly.Function}
    objects in non-zero tables. Due to a recent refactoring that unifies
    handling of dispatch tables, this works out of the box. The test
    coverage however is still useful, since code paths are slightly
    different for non-zero tables.
    
    R=ahaas@chromium.org
    TEST=mjsunit/wasm/type-reflection-with-anyref
    BUG=v8:7742
    
    Change-Id: I0cf4b0a8039bbef0422b06ee23744a949be8f1b1

--- test/mjsunit/wasm/type-reflection-with-anyref.js
+++ test/mjsunit/wasm/type-reflection-with-anyref.js
@@ -34,3 +34,43 @@ load('test/mjsunit/wasm/wasm-module-builder.js');
   assertEquals(false, type.mutable);
   assertEquals(2, Object.getOwnPropertyNames(type).length);
 })();
+
+// This is an extension of "type-reflection.js/TestFunctionTableSetAndCall" to
+// multiple table indexes. If --experimental-wasm-anyref is enabled by default
+// this test case can supersede the other one.
+(function TestFunctionMultiTableSetAndCall() {
+  let builder = new WasmModuleBuilder();
+  let fun1 = new WebAssembly.Function({parameters:[], results:["i32"]}, _ => 7);
+  let fun2 = new WebAssembly.Function({parameters:[], results:["i32"]}, _ => 9);
+  let fun3 = new WebAssembly.Function({parameters:[], results:["f64"]}, _ => 0);
+  let table = new WebAssembly.Table({element: "anyfunc", initial: 2});
+  let table_index0 = builder.addImportedTable("m", "table", 2);
+  let table_index1 = builder.addTable(kWasmAnyFunc, 1).exportAs("tbl").index;
+  let sig_index = builder.addType(kSig_i_v);
+  table.set(0, fun1);
+  builder.addFunction('main0', kSig_i_i)
+      .addBody([
+        kExprGetLocal, 0,
+        kExprCallIndirect, sig_index, table_index0
+      ])
+      .exportFunc();
+  builder.addFunction('main1', kSig_i_i)
+      .addBody([
+        kExprGetLocal, 0,
+        kExprCallIndirect, sig_index, table_index1
+      ])
+      .exportFunc();
+  let instance = builder.instantiate({ m: { table: table }});
+  assertEquals(7, instance.exports.main0(0));
+  table.set(1, fun2);
+  assertEquals(9, instance.exports.main0(1));
+  table.set(1, fun3);
+  assertTraps(kTrapFuncSigMismatch, () => instance.exports.main0(1));
+  assertTraps(kTrapFuncSigMismatch, () => instance.exports.main1(0));
+  instance.exports.tbl.set(0, fun1);
+  assertEquals(7, instance.exports.main1(0));
+  instance.exports.tbl.set(0, fun2);
+  assertEquals(9, instance.exports.main1(0));
+  instance.exports.tbl.set(0, fun3);
+  assertTraps(kTrapFuncSigMismatch, () => instance.exports.main1(0));
+})();
