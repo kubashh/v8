ebf44bf40fa6361b6af183b1d7c84db9f5d6c630
Ng Zhi An <zhin@chromium.org>
2019-09-10 13:05:32 -0700

    Reland "[wasm-simd] Implement F64x2ConvertI64x2 for x64"
    This is a reland of 306bb635b7e8d13c699b5eefe4fc304201acfbed
    
    Original change's description:
    > [wasm-simd] Implement F64x2ConvertI64x2 for x64
    > 
    > Bug: v8:8460
    > Change-Id: Icefb90c67af77ac93bd75b4e452ba426232de83a
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1710332
    > Commit-Queue: Zhi An Ng <zhin@chromium.org>
    > Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    > Reviewed-by: Bill Budge <bbudge@chromium.org>
    > Reviewed-by: Deepti Gandluri <gdeepti@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#63627}
    
    Bug: v8:8460
    Change-Id: I08d2c88e81ce51d3d1cfdf3d7d6ba34792e34e9e

--- test/cctest/wasm/test-run-wasm-simd.cc
+++ test/cctest/wasm/test-run-wasm-simd.cc
@@ -411,6 +411,12 @@ bool IsExtreme(float x) {
          (abs_x < kSmallFloatThreshold || abs_x > kLargeFloatThreshold);
 }
 
+/* TEST(ZHIN) { */
+/*   double x = 0x8000000000000401; */
+/*   double y = static_cast<double>(static_cast<uint64_t>(x)); */
+/*   CHECK_EQ(x, y); */
+/* } */
+
 WASM_SIMD_TEST(F32x4Splat) {
   WasmRunner<int32_t, float> r(execution_tier, lower_simd);
   // Set up a global to hold output vector.
@@ -1271,6 +1277,34 @@ WASM_SIMD_TEST_NO_LOWERING(I64x2MinU) {
 WASM_SIMD_TEST_NO_LOWERING(I64x2MaxU) {
   RunI64x2BinOpTest(execution_tier, lower_simd, kExprI64x2MaxU,
                     UnsignedMaximum);
+}
+
+WASM_SIMD_TEST_NO_LOWERING(F64x2ConvertI64x2) {
+  WasmRunner<int32_t, int64_t> r(execution_tier, lower_simd);
+  // Create two output vectors to hold signed and unsigned results.
+  double* g0 = r.builder().AddGlobal<double>(kWasmS128);
+  double* g1 = r.builder().AddGlobal<double>(kWasmS128);
+  // Build fn to splat test value, perform conversions, and write the results.
+  byte value = 0;
+  byte temp1 = r.AllocateLocal(kWasmS128);
+  BUILD(r, WASM_SET_LOCAL(temp1, WASM_SIMD_I64x2_SPLAT(WASM_GET_LOCAL(value))),
+        WASM_SET_GLOBAL(
+            0, WASM_SIMD_UNOP(kExprF64x2SConvertI64x2, WASM_GET_LOCAL(temp1))),
+        WASM_SET_GLOBAL(
+            1, WASM_SIMD_UNOP(kExprF64x2UConvertI64x2, WASM_GET_LOCAL(temp1))),
+        WASM_ONE);
+
+  FOR_INT64_INPUTS(x) {
+    r.Call(x);
+    double expected_signed = static_cast<double>(x);
+    double expected_unsigned = static_cast<double>(static_cast<uint64_t>(x));
+    for (int i = 0; i < 2; i++) {
+      CHECK_EQ(expected_signed, ReadLittleEndianValue<double>(&g0[i]));
+      CHECK_EQ(expected_unsigned, ReadLittleEndianValue<double>(&g1[i]));
+      printf("i: %d x: %lu expected_unsigned: %a read: %a\n", i, x,
+             expected_unsigned, ReadLittleEndianValue<double>(&g1[i]));
+    }
+  }
 }
 #endif  // V8_TARGET_ARCH_X64
 #endif  // V8_TARGET_ARCH_X64 || V8_TARGET_ARCH_ARM64
