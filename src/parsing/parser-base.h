999e7ea25ce1951f733cfd513839839c2158f8d0
Joshua Litt <joshualitt@chromium.org>
2019-09-10 10:36:28 -0700

    [hole-check-elimination] Simplest possible hole check elimination
    Bug: v8:7427
    Change-Id: Iedd36c146cefff7e6687fdad48d263889c5c8347

--- src/parsing/parser-base.h
+++ src/parsing/parser-base.h
@@ -986,6 +986,7 @@ class ParserBase {
     DeclarationScope* receiver_scope = closure_scope->GetReceiverScope();
     Variable* var = receiver_scope->receiver();
     var->set_is_used();
+    receiver_scope->set_has_this_reference();
     if (closure_scope == receiver_scope) {
       // It's possible that we're parsing the head of an arrow function, in
       // which case we haven't realized yet that closure_scope !=
@@ -993,6 +994,23 @@ class ParserBase {
       expression_scope()->RecordThisUse();
     } else {
       closure_scope->set_has_this_reference();
+      var->ForceContextAllocation();
+    }
+    return var;
+  }
+
+  V8_INLINE Variable* UseSuperCallReference() {
+    DeclarationScope* closure_scope = scope()->GetClosureScope();
+    DeclarationScope* receiver_scope = closure_scope->GetReceiverScope();
+    Variable* var = receiver_scope->receiver();
+    var->set_is_used();
+    if (closure_scope == receiver_scope) {
+      // It's possible that we're parsing the head of an arrow function, in
+      // which case we haven't realized yet that closure_scope !=
+      // receiver_scope. Mark through the ExpressionScope for now.
+      expression_scope()->RecordSuperCallReference();
+    } else {
+      closure_scope->set_has_super_call_reference();
       var->ForceContextAllocation();
     }
     return var;
@@ -1161,7 +1179,8 @@ class ParserBase {
   // list. This works because in the case of the parser, StatementListT is
   // a pointer whereas the preparser does not really modify the body.
   V8_INLINE void ParseStatementList(StatementListT* body,
-                                    Token::Value end_token);
+                                    Token::Value end_token,
+                                    bool is_top_level = false);
   StatementT ParseStatementListItem();
 
   StatementT ParseStatement(ZonePtrList<const AstRawString>* labels,
@@ -1823,6 +1842,12 @@ typename ParserBase<Impl>::ExpressionT ParserBase<Impl>::ParseExpression() {
   AcceptINScope scope(this, true);
   ExpressionT result = ParseExpressionCoverGrammar();
   expression_scope.ValidateExpression();
+  if (expression_scope.maybe_can_elide_this_hole_checks()) {
+    DeclarationScope* scope = GetReceiverScope();
+    if (!scope->has_this_reference()) {
+      scope->set_can_elide_this_hole_checks();
+    }
+  }
   return result;
 }
 
@@ -3473,8 +3498,8 @@ typename ParserBase<Impl>::ExpressionT ParserBase<Impl>::ParseSuperExpression(
     if (!is_new && peek() == Token::LPAREN && IsDerivedConstructor(kind)) {
       // TODO(rossberg): This might not be the correct FunctionState for the
       // method here.
-      expression_scope()->RecordThisUse();
-      UseThis();
+      expression_scope()->RecordSuperCallReference();
+      UseSuperCallReference();
       return impl()->NewSuperCallReference(pos);
     }
   }
@@ -4046,7 +4071,7 @@ void ParserBase<Impl>::ParseFunctionBody(
       } else if (IsAsyncFunction(kind)) {
         ParseAsyncFunctionBody(inner_scope, &inner_body);
       } else {
-        ParseStatementList(&inner_body, closing_token);
+        ParseStatementList(&inner_body, closing_token, true);
       }
 
       if (IsDerivedConstructor(kind)) {
@@ -4453,7 +4478,7 @@ void ParserBase<Impl>::ParseAsyncFunctionBody(Scope* scope,
   BlockT block = impl()->NullBlock();
   {
     StatementListT statements(pointer_buffer());
-    ParseStatementList(&statements, Token::RBRACE);
+    ParseStatementList(&statements, Token::RBRACE, true);
     block = factory()->NewBlock(true, statements);
   }
   impl()->RewriteAsyncFunctionBody(
@@ -4713,7 +4738,8 @@ typename ParserBase<Impl>::ExpressionT ParserBase<Impl>::ParseV8Intrinsic() {
 
 template <typename Impl>
 void ParserBase<Impl>::ParseStatementList(StatementListT* body,
-                                          Token::Value end_token) {
+                                          Token::Value end_token,
+                                          bool is_top_level) {
   // StatementList ::
   //   (StatementListItem)* <end_token>
   DCHECK_NOT_NULL(body);
@@ -4764,11 +4790,17 @@ void ParserBase<Impl>::ParseStatementList(StatementListT* body,
   // all scripts and functions get their own target stack thus avoiding illegal
   // breaks and continues across functions.
   TargetScopeT target_scope(this);
+  bool first_statement_was_super = peek() == Token::SUPER;
   while (peek() != end_token) {
     StatementT stat = ParseStatementListItem();
     if (impl()->IsNull(stat)) return;
     if (stat->IsEmptyStatement()) continue;
     body->Add(stat);
+  }
+
+  if (!(first_statement_was_super && is_top_level)) {
+    DeclarationScope* scope = GetReceiverScope();
+    scope->unset_can_elide_this_hole_checks();
   }
 }
 
