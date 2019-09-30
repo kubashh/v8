410c0ea605c5c9a7aeb2728c82664fddd69d2c4a
Philip Pfaffe <pfaffe@chromium.org>
2019-09-30 12:37:54 +0200

    Support move-only types and blocking Dequeue in LockedQueue.
    Add support for move-only types and a blocking mechanism on the
    LockedQueue API. DequeueWait can now be used to block a calling thread
    up until timeout for an element to be enqueued. This is going to be used
    as part of a concurrent task queue.
    
    Bug: v8:9783
    Change-Id: I9c89e50c46c8f9ae182e8dcd12849fadf245e974

--- test/unittests/utils/locked-queue-unittest.cc
+++ test/unittests/utils/locked-queue-unittest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "src/base/platform/time.h"
 #include "src/utils/locked-queue-inl.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -80,6 +81,31 @@ TEST(LockedQueue, MultipleRecords) {
     EXPECT_EQ(i, rec);
   }
   EXPECT_TRUE(queue.IsEmpty());
+}
+
+TEST(LockedQueue, MoveOnly) {
+  using MoveOnlyRecord = std::unique_ptr<int>;
+  LockedQueue<MoveOnlyRecord> queue;
+  MoveOnlyRecord element = std::make_unique<int>(4);
+
+  base::Optional<MoveOnlyRecord> empty = queue.Dequeue();
+  ASSERT_FALSE(empty.has_value());
+
+  queue.Enqueue(std::move(element));
+  empty = queue.Dequeue();
+  ASSERT_TRUE(empty.has_value());
+  ASSERT_EQ(**empty, 4);
+
+  empty = queue.Dequeue();
+  ASSERT_FALSE(empty.has_value());
+
+  empty = queue.DequeueWait(base::TimeDelta::FromMilliseconds(1));
+  ASSERT_FALSE(empty.has_value());
+
+  queue.Enqueue(std::make_unique<int>(4));
+  empty = queue.DequeueWait(base::TimeDelta::FromMilliseconds(1));
+  ASSERT_TRUE(empty.has_value());
+  ASSERT_EQ(**empty, 4);
 }
 
 }  // namespace internal
