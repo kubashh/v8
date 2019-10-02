8ab9ba34a45e831d18508103dd6d21a0396ae69f
Junliang Yan <jyan@ca.ibm.com>
2019-10-02 09:35:50 -0400

    [regexp] Disable regexp tier up on Big-Endian
    Disable because it's not yet supported on BE
    
    Bug: v8:9330
    Change-Id: Ia850801d410d3eeaccf9933dd2669f6077e2919c

--- src/regexp/regexp-bytecode-peephole.cc
+++ src/regexp/regexp-bytecode-peephole.cc
@@ -955,7 +955,7 @@ void RegExpBytecodePeephole::EmitArgument(int start_pc, const byte* bytecode,
       //
       // We load 4 bytes from position - 1 and shift out the bytecode.
 #ifdef V8_TARGET_BIG_ENDIAN
-#error Big-endian support is not yet implemented.
+      UNIMPLEMENTED();
 #else
       int32_t val = GetValue<int32_t>(bytecode, arg_pos - 1) >> kBitsPerByte;
 #endif  // V8_TARGET_BIG_ENDIAN
@@ -970,7 +970,7 @@ void RegExpBytecodePeephole::EmitArgument(int start_pc, const byte* bytecode,
               GetValue<int32_t>(&(*optimized_bytecode_buffer_.begin()),
                                 Length() - sizeof(uint32_t));
 #ifdef V8_TARGET_BIG_ENDIAN
-#error Big-endian support is not yet implemented.
+      UNIMPLEMENTED();
 #else
           DCHECK_EQ(prev_val & 0xFFFFFF00, 0);
           OverwriteValue<uint32_t>(pc() - sizeof(uint32_t),
