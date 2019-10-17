c4946c6282271a14b951c0568415a95225548aa4
Santiago Aboy Solanes <solanes@chromium.org>
2019-10-17 11:24:37 +0100

    [ptr-compr] Implement the DecompressionAvoider Reducer and its phase
    The DecompressionAvoider aims to avoid adding the root in AnyTagged or
    TaggedPointer loads. For the TaggedSigned case, we already solve it
    in instruction selection.
    
    The new phase will run only when pointer compression is enabled. For the
    moment, it's also requires FLAG_turbo_decompression_elimination to be
    false. This latter flag is only temporary to test out the implementation.
    
    The phase needs to be run when Machine nodes appear in the graph, i.e at the
    very end of the pipeline since it them to be present. Also, since this phase
    may change the load's MachineRepresentation from Tagged to Compressed, it's
    best to run it as late as possible in order to keep the phases that
    know about Compressed representation to a minimum.
    
    As an example, if we Load a Tagged value only to Store it back again (i.e
    Load -> Store nodes, with the Load being the Store's value) we don't need to
    fully decompress it since the Store will ignore the top bits.
    
    Bug: v8:7703
    Change-Id: I6b4aec203ab8cbb540b2513cabb1e2a5691ce938

---
+++ test/unittests/compiler/decompression-avoider-unittest.cc
@@ -0,0 +1,93 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/compiler/decompression-avoider.h"
+
+#include "test/unittests/compiler/graph-unittest.h"
+
+namespace v8 {
+namespace internal {
+namespace compiler {
+
+class DecompressionAvoiderTest : public GraphTest {
+ public:
+  DecompressionAvoiderTest()
+      : GraphTest(),
+        machine_(zone(), MachineType::PointerRepresentation(),
+                 MachineOperatorBuilder::kNoFlags) {}
+  ~DecompressionAvoiderTest() override = default;
+
+ protected:
+  void Reduce() {
+    DecompressionAvoider decompression_avoider(zone(), graph(), machine());
+    decompression_avoider.Reduce();
+  }
+
+  MachineRepresentation CompressedMachRep(MachineRepresentation mach_rep) {
+    if (mach_rep == MachineRepresentation::kTagged) {
+      return MachineRepresentation::kCompressed;
+    } else {
+      DCHECK_EQ(mach_rep, MachineRepresentation::kTaggedPointer);
+      return MachineRepresentation::kCompressedPointer;
+    }
+  }
+
+  MachineRepresentation CompressedMachRep(MachineType type) {
+    return CompressedMachRep(type.representation());
+  }
+
+  MachineRepresentation LoadMachRep(Node* node) {
+    return LoadRepresentationOf(node->op()).representation();
+  }
+
+  const MachineType types[2] = {MachineType::AnyTagged(),
+                                MachineType::TaggedPointer()};
+
+  StoreRepresentation CreateStoreRep(MachineType type) {
+    return StoreRepresentation(type.representation(),
+                               WriteBarrierKind::kFullWriteBarrier);
+  }
+
+  MachineOperatorBuilder* machine() { return &machine_; }
+
+ private:
+  MachineOperatorBuilder machine_;
+};
+
+// -----------------------------------------------------------------------------
+// Direct Load into Store.
+
+TEST_F(DecompressionAvoiderTest, DirectLoadStore) {
+  // Skip test if pointer compression is disabled, or decompression elimination
+  // enabled.
+  if (!COMPRESS_POINTERS_BOOL || FLAG_turbo_decompression_elimination) {
+    return;
+  }
+
+  // Define variables.
+  Node* const control = graph()->start();
+  Node* object = Parameter(Type::Any(), 0);
+  Node* effect = graph()->start();
+  Node* index = Parameter(Type::UnsignedSmall(), 1);
+
+  // Test for both AnyTagged and TaggedPointer.
+  for (size_t i = 0; i < arraysize(types); ++i) {
+    // Create the graph.
+    Node* base_pointer = graph()->NewNode(machine()->Load(types[i]), object,
+                                          index, effect, control);
+    Node* value = graph()->NewNode(machine()->Load(types[i]), base_pointer,
+                                   index, effect, control);
+    graph()->SetEnd(graph()->NewNode(machine()->Store(CreateStoreRep(types[i])),
+                                     object, index, value, effect, control));
+
+    // Change the loads, and test the change.
+    Reduce();
+    EXPECT_EQ(LoadMachRep(base_pointer), types[i].representation());
+    EXPECT_EQ(LoadMachRep(value), CompressedMachRep(types[i]));
+  }
+}
+
+}  // namespace compiler
+}  // namespace internal
+}  // namespace v8
