ccc1ea67bb846dcb1bef0b9e30f656c32bb91e8d
Gus Caplan <me@gus.host>
2019-10-26 09:26:49 -0700

    Rename and retype Symbol name to description
    Change-Id: I2a1ad1835b751237b350e56d64e3475459bfb7a6

--- src/ic/ic.cc
+++ src/ic/ic.cc
@@ -411,8 +411,8 @@ MaybeHandle<Object> LoadIC::Load(Handle<Object> object, Handle<Name> name) {
 
   if (name->IsPrivate()) {
     if (name->IsPrivateName() && !it.IsFound()) {
-      Handle<String> name_string(String::cast(Symbol::cast(*name).name()),
-                                 isolate());
+      Handle<String> name_string(
+          String::cast(Symbol::cast(*name).description()), isolate());
       return TypeError(MessageTemplate::kInvalidPrivateMemberRead, object,
                        name_string);
     }
@@ -1441,8 +1441,8 @@ MaybeHandle<Object> StoreIC::Store(Handle<Object> object, Handle<Name> name,
 
   if (name->IsPrivate()) {
     if (name->IsPrivateName() && !it.IsFound()) {
-      Handle<String> name_string(String::cast(Symbol::cast(*name).name()),
-                                 isolate());
+      Handle<String> name_string(
+          String::cast(Symbol::cast(*name).description()), isolate());
       return TypeError(MessageTemplate::kInvalidPrivateMemberWrite, object,
                        name_string);
     }
