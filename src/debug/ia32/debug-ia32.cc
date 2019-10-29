9e74c53e5d880c0275bbc80745636fa452d0d24e
Santiago Aboy Solanes <solanes@chromium.org>
2019-10-29 12:29:25 +0000

    [builtins] Remove ParameterCount uses from InvokeFunction(Code)
    CallDebugOnFunctionCall was always using Registers and not Immediates.
    Then ParameterCount is not really needed. Since updating that, we
    could update other functions, e.g InvokeFunction, to only use
    registers too.
    
    Also removed now irrelevant variables, e.g definitely_mismatches.
    
    Bug: v8:9771
    Change-Id: I83382c146dd47ccb8bb329f5becb5e941e4c3968

--- src/debug/ia32/debug-ia32.cc
+++ src/debug/ia32/debug-ia32.cc
@@ -42,9 +42,9 @@ void DebugCodegen::GenerateFrameDropperTrampoline(MacroAssembler* masm) {
 
   // The expected and actual argument counts don't matter as long as they match
   // and we don't enter the ArgumentsAdaptorTrampoline.
-  ParameterCount dummy(0);
+  __ mov(ecx, eax);
   __ mov(esi, FieldOperand(edi, JSFunction::kContextOffset));
-  __ InvokeFunctionCode(edi, no_reg, dummy, dummy, JUMP_FUNCTION);
+  __ InvokeFunctionCode(edi, no_reg, ecx, eax, JUMP_FUNCTION);
 }
 
 const bool LiveEdit::kFrameDropperSupported = true;
