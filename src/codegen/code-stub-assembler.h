37665a38039864196d9ee27612b7401055108027
Daniel Clifford <danno@chromium.org>
2019-11-08 21:50:46 +0100

    [torque] Implement Rest/Strict/Sloppy argument stubs in Torque
    Bug: v8:7793
    Change-Id: Ib950ce398f101779a4654353d08ce947b8e05a66

--- src/codegen/code-stub-assembler.h
+++ src/codegen/code-stub-assembler.h
@@ -2712,6 +2712,8 @@ class V8_EXPORT_PRIVATE CodeStubAssembler
   TNode<UintPtrT> DecodeWord(SloppyTNode<WordT> word, uint32_t shift,
                              uint32_t mask);
 
+  TNode<IntPtrT> DecodeScopeInfoHasContextExtension(TNode<IntPtrT> flags);
+
   // Returns a node that contains the updated values of a |BitField|.
   template <typename BitField>
   TNode<WordT> UpdateWord(TNode<WordT> word, TNode<WordT> value) {
