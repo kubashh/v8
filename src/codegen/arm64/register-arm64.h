47276cff7af65f401c6b1dd45ec577d018f006dd
Clemens Backes <clemensb@chromium.org>
2019-11-13 16:53:48 +0100

    [cleanup] Make Register::reg_code_ private
    Subclasses can now access it via {code()}, even in constexpr contexts.
    
    R=tebbi@chromium.org
    
    Bug: v8:9810
    Change-Id: I3cc6872f568f38db8cdbcda69ac0e203f839cda5

--- src/codegen/arm64/register-arm64.h
+++ src/codegen/arm64/register-arm64.h
@@ -151,15 +151,13 @@ class CPURegister : public RegisterBase<CPURegister, kRegAfterLast> {
   }
   bool IsValid() const { return reg_type_ != kNoRegister; }
   bool IsNone() const { return reg_type_ == kNoRegister; }
-  constexpr bool Is(const CPURegister& other) const {
-    return Aliases(other) && (reg_size_ == other.reg_size_);
-  }
+  constexpr bool Is(const CPURegister& other) const { return *this == other; }
   constexpr bool Aliases(const CPURegister& other) const {
-    return (reg_code_ == other.reg_code_) && (reg_type_ == other.reg_type_);
+    return (code() == other.code()) && (reg_type_ == other.reg_type_);
   }
 
   constexpr bool operator==(const CPURegister& other) const {
-    return Is(other);
+    return RegisterBase::operator==(other) && reg_size_ == other.reg_size_;
   }
 
   constexpr bool operator!=(const CPURegister& other) const {
