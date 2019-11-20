41a2bcfa278fdf3a64e8cb4443bec675fcf074e1
Paolo Severini <paolosev@microsoft.com>
2019-11-19 22:58:41 -0800

    Add initial support for Wasm debugging with LLDB: implements a GDB-remote stub
    This is the first piece of the wasm debugging prototype (besides the changes to
    add/remove breakpoints in WasmModuleObject made with
    https://chromium.googlesource.com/v8/v8.git/+/e699f39caed9a23f8e20bd3a0386a3236e272737).
    
    This changelist adds the infrastructure for a GDB-remote stub that will be used
    to manage debugging sessions via the gdb-remote protocol.
    It enables the creation and termination of debugging sessions over TCP
    connections that are managed in a separate thread.
    The logic to actually send, receive and decode GDB-remote packets will be part
    of a future changelist.
    
    Build with: v8_enable_wasm_gdb_remote_debugging = true
    Run with:
      --wasm-gdb-remote                  Enables Wasm debugging with LLDB
                                         (default: false)
      --wasm-gdb-remote-port             TCP port to be used for debugging
                                         (default: 8765)
      --wasm-pause-waiting-for-debugger  Pauses the execution of Wasm code waiting
                                         for a debugger (default: false)
      --trace-wasm-gdb-remote            Enables tracing of Gdb-remote packets
                                         (default: false)
    
    Note that most of this code is "borrowed" from the code of the Chromium NaCL
    GDB-remote stub (located in Chromium in src\native_client\src\trusted\debug_stub).
    
    Implementation details:
    - class GdbServer acts as a singleton manager for the gdb-remote stub. It is
      instantiated as soon as the first Wasm module is loaded in the Wasm engine.
    - class GdbServerThread spawns the worker thread for the TCP connection.
    - class Transport manages the socket connection, in a portable way.
    - class Session represents a remote debugging session.
    - class Target represents a debugging target and it’s the place where the
      debugging packets will be processed and will implement the logic to debug
      a Wasm engine.
    
    Bug: chromium:1010467
    Change-Id: Ib2324e5901f5ae1d855b96b99ef0995d407322b6

---
+++ src/wasm/gdb-server/gdb-server.cc
@@ -0,0 +1,38 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/wasm/gdb-server/gdb-server.h"
+
+#include "src/wasm/gdb-server/gdb-server-thread.h"
+#include "src/wasm/wasm-engine.h"
+
+namespace v8 {
+namespace internal {
+namespace wasm {
+namespace gdb_server {
+
+bool GdbServer::Initialize() {
+  thread_ = std::make_unique<GdbServerThread>(this);
+  // TODO(paolosev): does StartSynchronously hurt performances?
+  if (!thread_->StartSynchronously()) {
+    TRACE_GDB_REMOTE(
+        "Cannot initialize thread, GDB-remote debugging will be disabled.\n");
+    thread_ = nullptr;
+    return false;
+  }
+  return true;
+}
+
+void GdbServer::Terminate() {
+  if (thread_) {
+    thread_->Stop();
+    thread_->Join();
+    thread_ = nullptr;
+  }
+}
+
+}  // namespace gdb_server
+}  // namespace wasm
+}  // namespace internal
+}  // namespace v8
