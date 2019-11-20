e474f3e81edfe086ceddf58e9c150df410a9fa67
Paolo Severini <paolosev@microsoft.com>
2019-11-19 21:47:59 -0800

    Add initial support for Wasm debugging with LLDB: implements a GDB-remote stub
    This is the first piece of the wasm debugging prototype (besides the changes to
    add/remove breakpoints in WasmModuleObject made with
    https://chromium.googlesource.com/v8/v8.git/+/e699f39caed9a23f8e20bd3a0386a3236e272737).
    
    This changelist adds the infrastructure for a GDB-remote stub that will be used
    to manage debugging sessions via the gdb-remote protocol.
    It enables the creation and termination of debugging sessions over TCP
    connections that are managed in a separate thread.
    The logic to actually send, receive and decode GDB-remote packets will be part
    of a future changelist.
    
    Build with: v8_enable_wasm_gdb_remote_debugging = true
    Run with:
      --wasm-gdb-remote                  Enables Wasm debugging with LLDB
                                         (default: false)
      --wasm-gdb-remote-port             TCP port to be used for debugging
                                         (default: 8765)
      --wasm-pause-waiting-for-debugger  Pauses the execution of Wasm code waiting
                                         for a debugger (default: false)
      --trace-wasm-gdb-remote            Enables tracing of Gdb-remote packets
                                         (default: false)
    
    Note that most of this code is "borrowed" from the code of the Chromium NaCL
    GDB-remote stub (located in Chromium in src\native_client\src\trusted\debug_stub).
    
    Implementation details:
    - class GdbServer acts as a singleton manager for the gdb-remote stub. It is
      instantiated as soon as the first Wasm module is loaded in the Wasm engine.
    - class GdbServerThread spawns the worker thread for the TCP connection.
    - class Transport manages the socket connection, in a portable way.
    - class Session represents a remote debugging session.
    - class Target represents a debugging target and it’s the place where the
      debugging packets will be processed and will implement the logic to debug
      a Wasm engine.
    
    Bug: chromium:1010467
    Change-Id: Ib2324e5901f5ae1d855b96b99ef0995d407322b6

---
+++ src/wasm/gdb-server/gdb-server-thread.cc
@@ -0,0 +1,102 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/wasm/gdb-server/gdb-server-thread.h"
+#include "src/wasm/gdb-server/gdb-server.h"
+#include "src/wasm/gdb-server/session.h"
+
+namespace v8 {
+namespace internal {
+namespace wasm {
+namespace gdb_server {
+
+GdbServerThread::GdbServerThread(GdbServer* gdb_server)
+    : Thread(v8::base::Thread::Options("GdbServerThread")),
+      gdb_server_(gdb_server) {}
+
+void GdbServerThread::InitializeThread() {
+  // Executed in the GdbServer thread.
+  v8::base::MutexGuard guard(&mutex_);
+
+#ifdef _WIN32
+  // Initialize Winsock
+  WSADATA wsaData;
+  int iResult = ::WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (iResult != 0) {
+    TRACE_GDB_REMOTE("GdbServerThread::Run: WSAStartup failed\n");
+    return;
+  }
+#endif
+
+  // If the default port is not available, try any port.
+  std::unique_ptr<SocketBinding> socket_binding =
+      SocketBinding::Bind(FLAG_wasm_gdb_remote_port);
+  if (!socket_binding) {
+    socket_binding = SocketBinding::Bind(0);
+  }
+  if (!socket_binding) {
+    TRACE_GDB_REMOTE("GdbServerThread::Run: Failed to bind any TCP port\n");
+    return;
+  }
+  TRACE_GDB_REMOTE("gdb-remote(%d) : Connect GDB with 'target remote :%d\n",
+                   __LINE__, socket_binding->GetBoundPort());
+
+  transport_ = socket_binding->CreateTransport();
+  target_ = std::make_unique<Target>(gdb_server_);
+}
+
+void GdbServerThread::CleanupThread() {
+  // Executed in the GdbServer thread.
+  v8::base::MutexGuard guard(&mutex_);
+
+  target_ = nullptr;
+  transport_ = nullptr;
+
+#if _WIN32
+  ::WSACleanup();
+#endif
+}
+
+void GdbServerThread::Run() {
+  // Executed in the GdbServer thread.
+
+  InitializeThread();
+  while (!target_->IsTerminated()) {
+    // Wait for a connection.
+    if (!transport_->AcceptConnection()) {
+      continue;
+    }
+
+    // Create a new session for this connection
+    Session session(transport_.get());
+    TRACE_GDB_REMOTE("GdbServerThread: Connected\n");
+
+    // Run this session for as long as it lasts
+    target_->Run(&session);
+  }
+  CleanupThread();
+}
+
+void GdbServerThread::Stop() {
+  // Executed in the Isolate thread.
+
+  // Synchronized, becauses {Stop} might be called while {Run} is still
+  // initializing {transport_} and {target_}. If this happens and the
+  // GdbServer thread is blocked waiting for an incoming connection or
+  // for incoming packets, it will unblocked when {transport_} is closed.
+  v8::base::MutexGuard guard(&mutex_);
+
+  if (target_) {
+    target_->Terminate();
+  }
+
+  if (transport_) {
+    transport_->Close();
+  }
+}
+
+}  // namespace gdb_server
+}  // namespace wasm
+}  // namespace internal
+}  // namespace v8
