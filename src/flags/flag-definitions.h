c355e1ddf280c536d63b10b53c34f1b40f646f26
Joshua Litt <joshualitt@chromium.org>
2019-11-25 14:43:41 -0800

    [regexp] Stage match indices.
    Bug: v8:9548
    Change-Id: Id6be43433b12733e39db712559715ba30e681059

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -206,7 +206,6 @@ DEFINE_IMPLICATION(harmony_import_meta, harmony_dynamic_import)
 #define HARMONY_INPROGRESS_BASE(V)                                    \
   V(harmony_regexp_sequence, "RegExp Unicode sequence properties")    \
   V(harmony_weak_refs, "harmony weak references")                     \
-  V(harmony_regexp_match_indices, "harmony regexp match indices")     \
   V(harmony_top_level_await, "harmony top level await")
 
 #ifdef V8_INTL_SUPPORT
@@ -219,6 +218,7 @@ DEFINE_IMPLICATION(harmony_import_meta, harmony_dynamic_import)
 
 // Features that are complete (but still behind --harmony/es-staging flag).
 #define HARMONY_STAGED_BASE(V)                                        \
+  V(harmony_regexp_match_indices, "harmony regexp match indices")     \
   V(harmony_string_replaceall, "harmony String.prototype.replaceAll") \
   V(harmony_private_methods, "harmony private methods in class literals")
 
