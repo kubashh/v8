46e2f325987045e1df931d9bdaf88efcb26ecef3
Maya Lekova <mslekova@chromium.org>
2019-12-05 11:55:34 +0100

    [turbofan] Improve serializer precision for jumps
    Bug: v8:7790
    Change-Id: Ic79d64c9eaedf608d26e3265d4b27d21f7f3dfe1

---
+++ test/mjsunit/compiler/serializer-dead-after-jump.js
@@ -0,0 +1,35 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --allow-natives-syntax --opt --no-always-opt
+
+function f(x) {
+  %TurbofanStaticAssert(x.foo === 42);
+}
+
+function main(b) {
+  const x = new Object();
+  const y = x;
+  if (b) {
+  } else {
+    x.foo = 42;
+    f(y);
+  }
+}
+
+
+%PrepareFunctionForOptimization(f);
+%PrepareFunctionForOptimization(main);
+
+f({a: 1});
+f({b: 1});
+f({c: 1});
+f({d: 1});
+
+main(true);
+main(true);
+main(false);
+main(false);
+%OptimizeFunctionOnNextCall(main);
+main(Math.pow(2, 10) > 1000);
