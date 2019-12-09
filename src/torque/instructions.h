a8c1b508f8b75e240b17d120201b7228006a8393
Tobias Tebbi <tebbi@chromium.org>
2019-12-09 13:31:08 +0100

    [torque] mutliple arrays in one object
    Bug: v8:7793
    Change-Id: Ifa414b42278e572a0c577bf9da3d37f80771a258

--- src/torque/instructions.h
+++ src/torque/instructions.h
@@ -30,7 +30,6 @@ class RuntimeFunction;
   V(DeleteRangeInstruction)           \
   V(PushUninitializedInstruction)     \
   V(PushBuiltinPointerInstruction)    \
-  V(CreateFieldReferenceInstruction)  \
   V(LoadReferenceInstruction)         \
   V(StoreReferenceInstruction)        \
   V(CallCsaMacroInstruction)          \
@@ -202,17 +201,6 @@ struct NamespaceConstantInstruction : InstructionBase {
       : constant(constant) {}
 
   NamespaceConstant* constant;
-};
-
-struct CreateFieldReferenceInstruction : InstructionBase {
-  TORQUE_INSTRUCTION_BOILERPLATE()
-  CreateFieldReferenceInstruction(const ClassType* type, std::string field_name)
-      : type(type), field_name(std::move(field_name)) {
-    // Trigger errors early.
-    this->type->LookupField(this->field_name);
-  }
-  const ClassType* type;
-  std::string field_name;
 };
 
 struct LoadReferenceInstruction : InstructionBase {
