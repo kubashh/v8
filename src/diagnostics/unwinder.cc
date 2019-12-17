64167db7cf5d9138acfda64f2be9c212dda4e83d
Peter Marshall <petermarshall@chromium.org>
2019-12-17 15:43:44 +0100

    [unwinder] Add a new API based on code pages for arm32 support
    This new API uses the code pages rather than code ranges approach.
    
    It's supported on arm32, as well as the previous two supported
    platforms, x64 and arm64.
    
    Deprecate the old API which only works on x64 and arm64 to reduce the
    maintenance overhead of keeping both. Users of the old API should
    migrate to the new one as it can be used all on supported platforms.
    
    We keep the tests for the old API by ignoring deprecation warnings so
    that we don't accidentally break it while it is still in the codebase.
    
    Design doc:
    https://docs.google.com/document/d/1VGwUult5AHLRk658VetwEHMOmDDxA2eDQs9lDFMZTE0
    
    Bug: v8:8116
    Change-Id: I1de8246a48fc1b4991603501ea6087db6b43fdd9

--- src/diagnostics/unwinder.cc
+++ src/diagnostics/unwinder.cc
@@ -19,6 +19,15 @@ bool PCIsInCodeRange(const v8::MemoryRange& code_range, void* pc) {
   return pc_as_byte >= start && pc_as_byte < end;
 }
 
+bool PCIsInCodePages(size_t code_pages_length, MemoryRange* code_pages,
+                     void* pc) {
+  for (size_t i = 0; i < code_pages_length; i++) {
+    const MemoryRange& range = code_pages[i];
+    if (PCIsInCodeRange(range, pc)) return true;
+  }
+  return false;
+}
+
 bool IsInJSEntryRange(const UnwindState& unwind_state, void* pc) {
   return PCIsInCodeRange(unwind_state.js_entry_stub.code, pc) ||
          PCIsInCodeRange(unwind_state.js_construct_entry_stub.code, pc) ||
@@ -27,6 +36,21 @@ bool IsInJSEntryRange(const UnwindState& unwind_state, void* pc) {
 
 bool IsInUnsafeJSEntryRange(const UnwindState& unwind_state, void* pc) {
   return IsInJSEntryRange(unwind_state, pc);
+
+  // TODO(petermarshall): We can be more precise by checking whether we are
+  // in JSEntry but after frame setup and before frame teardown, in which case
+  // we are safe to unwind the stack. For now, we bail out if the PC is anywhere
+  // within JSEntry.
+}
+
+bool IsInJSEntryRange(const JSEntryStubs& entry_stubs, void* pc) {
+  return PCIsInCodeRange(entry_stubs.js_entry_stub.code, pc) ||
+         PCIsInCodeRange(entry_stubs.js_construct_entry_stub.code, pc) ||
+         PCIsInCodeRange(entry_stubs.js_run_microtasks_entry_stub.code, pc);
+}
+
+bool IsInUnsafeJSEntryRange(const JSEntryStubs& entry_stubs, void* pc) {
+  return IsInJSEntryRange(entry_stubs, pc);
 
   // TODO(petermarshall): We can be more precise by checking whether we are
   // in JSEntry but after frame setup and before frame teardown, in which case
@@ -50,6 +74,18 @@ void* GetReturnAddressFromFP(void* fp, void* pc,
       Load(reinterpret_cast<i::Address>(fp) + caller_pc_offset));
 }
 
+void* GetReturnAddressFromFP(void* fp, void* pc,
+                             const JSEntryStubs& entry_stubs) {
+  int caller_pc_offset = i::CommonFrameConstants::kCallerPCOffset;
+#ifdef V8_TARGET_ARCH_ARM64
+  if (IsInJSEntryRange(entry_stubs, pc)) {
+    caller_pc_offset = i::EntryFrameConstants::kDirectCallerPCOffset;
+  }
+#endif
+  return reinterpret_cast<void*>(
+      Load(reinterpret_cast<i::Address>(fp) + caller_pc_offset));
+}
+
 void* GetCallerFPFromFP(void* fp, void* pc,
                         const v8::UnwindState& unwind_state) {
   int caller_fp_offset = i::CommonFrameConstants::kCallerFPOffset;
@@ -62,11 +98,33 @@ void* GetCallerFPFromFP(void* fp, void* pc,
       Load(reinterpret_cast<i::Address>(fp) + caller_fp_offset));
 }
 
+void* GetCallerFPFromFP(void* fp, void* pc, const JSEntryStubs& entry_stubs) {
+  int caller_fp_offset = i::CommonFrameConstants::kCallerFPOffset;
+#ifdef V8_TARGET_ARCH_ARM64
+  if (IsInJSEntryRange(entry_stubs, pc)) {
+    caller_fp_offset = i::EntryFrameConstants::kDirectCallerFPOffset;
+  }
+#endif
+  return reinterpret_cast<void*>(
+      Load(reinterpret_cast<i::Address>(fp) + caller_fp_offset));
+}
+
 void* GetCallerSPFromFP(void* fp, void* pc,
                         const v8::UnwindState& unwind_state) {
   int caller_sp_offset = i::CommonFrameConstants::kCallerSPOffset;
 #ifdef V8_TARGET_ARCH_ARM64
   if (IsInJSEntryRange(unwind_state, pc)) {
+    caller_sp_offset = i::EntryFrameConstants::kDirectCallerSPOffset;
+  }
+#endif
+  return reinterpret_cast<void*>(reinterpret_cast<i::Address>(fp) +
+                                 caller_sp_offset);
+}
+
+void* GetCallerSPFromFP(void* fp, void* pc, const JSEntryStubs& entry_stubs) {
+  int caller_sp_offset = i::CommonFrameConstants::kCallerSPOffset;
+#ifdef V8_TARGET_ARCH_ARM64
+  if (IsInJSEntryRange(entry_stubs, pc)) {
     caller_sp_offset = i::EntryFrameConstants::kDirectCallerSPOffset;
   }
 #endif
@@ -121,9 +179,57 @@ bool Unwinder::TryUnwindV8Frames(const UnwindState& unwind_state,
   return false;
 }
 
+bool Unwinder::TryUnwindV8Frames(const JSEntryStubs& entry_stubs,
+                                 size_t code_pages_length,
+                                 MemoryRange* code_pages,
+                                 RegisterState* register_state,
+                                 const void* stack_base) {
+  const void* stack_top = register_state->sp;
+
+  void* pc = register_state->pc;
+  if (PCIsInV8(code_pages_length, code_pages, pc) &&
+      !IsInUnsafeJSEntryRange(entry_stubs, pc)) {
+    void* current_fp = register_state->fp;
+    if (!AddressIsInStack(current_fp, stack_base, stack_top)) return false;
+
+    // Peek at the return address that the caller pushed. If it's in V8, then we
+    // assume the caller frame is a JS frame and continue to unwind.
+    void* next_pc = GetReturnAddressFromFP(current_fp, pc, entry_stubs);
+    while (PCIsInV8(code_pages_length, code_pages, next_pc)) {
+      current_fp = GetCallerFPFromFP(current_fp, pc, entry_stubs);
+      if (!AddressIsInStack(current_fp, stack_base, stack_top)) return false;
+      pc = next_pc;
+      next_pc = GetReturnAddressFromFP(current_fp, pc, entry_stubs);
+    }
+
+    void* final_sp = GetCallerSPFromFP(current_fp, pc, entry_stubs);
+    if (!AddressIsInStack(final_sp, stack_base, stack_top)) return false;
+    register_state->sp = final_sp;
+
+    // We don't check that the final FP value is within the stack bounds because
+    // this is just the rbp value that JSEntryStub pushed. On platforms like
+    // Win64 this is not used as a dedicated FP register, and could contain
+    // anything.
+    void* final_fp = GetCallerFPFromFP(current_fp, pc, entry_stubs);
+    register_state->fp = final_fp;
+
+    register_state->pc = next_pc;
+
+    // Link register no longer valid after unwinding.
+    register_state->lr = nullptr;
+    return true;
+  }
+  return false;
+}
+
 bool Unwinder::PCIsInV8(const UnwindState& unwind_state, void* pc) {
   return pc && (PCIsInCodeRange(unwind_state.code_range, pc) ||
                 PCIsInCodeRange(unwind_state.embedded_code_range, pc));
+}
+
+bool Unwinder::PCIsInV8(size_t code_pages_length, MemoryRange* code_pages,
+                        void* pc) {
+  return pc && PCIsInCodePages(code_pages_length, code_pages, pc);
 }
 
 }  // namespace v8
