843b58ef281100064f5f38a2c2808dce6ed74173
Santiago Aboy Solanes <solanes@chromium.org>
2019-12-19 14:49:46 +0000

    [WIP] Stack
    Change-Id: I6156de48b3bc0b519dd21190a8e6214fbe96c78d

--- src/builtins/x64/builtins-x64.cc
+++ src/builtins/x64/builtins-x64.cc
@@ -65,6 +65,18 @@ static void GenerateTailCallToReturnedCode(MacroAssembler* masm,
 
 namespace {
 
+Operand StackLimitAsOperand(MacroAssembler* masm) {
+  DCHECK(masm->root_array_available());
+  Isolate* isolate = masm->isolate();
+  ExternalReference limit = ExternalReference::address_of_jslimit(isolate);
+  DCHECK(TurboAssembler::IsAddressableThroughRootRegister(isolate, limit));
+
+  intptr_t offset =
+      TurboAssembler::RootRegisterOffsetForExternalReference(isolate, limit);
+  CHECK(is_int32(offset));
+  return Operand(kRootRegister, static_cast<int32_t>(offset));
+}
+
 Operand RealStackLimitAsOperand(MacroAssembler* masm) {
   DCHECK(masm->root_array_available());
   Isolate* isolate = masm->isolate();
@@ -1158,6 +1170,29 @@ void Builtins::Generate_InterpreterEntryTrampoline(MacroAssembler* masm) {
   __ j(zero, &no_incoming_new_target_or_generator_register, Label::kNear);
   __ movq(Operand(rbp, rcx, times_system_pointer_size, 0), rdx);
   __ bind(&no_incoming_new_target_or_generator_register);
+
+  // Perform interrupt stack check
+  // TODO(solanes): defer the stack_check_interrupt
+  Label ok;
+  __ cmpq(rsp, StackLimitAsOperand(masm));
+  __ j(above, &ok);
+  __ movq(
+      Operand(rbp, InterpreterFrameConstants::kBytecodeOffsetFromFp),
+      Immediate(Smi::FromInt(BytecodeArray::kHeaderSize - kHeapObjectTag - 1)));
+  __ CallRuntime(Runtime::kStackGuard);
+  __ movq(Operand(rbp, InterpreterFrameConstants::kBytecodeOffsetFromFp),
+          Immediate(Smi::FromInt(BytecodeArray::kHeaderSize - kHeapObjectTag)));
+
+  // Get bytecode array and bytecode offset from the stack frame.
+  __ movq(kInterpreterBytecodeArrayRegister,
+          Operand(rbp, InterpreterFrameConstants::kBytecodeArrayFromFp));
+  __ SmiUntag(kInterpreterBytecodeOffsetRegister,
+              Operand(rbp, InterpreterFrameConstants::kBytecodeOffsetFromFp));
+
+  // Load accumulator with undefined.
+  __ LoadRoot(kInterpreterAccumulatorRegister, RootIndex::kUndefinedValue);
+
+  __ bind(&ok);
 
   // The accumulator is already loaded with undefined.
 
