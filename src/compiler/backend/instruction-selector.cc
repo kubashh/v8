484a868d6f61d507125d22610a71c39b1f7aee85
Ng Zhi An <zhin@chromium.org>
2020-01-07 11:25:00 -0800

    [wasm-simd] Implement v128.andnot for arm64
    Bug: v8:10082
    Change-Id: I68e540c5b68c62fd6d43075e5244a9794d6d3eda

--- src/compiler/backend/instruction-selector.cc
+++ src/compiler/backend/instruction-selector.cc
@@ -2636,15 +2636,15 @@ void InstructionSelector::VisitF64x2SConvertI64x2(Node* node) {
 void InstructionSelector::VisitF64x2UConvertI64x2(Node* node) {
   UNIMPLEMENTED();
 }
-void InstructionSelector::VisitS128AndNot(Node* node) { UNIMPLEMENTED(); }
 #if !V8_TARGET_ARCH_ARM64
-#if !V8_TARGET_ARCH_ARM
 void InstructionSelector::VisitI16x8RoundingAverageU(Node* node) {
   UNIMPLEMENTED();
 }
 void InstructionSelector::VisitI8x16RoundingAverageU(Node* node) {
   UNIMPLEMENTED();
 }
+void InstructionSelector::VisitS128AndNot(Node* node) { UNIMPLEMENTED(); }
+#if !V8_TARGET_ARCH_ARM
 void InstructionSelector::VisitLoadTransform(Node* node) { UNIMPLEMENTED(); }
 #endif  // !V8_TARGET_ARCH_ARM
 #if !V8_TARGET_ARCH_IA32
