74680fbf896c962d8c089c11ed5181f662c113cf
Michael Lippautz <mlippautz@chromium.org>
2020-01-15 15:28:34 +0100

    heap: Avoid allocating a code range when running in jitless mode
    Jitless mode does not allocate executable memory, so we can avoid reserving
    memory in such configurations.
    
    Bug: v8:10033
    Change-Id: Ie6a943084e3bade85848e3219cb4d8779ed34830

--- src/api/api.cc
+++ src/api/api.cc
@@ -985,7 +985,7 @@ void ResourceConstraints::ConfigureDefaultsFromHeapSize(
     set_initial_young_generation_size_in_bytes(young_generation);
     set_initial_old_generation_size_in_bytes(old_generation);
   }
-  if (i::kRequiresCodeRange) {
+  if (i::Heap::RequiresCodeRange()) {
     set_code_range_size_in_bytes(
         i::Min(i::kMaximalCodeRangeSize, maximum_heap_size_in_bytes));
   }
@@ -1000,7 +1000,7 @@ void ResourceConstraints::ConfigureDefaults(uint64_t physical_memory,
   set_max_young_generation_size_in_bytes(young_generation);
   set_max_old_generation_size_in_bytes(old_generation);
 
-  if (virtual_memory_limit > 0 && i::kRequiresCodeRange) {
+  if (virtual_memory_limit > 0 && i::Heap::RequiresCodeRange()) {
     set_code_range_size_in_bytes(
         i::Min(i::kMaximalCodeRangeSize,
                static_cast<size_t>(virtual_memory_limit / 8)));
