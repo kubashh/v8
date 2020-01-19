7052254829906962db9a77f5d39bbc11b0db156f
Ulan Degenbaev <ulan@chromium.org>
2020-01-19 22:23:52 +0100

    not for landing linux-rel
    Change-Id: Ie6da5320af8e933809e5ced290d67b57953ab084

--- src/heap/memory-measurement.cc
+++ src/heap/memory-measurement.cc
@@ -313,6 +313,15 @@ std::unique_ptr<v8::MeasureMemoryDelegate> MemoryMeasurement::DefaultDelegate(
                                                  mode);
 }
 
+void NativeContextInferrer::InferAndSetContext(
+    Isolate* isolate, Map map, HeapObject object,
+    MarkingWorklists* marking_worklists) {
+  Address context = marking_worklists->Context();
+  if (Infer(isolate, map, object, &context)) {
+    marking_worklists->SwitchToContext(context);
+  }
+}
+
 bool NativeContextInferrer::InferForJSFunction(JSFunction function,
                                                Address* native_context) {
   if (function.has_context()) {
