ea8531728d4834ecc6cef34cbc65950682738770
Santiago Aboy Solanes <solanes@chromium.org>
2020-02-07 11:51:50 +0000

    [interpreter] Make FunctionEntry StackCheck bytecodes implicit
    FunctionEntry StackChecks is one of the two cases where we generate a
    StackCheck bytecode. In these cases, we do stack check against the js
    limit (not to be confused with the real js limit). Their purpose is to
    be able to interrupt the running code.
    
    We can omit the FunctionEntry StackCheck by embedding its code into
    the InterpreterEntryTrampoline builtin. We save one bytecode per
    interpreted function.
    
    This change has rippling effects for optimized code, as well as the
    deoptimizer.
    
    Bug: v8:10149, v8:9977
    Change-Id: I6156de48b3bc0b519dd21190a8e6214fbe96c78d

--- src/codegen/source-position-table.cc
+++ src/codegen/source-position-table.cc
@@ -149,7 +149,9 @@ void SourcePositionTableBuilder::AddEntry(const PositionTableEntry& entry) {
   EncodeEntry(&bytes_, tmp);
   previous_ = entry;
 #ifdef ENABLE_SLOW_DCHECKS
-  raw_entries_.push_back(entry);
+  if (entry.code_offset != kFunctionEntryBytecodeOffset) {
+    raw_entries_.push_back(entry);
+  }
 #endif
 }
 
@@ -191,16 +193,26 @@ OwnedVector<byte> SourcePositionTableBuilder::ToSourcePositionTableVector() {
   return table;
 }
 
+void SourcePositionTableIterator::Initialize() {
+  Advance();
+  if (current_.code_offset == kFunctionEntryBytecodeOffset && !done()) {
+    // Set the function entry offset and advance the source position iterator to
+    // have the first 'real' value.
+    function_entry_offset_ = source_position().ScriptOffset();
+    Advance();
+  }
+}
+
 SourcePositionTableIterator::SourcePositionTableIterator(ByteArray byte_array,
                                                          IterationFilter filter)
     : raw_table_(VectorFromByteArray(byte_array)), filter_(filter) {
-  Advance();
+  Initialize();
 }
 
 SourcePositionTableIterator::SourcePositionTableIterator(
     Handle<ByteArray> byte_array, IterationFilter filter)
     : table_(byte_array), filter_(filter) {
-  Advance();
+  Initialize();
 #ifdef DEBUG
   // We can enable allocation because we keep the table in a handle.
   no_gc.Release();
@@ -210,7 +222,7 @@ SourcePositionTableIterator::SourcePositionTableIterator(
 SourcePositionTableIterator::SourcePositionTableIterator(
     Vector<const byte> bytes, IterationFilter filter)
     : raw_table_(bytes), filter_(filter) {
-  Advance();
+  Initialize();
 #ifdef DEBUG
   // We can enable allocation because the underlying vector does not move.
   no_gc.Release();
