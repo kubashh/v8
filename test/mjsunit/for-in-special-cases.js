df1e971d8a892cea1a08eea04b76e8794e158219
Shiyu Zhang <shiyu.zhang@intel.com>
2020-02-14 16:52:22 +0800

    [runtime] Handle element in for-in fast path that uses PrototypeInfo cache
    This can speed up the below micro-bench by 3x and improve JetStream2-tagcloud-SP case by ~2%.
    
    Object.prototype.foo = function() {};
    let array = ['a','b','c','d','e'];
    let start = Date.now();
    for (let i = 0; i < 1e5; i++) {
    	for (let j in array) {}
    }
    console.log(Date.now() - start);
    
    Contributed by tao.pan@intel.com
    
    Change-Id: I44c948c2e4c28b8e42192f36802a5ea0f82bbe25

--- test/mjsunit/for-in-special-cases.js
+++ test/mjsunit/for-in-special-cases.js
@@ -179,7 +179,7 @@ for_in_string_prototype();
   }
 })();
 
-(function for_in_prototype_change_element() {
+(function for_in_prototype_change_element1() {
   let prototype1 = {prop: 0, prop1: 1};
   let derived1 = {prop2: 2, prop3: 3};
 
@@ -196,6 +196,31 @@ for_in_string_prototype();
   derived1.__proto__ = {1: 1, 3: 3};
   for (let i = 0; i < 3; i++) {
     assertEquals(['prop2', 'prop3', '1', '3'], Accumulate(derived1));
+  }
+})();
+
+(function for_in_prototype_change_element2() {
+  Array.prototype.__proto__ = {'A': 1};
+  let array = ['a', 'b', 'c', 'd', 'e'];
+  for (let i = 0; i < 3; i++) {
+    assertEquals(['0', '1', '2', '3', '4', 'A'], Accumulate(array));
+  }
+  Array.prototype[10] = 'b';
+  for (let i = 0; i < 3; i++) {
+    assertEquals(['0', '1', '2', '3', '4', '10', 'A'], Accumulate(array));
+  }
+})();
+
+(function for_in_prototype_change_element3() {
+  let prototype = {prop: 0};
+  let holey_array = {1: 'a', 3: 'b'};
+  Object.setPrototypeOf(holey_array, prototype);
+  for (let i = 0; i < 3; i++) {
+    assertEquals(['1', '3', 'prop'], Accumulate(holey_array));
+  }
+  prototype[10] = 'b';
+  for (let i = 0; i < 3; i++) {
+    assertEquals(['1', '3', '10', 'prop'], Accumulate(holey_array));
   }
 })();
 
