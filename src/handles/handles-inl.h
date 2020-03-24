64d46eb5502ea9415b7f9d2bf96e721fcbccfaea
Dominik Inführ <dinfuehr@chromium.org>
2020-03-24 18:35:59 +0100

    [heap] Introduce LocalHandleScope for background threads
    Add LocalHandleScope to allow for local handles in LocalHeaps
    (background threads). This class is similar to HandleScope which still
    needs to be used on the main thread. When performing a GC, the main
    thread halts all background threads at a safepoint such that it can
    safely iterate their roots.
    
    Bug: v8:10315
    Change-Id: Id8f5d54cc2535e004081ccdef15dc03a39b2d0f0

--- src/handles/handles-inl.h
+++ src/handles/handles-inl.h
@@ -8,6 +8,8 @@
 #include "src/execution/isolate.h"
 #include "src/execution/off-thread-isolate.h"
 #include "src/handles/handles.h"
+#include "src/handles/local-handles-inl.h"
+#include "src/heap/local-heap.h"
 #include "src/sanitizer/msan.h"
 
 namespace v8 {
@@ -18,6 +20,9 @@ HandleBase::HandleBase(Address object, Isolate* isolate)
 
 HandleBase::HandleBase(Address object, OffThreadIsolate* isolate)
     : location_(isolate->NewHandle(object)) {}
+
+HandleBase::HandleBase(Address object, LocalHeap* local_heap)
+    : location_(LocalHandleScope::GetHandle(local_heap, object)) {}
 
 // Allocate a new handle for the object, do not canonicalize.
 
@@ -42,6 +47,10 @@ Handle<T>::Handle(T object, OffThreadIsolate* isolate)
     : HandleBase(object.ptr(), isolate) {}
 
 template <typename T>
+Handle<T>::Handle(T object, LocalHeap* local_heap)
+    : HandleBase(object.ptr(), local_heap) {}
+
+template <typename T>
 V8_INLINE Handle<T> handle(T object, Isolate* isolate) {
   return Handle<T>(object, isolate);
 }
@@ -49,6 +58,11 @@ V8_INLINE Handle<T> handle(T object, Isolate* isolate) {
 template <typename T>
 V8_INLINE Handle<T> handle(T object, OffThreadIsolate* isolate) {
   return Handle<T>(object, isolate);
+}
+
+template <typename T>
+V8_INLINE Handle<T> handle(T object, LocalHeap* local_heap) {
+  return Handle<T>(object, local_heap);
 }
 
 // Convenience overloads for when we already have a Handle, but want
@@ -60,6 +74,10 @@ V8_INLINE Handle<T> handle(Handle<T> handle, Isolate* isolate) {
 template <typename T>
 V8_INLINE Handle<T> handle(Handle<T> handle, OffThreadIsolate* isolate) {
   return Handle<T>(*handle);
+}
+template <typename T>
+V8_INLINE Handle<T> handle(Handle<T> handle, LocalHeap* local_heap) {
+  return Handle<T>(*handle, local_heap);
 }
 
 template <typename T>
