e029b6a971d7821f415717e3d58c266c3d42661f
Dominik Inführ <dinfuehr@chromium.org>
2020-03-24 11:36:42 +0100

    [heap] Introduce LocalHandleScope for background threads
    Add LocalHandleScope to allow for local handles in LocalHeaps
    (background threads). When performing a GC, the main thread halts all
    background threads at a safepoint such that it can safely iterate their
    roots.
    
    Bug: v8:10315
    Change-Id: Id8f5d54cc2535e004081ccdef15dc03a39b2d0f0

--- src/handles/handles.cc
+++ src/handles/handles.cc
@@ -197,5 +197,51 @@ std::unique_ptr<DeferredHandles> DeferredHandleScope::Detach() {
   return deferred;
 }
 
+LocalHandles::LocalHandles() { scope_.Initialize(); }
+
+void LocalHandles::Iterate(RootVisitor* visitor) {
+  for (int i = 0; i < static_cast<int>(blocks_.size()) - 1; i++) {
+    Address* block = blocks_[i];
+    visitor->VisitRootPointers(Root::kHandleScope, nullptr,
+                               FullObjectSlot(block),
+                               FullObjectSlot(&block[kHandleBlockSize]));
+  }
+
+  if (!blocks_.empty()) {
+    Address* block = blocks_.back();
+    visitor->VisitRootPointers(Root::kHandleScope, nullptr,
+                               FullObjectSlot(block),
+                               FullObjectSlot(scope_.next));
+  }
+}
+
+Address* LocalHandles::AddBlock() {
+  DCHECK_EQ(scope_.next, scope_.limit);
+  Address* block = NewArray<Address>(kHandleBlockSize);
+  blocks_.push_back(block);
+  scope_.next = block;
+  scope_.limit = block + kHandleBlockSize;
+  return block;
+}
+
+void LocalHandles::RemoveBlocks() {
+  while (!blocks_.empty()) {
+    Address* block_start = blocks_.back();
+    Address* block_limit = block_start + kHandleBlockSize;
+
+    if (block_limit == scope_.limit) {
+      break;
+    }
+
+    blocks_.pop_back();
+
+#ifdef ENABLE_HANDLE_ZAPPING
+    HandleScope::ZapRange(block_start, block_limit);
+#endif
+
+    DeleteArray(block_start);
+  }
+}
+
 }  // namespace internal
 }  // namespace v8
