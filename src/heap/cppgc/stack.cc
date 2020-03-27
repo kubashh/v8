6933b32c97e3e998314311be4694c6603a670f2f
Michael Lippautz <mlippautz@chromium.org>
2020-03-27 23:33:46 +0100

    cppgc: Conservative stack scanning
    Adds support for conservative stack scanning on x64. The trampolines
    saving callee-saved registers are compiled using clang (non-Windows)
    and MASM (Windows). This is using the default toolchain for assembly
    in Chromium/V8.
    
    This differs from Oilpan in Chromium where x86 and x64 are compiled
    using NASM [1]. V8 does not yet require this dependency and building
    the trampolines natively avoids it. (NASM also requires separate
    blocks for x64 Windows and non-Windows.) On non-x86/x64 platforms
    Chromium also uses clang, so there's little benefit in keeping the
    dependency.
    
    The trampolines are tested when building with clang.
    
    Other platforms follow in separate CLs.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/renderer/platform/heap/asm/SaveRegisters_x86.asm
    
    Change-Id: Ice2e23e44391aa94147abe75ee0b5afac458b8f8
    Bug: chromium:1056170

---
+++ src/heap/cppgc/stack.cc
@@ -0,0 +1,50 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/heap/cppgc/stack.h"
+
+#include <limits>
+
+#include "src/base/platform/platform.h"
+#include "src/heap/cppgc/globals.h"
+#include "src/heap/cppgc/sanitizers.h"
+
+namespace cppgc {
+namespace internal {
+
+using IterateStackCallback = void (Stack::*)(StackVisitor*, intptr_t*) const;
+extern "C" void PushAllRegistersAndIterateStack(const Stack*, StackVisitor*,
+                                                IterateStackCallback);
+
+Stack::Stack(void* stack_start) : stack_start_(stack_start) {}
+
+bool Stack::IsOnStack(void* slot) const {
+  void* raw_slot = v8::base::Stack::GetStackSlot(slot);
+  return v8::base::Stack::GetCurrentStackPosition() <= raw_slot &&
+         raw_slot <= stack_start_;
+}
+
+#ifdef V8_TARGET_ARCH_X64
+void Stack::IteratePointers(StackVisitor* visitor) const {
+  PushAllRegistersAndIterateStack(this, visitor, &Stack::IteratePointersImpl);
+}
+#endif  // V8_TARGET_ARCH_X64
+
+// No ASAN support as method accesses redzones while walking the stack.
+NO_SANITIZE_ADDRESS
+void Stack::IteratePointersImpl(StackVisitor* visitor,
+                                intptr_t* stack_end) const {
+  uintptr_t* current = reinterpret_cast<uintptr_t*>(
+      reinterpret_cast<uintptr_t>(stack_end) & ~kAddressMask);
+  for (; current < stack_start_; ++current) {
+    uintptr_t address = *current;
+    if (address == 0) continue;
+    // TODO(mlippautz): MSAN unpoison |address| which allows working with
+    // uninitialized and poisoned stacks.
+    visitor->VisitPointer(reinterpret_cast<void*>(address));
+  }
+}
+
+}  // namespace internal
+}  // namespace cppgc
