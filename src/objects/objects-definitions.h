1d9d15fd61e73fcb3d3996af42d69dcf9cb3670c
Ng Zhi An <zhin@chromium.org>
2020-04-01 12:43:06 -0700

    Introduce WasmValue for debugging
    WasmValue holds a Wasm value with its type. This will be exposed to the
    inspector (via a to-be-created class in debug_interface.h) for debugging
    in DevTools.
    
    Design at http://doc/1XQlX6DWsv6BPYnRtw-JZSASPEjsRlyXLnke7TTQ9Wrg.
    
    Bug: v8:10347
    Change-Id: Ib523e617d46fdf1adb13d13bf49749c4ce23a126

--- src/objects/objects-definitions.h
+++ src/objects/objects-definitions.h
@@ -127,6 +127,7 @@ namespace internal {
     async_generator_request)                                                  \
   V(_, BREAK_POINT_TYPE, BreakPoint, break_point)                             \
   V(_, BREAK_POINT_INFO_TYPE, BreakPointInfo, break_point_info)               \
+  V(_, WASM_VALUE_TYPE, WasmValue, wasm_value)                                \
   V(_, CACHED_TEMPLATE_OBJECT_TYPE, CachedTemplateObject,                     \
     cached_template_object)                                                   \
   V(_, CLASS_POSITIONS_TYPE, ClassPositions, class_positions)                 \
