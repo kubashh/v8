02117e236f81bed4def6868cab1e59c0dac331ac
Shu-yu Guo <syg@chromium.org>
2020-04-07 16:59:13 -0700

    [weakrefs] Split out FinalizationRegistry#cleanupSome to a different flag
    Apple currently objects to cleanupSome but agrees to shipping the rest
    of WeakRefs. Separate out cleanupSome to its own flag so the rest of
    WeakRefs may ship.
    
    Bug: v8:8179
    Change-Id: I6159fc743c9cb658860d4260b0dcb95e54630fdc

--- test/mjsunit/harmony/weakrefs/basics.js
+++ test/mjsunit/harmony/weakrefs/basics.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --harmony-weak-refs
+// Flags: --harmony-weak-refs-with-cleanup-some
 
 (function TestConstructFinalizationRegistry() {
   let fg = new FinalizationRegistry(() => {});
