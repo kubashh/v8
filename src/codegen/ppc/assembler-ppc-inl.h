9fa632874d274434a8f5a2ba6ff7c57ac7611aa3
Milad Farazmand <miladfar@ca.ibm.com>
2020-04-16 14:38:26 +0000

    PPC: [wasm-simd] Implement Simd128 Load and Store
    Change-Id: I436c779613e7ddf4b5c830807414dbc8787b89f5

--- src/codegen/ppc/assembler-ppc-inl.h
+++ src/codegen/ppc/assembler-ppc-inl.h
@@ -48,7 +48,7 @@ namespace internal {
 
 bool CpuFeatures::SupportsOptimizer() { return true; }
 
-bool CpuFeatures::SupportsWasmSimd128() { return false; }
+bool CpuFeatures::SupportsWasmSimd128() { return true; }
 
 void RelocInfo::apply(intptr_t delta) {
   // absolute code pointer inside code object moves with the code object.
