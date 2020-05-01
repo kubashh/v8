ba2d457d185e6a89ad59ee6c0089f919734fd318
Santiago Aboy Solanes <solanes@chromium.org>
2020-05-01 15:49:08 +0100

    [compiler] Push up code to revisit uses in in-place replacements
    If a node is reduced in-place (i.e not replaced by another node) we
    check its inputs, and if we Recurse on at least one input we return
    early. If this happens, we weren't revisiting its uses.
    
    This CL changes this since we could have been missing revisiting of some
    uses.
    
    Change-Id: I7683a0747cec38484a047c6032980b5676b2d886

--- src/compiler/graph-reducer.cc
+++ src/compiler/graph-reducer.cc
@@ -161,6 +161,12 @@ void GraphReducer::ReduceTop() {
   // Check if the reduction is an in-place update of the {node}.
   Node* const replacement = reduction.replacement();
   if (replacement == node) {
+    // Revisit all uses of the node.
+    for (Node* const user : node->uses()) {
+      // Don't revisit this node if it refers to itself.
+      if (user != node) Revisit(user);
+    }
+
     // In-place update of {node}, may need to recurse on an input.
     Node::Inputs node_inputs = node->inputs();
     for (int i = 0; i < node_inputs.count(); ++i) {
@@ -178,12 +184,6 @@ void GraphReducer::ReduceTop() {
   // Check if we have a new replacement.
   if (replacement != node) {
     Replace(node, replacement, max_id);
-  } else {
-    // Revisit all uses of the node.
-    for (Node* const user : node->uses()) {
-      // Don't revisit this node if it refers to itself.
-      if (user != node) Revisit(user);
-    }
   }
 }
 
