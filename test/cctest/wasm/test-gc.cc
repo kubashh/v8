bcaf2636a0d55bb358b67f4b132ebf1986159af4
Manos Koukoutos <manoskouk@chromium.org>
2020-05-19 15:10:33 +0000

    [wasm-gc] Implement 'let' opcode.
    Changes:
    - Implement the 'let' opcode, as per
    https://github.com/WebAssembly/function-references/blob/master/proposals/function-references/Overview.md#local-bindings
    - Change ControlBase to accept an additional 'locals_count' parameter.
    - Implement required test infrastructure and write some simple tests.
    
    Bug: v8:7748
    Change-Id: I39d60d1f0c26016c8f89c009dc5f4119b0c73c87

--- test/cctest/wasm/test-gc.cc
+++ test/cctest/wasm/test-gc.cc
@@ -158,6 +158,50 @@ WASM_EXEC_TEST(BasicStruct) {
   n->EmitCode(n_code, sizeof(n_code));
   // Result: 0b1001
 
+  // Test 'let'
+  WasmFunctionBuilder* let_test_1 = builder->AddFunction(sigs.i_v());
+  let_test_1->builder()->AddExport(CStrVector("let_test_1"), let_test_1);
+  uint32_t let_local_index = 0;
+  uint32_t let_field_index = 0;
+  byte let_code[] = {
+      WASM_LET_1_I(WASM_REF_TYPE(type_index),
+                   WASM_STRUCT_NEW(type_index, WASM_I32V(42), WASM_I32V(52)),
+                   WASM_STRUCT_GET(type_index, let_field_index,
+                                   WASM_GET_LOCAL(let_local_index))),
+      kExprEnd};
+  let_test_1->EmitCode(let_code, sizeof(let_code));
+
+  WasmFunctionBuilder* let_test_2 = builder->AddFunction(sigs.i_v());
+  let_test_2->builder()->AddExport(CStrVector("let_test_2"), let_test_2);
+  uint32_t let_2_field_index = 0;
+  byte let_code_2[] = {
+      WASM_LET_2_I(WASM_I32_TYPE, WASM_I32_ADD(WASM_I32V(42), WASM_I32V(-32)),
+                   WASM_REF_TYPE(type_index),
+                   WASM_STRUCT_NEW(type_index, WASM_I32V(42), WASM_I32V(52)),
+                   WASM_I32_MUL(WASM_STRUCT_GET(type_index, let_2_field_index,
+                                                WASM_GET_LOCAL(1)),
+                                WASM_GET_LOCAL(0))),
+      kExprEnd};
+  let_test_2->EmitCode(let_code_2, sizeof(let_code_2));
+
+  WasmFunctionBuilder* let_test_locals = builder->AddFunction(sigs.i_i());
+  let_test_locals->builder()->AddExport(CStrVector("let_test_locals"),
+                                        let_test_locals);
+  let_test_locals->AddLocal(kWasmI32);
+  byte let_code_locals[] = {
+      WASM_SET_LOCAL(1, WASM_I32V(100)),
+      WASM_LET_2_I(
+          WASM_I32_TYPE, WASM_I32V(1), WASM_I32_TYPE, WASM_I32V(10),
+          WASM_I32_SUB(WASM_I32_ADD(WASM_GET_LOCAL(0),     // 1st let-local
+                                    WASM_GET_LOCAL(2)),    // Parameter
+                       WASM_I32_ADD(WASM_GET_LOCAL(1),     // 2nd let-local
+                                    WASM_GET_LOCAL(3)))),  // Function local
+      kExprEnd};
+  // Result: (1 + 1000) - (10 + 100) = 891
+  let_test_locals->EmitCode(let_code_locals, sizeof(let_code_locals));
+
+  /************************* End of test definitions *************************/
+
   ZoneBuffer buffer(&zone);
   builder->WriteTo(&buffer);
 
@@ -165,10 +209,11 @@ WASM_EXEC_TEST(BasicStruct) {
   HandleScope scope(isolate);
   testing::SetupIsolateForWasmModule(isolate);
   ErrorThrower thrower(isolate, "Test");
-  Handle<WasmInstanceObject> instance =
+  MaybeHandle<WasmInstanceObject> maybe_instance =
       testing::CompileAndInstantiateForTesting(
-          isolate, &thrower, ModuleWireBytes(buffer.begin(), buffer.end()))
-          .ToHandleChecked();
+          isolate, &thrower, ModuleWireBytes(buffer.begin(), buffer.end()));
+  if (thrower.error()) FATAL("%s", thrower.error_msg());
+  Handle<WasmInstanceObject> instance = maybe_instance.ToHandleChecked();
 
   CHECK_EQ(42, testing::CallWasmFunctionForTesting(isolate, instance, &thrower,
                                                    "f", 0, nullptr));
@@ -200,6 +245,17 @@ WASM_EXEC_TEST(BasicStruct) {
 
   CHECK_EQ(0b1001, testing::CallWasmFunctionForTesting(
                        isolate, instance, &thrower, "n", 0, nullptr));
+
+  CHECK_EQ(42, testing::CallWasmFunctionForTesting(isolate, instance, &thrower,
+                                                   "let_test_1", 0, nullptr));
+
+  CHECK_EQ(420, testing::CallWasmFunctionForTesting(isolate, instance, &thrower,
+                                                    "let_test_2", 0, nullptr));
+
+  Handle<Object> let_local_args[] = {handle(Smi::FromInt(1000), isolate)};
+  CHECK_EQ(891, testing::CallWasmFunctionForTesting(isolate, instance, &thrower,
+                                                    "let_test_locals", 1,
+                                                    let_local_args));
 }
 
 WASM_EXEC_TEST(BasicArray) {
