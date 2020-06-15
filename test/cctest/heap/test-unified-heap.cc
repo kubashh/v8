52bd6d74a24c4765c1bc415ce95be8124c08ef08
Michael Lippautz <mlippautz@chromium.org>
2020-06-15 19:00:20 +0200

    cppgc-js: Implement atomic unified heap GC
    Add v8::CppHeap as an implementation of a cppgc heap that
    integrates with V8's existing EmbedderHeapTracer API. The
    current implementation only support non-incremental marking.
    
    Bug: chromium:1056170
    Change-Id: I4a09eb5ae57f5c7defe35eb3fe346627eb492473

---
+++ test/cctest/heap/test-unified-heap.cc
@@ -0,0 +1,103 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "include/cppgc/allocation.h"
+#include "include/cppgc/garbage-collected.h"
+#include "include/cppgc/platform.h"
+#include "src/api/api-inl.h"
+#include "src/heap/cppgc-js/cpp-heap.h"
+#include "src/objects/objects-inl.h"
+#include "test/cctest/cctest.h"
+
+namespace v8 {
+namespace internal {
+
+namespace {
+
+v8::Local<v8::Object> ConstructTraceableJSApiObject(
+    v8::Local<v8::Context> context, void* first_field, void* second_field) {
+  v8::EscapableHandleScope scope(context->GetIsolate());
+  v8::Local<v8::FunctionTemplate> function_t =
+      v8::FunctionTemplate::New(context->GetIsolate());
+  v8::Local<v8::ObjectTemplate> instance_t = function_t->InstanceTemplate();
+  instance_t->SetInternalFieldCount(2);
+  v8::Local<v8::Function> function =
+      function_t->GetFunction(context).ToLocalChecked();
+  v8::Local<v8::Object> instance =
+      function->NewInstance(context).ToLocalChecked();
+  instance->SetAlignedPointerInInternalField(0, first_field);
+  instance->SetAlignedPointerInInternalField(1, second_field);
+  CHECK(!instance.IsEmpty());
+  i::Handle<i::JSReceiver> js_obj = v8::Utils::OpenHandle(*instance);
+  CHECK_EQ(i::JS_API_OBJECT_TYPE, js_obj->map().instance_type());
+  return scope.Escape(instance);
+}
+
+void ResetWrappableConnection(v8::Local<v8::Object> api_object) {
+  api_object->SetAlignedPointerInInternalField(0, nullptr);
+  api_object->SetAlignedPointerInInternalField(1, nullptr);
+}
+
+class UnifiedHeapEnvironment final {
+ public:
+  UnifiedHeapEnvironment()
+      : saved_incremental_marking_wrappers_(FLAG_incremental_marking_wrappers) {
+    FLAG_incremental_marking_wrappers = false;
+    cppgc::InitializeProcess(V8::GetCurrentPlatform()->GetPageAllocator());
+    cpp_heap_ = std::make_unique<CppHeap>(CcTest::isolate(), 0);
+    CcTest::i_isolate()->heap()->SetEmbedderHeapTracer(&cpp_heap());
+  }
+
+  ~UnifiedHeapEnvironment() {
+    CcTest::i_isolate()->heap()->SetEmbedderHeapTracer(nullptr);
+    FLAG_incremental_marking_wrappers = saved_incremental_marking_wrappers_;
+    cppgc::ShutdownProcess();
+  }
+
+  CppHeap& cpp_heap() const { return *cpp_heap_.get(); }
+
+  cppgc::AllocationHandle& allocation_handle() {
+    return cpp_heap().object_allocator();
+  }
+
+ private:
+  std::unique_ptr<CppHeap> cpp_heap_;
+  bool saved_incremental_marking_wrappers_;
+};
+
+class Wrappable final : public cppgc::GarbageCollected<Wrappable> {
+ public:
+  static size_t destructor_callcount;
+
+  ~Wrappable() { destructor_callcount++; }
+
+  void Trace(cppgc::Visitor* visitor) const {}
+};
+
+size_t Wrappable::destructor_callcount = 0;
+
+}  // namespace
+
+TEST(UnifiedHeapFindsV8ToBlinkReference) {
+  UnifiedHeapEnvironment env;
+  v8::HandleScope scope(CcTest::isolate());
+  v8::Local<v8::Context> context = v8::Context::New(CcTest::isolate());
+  v8::Context::Scope context_scope(context);
+  auto* wrappable =
+      cppgc::MakeGarbageCollected<Wrappable>(env.allocation_handle());
+  v8::Local<v8::Object> api_object =
+      ConstructTraceableJSApiObject(context, wrappable, wrappable);
+  CHECK(!api_object.IsEmpty());
+  wrappable = reinterpret_cast<Wrappable*>(
+      api_object->GetAlignedPointerFromInternalField(0));
+  CHECK_EQ(0u, Wrappable::destructor_callcount);
+  CcTest::CollectGarbage(OLD_SPACE);
+  CHECK_EQ(0u, Wrappable::destructor_callcount);
+  ResetWrappableConnection(api_object);
+  CcTest::CollectGarbage(OLD_SPACE);
+  CHECK_EQ(1u, Wrappable::destructor_callcount);
+}
+
+}  // namespace internal
+}  // namespace v8
