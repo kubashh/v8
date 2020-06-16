e9ccef799c48cc668b90353285d15a2738c65721
johnx <johnx@google.com>
2020-06-16 19:34:02 +0800

    Demo CL for platform poem usage
    Bug: 156155426
    Change-Id: Ic00c64e7f39f539fdc340e317e755e43fba6fe9d

--- src/base/small-vector.h
+++ src/base/small-vector.h
@@ -12,6 +12,17 @@
 #include "src/base/bits.h"
 #include "src/base/macros.h"
 
+#if defined(STARBOARD)
+// These common Starboard API replacements are not needed for evergreen but
+// some builds can not find definitions for free/malloc above Starboard.
+#include "starboard/memory.h"
+#include "starboard/string.h"
+
+#define malloc(x) SbMemoryAllocate(x)
+#define free(x) SbMemoryDeallocate(x)
+#define memcpy(x, y, z) SbMemoryCopy(x, y, z)
+#endif
+
 namespace v8 {
 namespace base {
 
@@ -170,5 +181,11 @@ class SmallVector {
 
 }  // namespace base
 }  // namespace v8
+
+#if defined(STARBOARD)
+#undef malloc
+#undef free
+#undef memcpy
+#endif
 
 #endif  // V8_BASE_SMALL_VECTOR_H_
