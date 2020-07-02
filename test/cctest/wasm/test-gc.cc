25f423488ae8f3553cfd757e4c7dc7a113bf909f
Manos Koukoutos <manoskouk@chromium.org>
2020-07-02 08:59:39 +0000

    [wasm-gc] Test improvements
    - Adapt subtyping unittest to the new HeapType API
    - Generalize WASM_REF_TYPE macro
    
    Bug: v8:7748
    Change-Id: Ifdb24408d7a34a6470ddccdf3dc8536cbb93dece

--- test/cctest/wasm/test-gc.cc
+++ test/cctest/wasm/test-gc.cc
@@ -208,18 +208,17 @@ TEST(WasmRefAsNonNull) {
   ValueType kOptRefType = optref(type_index);
   FunctionSig sig_q_v(1, 0, kRefTypes);
 
-  uint32_t k_global_index = tester.AddGlobal(
+  uint32_t global_index = tester.AddGlobal(
       kOptRefType, true, WasmInitExpr(WasmInitExpr::kRefNullConst));
-  uint32_t k_field_index = 0;
+  uint32_t field_index = 0;
   tester.DefineFunction(
       "f", tester.sigs.i_v(), {},
-      {WASM_SET_GLOBAL(
-           k_global_index,
-           WASM_STRUCT_NEW(type_index, WASM_I32V(55), WASM_I32V(66))),
+      {WASM_SET_GLOBAL(global_index, WASM_STRUCT_NEW(type_index, WASM_I32V(55),
+                                                     WASM_I32V(66))),
        WASM_STRUCT_GET(
-           type_index, k_field_index,
+           type_index, field_index,
            WASM_REF_AS_NON_NULL(WASM_IF_ELSE_R(kOptRefType, WASM_I32V(1),
-                                               WASM_GET_GLOBAL(k_global_index),
+                                               WASM_GET_GLOBAL(global_index),
                                                WASM_REF_NULL(type_index)))),
        kExprEnd});
 
@@ -380,7 +379,7 @@ TEST(WasmLetInstruction) {
   uint32_t let_field_index = 0;
   tester.DefineFunction(
       "let_test_1", tester.sigs.i_v(), {},
-      {WASM_LET_1_I(WASM_REF_TYPE(type_index),
+      {WASM_LET_1_I(WASM_SEQ(kLocalRef, type_index),
                     WASM_STRUCT_NEW(type_index, WASM_I32V(42), WASM_I32V(52)),
                     WASM_STRUCT_GET(type_index, let_field_index,
                                     WASM_GET_LOCAL(let_local_index))),
@@ -390,7 +389,7 @@ TEST(WasmLetInstruction) {
   tester.DefineFunction(
       "let_test_2", tester.sigs.i_v(), {},
       {WASM_LET_2_I(kLocalI32, WASM_I32_ADD(WASM_I32V(42), WASM_I32V(-32)),
-                    WASM_REF_TYPE(type_index),
+                    WASM_SEQ(kLocalRef, type_index),
                     WASM_STRUCT_NEW(type_index, WASM_I32V(42), WASM_I32V(52)),
                     WASM_I32_MUL(WASM_STRUCT_GET(type_index, let_2_field_index,
                                                  WASM_GET_LOCAL(1)),
