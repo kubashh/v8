501d4b240d695eb160550c121eac785deae0c65e
Zeynep Cankara <zcankara@google.com>
2020-07-03 11:54:06 +0100

    [tools][system-analyzer] Support time logging for IC Events
    This CL does attempt to track time of IC event entries.
    Implement a timeline view UI for IC Events.
    
    Change-Id: I2a0f652e2657bdebe8cecd7862a7545f7b050cdb

--- tools/system-analyzer/ic-panel.mjs
+++ tools/system-analyzer/ic-panel.mjs
@@ -12,11 +12,18 @@ defineCustomElement('ic-panel', (templateText) =>
     shadowRoot.innerHTML = templateText;
     this.groupKeySelect.addEventListener(
         'change', e => this.updateTable(e));
+    /*
     this.filterICBtnSelect.addEventListener(
         'click', e => this.handleICFilter(e));
+    */
+    this.$('#filterICTimeBtn').addEventListener(
+      'click', e => this.handleICTimeFilter(e));
     this._noOfItems = 100;
+    this._startTime = 0;
+    this._endTime = 0;
   }
 
+  // DOM item selector
   $(id) {
     return this.shadowRoot.querySelector(id);
   }
@@ -28,10 +35,11 @@ defineCustomElement('ic-panel', (templateText) =>
   get groupKeySelect() {
     return this.$('#group-key');
   }
-
+  /*
   get filterICBtnSelect() {
     return this.$('#filterICBtn');
   }
+  */
 
   get tableSelect() {
     return this.$('#table');
@@ -49,12 +57,48 @@ defineCustomElement('ic-panel', (templateText) =>
     return this.querySelectorAll("span");
   }
 
+  // init
+  receiveData(entries){
+    this.fileEntries = entries;
+    this.entries = entries;
+    this.updateTable();
+  }
+
+  // file entries
+  set fileEntries(value){
+    this._fileEntries = value;
+  }
+
+  get fileEntries(){
+    return this._fileEntries;
+  }
+
+  set entries(value){
+    this._entries = value;
+  }
+
+  get entries(){
+    return this._entries;
+  }
+
+  filterEntriesByTime(){
+    let newEntries = [];
+    for (let entry of this.fileEntries) {
+      if(entry.time >= this._startTime && entry.time <= this._endTime){
+        newEntries.push(entry);
+      }
+    }
+    this.entries = newEntries;
+  }
+
   updateTable(event) {
     let select = this.groupKeySelect;
     let key = select.options[select.selectedIndex].text;
     let tableBody = this.tableBodySelect;
     this.removeAllChildren(tableBody);
-    let groups = Group.groupBy(entries, key, true);
+    //this.processTimelineIC(entries);
+    let groups = Group.groupBy(this.entries, key, true);
+    console.log(groups);
     this.display(groups, tableBody);
   }
 
@@ -102,10 +146,9 @@ defineCustomElement('ic-panel', (templateText) =>
     return this._noOfItems;
   }
 
-
   display(entries, parent) {
     let fragment = document.createDocumentFragment();
-    let max = Math.min(this.noOfItems, entries.length)
+    let max = Math.min(100, entries.length)
     for (let i = 0; i < max; i++) {
       let entry = entries[i];
       let tr = document.createElement("tr");
@@ -126,6 +169,7 @@ defineCustomElement('ic-panel', (templateText) =>
     }
     parent.appendChild(fragment);
   }
+
 
   displayDrilldown(entry, previousSibling) {
     let tr = document.createElement('tr');
@@ -183,14 +227,25 @@ defineCustomElement('ic-panel', (templateText) =>
     }
   }
 
+  //TODO(ZC): Emit event if the address has a valid V8-Map state
+  sendMapClickedEvent(entry){
+    let dataModel = Object.create(null);
+    let selectedMap = V8Map.get("0x" + entry.map);
+    if(selectedMap){
+      dataModel.map = selectedMap;
+    }
+    this.dispatchEvent(new CustomEvent(
+      'click', {bubbles: true, composed: true, detail: dataModel}));
+  }
+
+
   //TODO(zc): Function processing the timestamps of ICEvents
   // Processes the IC Events which have V8Map's in the map-processor
+  /*
   processICEventTime(){
     let ICTimeToEvent = new Map();
-    // save the occurance time of V8Maps
     let eventTimes = []
-    console.log("Num of stats: " + entries.length);
-    // fetch V8 Maps from the IC Events
+    console.log("Num of entries: " + entries.length);
     entries.forEach(element => {
       let v8Map = V8Map.get("0x" + element.map);
       if(!v8Map){
@@ -208,10 +263,46 @@ defineCustomElement('ic-panel', (templateText) =>
     }
     return eventList;
   }
+  */
 
+  /*
   handleICFilter(e){
     let noOfItemsInput = parseInt(this.$('#filter-input').value);
     this.noOfItems = noOfItemsInput;
     this.updateTable(e);
   }
+  */
+
+   handleICTimeFilter(e) {
+    this._startTime = parseInt(this.$('#filter-time-start').value);
+    console.assert(this._startTime > 0, { errorMsg: "start time must be a non-negative integer!" });
+    this._endTime = parseInt(this.$('#filter-time-end').value);
+    console.assert(this._endTime <= this._fileEntries[this._fileEntries.length - 1].time,
+      { errorMsg: "end time must be smaller or equal to the the time of the last event!" });
+    console.assert(this._startTime > this._fileEntries[this._fileEntries.length - 1],
+      { errorMsg: "end time must be smaller than the start time!" });
+    this.filterEntriesByTime();
+    this.updateTable(e);
+  }
+
+  // process IC events and display in a Timeline view
+  //TODO(zc): new functionality
+  /*
+  processTimelineIC(entries){
+    let parent = this.$('#details')
+    let marginLeft = 0;
+    for (let index = 0; index < entries.length; index++) {
+      const entry = entries[index];
+      let div = document.createElement("div");
+      div.className = 'TL-cell';
+      div.style = "width:150px; margin-left:0px;";
+      let entryDetails = "type: " + entry.type + " category: " + entry.category + ' (' + entry.oldState + '->' + entry.newState + ') ' +
+      ' (map: 0x' + entry.map.toString(16) + ')' + " key: " + entry.key;
+      //console.log(entry);
+      div.innerHTML = entryDetails;
+      marginLeft += 100;
+      parent.append(div);
+    }
+  }
+  */
 });
