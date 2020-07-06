b9e622eee1afa5a95fe7e11986493cb1e7f04fcf
Zeynep Cankara <zcankara@google.com>
2020-07-06 05:41:30 +0100

    [tools][system-analyzer] Support time logging for IC Events
    This CL does attempt to track time of IC event entries.
    Implement a timeline view UI for IC Events.
    
    Change-Id: I2a0f652e2657bdebe8cecd7862a7545f7b050cdb

--- tools/system-analyzer/timeline-panel.mjs
+++ tools/system-analyzer/timeline-panel.mjs
@@ -9,9 +9,17 @@ defineCustomElement('timeline-panel', (templateText) =>
     const shadowRoot = this.attachShadow({mode: 'open'});
     shadowRoot.innerHTML = templateText;
     this.timelineOverviewSelect.addEventListener(
-        'mousemove', e => this.handleTimelineIndicatorMove(e));
+      'mousemove', e => this.handleTimelineIndicatorMove(e));
+    //TODO(zc) trigger resize event
+    this.$('div.leftMask').addEventListener(
+      'mousedown', e => this.handleTimelineIndicatorResizeMouseDown(e));
+    this.$('div.rightMask').addEventListener(
+      'mousedown', e => this.handleTimelineIndicatorResizeMouseDown(e));
+    window.addEventListener(
+      'mousemove', e => this.handleTimelineIndicatorResizeMouseMove(e));
+    window.addEventListener(
+      'mouseup', e => this.handleTimelineIndicatorResizeMouseUp(e));
   }
-
 
   $(id) {
     return this.shadowRoot.querySelector(id);
@@ -49,4 +57,44 @@ defineCustomElement('timeline-panel', (templateText) =>
     this.timelineSelect.scrollLeft += event.movementX / factor;
   }
 
+  handleTimelineIndicatorResizeMouseDown(e) {
+    const timelineOverviewIndicator = this.timelineOverviewIndicatorSelect;
+    if (e.buttons == 0) return;
+    console.log("resize initiated");
+    this.resize = true;
+    let original_width = parseFloat(timelineOverviewIndicator.clientWidth);
+    let original_mouse_x = e.pageX;
+    let original_x = timelineOverviewIndicator.getBoundingClientRect().left;
+    let width = original_width + (e.pageX - original_mouse_x);
+    console.log("original_width: ", original_width, " original_mouse_x: ", original_mouse_x, " original_x: ", original_x, " width: ", width);
+  }
+
+  handleTimelineIndicatorResizeMouseMove(e) {
+    const timelineOverviewIndicator = this.timelineOverviewIndicatorSelect;
+    if (this.resize === true) {
+      console.log("resize continue");
+    }
+  }
+
+  handleTimelineIndicatorResizeMouseUp(e) {
+    const timelineOverviewIndicator = this.timelineOverviewIndicatorSelect;
+    let original_width = parseFloat(timelineOverviewIndicator.clientWidth);
+    let original_mouse_x = e.pageX;
+    let original_x = timelineOverviewIndicator.getBoundingClientRect().left;
+    const width = original_width + (e.pageX - original_mouse_x);
+    if (this.resize === true) {
+      console.log("end resize");
+      //TODO(zc) It automatically resets itself...
+      timelineOverviewIndicator.style.width = width + 100 + 'px';
+      this.resize = false;
+    }
+  }
+
 });
+
+
+
+
+
+
+
