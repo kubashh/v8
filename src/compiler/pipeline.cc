efe6a6aa7777de55ae67a8df20e4a7f5693325a6
Santiago Aboy Solanes <solanes@chromium.org>
2020-07-08 16:14:06 +0100

    [compiler] Don't serialize BytecodeArrayData's source_positions_
    This CL adds functionality to read the source positions directly
    from the JS heap rather than from serialized data.
    
    In order to do this, we create a PersistentHandles container in the
    OptimizedCompilationInfo which gets passed onto the JSHeapBroker. This
    allows us to create the handles in the main thread and pass them safely
    to the background thread.
    
    In order to read safely from the background thread, we need a LocalHeap
    which blocks the GC from running and potentially moving the handles.
    This LocalHeap is created only when the JSHeapBroker has finalized
    serializing and destroyed when retiring it.
    
    Bug: v8:7790
    Change-Id: I19f8b08d12e5be0a3df34d6af2043310c0c7b6fe

--- src/compiler/pipeline.cc
+++ src/compiler/pipeline.cc
@@ -152,7 +152,8 @@ class PipelineData {
         codegen_zone_(codegen_zone_scope_.zone()),
         broker_(new JSHeapBroker(
             isolate_, info_->zone(), info_->trace_heap_broker(),
-            is_concurrent_inlining, info->native_context_independent())),
+            is_concurrent_inlining, info->native_context_independent(),
+            info->persistent_handles())),
         register_allocation_zone_scope_(zone_stats_,
                                         kRegisterAllocationZoneName),
         register_allocation_zone_(register_allocation_zone_scope_.zone()),
@@ -1125,7 +1126,8 @@ PipelineCompilationJob::Status PipelineCompilationJob::ExecuteJobImpl(
   // Ensure that the RuntimeCallStats table is only available during execution
   // and not during finalization as that might be on a different thread.
   PipelineJobScope scope(&data_, stats);
-  if (data_.broker()->is_concurrent_inlining()) {
+  const bool concurrent_inlining = data_.broker()->is_concurrent_inlining();
+  if (concurrent_inlining) {
     if (!pipeline_.CreateGraph()) {
       return AbortOptimization(BailoutReason::kGraphBuildingFailed);
     }
