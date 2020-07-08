e0c8dc23ba5705021a5c2dcbd359b82abc524fbf
Omer Katz <omerkatz@chromium.org>
2020-07-08 16:33:32 +0200

    cppgc: Use object start bitmap to trace mixins
    This CL removes the GetTraceDescriptor virtual call from garbage
    collected mixins and replaces it with querying the object start
    bitmap.
    
    The CL also removes the mixin macros which are now no longer needed.
    
    Bug: chromium:1056170
    Change-Id: I27ed299f93025d09a3bb3f0d17b14bed3c200565

--- src/heap/cppgc/marking-state.h
+++ src/heap/cppgc/marking-state.h
@@ -5,12 +5,12 @@
 #ifndef V8_HEAP_CPPGC_MARKING_STATE_H_
 #define V8_HEAP_CPPGC_MARKING_STATE_H_
 
-#include "include/cppgc/trace-trait.h"
 #include "src/heap/cppgc/globals.h"
 #include "src/heap/cppgc/heap-object-header.h"
 #include "src/heap/cppgc/heap-page.h"
 #include "src/heap/cppgc/liveness-broker.h"
 #include "src/heap/cppgc/marking-worklists.h"
+#include "src/heap/cppgc/trace-trait.h"
 
 namespace cppgc {
 namespace internal {
@@ -71,13 +71,6 @@ MarkingState::MarkingState(
 
 void MarkingState::MarkAndPush(const void* object, TraceDescriptor desc) {
   DCHECK_NOT_NULL(object);
-  if (desc.base_object_payload ==
-      cppgc::GarbageCollectedMixin::kNotFullyConstructedObject) {
-    // This means that the objects are not-yet-fully-constructed. See comments
-    // on GarbageCollectedMixin for how those objects are handled.
-    not_fully_constructed_worklist_.Push(object);
-    return;
-  }
   MarkAndPush(HeapObjectHeader::FromPayload(
                   const_cast<void*>(desc.base_object_payload)),
               desc);
@@ -128,9 +121,7 @@ void MarkingState::RegisterWeakReferenceIfNeeded(const void* object,
   // Filter out already marked values. The write barrier for WeakMember
   // ensures that any newly set value after this point is kept alive and does
   // not require the callback.
-  if (desc.base_object_payload !=
-          cppgc::GarbageCollectedMixin::kNotFullyConstructedObject &&
-      HeapObjectHeader::FromPayload(desc.base_object_payload)
+  if (HeapObjectHeader::FromPayload(desc.base_object_payload)
           .IsMarked<HeapObjectHeader::AccessMode::kAtomic>())
     return;
   RegisterWeakCallback(weak_callback, parameter);
@@ -140,12 +131,6 @@ void MarkingState::InvokeWeakRootsCallbackIfNeeded(const void* object,
                                                    TraceDescriptor desc,
                                                    WeakCallback weak_callback,
                                                    const void* parameter) {
-  if (desc.base_object_payload ==
-      cppgc::GarbageCollectedMixin::kNotFullyConstructedObject) {
-    // This method is only called at the end of marking. If the object is in
-    // construction, then it should be reachable from the stack.
-    return;
-  }
   // Since weak roots are only traced at the end of marking, we can execute
   // the callback instead of registering it.
   weak_callback(LivenessBrokerFactory::Create(), parameter);
