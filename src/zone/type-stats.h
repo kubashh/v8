49d8581dd52d31931f2afe48b58ed2ba8915911d
Igor Sheludko <ishell@chromium.org>
2020-07-08 18:13:03 +0200

    WIP: [zone-stats] Collect per-object-type zone stats
    Bug: v8:10572
    Change-Id: I938d9e264cfe6a8b63a89db87d187d8e2be63c8b

---
+++ src/zone/type-stats.h
@@ -0,0 +1,48 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_ZONE_TYPE_STATS_H_
+#define V8_ZONE_TYPE_STATS_H_
+
+#include <iosfwd>
+#include <typeindex>
+#include <unordered_map>
+
+#include "src/common/globals.h"
+
+namespace v8 {
+namespace internal {
+
+class TypeStats;
+
+#ifdef V8_ENABLE_PRECISE_ZONE_STATS
+
+class TypeStats {
+ public:
+  TypeStats() = default;
+
+  // Adds given delta to type_id's type.
+  void Add(std::type_index type_id, size_t delta) { map_[type_id] += delta; }
+
+  // Merges other stats into this stats object.
+  void MergeWith(const TypeStats& other);
+
+  // Prints recorded statisticts to stdout.
+  void Dump() const;
+
+  // Prints recorded statisticts to the stream as JSON array:
+  // [ {type: XXX, size: YYY}, ..., {type: XXX, size: YYY} ].
+  void DumpJSON(std::ostringstream& out) const;
+
+ private:
+  using HashMap = std::unordered_map<std::type_index, size_t>;
+  HashMap map_;
+};
+
+#endif  // V8_ENABLE_PRECISE_ZONE_STATS
+
+}  // namespace internal
+}  // namespace v8
+
+#endif  // V8_ZONE_TYPE_STATS_H_
