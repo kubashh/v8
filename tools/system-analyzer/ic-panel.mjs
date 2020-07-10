a826e3adb679c24847c5a8449421f9287f9ec003
Zeynep Cankara <zcankara@google.com>
2020-07-10 17:55:45 +0100

    [tools][system-analyzer] Support time logging for IC Events
    This CL does attempt to track time of IC event entries.
    Implement a timeline view UI for IC Events.
    
    Bug: v8:10667
    
    Change-Id: I2a0f652e2657bdebe8cecd7862a7545f7b050cdb

--- tools/system-analyzer/ic-panel.mjs
+++ tools/system-analyzer/ic-panel.mjs
@@ -12,11 +12,14 @@ defineCustomElement('ic-panel', (templateText) =>
     shadowRoot.innerHTML = templateText;
     this.groupKeySelect.addEventListener(
         'change', e => this.updateTable(e));
-    this.filterICBtnSelect.addEventListener(
-        'click', e => this.handleICFilter(e));
+    this.$('#filterICTimeBtn').addEventListener(
+      'click', e => this.handleICTimeFilter(e));
     this._noOfItems = 100;
+    this._startTime = 0;
+    this._endTime = 0;
   }
 
+  // DOM item selector
   $(id) {
     return this.shadowRoot.querySelector(id);
   }
@@ -27,10 +30,6 @@ defineCustomElement('ic-panel', (templateText) =>
 
   get groupKeySelect() {
     return this.$('#group-key');
-  }
-
-  get filterICBtnSelect() {
-    return this.$('#filterICBtn');
   }
 
   get tableSelect() {
@@ -49,13 +48,44 @@ defineCustomElement('ic-panel', (templateText) =>
     return this.querySelectorAll("span");
   }
 
-  updateTable(event) {
+  // init
+  receiveData(entries) {
+    this.filteredEntries = entries;
+    this.entries = entries;
+    this.updateTable();
+  }
+
+  // file entries
+  set filteredEntries(value){
+    this._filteredEntries = value;
+  }
+
+  get filteredEntries(){
+    return this._filteredEntries;
+  }
+
+  set entries(value){
+    this._entries = value;
+  }
+
+  get entries(){
+    return this._entries;
+  }
+
+   filterEntriesByTime() {
+    this.entries =  this.filteredEntries.filter(e => e.time >= this._startTime && e.time <= this._endTime);
+  }
+
+   updateTable(event) {
     let select = this.groupKeySelect;
     let key = select.options[select.selectedIndex].text;
     let tableBody = this.tableBodySelect;
     this.removeAllChildren(tableBody);
-    let groups = Group.groupBy(entries, key, true);
+    let groups = Group.groupBy(this.entries, key, true);
     this.display(groups, tableBody);
+    //TODO(zcankara) dispatch event that the IC events processed
+    this.dispatchEvent(new CustomEvent(
+      'change', {bubbles: true, composed: true, detail: this.entries}));
   }
 
   escapeHtml(unsafe) {
@@ -102,10 +132,9 @@ defineCustomElement('ic-panel', (templateText) =>
     return this._noOfItems;
   }
 
-
   display(entries, parent) {
     let fragment = document.createDocumentFragment();
-    let max = Math.min(this.noOfItems, entries.length)
+    let max = entries.length;
     for (let i = 0; i < max; i++) {
       let entry = entries[i];
       let tr = document.createElement("tr");
@@ -126,6 +155,7 @@ defineCustomElement('ic-panel', (templateText) =>
     }
     parent.appendChild(fragment);
   }
+
 
   displayDrilldown(entry, previousSibling) {
     let tr = document.createElement('tr');
@@ -183,35 +213,17 @@ defineCustomElement('ic-panel', (templateText) =>
     }
   }
 
-  //TODO(zc): Function processing the timestamps of ICEvents
-  // Processes the IC Events which have V8Map's in the map-processor
-  processICEventTime(){
-    let ICTimeToEvent = new Map();
-    // save the occurance time of V8Maps
-    let eventTimes = []
-    console.log("Num of stats: " + entries.length);
-    // fetch V8 Maps from the IC Events
-    entries.forEach(element => {
-      let v8Map = V8Map.get("0x" + element.map);
-      if(!v8Map){
-        ICTimeToEvent.set(-1, element);
-      } else {
-        ICTimeToEvent.set(v8Map.time, element);
-        eventTimes.push(v8Map.time);
-      }
-    });
-    eventTimes.sort();
-    // save the IC events which have Map states
-    let eventsList = [];
-    for(let i = 0;  i < eventTimes.length; i++){
-      eventsList.push(ICTimeToEvent.get(eventTimes[i]));
-    }
-    return eventList;
-  }
-
-  handleICFilter(e){
-    let noOfItemsInput = parseInt(this.$('#filter-input').value);
-    this.noOfItems = noOfItemsInput;
+  handleICTimeFilter(e) {
+    this._startTime = parseInt(this.$('#filter-time-start').value);
+    console.assert(this._startTime >= 0, { errorMsg: "start time must be a non-negative integer!" });
+    this._endTime = parseInt(this.$('#filter-time-end').value);
+    console.assert(this._endTime <= this._filteredEntries[this._filteredEntries.length - 1].time,
+      { errorMsg: "end time must be smaller or equal to the the time of the last event!" });
+    console.assert(this._startTime < this._endTime,
+      { errorMsg: "end time must be smaller than the start time!" });
+    this.filterEntriesByTime();
     this.updateTable(e);
   }
+
+
 });
