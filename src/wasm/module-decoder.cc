33c216c15df26f25cc0fcc915338be6bee52e493
Manos Koukoutos <manoskouk@chromium.org>
2020-07-20 11:02:27 +0000

    [wasm][fuzzer] Return early if element segment offset init. fails
    Bug: chromium:1103839
    Change-Id: Id70b4632fb15cb16c16cb94ad0f7676e5293769d

--- src/wasm/module-decoder.cc
+++ src/wasm/module-decoder.cc
@@ -2026,6 +2026,10 @@ class ModuleDecoderImpl : public Decoder {
 
     if (*status == WasmElemSegment::kStatusActive) {
       *offset = consume_init_expr(module_.get(), kWasmI32);
+      if (offset->kind() == WasmInitExpr::kNone) {
+        // Failed to parse offset initializer, return early.
+        return;
+      }
     }
 
     if (*status == WasmElemSegment::kStatusActive && !has_table_index) {
