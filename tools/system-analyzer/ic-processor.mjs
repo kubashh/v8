3600cb667df20402eb752a09438cf065226b0271
Zeynep Cankara <zcankara@google.com>
2020-07-24 15:20:41 +0100

    [tools][system-analyzer] Add Generic Event Class
    This CL adds a generic Event Class to unify common
    methods of IC and Map events. The Entry Class for IC
    Events and V8Map Class for Map Events inherits from
    this generic Event Class.
    
    Bug: v8:10644, v8:10735
    
    Change-Id: I77d68fb40ee0ffbe297fcd1a13c3e2b746938168

--- tools/system-analyzer/ic-processor.mjs
+++ tools/system-analyzer/ic-processor.mjs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {Event} from './event.mjs';
+
 /**
  * Parser for dynamic code optimization state.
  */
@@ -194,12 +196,11 @@ class CustomIcProcessor extends IcProcessor {
   }
 };
 
-class Entry {
+class Entry extends Event {
   constructor(
       type, fn_file, time, line, column, key, oldState, newState, map, reason,
       additional) {
-    this.time = time;
-    this.type = type;
+    super(type, time);
     this.category = 'other';
     if (this.type.indexOf('Store') !== -1) {
       this.category = 'Store';
@@ -219,6 +220,7 @@ class Entry {
     this.reason = reason;
     this.additional = additional;
   }
+
 
   parseMapProperties(parts, offset) {
     let next = parts[++offset];
--- tools/system-analyzer/map-processor.mjs
+++ tools/system-analyzer/map-processor.mjs
@@ -4,6 +4,7 @@
 
 
 // ===========================================================================
+import {Event} from './event.mjs';
 const kChunkHeight = 250;
 const kChunkWidth = 10;
 
@@ -221,14 +222,14 @@ class MapProcessor extends LogReader {
 
 // ===========================================================================
 
-class V8Map {
+class V8Map extends Event {
   constructor(id, time = -1) {
+    super(id, time);
     if (!id) throw 'Invalid ID';
     this.id = id;
-    this.time = time;
     if (!(time > 0)) throw 'Invalid time';
-    this.description = '';
     this.edge = void 0;
+    this.description = '';
     this.children = [];
     this.depth = 0;
     this._isDeprecated = false;
