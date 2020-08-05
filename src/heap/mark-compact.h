b0890776d6df2b5fa6b6fdd4066fbbaee2771b45
Dominik Inführ <dinfuehr@chromium.org>
2020-08-05 22:28:15 +0200

    [heap] Remove ArrayBufferTracker
    ArrayBufferTracker was already replaced with ArrayBufferList and
    ArrayBufferSweeper. Now that ArrayBufferSweeper is used in production,
    we can remove the unused ArrayBufferTracker mechanism.
    
    Bug: v8:10064
    Change-Id: I479169c76b6c5c634672024f77e689bb64a36504

--- src/heap/mark-compact.h
+++ src/heap/mark-compact.h
@@ -720,9 +720,6 @@ class MarkCompactCollector final : public MarkCompactCollectorBase {
   UpdatingItem* CreateRememberedSetUpdatingItem(
       MemoryChunk* chunk, RememberedSetUpdatingMode updating_mode) override;
 
-  int CollectNewSpaceArrayBufferTrackerItems(ItemParallelJob* job);
-  int CollectOldSpaceArrayBufferTrackerItems(ItemParallelJob* job);
-
   void ReleaseEvacuationCandidates();
   void PostProcessEvacuationCandidates();
   void ReportAbortedEvacuationCandidate(HeapObject failed_object,
@@ -863,8 +860,6 @@ class MinorMarkCompactCollector final : public MarkCompactCollectorBase {
                                           Address end) override;
   UpdatingItem* CreateRememberedSetUpdatingItem(
       MemoryChunk* chunk, RememberedSetUpdatingMode updating_mode) override;
-
-  int CollectNewSpaceArrayBufferTrackerItems(ItemParallelJob* job);
 
   int NumberOfParallelMarkingTasks(int pages);
 
