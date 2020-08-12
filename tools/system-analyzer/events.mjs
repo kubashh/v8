4732a2656fb8d07ea5036f9313775d0d3add83d3
Zeynep Cankara <zcankara@google.com>
2020-08-12 05:14:22 +0100

    [tools][system-analyzer] Change naming conventions
    This CL establishes a naming consistency
    across the app by renaming classes.
    
    Class Name Changes:
    SelectEvent -> FocusEvent
    Entry -> IcLogEvent
    V8Map -> MapLogEvent
    
    Bug: v8:10644
    Change-Id: Id075d9aa36ac6f03af0224feb0e38985b1445013

--- tools/system-analyzer/events.mjs
+++ tools/system-analyzer/events.mjs
@@ -1,23 +1,29 @@
-
 // Copyright 2020 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 class SelectionEvent extends CustomEvent {
-  constructor(entries){
-    super('showentries', {bubbles: true, composed: true});
-    if(!Array.isArray(entries) || entries.length == 0){
-      throw new Error('No valid entries selected!')
+  constructor(entries) {
+    super(SelectionEvent.showEntries, { bubbles: true, composed: true });
+    if (!Array.isArray(entries) || entries.length == 0) {
+      throw new Error("No valid entries selected!");
     }
     this.entries = entries;
   }
-}
-
-class SelectEvent extends CustomEvent {
-  constructor(entry){
-    super('showentrydetail', {bubbles: true, composed: true});
-    this.entry = entry;
+  static get showEntries() {
+    return "showentries";
   }
 }
 
-export {SelectionEvent, SelectEvent};
+class FocusEvent extends CustomEvent {
+  static showEntryDetail = "showentrydetail";
+  constructor(entry) {
+    super(FocusEvent.showEntryDetail, { bubbles: true, composed: true });
+    this.entry = entry;
+  }
+  static get showEntryDetail() {
+    return "showentrydetail";
+  }
+}
+
+export { SelectionEvent, FocusEvent };
