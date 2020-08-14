a6b05744541a496da4ec5c6dd42b1ac0ea7fb315
Mythri A <mythria@chromium.org>
2020-08-14 15:28:47 +0100

    [turboprop] For double fields check if the loaded value is a HeapNumber
    With in-place representation changes we can transition from a double
    field to a Tagged field without changing the map. So it is not always
    safe to assume the loaded value would be a HeapNumber. TurboFan takes a
    dependency on the field representation to ensure the code is deoptimized
    on any changes. With dynamic map checks, it is not possible to take such
    a dependency. Hence check the loaded value is a HeapNumber and
    deoptimize otherwise.
    
    Bug:v8:1112155,v8:10582
    Change-Id: I910ac1c0781ac8842fdbf272f9173b55b02923ba

--- src/compiler/property-access-builder.cc
+++ src/compiler/property-access-builder.cc
@@ -210,6 +210,25 @@ Node* PropertyAccessBuilder::BuildLoadDataField(NameRef const& name,
                                           field_access.const_field_info};
       storage = *effect = graph()->NewNode(
           simplified()->LoadField(storage_access), storage, *effect, *control);
+      if (!dependencies()) {
+        // We expect the loaded value to be a heap number here. With
+        // in-place field representation changes it is possible this is a
+        // no longer a heap number without map transitions. If We haven't taken
+        // a dependency on field represnetation, we should verify the loaded
+        // value is a heap number.
+        storage = *effect = graph()->NewNode(simplified()->CheckHeapObject(),
+                                             storage, *effect, *control);
+        Node* map;
+        map = *effect =
+            graph()->NewNode(simplified()->LoadField(AccessBuilder::ForMap()),
+                             storage, *effect, *control);
+        Node* is_heap_number =
+            graph()->NewNode(simplified()->ReferenceEqual(), map,
+                             jsgraph()->HeapNumberMapConstant());
+        *effect = graph()->NewNode(
+            simplified()->CheckIf(DeoptimizeReason::kNotAHeapNumber),
+            is_heap_number, *effect, *control);
+      }
       field_access.offset = HeapNumber::kValueOffset;
       field_access.name = MaybeHandle<Name>();
     }
