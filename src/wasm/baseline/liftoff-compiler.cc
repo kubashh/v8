5c894a8595674a8ad8ef54540d5927459097d3c4
Ng Zhi An <zhin@chromium.org>
2020-09-03 14:36:56 -0700

    [liftoff][wasm-simd] Check SIMD support for more operations
    We need to also check SIMD support for v128.const and i8x16.shuffle,
    these are separate functions, not part of the SimdOp or SimdLaneOp.
    
    This was discovered while running some JS tests when trying to stage
    SIMD.
    
    Bug: v8:10835
    Change-Id: I51c6e65cc9397058e0be83cf4116ab4bfe4c4ec9

--- src/wasm/baseline/liftoff-compiler.cc
+++ src/wasm/baseline/liftoff-compiler.cc
@@ -2854,6 +2854,9 @@ class LiftoffCompiler {
 
   void S128Const(FullDecoder* decoder, const Simd128Immediate<validate>& imm,
                  Value* result) {
+    if (!CpuFeatures::SupportsWasmSimd128()) {
+      return unsupported(decoder, kSimd, "simd");
+    }
     constexpr RegClass result_rc = reg_class_for(ValueType::kS128);
     LiftoffRegister dst = __ GetUnusedRegister(result_rc, {});
     bool all_zeroes = std::all_of(std::begin(imm.value), std::end(imm.value),
@@ -2875,6 +2878,9 @@ class LiftoffCompiler {
                          const Simd128Immediate<validate>& imm,
                          const Value& input0, const Value& input1,
                          Value* result) {
+    if (!CpuFeatures::SupportsWasmSimd128()) {
+      return unsupported(decoder, kSimd, "simd");
+    }
     static constexpr RegClass result_rc = reg_class_for(ValueType::kS128);
     LiftoffRegister rhs = __ PopToRegister();
     LiftoffRegister lhs = __ PopToRegister(LiftoffRegList::ForRegs(rhs));
