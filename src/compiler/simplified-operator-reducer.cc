caf5e725b6c7a0778404f5fed195b6d8ab509ea6
Z Nguyen-Huu <duongn@microsoft.com>
2020-09-09 10:56:36 -0700

    [turbofan] Reduce consecutive overflow addition with constants
    Using associative property of addition: (x + A) + B => x + (A + B).
    Note: A and B need to have the same sign and we need to check that
    (x + A) isn't used anywhere else.
    
    20% perf improvement of the following function.
    
    function f(n) {
      var c = 0;
      for (var i = 0; i < n; i++) {
        c = c + 2 + 3;
      }
      return c;
    }
    for n = 10_000_000.
    
    Before: 7.31s.
    After: 6.05s.
    
    Bug: v8:10305
    Change-Id: If45d1cad6128a9a25cb9f43a4828ae28d594a84b

--- src/compiler/simplified-operator-reducer.cc
+++ src/compiler/simplified-operator-reducer.cc
@@ -220,6 +220,33 @@ Reduction SimplifiedOperatorReducer::Reduce(Node* node) {
       if (m.left().node() == m.right().node()) return ReplaceBoolean(true);
       break;
     }
+    case IrOpcode::kCheckedInt32Add: {
+      // (x + a) + b => x + (a + b) where a and b are constants and have the
+      // same sign.
+      Int32BinopMatcher m(node);
+      if (m.right().HasValue()) {
+        Node* checked_int32_add = m.left().node();
+        if (checked_int32_add->opcode() == IrOpcode::kCheckedInt32Add &&
+            NodeProperties::GetEffectInput(node) == checked_int32_add) {
+          Int32BinopMatcher n(checked_int32_add);
+          if (n.right().HasValue() &&
+              (n.right().Value() >= 0) == (m.right().Value() >= 0)) {
+            int32_t val;
+            bool ovf = base::bits::SignedAddOverflow32(n.right().Value(),
+                                                       m.right().Value(), &val);
+            if (!ovf) {
+              node->ReplaceInput(0, n.left().node());
+              node->ReplaceInput(1, jsgraph()->Int32Constant(val));
+              // Compute x + (a + b) should not depend on x + a in this case.
+              Node* effect = NodeProperties::GetEffectInput(checked_int32_add);
+              NodeProperties::ReplaceEffectInput(node, effect);
+              return Changed(node);
+            }
+          }
+        }
+      }
+      break;
+    }
     default:
       break;
   }
