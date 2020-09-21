80e505c2ac6d56138fa1329657292017e8ec94cf
Ulan Degenbaev <ulan@chromium.org>
2020-09-21 20:25:36 +0200

    [heap] Fix regressions in the configuration without concurrent marking
    Building and running tests with v8_enabled_concurrent_marking=false
    currently produces two failures:
    1) Segmentation fault on attempt to mark a read-only object.
       This is fixed by changing MarkBit::Set to be a no-op if the object
       is already marked (which is the case for the readonly space).
    
    Change-Id: Ib3a238fc34c8f20c697470e0bd4ac427fb4bdc0e

--- src/heap/marking-barrier.cc
+++ src/heap/marking-barrier.cc
@@ -60,10 +60,6 @@ void MarkingBarrier::Write(Code host, RelocInfo* reloc_info, HeapObject value) {
 
 void MarkingBarrier::Write(JSArrayBuffer host,
                            ArrayBufferExtension* extension) {
-  if (!V8_CONCURRENT_MARKING_BOOL && marking_state_.IsBlack(host)) {
-    // The extension will be marked when the marker visits the host object.
-    return;
-  }
   extension->Mark();
 }
 
