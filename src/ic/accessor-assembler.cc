21b889d37d4a6fcc918232a98594953af7339290
Samuel Groß <saelo@google.com>
2020-09-21 15:47:19 +0200

    [sandbox][x64] Access external pointer through a table
    This change moves external pointers into a separate table datastructure
    and turns external pointers in heap objects into indices into that
    table.
    
    This CL implements one of two possible ownership models for the table
    entries. With this one, every heap objects owns its table entries. The
    entries are allocated when the owning object is allocated and they are
    freed when the object is garbage collected.
    
    As such, setting external pointer fields does not require allocation of
    table entries and the memory management of the table is simpler. On the
    other hand, table indices cannot be shared between different objects and
    table entries are allocated even when the external pointers are never
    set (although for the common cases it should be possible to only
    allocate table entries when they are needed).
    
    This CL does not yet implement freeing of external pointer table entires
    (this has to happen in object finalizers) and it also does not correctly
    handle EmbedderDataSlots.
    
    Bug: v8:10391
    Change-Id: I4d37785295c25a7d1dcbc9871dd5887b9d788a4f

--- src/ic/accessor-assembler.cc
+++ src/ic/accessor-assembler.cc
@@ -210,8 +210,7 @@ void AccessorAssembler::HandleLoadAccessor(
 
   TNode<Foreign> foreign = LoadObjectField<Foreign>(
       call_handler_info, CallHandlerInfo::kJsCallbackOffset);
-  TNode<RawPtrT> callback =
-      DecodeExternalPointer(LoadForeignForeignAddress(foreign));
+  TNode<RawPtrT> callback = LoadForeignForeignAddressPtr(foreign);
   TNode<Object> data =
       LoadObjectField(call_handler_info, CallHandlerInfo::kDataOffset);
 
@@ -1659,8 +1658,7 @@ void AccessorAssembler::HandleStoreICProtoHandler(
 
       TNode<Foreign> foreign = LoadObjectField<Foreign>(
           call_handler_info, CallHandlerInfo::kJsCallbackOffset);
-      TNode<RawPtrT> callback =
-          DecodeExternalPointer(LoadForeignForeignAddress(foreign));
+      TNode<RawPtrT> callback = LoadForeignForeignAddressPtr(foreign);
       TNode<Object> data =
           LoadObjectField(call_handler_info, CallHandlerInfo::kDataOffset);
 
