c0850666cc09a1274b4d14e9dcc167be1148b939
Camillo Bruni <cbruni@chromium.org>
2020-09-28 15:17:15 +0200

    [tools] Rename SourceMap.mjs to sourcemap.mjs
    Bug: v8:10667, v8:10933
    Change-Id: I06ebcdf12ee5acbd56ef3741af2b592b5d0351a1

--- tools/dumpcpp-driver.mjs
+++ tools/dumpcpp-driver.mjs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import "./sourcemap.mjs";
+import { WebInspector } from "./sourcemap.mjs";
 import {
     CppProcessor, ArgumentsProcessor, UnixCppEntriesProvider, 
     WindowsCppEntriesProvider, MacCppEntriesProvider
