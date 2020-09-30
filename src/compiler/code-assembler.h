d1b5c4ebe4afce74db7df659cf714387ba8a670a
Dan Elphick <delphick@chromium.org>
2020-09-30 15:58:07 +0100

    [CSA] Tnodify CodeAssembler::Parameter
    CodeAssembler::Parameter now takes a Type template parameter and
    performs a checked cast to it. There is also UncheckedParameter which
    returns a TNode but doesn't check the cast. The original Parameter
    method is still there as UntypedParameter.
    
    Parameter<T>(x) in many cases replaces CAST(Parameter(x)), where the
    cast is performed inside Parameter. Since Parameter is not a macro,
    this means it cannot see the original expression or its file name and
    line number. So the error messages are vaguely useful, Parameter<T>()
    takes a SourceLocation parameter which with a default value of
    SourceLocation::Current(), which at least gives us the file name and
    line number for the error message.
    
    Bug: v8:6949, v8:10933
    Change-Id: I27157bec7dc7462210c1eb9c430c0180217d25c1

--- src/compiler/code-assembler.h
+++ src/compiler/code-assembler.h
@@ -8,9 +8,11 @@
 #include <initializer_list>
 #include <map>
 #include <memory>
+#include <sstream>
 
 // Clients of this interface shouldn't depend on lots of compiler internals.
 // Do not include anything from src/compiler here!
+#include "include/cppgc/source-location.h"
 #include "src/base/macros.h"
 #include "src/base/type-traits.h"
 #include "src/builtins/builtins.h"
@@ -566,7 +568,27 @@ class V8_EXPORT_PRIVATE CodeAssembler {
 
   static constexpr int kTargetParameterIndex = -1;
 
-  Node* Parameter(int value);
+  template <class T>
+  TNode<T> Parameter(
+      int value, cppgc::SourceLocation loc = cppgc::SourceLocation::Current()) {
+    std::stringstream message;
+    message << "Parameter " << value;
+    if (loc.FileName()) {
+      value << " at " << loc.FileName() << ":" << loc.Line();
+    }
+    size_t buf_size = message.str().size() + 1;
+    char* message_dup = zone()->NewArray<char>(buf_size);
+    snprintf(message_dup, buf_size, "%s", message.str().c_str());
+
+    return Cast(UntypedParameter(value), message_dup);
+  }
+
+  template <class T>
+  TNode<T> UncheckedParameter(int value) {
+    return UncheckedCast<T>(UntypedParameter(value));
+  }
+
+  Node* UntypedParameter(int value);
 
   TNode<Context> GetJSContextParameter();
   void Return(TNode<Object> value);
