b929f36029bc0df66bd87347633c18a16274bb7e
Mythri A <mythria@chromium.org>
2020-10-09 15:09:39 +0100

    [turboprop] Add tiering up support for TurboProp
    This cl adds support for tiering up in TurboProp.This cl makes
    necessary changes to support tier up but doesn't tier up yet. More
    specifically this cl:
    1. Introduces a new flag for interrupt_budget_for_midtier and
    updates code to use the correct interrupt_budget.
    2. Introduces a flag turboprop_as_midtier and necessary support
    to tier up. When this flag is enabled, we introduce checks for tierup
    and updating interrupt budget.
    
    
    Bug:v8:9684
    Change-Id: I58785ce4b9de46488a22d3b4d0cebedac460a773

--- tools/testrunner/local/variants.py
+++ tools/testrunner/local/variants.py
@@ -59,7 +59,7 @@ INCOMPATIBLE_FLAGS_PER_VARIANT = {
   "stress_js_bg_compile_wasm_code_gc": ["--no-stress-background-compile"],
   "stress": ["--no-stress-opt", "--always-opt", "--no-always-opt", "--liftoff", "--max-inlined-bytecode-size=*",
              "--max-inlined-bytecode-size-cumulative=*", "--stress-inline"],
-  "turboprop": ["--turbo-inlining", "--interrupt-budget=*", "--no-turboprop"],
+  "turboprop": ["--turbo-inlining", "--no-turboprop"],
   "code_serializer": ["--cache=after-execute", "--cache=full-code-cache", "--cache=none"],
   "no_local_heaps": ["--concurrent-inlining", "--turboprop"],
   "experimental_regexp": ["--no-enable-experimental-regexp-engine"],
