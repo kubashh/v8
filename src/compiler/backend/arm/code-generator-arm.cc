ebbee8fcfc57236cd17cd6ca7c7ed276174eec4f
Victor Gomes <victorgomes@chromium.org>
2020-10-09 11:33:13 +0200

    [arm] Remove arguments adaptor frame
    Change-Id: I7df25ca2c7caabed429cfdc0b4aab0aeb5e03fcd
    Bug: v8:10201

--- src/compiler/backend/arm/code-generator-arm.cc
+++ src/compiler/backend/arm/code-generator-arm.cc
@@ -3758,9 +3758,8 @@ void CodeGenerator::AssembleConstructFrame() {
   }
 }
 
-void CodeGenerator::AssembleReturn(InstructionOperand* pop) {
+void CodeGenerator::AssembleReturn(InstructionOperand* additional_pop_count) {
   auto call_descriptor = linkage()->GetIncomingDescriptor();
-  int pop_count = static_cast<int>(call_descriptor->StackParameterCount());
 
   const int returns = frame()->GetReturnSlotCount();
   if (returns != 0) {
@@ -3786,32 +3785,76 @@ void CodeGenerator::AssembleReturn(InstructionOperand* pop) {
 
   unwinding_info_writer_.MarkBlockWillExit();
 
+  // We might need r4 and r5 for scratch.
+  DCHECK_EQ(0u, call_descriptor->CalleeSavedRegisters() & r4.bit());
+  DCHECK_EQ(0u, call_descriptor->CalleeSavedRegisters() & r5.bit());
+  const int parameter_count =
+      static_cast<int>(call_descriptor->StackParameterCount());
+
   ArmOperandConverter g(this, nullptr);
+  Register pop_reg = additional_pop_count->IsImmediate()
+                         ? r4
+                         : g.ToRegister(additional_pop_count);
+  Register argc_reg = pop_reg == r4 ? r5 : r4;
+#ifdef V8_NO_ARGUMENTS_ADAPTOR
+  // Functions with JS linkage have at least one parameter (the receiver).
+  // If {parameter_count} == 0, it means it is a builtin with
+  // kDontAdaptArgumentsSentinel, which takes care of JS arguments popping
+  // itself.
+  const bool drop_jsargs = frame_access_state()->has_frame() &&
+                           call_descriptor->IsJSFunctionCall() &&
+                           parameter_count != 0;
+#else
+  const bool drop_jsargs = false;
+#endif
   if (call_descriptor->IsCFunctionCall()) {
     AssembleDeconstructFrame();
   } else if (frame_access_state()->has_frame()) {
     // Canonicalize JSFunction return sites for now unless they have an variable
     // number of stack slot pops.
-    if (pop->IsImmediate() && g.ToConstant(pop).ToInt32() == 0) {
+    if (additional_pop_count->IsImmediate() &&
+        g.ToConstant(additional_pop_count).ToInt32() == 0) {
       if (return_label_.is_bound()) {
         __ b(&return_label_);
         return;
       } else {
         __ bind(&return_label_);
-        AssembleDeconstructFrame();
       }
-    } else {
-      AssembleDeconstructFrame();
     }
+    if (drop_jsargs) {
+      // Get the actual argument count.
+      __ ldr(argc_reg, MemOperand(fp, StandardFrameConstants::kArgCOffset));
+    }
+    AssembleDeconstructFrame();
   }
 
-  if (pop->IsImmediate()) {
-    DCHECK_EQ(Constant::kInt32, g.ToConstant(pop).type());
-    pop_count += g.ToConstant(pop).ToInt32();
+  if (drop_jsargs) {
+    // In addition to the slots given by {additional_pop_count}, we must pop all
+    // arguments from the stack (including the receiver). This number of
+    // arguments is given by max(1 + argc_reg, parameter_count).
+    const int parameter_count_without_receiver = parameter_count - 1;
+    if (parameter_count_without_receiver != 0) {
+      __ cmp(argc_reg, Operand(parameter_count_without_receiver));
+      __ mov(argc_reg, Operand(parameter_count_without_receiver), LeaveCC, lt);
+    }
+    // Add additional pop count.
+    if (additional_pop_count->IsImmediate()) {
+      int additional_count = g.ToConstant(additional_pop_count).ToInt32();
+      __ add(pop_reg, argc_reg,
+             Operand(additional_count + 1));  // Also pop the receiver.
+    } else {
+      __ add(pop_reg, pop_reg, argc_reg);
+      __ add(pop_reg, pop_reg, Operand(1));  // Also pop the receiver.
+    }
+    __ Drop(pop_reg);
+  } else if (additional_pop_count->IsImmediate()) {
+    DCHECK_EQ(Constant::kInt32, g.ToConstant(additional_pop_count).type());
+    int additional_count = g.ToConstant(additional_pop_count).ToInt32();
+    __ Drop(parameter_count + additional_count);
   } else {
-    __ Drop(g.ToRegister(pop));
+    __ Drop(parameter_count);
+    __ Drop(pop_reg);
   }
-  __ Drop(pop_count);
   __ Ret();
 }
 
