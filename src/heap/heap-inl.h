94316e64229ac38f061dc41aa991bb9f559f6bd6
Pierre Langlois <pierre.langlois@arm.com>
2020-10-12 10:18:05 +0100

    [heap] Make maximum regular code object size a runtime value.
    Executable V8 pages include 3 reserved OS pages: one for the writable
    header and two as guards. On systems with 64k OS pages, the amount of
    allocatable space left for objects can then be quite smaller than the
    page size, only 64k for each 256k page.
    
    This means regular code objects cannot be larger than 64k, while the
    maximum regular object size is fixed to 128k, half of the page size. As
    a result code object never reach this limit and we can end up filling
    regular pages with few large code objects.
    
    To fix this, we change the maximum code object size to be runtime value,
    set to half of the allocatable space per page. On systems with 64k OS
    pages, the limit will be 32k.
    
    Alternatively, we could increase the V8 page size to 512k on Arm64 linux
    so we wouldn't waste code space. However, systems with 4k OS pages are
    more common, and those with 64k pages tend to have more memory available
    so we should be able to live with it.
    
    Bug: v8:10808
    Change-Id: I5d807e7a3df89f1e9c648899e9ba2f8e2648264c

--- src/heap/heap-inl.h
+++ src/heap/heap-inl.h
@@ -185,10 +185,7 @@ AllocationResult Heap::AllocateRaw(int size_in_bytes, AllocationType type,
   IncrementObjectCounters();
 #endif
 
-  size_t large_object_threshold =
-      AllocationType::kCode == type
-          ? std::min(kMaxRegularHeapObjectSize, code_space()->AreaSize())
-          : kMaxRegularHeapObjectSize;
+  size_t large_object_threshold = MaxRegularHeapObjectSize(type);
   bool large_object =
       static_cast<size_t>(size_in_bytes) > large_object_threshold;
 
@@ -284,7 +281,7 @@ HeapObject Heap::AllocateRawWith(int size, AllocationType allocation,
   Heap* heap = isolate()->heap();
   if (allocation == AllocationType::kYoung &&
       alignment == AllocationAlignment::kWordAligned &&
-      size <= kMaxRegularHeapObjectSize) {
+      size <= MaxRegularHeapObjectSize(allocation)) {
     Address* top = heap->NewSpaceAllocationTopAddress();
     Address* limit = heap->NewSpaceAllocationLimitAddress();
     if ((*limit - *top >= static_cast<unsigned>(size)) &&
