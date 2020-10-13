a7eb5051c140399f908d7c20396fa8b7a9211dfa
Milad Fa <mfarazma@redhat.com>
2020-10-13 18:32:29 +0000

    AIX: workaround the aix FP glibc bug
    Due to a bug on AIX, some of the glibc FP functions do not
    preserve the sign bit when its input is passed by value and
    the output is rounded to 0:
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=97086
    
    This CL forces the use of "-0.0" in such cases.
    
    Change-Id: If9935596e32e97720f3cb22f27975267ac1124d7

--- src/wasm/wasm-external-refs.cc
+++ src/wasm/wasm-external-refs.cc
@@ -405,9 +405,15 @@ template <typename T, T (*float_round_op)(T)>
 void simd_float_round_wrapper(Address data) {
   constexpr int n = kSimd128Size / sizeof(T);
   for (int i = 0; i < n; i++) {
-    WriteUnalignedValue<T>(
-        data + (i * sizeof(T)),
-        float_round_op(ReadUnalignedValue<T>(data + (i * sizeof(T)))));
+    T input = ReadUnalignedValue<T>(data + (i * sizeof(T)));
+    T value = float_round_op(input);
+#if V8_OS_AIX
+    // glibc on aix has a bug when using ceil, trunc or nearbyint:
+    // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=97086
+    if (/*if -*/ signbit(input) && value == 0.0 && /*if +*/ !signbit(value))
+      value = -0.0;
+#endif
+    WriteUnalignedValue<T>(data + (i * sizeof(T)), value);
   }
 }
 
