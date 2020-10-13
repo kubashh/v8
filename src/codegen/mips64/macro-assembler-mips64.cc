5d0157484562d7ef7e9d4324d508ea34ab876153
Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
2020-10-13 02:37:54 -0400

    [mips] Fix DropAndRet with a frame that larger than int16_max
    Change-Id: I3f5dbb0fbae3862a6da4146b83e49986c8be3bdc

--- src/codegen/mips64/macro-assembler-mips64.cc
+++ src/codegen/mips64/macro-assembler-mips64.cc
@@ -4448,9 +4448,18 @@ void TurboAssembler::BranchAndLinkLong(Label* L, BranchDelaySlot bdslot) {
 }
 
 void TurboAssembler::DropAndRet(int drop) {
-  DCHECK(is_int16(drop * kPointerSize));
-  Ret(USE_DELAY_SLOT);
-  daddiu(sp, sp, drop * kPointerSize);
+  int64_t drop_size = drop * kSystemPointerSize;
+
+  if (is_int16(drop_size)) {
+    Ret(USE_DELAY_SLOT);
+    daddiu(sp, sp, drop_size);
+  } else {
+    UseScratchRegisterScope temps(this);
+    Register scratch = temps.Acquire();
+    li(scratch, drop_size);
+    Ret(USE_DELAY_SLOT);
+    daddu(sp, sp, scratch);
+  }
 }
 
 void TurboAssembler::DropAndRet(int drop, Condition cond, Register r1,
