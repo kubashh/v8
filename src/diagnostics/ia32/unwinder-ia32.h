ae6922cf7f28b5bc985aa54f4178d426cd22bccd
Santiago Aboy Solanes <solanes@chromium.org>
2020-10-16 11:38:58 +0100

    [unwinder] Restore callee saved registers after unwinding in arm32
    Bug: v8:10799
    Change-Id: Id912520b6a27e439e204bac47c0723a8f613be4b
    Fixed: v8:10799

---
+++ src/diagnostics/ia32/unwinder-ia32.h
@@ -0,0 +1,21 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef INCLUDED_FROM_UNWINDER_H
+#error This header must be included via unwinder.cc
+#endif
+
+// Dummy file to be able to compile
+#ifndef V8_DIAGNOSTICS_IA32_UNWINDER_IA32_H_
+#define V8_DIAGNOSTICS_IA32_UNWINDER_IA32_H_
+
+#include "src/diagnostics/unwinder.h"
+
+namespace v8 {
+
+void RestoreCalleeSavedRegisters(void* fp, RegisterState* register_state) {}
+
+}  // namespace v8
+
+#endif  // V8_DIAGNOSTICS_IA32_UNWINDER_IA32_H_
---
+++ src/diagnostics/unwinder.h
@@ -0,0 +1,38 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_DIAGNOSTICS_UNWINDER_H_
+#define V8_DIAGNOSTICS_UNWINDER_H_
+
+#include <algorithm>
+
+#include "include/v8.h"
+#include "src/common/globals.h"
+#include "src/execution/frame-constants.h"
+#include "src/execution/pointer-authentication.h"
+
+namespace v8 {
+
+// Helper methods, common to all archs
+const i::byte* CalculateEnd(const void* start, size_t length_in_bytes);
+
+bool PCIsInCodeRange(const v8::MemoryRange& code_range, void* pc);
+bool PCIsInCodePages(size_t code_pages_length, const MemoryRange* code_pages,
+                     void* pc);
+bool IsInJSEntryRange(const JSEntryStubs& entry_stubs, void* pc);
+bool IsInUnsafeJSEntryRange(const JSEntryStubs& entry_stubs, void* pc);
+
+i::Address Load(i::Address address);
+
+void* GetReturnAddressFromFP(void* fp, void* pc,
+                             const JSEntryStubs& entry_stubs);
+void* GetCallerFPFromFP(void* fp, void* pc, const JSEntryStubs& entry_stubs);
+void* GetCallerSPFromFP(void* fp, void* pc, const JSEntryStubs& entry_stubs);
+
+bool AddressIsInStack(const void* address, const void* stack_base,
+                      const void* stack_top);
+
+}  // namespace v8
+
+#endif  // V8_DIAGNOSTICS_UNWINDER_H_
