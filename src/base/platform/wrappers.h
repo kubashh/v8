7ae28054adbe8aab101efc2796edcdedb5bbb025
John Xu <johnx@google.com>
2020-11-09 10:55:44 +0100

    Replace libc functions with base wrappers
    Bug: v8:10927
    Change-Id: Icbdc0d7329ddd466e7d67a954246a35795b4dece

--- src/base/platform/wrappers.h
+++ src/base/platform/wrappers.h
@@ -11,19 +11,20 @@
 namespace v8 {
 namespace base {
 
-void* Malloc(size_t size);
+V8_BASE_EXPORT void* Malloc(size_t size);
 
-void* Realloc(void* memory, size_t size);
+V8_BASE_EXPORT void* Realloc(V8_BASE_EXPORT void* memory, size_t size);
 
-void Free(void* memory);
+V8_BASE_EXPORT void Free(V8_BASE_EXPORT void* memory);
 
-void* Calloc(size_t count, size_t size);
+V8_BASE_EXPORT void* Calloc(size_t count, size_t size);
 
-void* Memcpy(void* dest, const void* source, size_t count);
+V8_BASE_EXPORT void* Memcpy(V8_BASE_EXPORT void* dest,
+                            const V8_BASE_EXPORT void* source, size_t count);
 
-FILE* Fopen(const char* filename, const char* mode);
+V8_BASE_EXPORT FILE* Fopen(const char* filename, const char* mode);
 
-int Fclose(FILE* stream);
+V8_BASE_EXPORT int Fclose(FILE* stream);
 
 }  // namespace base
 }  // namespace v8
