e4dedcf315c0719d158461c2f3b857191f436fc7
Leszek Swirski <leszeks@chromium.org>
2020-11-16 15:35:23 +0100

    [asserts] Add combination assert scopes
    Add a "combination" assert scope class, which combines multiple existing
    assert scopes.  This will allow scopes with functional overlap, e.g.
    DisallowGarbageCollection and DisallowHeapAllocation, to share an assert
    type rather than rather than requiring users to remember to set both. To
    demonstrate this, this redefines DisallowGarbageCollection to a
    combination of DisallowHeapAllocation and a new DisallowSafepoints, and
    some of the DCHECKs checking both are simplified to only check one or
    the other, as appropriate.
    
    The combination classes become subclasses of the existing assert scopes,
    so that they can be used in their place as e.g. a function parameter,
    e.g. DisallowGarbageCollection can be passed to a function expecting
    const DisallowHeapAllocation&.
    
    As a drive-by, this also changes the per-thread assert scopes to use a
    bitmask, rather than a bool array, to store their per-thread data. The
    per-isolate scopes already used a bitmask, so this unifies the
    behaviour between the two.
    
    Change-Id: I209e0a56f45e124c0ccadbd9fb77f39e070612fe

--- src/codegen/compiler.cc
+++ src/codegen/compiler.cc
@@ -1660,7 +1660,6 @@ bool Compiler::CollectSourcePositions(Isolate* isolate,
   // Collecting source positions requires allocating a new source position
   // table.
   DCHECK(AllowHeapAllocation::IsAllowed());
-  DCHECK(AllowGarbageCollection::IsAllowed());
 
   Handle<BytecodeArray> bytecode =
       handle(shared_info->GetBytecodeArray(), isolate);
