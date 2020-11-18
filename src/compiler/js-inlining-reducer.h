65531e0adde94bef76dcd61314a60e98b126abb8
Jakob Gruber <jgruber@chromium.org>
2020-11-18 12:29:36 +0100

    [nci] Move minimorphic reductions to typed lowering
    Minimorphic property accesses will be used to enable further
    optimizations in NCI code, thus their implementation must move out of
    the (native-context-dependent) js-native-context-specialization
    compiler phase.
    
    This CL moves the implementation into typed lowering. For Turboprop,
    we will continue to embed NC-dependent map checks in the code for
    minimorphic accesses. For NCI code, the minimorphic map check will be
    implemented in an NC-independent way (e.g. by keeping map checks
    dynamic, except for maps located in RO space).
    
    Bug: v8:8888
    Change-Id: I6a80707bb554dc065233e1fa922edfd5da7c3926

---
+++ src/compiler/js-inlining-reducer.h
@@ -0,0 +1,75 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_COMPILER_JS_INLINING_REDUCER_H_
+#define V8_COMPILER_JS_INLINING_REDUCER_H_
+
+#include "src/compiler/graph-reducer.h"
+#include "src/compiler/js-graph.h"
+
+namespace v8 {
+namespace internal {
+namespace compiler {
+
+class JSGraph;
+class JSHeapBroker;
+class MinimorphicLoadPropertyAccessFeedback;
+
+// This reducer runs during the InliningPhase and reduces JS operators, and is
+// thus called "JSInliningReducer". Its scope is not well-defined; the intent
+// is for it to contain reductions that don't fit well into other reducers of
+// the InliningPhase.
+class V8_EXPORT_PRIVATE JSInliningReducer final : public AdvancedReducer {
+ public:
+  JSInliningReducer(Editor* editor, JSGraph* jsgraph, JSHeapBroker* broker);
+
+  JSInliningReducer(const JSInliningReducer&) = delete;
+  JSInliningReducer& operator=(const JSInliningReducer&) = delete;
+
+  const char* reducer_name() const override { return "JSInliningReducer"; }
+
+  inline Reduction Reduce(Node* node) final {
+    // Currently we only care about a small set of opcodes here. Check the
+    // opcode early to keep overhead low.
+
+    IrOpcode::Value opcode = node->opcode();
+    STATIC_ASSERT(IrOpcode::kJSLoadNamedFromSuper ==
+                  IrOpcode::kJSLoadNamed + 1);
+    if (opcode != IrOpcode::kJSLoadNamed &&
+        opcode != IrOpcode::kJSLoadNamedFromSuper) {
+      return NoChange();
+    }
+
+    return ReduceImpl(node, opcode);
+  }
+
+ private:
+  Reduction ReduceImpl(Node* node, IrOpcode::Value opcode);
+
+  Reduction ReduceJSLoadNamed(Node* node);
+  Reduction ReduceJSLoadNamedFromSuper(Node* node);
+
+  Reduction ReducePropertyAccess(Node* node,
+                                 base::Optional<NameRef> static_name,
+                                 FeedbackSource const& source);
+  Reduction ReduceMinimorphicPropertyAccess(
+      Node* node, MinimorphicLoadPropertyAccessFeedback const& feedback,
+      FeedbackSource const& source);
+
+  Graph* graph() const { return jsgraph()->graph(); }
+  JSGraph* jsgraph() const { return jsgraph_; }
+  JSHeapBroker* broker() const { return broker_; }
+  SimplifiedOperatorBuilder* simplified() const {
+    return jsgraph()->simplified();
+  }
+
+  JSGraph* const jsgraph_;
+  JSHeapBroker* const broker_;
+};
+
+}  // namespace compiler
+}  // namespace internal
+}  // namespace v8
+
+#endif  // V8_COMPILER_JS_INLINING_REDUCER_H_
