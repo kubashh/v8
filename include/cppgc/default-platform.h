5e1daefd0b2212bdac28f0b2eb65f4b82910a07a
Omer Katz <omerkatz@chromium.org>
2020-11-24 16:04:37 +0100

    cppgc: Fix and merge cppgc samples
    Both sample are essentially the same up to string constants since
    cppgc's default platform started using libplatform.
    The only diff between the sample is whether we call
    v8::V8::IntializePlatform or cppgc::InitializeProcess.
    
    Drive-by: replace CPPGC_BUILD_IN_V8 with CPPGC_IS_STANDALONE which is
              more descriptive.
    
    Bug: chromium:1056170
    Change-Id: I8fdeb59c3345af77f1bccd8b93255ab39b4d3181

--- include/cppgc/default-platform.h
+++ include/cppgc/default-platform.h
@@ -55,6 +55,11 @@ class V8_EXPORT DefaultPlatform : public Platform {
     return v8_platform_->GetTracingController();
   }
 
+  // Use this method instead of cppgc::InitializeProcess when using
+  // cppgc::DefaultPlatform. cppgc::DefaultPlatform::InitializeProcess will
+  // initialize cppgc and v8 if needed (for non-standalone builds).
+  static void InitializeProcess(DefaultPlatform*);
+
  protected:
   static constexpr v8::Isolate* kNoIsolate = nullptr;
 
