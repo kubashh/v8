911061fe5974cc4e1880f6e3d7836264582cbfd6
Michael Lippautz <mlippautz@chromium.org>
2020-11-24 15:35:52 +0100

    cppgc: Refactor write barriers
    Refactor write barriers and split calls, as e.g. DijkstraWriteBarrier
    also contained logic for recording slots (cards) for the young
    generation.
    
    The new API exposes the following:
    - GetWriteBarrierType(): Retrieving the type of barrier that must be
      emitted;
    - DijkstraWriteBarrier(), DijkstraWriteBarrierRange(): Dijkstra-style
      write barriers;
    - SteeleWriteBarrier(): Steele-style write barrier;
    - GenerationalBarrier(): Barrier for recording slots when using
      multiple generations;
    
    Compilers running with -O3 optimize the DijkstraWriteBarrierPolicy
    down to the same instructions as before the split.
    
    Change-Id: If68839cc6357b2f568986c9ce8ca753b1e96a70a
    Bug: chromium:1056170

--- include/cppgc/heap-consistency.h
+++ include/cppgc/heap-consistency.h
@@ -25,27 +25,38 @@ namespace subtle {
  */
 class HeapConsistency final {
  public:
+  using WriteBarrierResult = internal::WriteBarrier::Result;
+  using WriteBarrierType = internal::WriteBarrier::Type;
+
+  static V8_INLINE WriteBarrierType GetWriteBarrierType(
+      const void* slot, const void* value, WriteBarrierResult& result) {
+    return internal::WriteBarrier::GetWriteBarrierType(slot, value, result);
+  }
+
+  static V8_INLINE WriteBarrierType
+  GetWriteBarrierType(const void* slot, WriteBarrierResult& result) {
+    return internal::WriteBarrier::GetWriteBarrierType(slot, result);
+  }
+
   /**
    * Conservative Dijkstra-style write barrier that processes an object if it
    * has not yet been processed.
    *
-   * \param slot A slot containing the pointer to the object. The slot itself
-   *   must reside in an object that has been allocated using
-   *   `MakeGarbageCollected()`.
-   * \param value The pointer to the object. May be an interior pointer to a
+   * \param result A result retrieved from `GetWriteBarrierType()`.
+   * \param object The pointer to the object. May be an interior pointer to a
    *   an interface of the actual object.
    */
-  static V8_INLINE void DijkstraWriteBarrier(const void* slot,
-                                             const void* value) {
-    internal::WriteBarrier::DijkstraMarkingBarrier(slot, value);
+  static V8_INLINE void DijkstraWriteBarrier(const WriteBarrierResult& result,
+                                             const void* object) {
+    internal::WriteBarrier::DijkstraMarkingBarrier(result, object);
   }
 
   /**
    * Conservative Dijkstra-style write barrier that processes a range of
    * elements if they have not yet been processed.
    *
-   * \param heap_callback A callback to retrieve the corresponding heap if
-   *   necessary.
+   * \param result A result retrieved from `GetWriteBarrierType()`.
+   * \param heap The corresponding heap.
    * \param first_element Pointer to the first element that should be processed.
    *   The slot itself must reside in an object that has been allocated using
    *   `MakeGarbageCollected()`.
@@ -55,13 +66,12 @@ class HeapConsistency final {
    * \param trace_callback The trace callback that should be invoked for each
    *   element if necessary.
    */
-  template <typename LazyHeapCallback>
   static V8_INLINE void DijkstraWriteBarrierRange(
-      LazyHeapCallback heap_callback, const void* first_element,
-      size_t element_size, size_t number_of_elements,
+      const WriteBarrierResult& result, HeapHandle& heap,
+      const void* first_element, size_t element_size, size_t number_of_elements,
       TraceCallback trace_callback) {
     internal::WriteBarrier::DijkstraMarkingBarrierRange(
-        heap_callback, first_element, element_size, number_of_elements,
+        result, heap, first_element, element_size, number_of_elements,
         trace_callback);
   }
 
@@ -69,12 +79,28 @@ class HeapConsistency final {
    * Steele-style write barrier that re-processes an object if it has already
    * been processed.
    *
+   * \param result A result retrieved from `GetWriteBarrierType()`.
    * \param object The pointer to the object which must point to an object that
    *   has been allocated using `MakeGarbageCollected()`. Interior pointers are
    *   not supported.
    */
-  static V8_INLINE void SteeleWriteBarrier(const void* object) {
-    internal::WriteBarrier::SteeleMarkingBarrier(object);
+  static V8_INLINE void SteeleWriteBarrier(const WriteBarrierResult& result,
+                                           const void* object) {
+    internal::WriteBarrier::SteeleMarkingBarrier(result, object);
+  }
+
+  /**
+   * Generational barrier for maintaining consistency when running with multiple
+   * generations.
+   *
+   * \param result A result retrieved from `GetWriteBarrierType()`.
+   * \param slot Slot containing the pointer to the object. The slot itself
+   *   must reside in an object that has been allocated using
+   *   `MakeGarbageCollected()`.
+   */
+  static V8_INLINE void GenerationalBarrier(const WriteBarrierResult& result,
+                                            const void* slot) {
+    internal::WriteBarrier::GenerationalBarrier(result, slot);
   }
 
  private:
