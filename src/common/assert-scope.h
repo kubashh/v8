1bd52526c20b239098764c16eee759227755c3c0
Camillo Bruni <cbruni@chromium.org>
2021-01-08 10:11:52 +0100

    [tools] Add DisableGCMole scope
    Make sure gcmole detects issue in DisallowGarbageCollection scopes.
    
    DisallowGarbageCollection is widely used in the codebase to document
    code that doesn't allocate. However, this has the rather unexpected
    side-effect that gcmole is not run when such a scope is active.
    
    This CL changes the default behavior of gcmole to run even with
    DisallowGarbageCollection scopes present. This will give us the best
    results of both worlds, dynamic checks by the fuzzer, and static
    analysis by gcmole.
    
    To allow crazy local raw pointer operations there is a new
    DisableGCMole scope that explicitly disables gcmole.
    
    Change-Id: I0a78fb3b4ceaad35be9bcf7293d917a41f90c91f

--- src/common/assert-scope.h
+++ src/common/assert-scope.h
@@ -26,7 +26,8 @@ enum PerThreadAssertType {
   HANDLE_ALLOCATION_ASSERT,
   HANDLE_DEREFERENCE_ASSERT,
   CODE_DEPENDENCY_CHANGE_ASSERT,
-  CODE_ALLOCATION_ASSERT
+  CODE_ALLOCATION_ASSERT,
+  DISABLE_GC_MOLE,
 };
 
 enum PerIsolateAssertType {
@@ -35,7 +36,7 @@ enum PerIsolateAssertType {
   JAVASCRIPT_EXECUTION_DUMP,
   DEOPTIMIZATION_ASSERT,
   COMPILATION_ASSERT,
-  NO_EXCEPTION_ASSERT
+  NO_EXCEPTION_ASSERT,
 };
 
 template <PerThreadAssertType kType, bool kAllow>
@@ -191,6 +192,11 @@ using AllowCodeAllocation =
 // DisallowHeapAllocation by also forbidding safepoints.
 using DisallowGarbageCollection =
     CombinationAssertScope<DisallowSafepoints, DisallowHeapAllocation>;
+
+// Scope to skip gc mole verification in places where we do tricky raw
+// work.
+using DisableGCMole = PerThreadAssertScopeDebugOnly<DISABLE_GC_MOLE, true>;
+
 // The DISALLOW_GARBAGE_COLLECTION macro can be used to define a
 // DisallowGarbageCollection field in classes that isn't present in release
 // builds.
@@ -214,15 +220,6 @@ using DisallowHeapAccess =
 using AllowHeapAccess =
     CombinationAssertScope<AllowCodeDependencyChange, AllowHandleDereference,
                            AllowHandleAllocation, AllowHeapAllocation>;
-
-// The DISALLOW_GARBAGE_COLLECTION macro can be used to define a
-// DisallowSafepoints field in classes that isn't present in release
-// builds.
-#ifdef DEBUG
-#define DISALLOW_GARBAGE_COLLECTION(name) DisallowGarbageCollection name;
-#else
-#define DISALLOW_GARBAGE_COLLECTION(name)
-#endif
 
 class DisallowHeapAccessIf {
  public:
