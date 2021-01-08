bff8d38e6855ecaba2239aba4e9762cb2259736b
Sara Tang <sartang@microsoft.com>
2021-01-07 23:11:05 -1000

    Step 2 (of 2) for ETW integration into V8
    Design doc:
    https://docs.google.com/document/d/1xkXj94iExFgLWc_OszTNyNGi523ARaKMWPZTeomhI4U
    
    This is the second (and hopefully final!) change list needed to
    integrate ETW into V8. In particular, we added stack-walking
    functionality for JIT-ted functions!
    
    Some notes on instrumentation:
      - The gist of getting stack-walking in ETW is we need to emit events
        with specific event IDs. These events get stitched into a pseudo-PDB
        that is recognizable by ETW.
      - Unfortunately, we cannot rely on the TraceLogging API from the first
        CL, as it does not support specifying event IDs. Instead, Bill
        Ticehurst wrote an API that peels back the TraceLogging API just
        enough so that we can specify event IDs. This API is the entirety of
        etw-metdata.h
      - We attach a CodeEventHandler that logs a stack-walking event whenever
        code movement is triggered.
    
    Bug: v8:11043
    Change-Id: I1bf57c985b7375f045089027855b1c03878abb78

--- src/d8/d8.cc
+++ src/d8/d8.cc
@@ -3560,6 +3560,12 @@ bool Shell::SetOptions(int argc, char* argv[]) {
     } else if (strcmp(argv[i], "--fuzzy-module-file-extensions") == 0) {
       options.fuzzy_module_file_extensions = true;
       argv[i] = nullptr;
+#ifdef V8_ENABLE_SYSTEM_INSTRUMENTATION
+    } else if (strcmp(argv[i], "--enable-system-instrumentation") == 0) {
+      options.enable_system_instrumentation = true;
+      options.trace_enabled = true;
+      argv[i] = nullptr;
+#endif
     }
   }
 
@@ -4129,8 +4135,8 @@ int Shell::Main(int argc, char* argv[]) {
           ? v8::platform::InProcessStackDumping::kDisabled
           : v8::platform::InProcessStackDumping::kEnabled;
 
-  std::unique_ptr<platform::tracing::TracingController> tracing;
   std::ofstream trace_file;
+  std::unique_ptr<platform::tracing::TracingController> tracing;
   if (options.trace_enabled && !i::FLAG_verify_predictable) {
     tracing = std::make_unique<platform::tracing::TracingController>();
     const char* trace_path =
@@ -4151,10 +4157,23 @@ int Shell::Main(int argc, char* argv[]) {
 
     tracing->InitializeForPerfetto(&trace_file);
 #else
-    platform::tracing::TraceBuffer* trace_buffer =
-        platform::tracing::TraceBuffer::CreateTraceBufferRingBuffer(
-            platform::tracing::TraceBuffer::kRingBufferChunks,
-            platform::tracing::TraceWriter::CreateJSONTraceWriter(trace_file));
+    platform::tracing::TraceBuffer* trace_buffer = nullptr;
+#if defined(V8_ENABLE_SYSTEM_INSTRUMENTATION)
+    if (options.enable_system_instrumentation) {
+      trace_buffer =
+          platform::tracing::TraceBuffer::CreateTraceBufferRingBuffer(
+              platform::tracing::TraceBuffer::kRingBufferChunks,
+              platform::tracing::TraceWriter::
+                  CreateSystemInstrumentationTraceWriter());
+    }
+#endif  // V8_ENABLE_SYSTEM_INSTRUMENTATION
+    if (!trace_buffer) {
+      trace_buffer =
+          platform::tracing::TraceBuffer::CreateTraceBufferRingBuffer(
+              platform::tracing::TraceBuffer::kRingBufferChunks,
+              platform::tracing::TraceWriter::CreateJSONTraceWriter(
+                  trace_file));
+    }
     tracing->Initialize(trace_buffer);
 #endif  // V8_USE_PERFETTO
   }
@@ -4271,6 +4290,9 @@ int Shell::Main(int argc, char* argv[]) {
         } else {
           trace_config =
               platform::tracing::TraceConfig::CreateDefaultTraceConfig();
+          if (options.enable_system_instrumentation) {
+            trace_config->AddIncludedCategory("disabled-by-default-v8.compile");
+          }
         }
         tracing_controller->StartTracing(trace_config);
       }
---
+++ src/libplatform/tracing/recorder-win.cc
@@ -0,0 +1,145 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef V8_LIBPLATFORM_TRACING_RECORDER_WIN_H_
+#define V8_LIBPLATFORM_TRACING_RECORDER_WIN_H_
+
+#include <regex>
+
+#include "src/libplatform/tracing/etw-metadata.h"
+#include "src/libplatform/tracing/recorder.h"
+
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wc++98-compat-extra-semi"
+#endif
+
+#ifndef V8_ETW_GUID
+#define V8_ETW_GUID \
+  0x57277741, 0x3638, 0x4A4B, 0xBD, 0xBA, 0x0A, 0xC6, 0xE4, 0x5D, 0xA5, 0x6C
+#endif
+
+namespace v8 {
+namespace platform {
+namespace tracing {
+
+TRACELOGGING_DECLARE_PROVIDER(g_v8Provider);
+
+TRACELOGGING_DEFINE_PROVIDER(g_v8Provider, "V8.js", (V8_ETW_GUID));
+
+ScriptMapType* isolate_script_map;
+
+Recorder::Recorder() {
+  TraceLoggingRegister(g_v8Provider);
+  isolate_script_map = new ScriptMapType();
+}
+
+Recorder::~Recorder() {
+  if (g_v8Provider) {
+    TraceLoggingUnregister(g_v8Provider);
+  }
+}
+
+bool Recorder::IsEnabled() {
+  return TraceLoggingProviderEnabled(g_v8Provider, 0, 0);
+}
+
+bool Recorder::IsEnabled(const uint8_t level) {
+  return TraceLoggingProviderEnabled(g_v8Provider, level, 0);
+}
+
+void Recorder::AddEvent(TraceObject* trace_event) {
+  // TODO(sartang@microsoft.com): Figure out how to write the conditional
+  // arguments
+  wchar_t* wName = new wchar_t[4096];
+  MultiByteToWideChar(CP_ACP, 0, trace_event->name(), -1, wName, 4096);
+
+  wchar_t* wCategoryGroupName = new wchar_t[4096];
+  MultiByteToWideChar(CP_ACP, 0,
+                      TracingController::GetCategoryGroupName(
+                          trace_event->category_enabled_flag()),
+                      -1, wCategoryGroupName, 4096);
+
+  TraceLoggingWrite(g_v8Provider, "", TraceLoggingValue(wName, "Event Name"),
+                    TraceLoggingValue(trace_event->pid(), "pid"),
+                    TraceLoggingValue(trace_event->tid(), "tid"),
+                    TraceLoggingValue(trace_event->ts(), "ts"),
+                    TraceLoggingValue(trace_event->tts(), "tts"),
+                    TraceLoggingValue(trace_event->phase(), "phase"),
+                    TraceLoggingValue(wCategoryGroupName, "category"),
+                    TraceLoggingValue(trace_event->duration(), "dur"),
+                    TraceLoggingValue(trace_event->cpu_duration(), "tdur"));
+}
+
+void Recorder::CodeEventHandler(const JitCodeEvent* event) {
+  if (!IsEnabled()) return;
+
+  if (event->code_type != v8::JitCodeEvent::CodeType::JIT_CODE) return;
+  if (event->type != v8::JitCodeEvent::EventType::CODE_ADDED) return;
+
+  constexpr uint64_t kJScriptRuntimeKeyword = 1;
+  constexpr static auto source_load_event_meta =
+      EventMetadata(41, kJScriptRuntimeKeyword);
+  constexpr static auto source_load_event_fields = EventFields(
+      "SourceLoad", Field("SourceID", TlgInUINT64),
+      Field("ScriptContextID", TlgInPOINTER), Field("SourceFlags", TlgInUINT32),
+      Field("Url", TlgInUNICODESTRING));
+
+  constexpr static auto method_load_event_meta =
+      EventMetadata(9, kJScriptRuntimeKeyword);
+  constexpr static auto method_load_event_fields = EventFields(
+      "MethodLoad", Field("ScriptContextID", TlgInPOINTER),
+      Field("MethodStartAddress", TlgInPOINTER),
+      Field("MethodSize", TlgInUINT64), Field("MethodID", TlgInUINT32),
+      Field("MethodFlags", TlgInUINT16),
+      Field("MethodAddressRangeID", TlgInUINT16),
+      Field("SourceID", TlgInUINT64), Field("Line", TlgInUINT32),
+      Field("Column", TlgInUINT32), Field("MethodName", TlgInUNICODESTRING));
+
+  int name_len = static_cast<int>(event->name.len);
+  // Note: event->name.str is not null terminated.
+  std::wstring method_name(name_len + 1, '\0');
+  MultiByteToWideChar(
+      CP_UTF8, 0, event->name.str, name_len,
+      // Const cast needed as building with C++14 (not const in >= C++17)
+      const_cast<LPWSTR>(method_name.data()),
+      static_cast<int>(method_name.size()));
+
+  void* script_context = static_cast<void*>(event->isolate);
+  auto script = event->script;
+  if (!script.IsEmpty()) {
+    // if the first time seeing this source file, log the SourceLoad event
+    auto& script_map = (*isolate_script_map)[script_context];
+    if (script_map.find(&script) == script_map.end()) {
+      script_map.insert(&script);
+
+      std::wregex script_name_regex(L".* (.*):(?:\\d+).*");
+      std::wsmatch m;
+      std::wstring script_name = L"[unknown]";
+      if (std::regex_match(method_name, m, script_name_regex)) {
+        script_name = m[1].str();
+      }
+
+      LogEventData(g_v8Provider, &source_load_event_meta,
+                   &source_load_event_fields,
+                   (uint64_t)0,  // ScriptID
+                   script_context,
+                   (uint32_t)0,  // SourceFlags
+                   script_name);
+    }
+  }
+
+  LogEventData(g_v8Provider, &method_load_event_meta, &method_load_event_fields,
+               script_context, event->code_start, (uint64_t)event->code_len,
+               (uint32_t)0,               // MethodId
+               (uint16_t)0,               // MethodFlags
+               (uint16_t)0,               // MethodAddressRangeId
+               (uint64_t)0,               // ScriptID
+               (uint32_t)0, (uint32_t)0,  // Line & Column
+               method_name);
+}
+
+}  // namespace tracing
+}  // namespace platform
+}  // namespace v8
+
+#endif  // V8_LIBPLATFORM_TRACING_RECORDER_WIN_H_
