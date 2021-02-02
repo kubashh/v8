d13f1eee2fc5c1454077fd6a1ec416a677c2eaa0
Leszek Swirski <leszeks@chromium.org>
2021-02-02 15:24:31 +0100

    [sparkplug] Upstream Sparkplug
    Sparkplug is a new baseline, non-optimising second-tier compiler,
    designed to fit in the compiler trade-off space between Ignition and
    TurboProp/TurboFan.
    
    Design doc:
    https://docs.google.com/document/d/13c-xXmFOMcpUQNqo66XWQt3u46TsBjXrHrh4c045l-A/edit?usp=sharing
    
    Change-Id: Ideb7270db3d6548eedd8337a3f596eb6f8fea6b1

--- src/codegen/code-factory.cc
+++ src/codegen/code-factory.cc
@@ -410,6 +410,11 @@ Callable CodeFactory::InterpreterOnStackReplacement(Isolate* isolate) {
 }
 
 // static
+Callable CodeFactory::BaselineEpilogue(Isolate* isolate) {
+  return Builtins::CallableFor(isolate, Builtins::kBaselineEpilogue);
+}
+
+// static
 Callable CodeFactory::ArrayNoArgumentConstructor(
     Isolate* isolate, ElementsKind kind,
     AllocationSiteOverrideMode override_mode) {
