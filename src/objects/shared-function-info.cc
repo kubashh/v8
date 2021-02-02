f0206c7899afc7deefe7e34201858f5e3af11529
Leszek Swirski <leszeks@chromium.org>
2021-02-02 15:20:44 +0100

    [sparkplug] Upstream Sparkplug
    Sparkplug is a new baseline, non-optimising second-tier compiler,
    designed to fit in the compiler trade-off space between Ignition and
    TurboProp/TurboFan.
    
    Design doc:
    https://docs.google.com/document/d/13c-xXmFOMcpUQNqo66XWQt3u46TsBjXrHrh4c045l-A/edit?usp=sharing
    
    Change-Id: Ideb7270db3d6548eedd8337a3f596eb6f8fea6b1

--- src/objects/shared-function-info.cc
+++ src/objects/shared-function-info.cc
@@ -33,6 +33,7 @@ void SharedFunctionInfo::Init(ReadOnlyRoots ro_roots, int unique_id) {
   // of "uninitialized" marker here, but it's cheaper to use a valid buitin and
   // avoid having to do uninitialized checks elsewhere.
   set_builtin_id(Builtins::kIllegal);
+  set_sparkplug_code(ro_roots.undefined_value());
 
   // Set the name to the no-name sentinel, this can be updated later.
   set_name_or_scope_info(SharedFunctionInfo::kNoSharedNameSentinel,
@@ -413,6 +414,9 @@ std::ostream& operator<<(std::ostream& os, const SourceCodeOf& v) {
 MaybeHandle<Code> SharedFunctionInfo::TryGetCachedCode(Isolate* isolate) {
   if (!may_have_cached_code()) return {};
   Handle<SharedFunctionInfo> zis(*this, isolate);
+  if (!zis->sparkplug_code().IsUndefined(isolate)) {
+    return handle(Code::cast(zis->sparkplug_code()), isolate);
+  }
   return isolate->compilation_cache()->LookupCode(zis);
 }
 
