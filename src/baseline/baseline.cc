8f786c7947d5eb8540d1fa247fee811dcafbd37b
Leszek Swirski <leszeks@chromium.org>
2021-02-02 15:39:51 +0100

    [sparkplug] Upstream Sparkplug
    Sparkplug is a new baseline, non-optimising second-tier compiler,
    designed to fit in the compiler trade-off space between Ignition and
    TurboProp/TurboFan.
    
    Design doc:
    https://docs.google.com/document/d/13c-xXmFOMcpUQNqo66XWQt3u46TsBjXrHrh4c045l-A/edit?usp=sharing
    
    Change-Id: Ideb7270db3d6548eedd8337a3f596eb6f8fea6b1

---
+++ src/baseline/baseline.cc
@@ -0,0 +1,76 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/baseline/baseline.h"
+
+#include "src/baseline/baseline-compiler.h"
+#include "src/logging/counters.h"
+#include "src/heap/factory-inl.h"
+#include "src/objects/shared-function-info-inl.h"
+#include "src/objects/script-inl.h"
+
+namespace v8 {
+namespace internal {
+
+Handle<Code> CompileWithBaseline(Isolate* isolate, int formal_parameter_count,
+                                 Handle<BytecodeArray> bytecode) {
+  RuntimeCallTimerScope runtimeTimer(
+      isolate, RuntimeCallCounterId::kCompileWithBaseline);
+  BaselineCompiler compiler(isolate, formal_parameter_count, bytecode);
+
+  compiler.GenerateCode();
+
+  return compiler.Build(isolate);
+}
+
+Handle<Code> CompileWithBaseline(Isolate* isolate,
+                                 Handle<SharedFunctionInfo> shared) {
+  if (shared->HasBaselineData()) {
+    return handle(shared->baseline_data().baseline_code(), isolate);
+  }
+
+  if (FLAG_trace_opt) {
+    PrintF("[compiling method ");
+    shared->ShortPrint();
+    PrintF(" using Sparkplug]\n");
+  }
+
+  base::ElapsedTimer timer;
+  timer.Start();
+
+  Handle<Code> code =
+      CompileWithBaseline(isolate, shared->internal_formal_parameter_count(),
+                          handle(shared->GetBytecodeArray(isolate), isolate));
+
+  Handle<BaselineData> baseline_data = Handle<BaselineData>::cast(
+      isolate->factory()->NewStruct(BASELINE_DATA_TYPE, AllocationType::kOld));
+  baseline_data->set_baseline_code(*code);
+  baseline_data->set_data(
+      HeapObject::cast(shared->function_data(kAcquireLoad)));
+
+  shared->set_baseline_data(*baseline_data);
+
+  if (FLAG_print_code) {
+    code->Print();
+  }
+
+  if (shared->script().IsScript()) {
+    Compiler::LogFunctionCompilation(isolate,
+      CodeEventListener::LAZY_COMPILE_TAG, shared,
+      handle(Script::cast(shared->script()), isolate),
+      Handle<AbstractCode>::cast(code), false, timer.Elapsed().InMillisecondsF());
+  }
+
+  if (FLAG_trace_opt) {
+    PrintF("[completed compiling ");
+    shared->ShortPrint();
+    PrintF(" using Sparkplug - took %0.3f ms]\n",
+           timer.Elapsed().InMillisecondsF());
+  }
+
+  return code;
+}
+
+}  // namespace internal
+}  // namespace v8
---
+++ src/baseline/x64/baseline-compiler-x64-inl.h
@@ -0,0 +1,226 @@
+// Copyright 2021 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_BASELINE_X64_BASELINE_COMPILER_X64_H_
+#define V8_BASELINE_X64_BASELINE_COMPILER_X64_H_
+
+#include "src/baseline/baseline-compiler.h"
+#include "src/objects/code-inl.h"
+
+namespace v8 {
+namespace internal {
+
+namespace {
+
+#ifdef DEBUG
+bool Clobbers(Register target, MemOperand op) {
+  return op.AddressUsesRegister(target);
+}
+#endif
+
+}  // namespace
+
+MemOperand BaselineAssembler::RegisterFrameOperand(
+    interpreter::Register interpreter_register) {
+  return MemOperand(rbp, interpreter_register.ToOperand() * kSystemPointerSize);
+}
+
+void BaselineAssembler::Move(interpreter::Register output, Register source) {
+  return movq(RegisterFrameOperand(output), source);
+}
+void BaselineAssembler::CallBuiltin(Builtins::Name builtin) {
+  call(EntryFromBuiltinIndexAsOperand(builtin));
+}
+
+void BaselineAssembler::TailCallBuiltin(Builtins::Name builtin) {
+  jmp(EntryFromBuiltinIndexAsOperand(builtin));
+}
+
+void BaselineAssembler::LoadTaggedPointerField(Register output, Register source,
+                                               int offset) {
+  MacroAssembler::LoadTaggedPointerField(output, FieldOperand(source, offset));
+}
+void BaselineAssembler::LoadTaggedSignedField(Register output, Register source,
+                                              int offset) {
+  MacroAssembler::LoadTaggedSignedField(output, FieldOperand(source, offset));
+}
+void BaselineAssembler::LoadTaggedAnyField(Register output, Register source,
+                                           int offset) {
+  LoadAnyTaggedField(output, FieldOperand(source, offset));
+}
+void BaselineAssembler::StoreTaggedSignedField(Register target, int offset,
+                                               Smi value) {
+  StoreTaggedField(FieldOperand(target, offset), Immediate(value));
+}
+void BaselineAssembler::StoreTaggedFieldWithWriteBarrier(Register target,
+                                                         int offset,
+                                                         Register value,
+                                                         Register scratch) {
+  DCHECK_NE(target, scratch);
+  DCHECK_NE(value, scratch);
+  StoreTaggedField(FieldOperand(target, offset), value);
+  RecordWriteField(target, offset, value, scratch, kDontSaveFPRegs);
+}
+void BaselineAssembler::StoreTaggedFieldNoWriteBarrier(Register target,
+                                                       int offset,
+                                                       Register value) {
+  StoreTaggedField(FieldOperand(target, offset), value);
+}
+
+void BaselineAssembler::AddSmi(Register lhs, Smi rhs) {
+  addl(lhs, Immediate(rhs));
+}
+void BaselineAssembler::AddSmi(Register lhs, Register rhs) { addl(lhs, rhs); }
+void BaselineAssembler::SubSmi(Register lhs, Smi rhs) {
+  subl(lhs, Immediate(rhs));
+}
+void BaselineAssembler::SubSmi(Register lhs, Register rhs) { subl(lhs, rhs); }
+void BaselineAssembler::MulSmi(Register lhs, Register rhs) {
+  DCHECK_EQ(lhs, rax);
+  DCHECK_NE(lhs, rhs);
+  SmiUntag(lhs);
+  mull(rhs);
+}
+void BaselineAssembler::BitwiseOrSmi(Register lhs, Smi rhs) {
+  orl(lhs, Immediate(rhs));
+}
+void BaselineAssembler::BitwiseOrSmi(Register lhs, Register rhs) {
+  orl(lhs, rhs);
+}
+void BaselineAssembler::BitwiseXorSmi(Register lhs, Smi rhs) {
+  xorl(lhs, Immediate(rhs));
+}
+void BaselineAssembler::BitwiseXorSmi(Register lhs, Register rhs) {
+  xorl(lhs, rhs);
+}
+void BaselineAssembler::BitwiseAndSmi(Register lhs, Smi rhs) {
+  andl(lhs, Immediate(rhs));
+}
+void BaselineAssembler::BitwiseAndSmi(Register lhs, Register rhs) {
+  andl(lhs, rhs);
+}
+void BaselineAssembler::ShiftLeftSmi(Register lhs, int32_t rhs) {
+  shll(lhs, Immediate(rhs));
+}
+void BaselineAssembler::ShiftRightSmi(Register lhs, int32_t rhs) {
+  sarl(lhs, Immediate(rhs));
+}
+void BaselineAssembler::ShiftRightLogicalSmi(Register lhs, int32_t rhs) {
+  shrl(lhs, Immediate(rhs));
+}
+
+void BaselineAssembler::Switch(Register reg, int case_value_base,
+                               Label** labels, int num_labels) {
+  Label fallthrough, jump_table;
+  if (case_value_base > 0) {
+    subq(reg, Immediate(case_value_base));
+  }
+  cmpq(reg, Immediate(num_labels));
+  j(above_equal, &fallthrough);
+  leaq(kScratchRegister, MemOperand(&jump_table));
+  jmp(MemOperand(kScratchRegister, reg, times_8, 0));
+  // Emit the jump table inline, under the assumption that it's not too big.
+  Align(kSystemPointerSize);
+  bind(&jump_table);
+  for (int i = 0; i < num_labels; ++i) {
+    dq(labels[i]);
+  }
+  bind(&fallthrough);
+}
+
+#define __ masm_.
+
+void BaselineCompiler::Prologue() {
+  __ Move(kInterpreterBytecodeArrayRegister, bytecode_);
+  DCHECK_EQ(kJSFunctionRegister, kJavaScriptCallTargetRegister);
+  CallBuiltin(Builtins::kBaselinePrologue, kContextRegister,
+              kJSFunctionRegister, kJavaScriptCallArgCountRegister,
+              kInterpreterBytecodeArrayRegister);
+
+  FrameScope frame_scope(&masm_, StackFrame::MANUAL);
+  __ Push(rbp);  // Caller's frame pointer.
+  __ Move(rbp, rsp);
+
+  // BaselinePrologue has already pushed everything on the stack,
+  // just increase the SP by stackframe size anc continue
+  __ subq(rsp, Immediate(kSystemPointerSize * 5));
+
+  // Stack check. This folds the checks for both the interrupt stack limit
+  // check and the real stack limit into one by just checking for the
+  // interrupt limit. The interrupt limit is either equal to the real stack
+  // limit or tighter. By ensuring we have space until that limit after
+  // building the frame we can quickly precheck both at once.
+  __ Move(kScratchRegister, rsp);
+  __ subq(kScratchRegister, Immediate(bytecode_->frame_size()));
+  __ cmpq(kScratchRegister,
+          __ StackLimitAsOperand(StackLimitKind::kInterruptStackLimit));
+  Label after_stack_check;
+  __ j(above_equal, &after_stack_check, Label::kNear);
+
+  __ RecordComment("[ Stack/interrupt check");
+  // Save incoming new target or generator
+  __ Push(rdx);
+  __ CallRuntime(Runtime::kStackGuard);
+  AddAsStartPosition();
+  __ Pop(rdx);
+  __ RecordComment("]");
+
+  __ bind(&after_stack_check);
+
+  PrologueFillFrame();
+}
+
+void BaselineCompiler::PrologueFillFrame() {
+  __ RecordComment("[ Fill frame");
+  // Inlined register frame fill
+  interpreter::Register new_target_or_generator_register =
+      bytecode_->incoming_new_target_or_generator_register();
+  __ LoadRoot(kInterpreterAccumulatorRegister, RootIndex::kUndefinedValue);
+  int register_count = bytecode_->register_count();
+  // Magic value
+  const int kLoopUnrollSize = 8;
+  const int new_target_index = new_target_or_generator_register.index();
+  const bool has_new_target = new_target_index != kMaxInt;
+  int i = 0;
+  if (has_new_target) {
+    DCHECK_LE(new_target_index, register_count);
+    for (; i < new_target_index; i++) {
+      __ Push(kInterpreterAccumulatorRegister);
+    }
+    // Push new_target_or_generator.
+    __ Push(rdx);
+    i++;
+    // printf("%i\n", new_target_index);
+  }
+  if (register_count < 2 * kLoopUnrollSize) {
+    // If the frame is small enough, just unroll the frame fill completely.
+    for (; i < register_count; ++i) {
+      __ Push(kInterpreterAccumulatorRegister);
+    }
+  } else {
+    register_count -= i;
+    i = 0;
+    // Extract the first few registers to round to the unroll size.
+    int first_registers = register_count % kLoopUnrollSize;
+    for (; i < first_registers; ++i) {
+      __ Push(kInterpreterAccumulatorRegister);
+    }
+    __ Move(kScratchRegister, Immediate(register_count / kLoopUnrollSize));
+    Label loop;
+    __ bind(&loop);
+    for (int j = 0; j < kLoopUnrollSize; ++j) {
+      __ Push(kInterpreterAccumulatorRegister);
+    }
+    __ decl(kScratchRegister);
+    __ j(not_zero, &loop);
+  }
+  __ RecordComment("]");
+}
+
+#undef __
+
+}  // namespace internal
+}  // namespace v8
+
+#endif  // V8_BASELINE_X64_BASELINE_COMPILER_X64_H_
