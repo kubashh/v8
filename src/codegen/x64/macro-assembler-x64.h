c32a6879284ad392cd732cce02ba15e43c829118
Ng Zhi An <zhin@chromium.org>
2021-02-08 15:19:23 -0800

    [wasm-simd][x64][liftoff] Implement double precision conversions
    Extract codegen into macro-assembler functions for reuse in Liftoff.
    
    Some minor tweaks in I32x4TruncSatF64x2SZero and I32x4TruncSatF64x2UZero
    to check dst and src overlap and move to scratch/dst accordingly. In
    TurboFan we can set these restrictions in the instruction-selector, but
    not in Liftoff. This doesn't make TurboFan codegen any worse, since
    those restrictions are still in place.
    
    Bug: v8:11265
    Change-Id: Ib6b3ebeb5fed99eddd0700fb4aba91d4168c3213

--- src/codegen/x64/macro-assembler-x64.h
+++ src/codegen/x64/macro-assembler-x64.h
@@ -608,6 +608,10 @@ class V8_EXPORT_PRIVATE TurboAssembler : public TurboAssemblerBase {
   void S128Store32Lane(Operand dst, XMMRegister src, uint8_t laneidx);
   void S128Store64Lane(Operand dst, XMMRegister src, uint8_t laneidx);
 
+  void F64x2ConvertLowI32x4U(XMMRegister dst, XMMRegister src);
+  void I32x4TruncSatF64x2SZero(XMMRegister dst, XMMRegister src);
+  void I32x4TruncSatF64x2UZero(XMMRegister dst, XMMRegister src);
+
   void Abspd(XMMRegister dst);
   void Negpd(XMMRegister dst);
 
