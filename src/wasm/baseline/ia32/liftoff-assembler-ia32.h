ff6f9010e29981f3546b30ed8fa7dd299a15a6fc
Ng Zhi An <zhin@chromium.org>
2021-02-09 13:29:39 -0800

    [wasm-simd][liftoff][x64] Implement v64x2.alltrue and i64x2.ne
    Other archs will come later.
    
    Bug: v8:11347,v8:11348
    Change-Id: I9ea656b9c7ce03c9dafb631dd67f6e2f7d4346a2

--- src/wasm/baseline/ia32/liftoff-assembler-ia32.h
+++ src/wasm/baseline/ia32/liftoff-assembler-ia32.h
@@ -3102,6 +3102,11 @@ void LiftoffAssembler::emit_i64x2_eq(LiftoffRegister dst, LiftoffRegister lhs,
       this, dst, lhs, rhs, SSE4_1);
 }
 
+void LiftoffAssembler::emit_i64x2_ne(LiftoffRegister dst, LiftoffRegister lhs,
+                                     LiftoffRegister rhs) {
+  bailout(kSimd, "i64x2_ne");
+}
+
 void LiftoffAssembler::emit_f32x4_eq(LiftoffRegister dst, LiftoffRegister lhs,
                                      LiftoffRegister rhs) {
   liftoff::EmitSimdCommutativeBinOp<&Assembler::vcmpeqps, &Assembler::cmpeqps>(
@@ -3808,6 +3813,11 @@ void LiftoffAssembler::emit_i64x2_neg(LiftoffRegister dst,
     psubq(reg, src.fp());
     if (dst.fp() != reg) movaps(dst.fp(), reg);
   }
+}
+
+void LiftoffAssembler::emit_v64x2_alltrue(LiftoffRegister dst,
+                                          LiftoffRegister src) {
+  bailout(kSimd, "v64x2_alltrue");
 }
 
 void LiftoffAssembler::emit_i64x2_shl(LiftoffRegister dst, LiftoffRegister lhs,
