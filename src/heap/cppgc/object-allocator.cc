cbde4d4fd1d49e30d70da8c95cab7cc74c5931ff
Michael Lippautz <mlippautz@chromium.org>
2021-02-15 21:51:14 +0100

    cppgc: Fix allocation during sweeping
    Lazy sweeping may have found a memory block not positioned at the head
    of the corresponding bucket. Such a block is not found during a
    subsequent free list allocation, as such allocations do not linearly
    walk the free list.
    
    Bug: chromium:1056170
    Change-Id: I288b6ad768987705d86fc78d0aa6fe46e99417b9

--- src/heap/cppgc/object-allocator.cc
+++ src/heap/cppgc/object-allocator.cc
@@ -138,13 +138,19 @@ void* ObjectAllocator::OutOfLineAllocateImpl(NormalPageSpace* space,
   // this allocation or we finish sweeping all pages of this heap.
   Sweeper& sweeper = raw_heap_->heap()->sweeper();
   if (sweeper.SweepForAllocationIfRunning(space, size)) {
-    void* result = AllocateFromFreeList(space, size, gcinfo);
-    DCHECK_NOT_NULL(result);
-    return result;
+    // Sweeper found a block of at least `size` bytes. Allocation from the free
+    // list may still fail as actual  buckets are not exhaustively searched for
+    // a suitable block. Instead, buckets are tested from larger sizes to
+    // smaller sizes. For the bucket that may exactly fit the allocation of
+    // `size` bytes (no overallocation), only the first entry is checked.
+    if (void* result = AllocateFromFreeList(space, size, gcinfo)) {
+      return result;
+    }
   }
 
   // 4. Complete sweeping.
   sweeper.FinishIfRunning();
+  // TODO(chromium:1056170): Make use of the synchronously freed memory.
 
   // 5. Add a new page to this heap.
   auto* new_page = NormalPage::Create(page_backend_, space);
