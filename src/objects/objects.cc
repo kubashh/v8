589c0962f57bbc5d8a24781f097dcdd81323e27d
Frank Emrich <emrich@google.com>
2021-02-25 18:47:03 +0100

    [dict-proto] C++ implementation of SwissNameDictionary, pt. 7
    This CL is part of a series that adds the C++ implementation of
    SwissNameDictionary, a deterministic property backing store based on
    Swiss Tables.
    
    This CL adds an in-place version of Rehash and a small change to
    HeapObject::HeapObjectShortPrint.
    
    Bug: v8:11388
    Change-Id: I578ef8f5638a6201111602ffef7f2d4a5a257bcd

--- src/objects/objects.cc
+++ src/objects/objects.cc
@@ -1960,6 +1960,14 @@ void HeapObject::HeapObjectShortPrint(std::ostream& os) {  // NOLINT
     case NAME_DICTIONARY_TYPE:
       os << "<NameDictionary[" << FixedArray::cast(*this).length() << "]>";
       break;
+    case SWISS_NAME_DICTIONARY_TYPE: {
+      int size = SwissNameDictionary::SizeFor(
+          SwissNameDictionary::cast(*this).Capacity());
+      // To produce output similar to FixedArray-based tables, print space
+      // occupied by non-header data in multiples of kTaggedSize.
+      int length = (size - kHeaderSize) / kTaggedSize;
+      os << "<SwissNameDictionary[" << length << "]>";
+    } break;
     case GLOBAL_DICTIONARY_TYPE:
       os << "<GlobalDictionary[" << FixedArray::cast(*this).length() << "]>";
       break;
@@ -2351,6 +2359,7 @@ bool HeapObject::NeedsRehashing(InstanceType instance_type) const {
     case SMALL_ORDERED_HASH_MAP_TYPE:
     case SMALL_ORDERED_HASH_SET_TYPE:
     case SMALL_ORDERED_NAME_DICTIONARY_TYPE:
+    case SWISS_NAME_DICTIONARY_TYPE:
     case JS_MAP_TYPE:
     case JS_SET_TYPE:
       return true;
@@ -2374,6 +2383,7 @@ bool HeapObject::CanBeRehashed() const {
     case GLOBAL_DICTIONARY_TYPE:
     case NUMBER_DICTIONARY_TYPE:
     case SIMPLE_NUMBER_DICTIONARY_TYPE:
+    case SWISS_NAME_DICTIONARY_TYPE:
       return true;
     case DESCRIPTOR_ARRAY_TYPE:
     case STRONG_DESCRIPTOR_ARRAY_TYPE:
@@ -2398,6 +2408,9 @@ void HeapObject::RehashBasedOnMap(Isolate* isolate) {
       UNREACHABLE();
     case NAME_DICTIONARY_TYPE:
       NameDictionary::cast(*this).Rehash(isolate);
+      break;
+    case SWISS_NAME_DICTIONARY_TYPE:
+      SwissNameDictionary::cast(*this).Rehash(isolate);
       break;
     case GLOBAL_DICTIONARY_TYPE:
       GlobalDictionary::cast(*this).Rehash(isolate);
