5670aa22b393e81b67b3e1cc17bea0b2f26ee926
Michael Lippautz <mlippautz@chromium.org>
2021-03-05 00:10:28 +0100

    cppgc: Add explicit memory management functions
    Change-Id: I7f2d86d9639e8b805f79a8fd0a346903f63171e5

---
+++ include/cppgc/explicit-management.h
@@ -0,0 +1,39 @@
+// Copyright 2021 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef INCLUDE_CPPGC_EXPLICIT_MANAGEMENT_H_
+#define INCLUDE_CPPGC_EXPLICIT_MANAGEMENT_H_
+
+#include <cstddef>
+
+#include "cppgc/internal/logging.h"
+#include "cppgc/type-traits.h"
+
+namespace cppgc {
+namespace internal {
+
+bool TryFree(void*);
+
+}  // namespace internal
+
+namespace subtle {
+
+/**
+ * Tries to immediately reclaim `object`.
+ *
+ * \param object Reference to an object that is of type `GarbageCollected` and
+ *   should be immediately reclaimed.
+ * \param returns whether the `object` was reclaimed by the garbage collector.
+ */
+template <typename T>
+bool TryFree(T* object) {
+  static_assert(IsGarbageCollectedTypeV<T>,
+                "Object must be of type GarbageCollected.");
+  return internal::TryFree(object);
+}
+
+}  // namespace subtle
+}  // namespace cppgc
+
+#endif  // INCLUDE_CPPGC_EXPLICIT_MANAGEMENT_H_
