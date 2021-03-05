1c15c4b52b99e703bf79c50aafcdf6a8da738e56
Michael Lippautz <mlippautz@chromium.org>
2021-03-05 09:25:23 +0100

    cppgc: Add explicit TryFree() call
    Add an explicit TryFree() call that can be used to reclaim objects
    that are guaranteed to not be referenced anymore by the embedder. It
    is up to the embedder to ensure correctness.
    
    Change-Id: I7f2d86d9639e8b805f79a8fd0a346903f63171e5
    Bug: chromium:1056170

---
+++ include/cppgc/explicit-management.h
@@ -0,0 +1,43 @@
+// Copyright 2021 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef INCLUDE_CPPGC_EXPLICIT_MANAGEMENT_H_
+#define INCLUDE_CPPGC_EXPLICIT_MANAGEMENT_H_
+
+#include <cstddef>
+
+#include "cppgc/internal/logging.h"
+#include "cppgc/type-traits.h"
+
+namespace cppgc {
+namespace internal {
+
+V8_EXPORT bool TryFree(const void*);
+
+}  // namespace internal
+
+namespace subtle {
+
+/**
+ * Tries to immediately reclaim `object`. It is up to the embedder to guarantee
+ * that no other object holds a reference to `object` after calling `TryFree()`.
+ *
+ * \param object Reference to an object that is of type `GarbageCollected` and
+ *   should be immediately reclaimed.
+ * \param returns whether the `object` was reclaimed by the garbage collector.
+ */
+template <typename T>
+bool TryFree(const T* object) {
+  static_assert(IsGarbageCollectedTypeV<T>,
+                "Object must be of type GarbageCollected.");
+  if (!object) {
+    return true;
+  }
+  return internal::TryFree(object);
+}
+
+}  // namespace subtle
+}  // namespace cppgc
+
+#endif  // INCLUDE_CPPGC_EXPLICIT_MANAGEMENT_H_
