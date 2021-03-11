d2a263e626943a4fe1f166164fd75fadc33115e6
Frank Emrich <emrich@google.com>
2021-03-11 11:07:11 +0100

    [csa] Make CTZ, CLZ, and POPCOUNT available in CSA
    This CL makes CTZ (count trailing zeros) and POPCOUNT (count set bits),
    which are optional ops in the raw machine assembler, available in CSA.
    A fallback exists for the case that they are not available.
    
    This CL also adds the 64 bit version of the mandatory CLZ (count
    leading zeros) op available.
    
    Change-Id: I53cd6738b8ede8bd5842a83bb1161299824d39c9

--- src/codegen/code-stub-assembler.cc
+++ src/codegen/code-stub-assembler.cc
@@ -549,6 +549,97 @@ TNode<Float64T> CodeStubAssembler::Float64Trunc(TNode<Float64T> x) {
   return var_x.value();
 }
 
+TNode<Int64T> CodeStubAssembler::Word64PopulationCount(
+    TNode<Word64T> value_word) {
+  if (IsWord64PopcntSupported()) {
+    return Word64Popcnt(value_word);
+  }
+
+  // Taken from slow path of base::bits::CountPopulation, the comments here show
+  // C++ code and comments from there for reference.
+  // Fall back to divide-and-conquer popcount (see "Hacker's Delight" by Henry
+  // S. Warren,  Jr.), chapter 5-1.
+  constexpr uint64_t mask[] = {0x5555555555555555, 0x3333333333333333,
+                               0x0f0f0f0f0f0f0f0f};
+
+  TNode<Uint64T> value = Unsigned(value_word);
+  TNode<Uint64T> lhs, rhs;
+  // Start with 64 buckets of 1 bits, holding values from [0,1].
+  // {value = ((value >> 1) & mask[0]) + (value & mask[0])}
+  lhs = Word64And(Word64Shr(value, Uint64Constant(1)), Uint64Constant(mask[0]));
+  rhs = Word64And(value, Uint64Constant(mask[0]));
+  value = Uint64Add(lhs, rhs);
+
+  // Having 32 buckets of 2 bits, holding values from [0,2] now.
+  // {value = ((value >> 2) & mask[1]) + (value & mask[1])}
+  lhs = Word64And(Word64Shr(value, Uint64Constant(2)), Uint64Constant(mask[1]));
+  rhs = Word64And(value, Uint64Constant(mask[1]));
+  value = Uint64Add(lhs, rhs);
+
+  // Having 16 buckets of 4 bits, holding values from [0,4] now.
+  // {value = ((value >> 4) & mask[2]) + (value & mask[2])}
+  lhs = Word64And(Word64Shr(value, Uint64Constant(4)), Uint64Constant(mask[2]));
+  rhs = Word64And(value, Uint64Constant(mask[2]));
+  value = Uint64Add(lhs, rhs);
+
+  // Having 8 buckets of 8 bits, holding values from [0,8] now.
+  // From this point on, the buckets are bigger than the number of bits
+  // required to hold the values, and the buckets are bigger the maximum
+  // result, so there's no need to mask value anymore, since there's no
+  // more risk of overflow between buckets.
+  // {value = (value >> 8) + value}
+  lhs = Word64Shr(value, Uint64Constant(8));
+  value = Uint64Add(lhs, value);
+
+  // Having 4 buckets of 16 bits, holding values from [0,16] now.
+  // {value = (value >> 16) + value}
+  lhs = Word64Shr(value, Uint64Constant(16));
+  value = Uint64Add(lhs, value);
+
+  // Having 2 buckets of 32 bits, holding values from [0,32] now.
+  // {value = (value >> 32) + value}
+  lhs = Word64Shr(value, Uint64Constant(32));
+  value = Uint64Add(lhs, value);
+
+  // Having 1 buckets of 64 bits, holding values from [0,64] now.
+  // {return static_cast<unsigned>(value & 0xff)}
+  return Signed(Word64And(value, Uint64Constant(0xff)));
+}
+
+TNode<Int32T> CodeStubAssembler::Word32PopulationCount(TNode<Word32T> value) {
+  if (IsWord32PopcntSupported()) {
+    return Word32Popcnt(value);
+  }
+
+  TNode<Int64T> res64 = Word64Popcnt(ChangeUint32ToUint64(value));
+  return TruncateInt64ToInt32(Signed(res64));
+}
+
+TNode<Int64T> CodeStubAssembler::Word64CountTrailingZeros(
+    TNode<Word64T> value) {
+  if (IsWord64CtzSupported()) {
+    return Word64Ctz(value);
+  }
+
+  // Same fallback as in base::bits::CountTrailingZeros.
+  // Fall back to popcount (see "Hacker's Delight" by Henry S. Warren, Jr.),
+  // chapter 5-4. On x64, since is faster than counting in a loop and faster
+  // than doing binary search.
+  TNode<Word64T> lhs = Word64Not(value);
+  TNode<Word64T> rhs = Uint64Sub(Unsigned(value), Uint64Constant(1));
+  return Word64Popcnt(Word64And(lhs, rhs));
+}
+
+TNode<Int32T> CodeStubAssembler::Word32CountTrailingZeros(
+    TNode<Word32T> value) {
+  if (IsWord32CtzSupported()) {
+    return Word32Ctz(value);
+  }
+
+  TNode<Int64T> res64 = Word64Ctz(ChangeUint32ToUint64(value));
+  return TruncateInt64ToInt32(Signed(res64));
+}
+
 template <>
 TNode<Smi> CodeStubAssembler::TaggedToParameter(TNode<Smi> value) {
   return value;
