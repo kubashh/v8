716b91f4592e0721a1fd93335c010cbe57987b42
Andreas Haas <ahaas@chromium.org>
2021-03-15 13:42:13 +0100

    Reland "[wasm] Emit safepoint info for callee-saved registers in the deopt-index"
    The original CL was reverted because PC authentication was missing for
    the `caller_pc` in the stack walk. This caused a crash on the CFI bot.
    
    PS1 is the original CL, later patch sets contain the fix.
    
    Original Message:
    
    [wasm] Emit safepoint info for callee-saved registers in the deopt-index
    
    Encode safepoint info of callee-saved registers in the deopt index of
    the normal safepoint.
    
    R=clemensb@chromium.org, jkummerow@chromium.org
    
    Change-Id: I633cd715eccc697e888cd381e3bda1a47d0d0851

--- src/wasm/baseline/liftoff-compiler.cc
+++ src/wasm/baseline/liftoff-compiler.cc
@@ -1027,8 +1027,7 @@ class LiftoffCompiler {
     source_position_table_builder_.AddPosition(
         __ pc_offset(), SourcePosition(decoder->position()), true);
     __ CallRuntimeStub(WasmCode::kWasmDebugBreak);
-    // TODO(ahaas): Define a proper safepoint here.
-    safepoint_table_builder_.DefineSafepoint(&asm_);
+    DefineSafepointWithCalleeSavedRegisters();
     RegisterDebugSideTableEntry(decoder,
                                 DebugSideTableBuilder::kAllowRegisters);
   }
@@ -5907,6 +5906,11 @@ class LiftoffCompiler {
   void DefineSafepoint() {
     Safepoint safepoint = safepoint_table_builder_.DefineSafepoint(&asm_);
     __ cache_state()->DefineSafepoint(safepoint);
+  }
+
+  void DefineSafepointWithCalleeSavedRegisters() {
+    Safepoint safepoint = safepoint_table_builder_.DefineSafepoint(&asm_);
+    __ cache_state()->DefineSafepointWithCalleeSavedRegisters(safepoint);
   }
 
   Register LoadInstanceIntoRegister(LiftoffRegList pinned, Register fallback) {
