fc09ff00e559b0fd2f9e69edfb8ddbddb94f2628
Ross McIlroy <rmcilroy@chromium.org>
2021-04-27 15:50:39 +0100

    [Not For Review] Enable TurboProp as top-tier for testing
    Change-Id: I3194aceb6c1b7a7292ffffaa80eb876c0ee565c0
    
    
    [Not For Review] Enable TurboProp with TF tiering for testing
    
    Change-Id: I6bc828daa567488cf2d3ef25b740bcf066ebb102

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -593,11 +593,11 @@ DEFINE_BOOL(trace_migration, false, "trace object migration")
 DEFINE_BOOL(trace_generalization, false, "trace map generalization")
 
 // Flags for TurboProp.
-DEFINE_BOOL(turboprop, false, "enable experimental turboprop mid-tier compiler")
+DEFINE_BOOL(turboprop, true, "enable experimental turboprop mid-tier compiler")
 DEFINE_BOOL(turboprop_mid_tier_reg_alloc, true,
             "enable mid-tier register allocator for turboprop")
 DEFINE_BOOL(
-    turboprop_as_toptier, false,
+    turboprop_as_toptier, true,
     "enable experimental turboprop compiler without further tierup to turbofan")
 DEFINE_IMPLICATION(turboprop_as_toptier, turboprop)
 DEFINE_VALUE_IMPLICATION(turboprop, interrupt_budget, 20 * KB)
