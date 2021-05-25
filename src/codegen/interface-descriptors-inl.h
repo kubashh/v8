a4c2bdece0ed871793531da087bb2d486e557c0c
Camillo Bruni <cbruni@chromium.org>
2021-05-25 23:05:55 +0200

    [builtins][x64] Use callee-saved registers for write barrier stubs
    Calls to the record write stub are quite frequent and the caller has to
    save all registers used by the builtin.
    
    This CL moves the register saving to the builtin itself, reducing the
    call-site code size significantly in many cases.
    
    Follow-up CLs with introduce the same behaviour to other platforms.
    
    - CallRecordWriteStubSaveRegisters preserves the existing behaviour and
      saves clobbered registers.
    - CallRecordWriteStub expects the registers to match the ones specified
      in the WriteBarrierDescriptor for more compact code.
    - RegisterArray has to be a unique list
    
    
    
    Bug: v8:11420
    Change-Id: I3a0c500fbe26f82ee2243a61dbf574fd31656982

--- src/codegen/interface-descriptors-inl.h
+++ src/codegen/interface-descriptors-inl.h
@@ -69,6 +69,8 @@ void StaticCallInterfaceDescriptor<DerivedDescriptor>::Initialize(
 
   if (DerivedDescriptor::kRestrictAllocatableRegisters) {
     data->RestrictAllocatableRegisters(registers.data(), registers.size());
+  } else {
+    DCHECK(!DerivedDescriptor::kCalleeSaveRegisters);
   }
 
   data->InitializeRegisters(
@@ -184,6 +186,33 @@ constexpr Register FastNewObjectDescriptor::TargetRegister() {
 // static
 constexpr Register FastNewObjectDescriptor::NewTargetRegister() {
   return kJavaScriptCallNewTargetRegister;
+}
+
+// static
+constexpr Register WriteBarrierDescriptor::ObjectRegister() {
+  return registers()[kObject];
+}
+// static
+constexpr Register WriteBarrierDescriptor::SlotAddressRegister() {
+  return registers()[kSlotAddress];
+}
+
+// static
+constexpr Register WriteBarrierDescriptor::ValueRegister() {
+  return registers()[kSlotAddress + 1];
+}
+
+// static
+constexpr RegList WriteBarrierDescriptor::ComputeSavedRegisters(
+    Register object, Register slot_address) {
+  DCHECK(!AreAliased(object, slot_address));
+  // Only push clobbered registers.
+  RegList registers = 0;
+  if (object != ObjectRegister()) registers |= ObjectRegister().bit();
+  if (slot_address != SlotAddressRegister()) {
+    registers |= SlotAddressRegister().bit();
+  }
+  return registers;
 }
 
 // static
