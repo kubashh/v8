2a9b4f78597936fbc6fbc2657bc23dcd4bb88942
Camillo Bruni <cbruni@chromium.org>
2021-05-31 13:38:17 +0200

    [sparkplug][x64] Reduce CallRecordWrite instructions
    Directly use the correct registers for calling the RecordWrite stubs
    in sparkplug. To keep changes to existing builtins minimal there are
    certain register requirements which are no made explicit in
    WriteBarrierDescriptor::Verify.
    
    
    Bug: v8:11420
    Change-Id: I3a0c500fbe26f82ee2243a61dbf574fd31656982

--- src/codegen/x64/interface-descriptors-x64-inl.h
+++ src/codegen/x64/interface-descriptors-x64-inl.h
@@ -20,8 +20,14 @@ constexpr auto CallInterfaceDescriptor::DefaultRegisterArray() {
 
 // static
 constexpr auto WriteBarrierDescriptor::registers() {
-  return RegisterArray(arg_reg_1, arg_reg_2, arg_reg_3, arg_reg_4,
-                       kReturnRegister0);
+#if V8_TARGET_OS_WIN
+  return RegisterArray(kJSFunctionRegister, arg_reg_3, arg_reg_1,
+                       kReturnRegister0, arg_reg_4, arg_reg_2,
+                       kContextRegister);
+#else
+  return RegisterArray(kJSFunctionRegister, rbx, arg_reg_3, arg_reg_4,
+                       kReturnRegister0, kContextRegister);
+#endif  // V8_TARGET_OS_WIN
 }
 
 #ifdef V8_IS_TSAN
