e30f56d17b2ea85661549f5fa218cb01bd2df2fc
Frank Tang <ftang@chromium.org>
2021-06-15 15:58:18 -0700

    [intl] Implement Intl Enumeration API
    flag --harmony_intl_enumeration
    
    Bug: v8:10743
    Change-Id: I3bfe0c25b50eb55025498534eba975a20723bd2f

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -279,7 +279,9 @@ DEFINE_BOOL(harmony_shipping, true, "enable all shipped harmony features")
     "harmony ResizableArrayBuffer / GrowableSharedArrayBuffer")
 
 #ifdef V8_INTL_SUPPORT
-#define HARMONY_INPROGRESS(V) HARMONY_INPROGRESS_BASE(V)
+#define HARMONY_INPROGRESS(V) \
+  HARMONY_INPROGRESS_BASE(V)  \
+  V(harmony_intl_enumeration, "Intl Enumberation API")
 #else
 #define HARMONY_INPROGRESS(V) HARMONY_INPROGRESS_BASE(V)
 #endif
