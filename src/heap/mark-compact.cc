5b7813238a50fa6085902a201d6b694a843f1a28
Dominik Inführ <dinfuehr@chromium.org>
2021-06-15 22:45:53 +0200

    [heap] Perform shared collection
    Bug: v8:11708
    Change-Id: I45ac50e6b4a1e9270f9e39b69f9b8ee5e6e14134

--- src/heap/mark-compact.cc
+++ src/heap/mark-compact.cc
@@ -121,6 +121,11 @@ class MarkingVerifier : public ObjectVisitor, public RootVisitor {
 
 void MarkingVerifier::VerifyRoots() {
   heap_->IterateRoots(this, base::EnumSet<SkipRoot>{SkipRoot::kWeak});
+
+  heap_->isolate()->IterateClientIsolates([this](Isolate* client) {
+    client->heap()->IterateRoots(this,
+                                 base::EnumSet<SkipRoot>{SkipRoot::kWeak});
+  });
 }
 
 void MarkingVerifier::VerifyMarkingOnPage(const Page* page, Address start,
@@ -244,9 +249,10 @@ class FullMarkingVerifier : public MarkingVerifier {
 
  private:
   V8_INLINE void VerifyHeapObjectImpl(HeapObject heap_object) {
-    if (!heap_->IsShared() &&
+    if (heap_->IsShared() !=
         BasicMemoryChunk::FromHeapObject(heap_object)->InSharedHeap())
       return;
+
     CHECK(marking_state_->IsBlackOrGrey(heap_object));
   }
 
@@ -305,6 +311,11 @@ class EvacuationVerifier : public ObjectVisitor, public RootVisitor {
 
 void EvacuationVerifier::VerifyRoots() {
   heap_->IterateRoots(this, base::EnumSet<SkipRoot>{SkipRoot::kWeak});
+
+  heap_->isolate()->IterateClientIsolates([this](Isolate* client) {
+    client->heap()->IterateRoots(this,
+                                 base::EnumSet<SkipRoot>{SkipRoot::kWeak});
+  });
 }
 
 void EvacuationVerifier::VerifyEvacuationOnPage(Address start, Address end) {
@@ -355,6 +366,10 @@ class FullEvacuationVerifier : public EvacuationVerifier {
 
  protected:
   V8_INLINE void VerifyHeapObjectImpl(HeapObject heap_object) {
+    if (heap_->IsShared() !=
+        BasicMemoryChunk::FromHeapObject(heap_object)->InSharedHeap())
+      return;
+
     CHECK_IMPLIES(Heap::InYoungGeneration(heap_object),
                   Heap::InToPage(heap_object));
     CHECK(!MarkCompactCollector::IsOnEvacuationCandidate(heap_object));
@@ -1000,7 +1015,7 @@ class MarkCompactCollector::RootMarkingVisitor final : public RootVisitor {
     HeapObject heap_object = HeapObject::cast(object);
     BasicMemoryChunk* target_page =
         BasicMemoryChunk::FromHeapObject(heap_object);
-    if (!is_shared_heap_ && target_page->InSharedHeap()) return;
+    if (is_shared_heap_ != target_page->InSharedHeap()) return;
     collector_->MarkRootObject(root, heap_object);
   }
 
@@ -1621,8 +1636,18 @@ void MarkCompactCollector::MarkRoots(RootVisitor* root_visitor,
   // etc., and all objects reachable from them.
   heap()->IterateRoots(root_visitor, base::EnumSet<SkipRoot>{SkipRoot::kWeak});
 
+  isolate()->IterateClientIsolates([root_visitor](Isolate* client) {
+    client->heap()->IterateRoots(root_visitor,
+                                 base::EnumSet<SkipRoot>{SkipRoot::kWeak});
+  });
+
   // Custom marking for top optimized frame.
-  ProcessTopOptimizedFrame(custom_root_body_visitor);
+  ProcessTopOptimizedFrame(custom_root_body_visitor, isolate());
+
+  isolate()->IterateClientIsolates(
+      [this, custom_root_body_visitor](Isolate* client) {
+        ProcessTopOptimizedFrame(custom_root_body_visitor, client);
+      });
 }
 
 void MarkCompactCollector::VisitObject(HeapObject obj) {
@@ -1911,13 +1936,14 @@ void MarkCompactCollector::ProcessEphemeronMarking() {
   CHECK(heap()->local_embedder_heap_tracer()->IsRemoteTracingDone());
 }
 
-void MarkCompactCollector::ProcessTopOptimizedFrame(ObjectVisitor* visitor) {
-  for (StackFrameIterator it(isolate(), isolate()->thread_local_top());
-       !it.done(); it.Advance()) {
+void MarkCompactCollector::ProcessTopOptimizedFrame(ObjectVisitor* visitor,
+                                                    Isolate* isolate) {
+  for (StackFrameIterator it(isolate, isolate->thread_local_top()); !it.done();
+       it.Advance()) {
     if (it.frame()->is_unoptimized()) return;
     if (it.frame()->type() == StackFrame::OPTIMIZED) {
       Code code = it.frame()->LookupCode();
-      if (!code.CanDeoptAt(isolate(), it.frame()->pc())) {
+      if (!code.CanDeoptAt(isolate, it.frame()->pc())) {
         Code::BodyDescriptor::IterateBody(code.map(), code, visitor);
       }
       return;
@@ -3976,6 +4002,12 @@ void MarkCompactCollector::UpdatePointersAfterEvacuation() {
     // The external string table is updated at the end.
     heap_->IterateRoots(&updating_visitor, base::EnumSet<SkipRoot>{
                                                SkipRoot::kExternalStringTable});
+
+    isolate()->IterateClientIsolates([&updating_visitor](Isolate* client) {
+      client->heap()->IterateRoots(
+          &updating_visitor,
+          base::EnumSet<SkipRoot>{SkipRoot::kExternalStringTable});
+    });
   }
 
   {
