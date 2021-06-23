ba5df028725fbaef0d0084df6fb986f190bfed82
Thibaud Michaud <thibaudm@chromium.org>
2021-06-23 17:28:49 +0200

    [wasm][liftoff][ia32][x64] Detect SIMD NaNs for fuzzing
    R=clemensb@chromium.org
    
    Bug: v8:11856
    Change-Id: I9764e3e2944690ed0883afdab20afd47fdd4acfa

--- src/wasm/baseline/arm/liftoff-assembler-arm.h
+++ src/wasm/baseline/arm/liftoff-assembler-arm.h
@@ -4238,6 +4238,12 @@ void LiftoffAssembler::emit_set_if_nan(Register dst, DoubleRegister src,
   UNIMPLEMENTED();
 }
 
+void LiftoffAssembler::emit_s128_set_if_nan(Register dst, DoubleRegister src,
+                                            DoubleRegister tmp,
+                                            ValueKind lane_kind) {
+  UNIMPLEMENTED();
+}
+
 void LiftoffStackSlots::Construct(int param_slots) {
   DCHECK_LT(0, slots_.size());
   SortInPushOrder();
