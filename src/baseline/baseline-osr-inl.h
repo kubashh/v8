6eedf4dd9f5222ae5250f7e7474dc0d4f0dabe59
Patrick Thier <pthier@chromium.org>
2021-06-24 15:58:45 +0000

    [sparkplug] Improve OSR for batch compilation
    - Remove possibility to pass frame to %BaselineOsr (was unused and adds
    overhead to the normal path)
    - Arm back edges for OSR of all functions compiled in a batch
    
    Bug: v8:11790
    Change-Id: Ifb1016935296a172914f99e8b2a1742f618a2be0

--- src/baseline/baseline-osr-inl.h
+++ src/baseline/baseline-osr-inl.h
@@ -6,7 +6,6 @@
 #define V8_BASELINE_BASELINE_OSR_INL_H_
 
 #include "src/baseline/baseline-batch-compiler.h"
-#include "src/execution/frames.h"
 #include "src/execution/isolate-inl.h"
 
 namespace v8 {
@@ -16,33 +15,17 @@ enum CompilationMode { kCompileImmediate, kCompileBatch };
 
 inline void OSRInterpreterFrameToBaseline(Isolate* isolate,
                                           Handle<JSFunction> function,
-                                          UnoptimizedFrame* frame,
                                           CompilationMode compilation_mode) {
   IsCompiledScope is_compiled_scope(
       function->shared().is_compiled_scope(isolate));
-  bool is_compiled = false;
   switch (compilation_mode) {
     case kCompileBatch:
-      is_compiled =
-          isolate->baseline_batch_compiler()->EnqueueFunction(function);
+      isolate->baseline_batch_compiler()->EnqueueFunction(function);
       break;
     case kCompileImmediate:
-      is_compiled = Compiler::CompileBaseline(
-          isolate, function, Compiler::CLEAR_EXCEPTION, &is_compiled_scope);
+      Compiler::CompileBaseline(isolate, function, Compiler::CLEAR_EXCEPTION,
+                                &is_compiled_scope);
       break;
-  }
-  if (is_compiled) {
-    if (V8_LIKELY(FLAG_use_osr)) {
-      DCHECK_NOT_NULL(frame);
-      if (FLAG_trace_osr) {
-        CodeTracer::Scope scope(isolate->GetCodeTracer());
-        PrintF(scope.file(),
-               "[OSR - Entry at OSR bytecode offset %d into baseline code]\n",
-               frame->GetBytecodeOffset());
-      }
-      frame->GetBytecodeArray().set_osr_loop_nesting_level(
-          AbstractCode::kMaxLoopNestingMarker);
-    }
   }
 }
 
