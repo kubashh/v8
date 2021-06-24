53d4b45e149513776eb361c0c2dcbe483ba4d3c7
Peter Kasting <pkasting@chromium.org>
2021-06-24 07:57:04 -0700

    Fix -Wloop-analysis warnings in V8.
    These indicate when a range-based for loop is using an index whose type
    (value, pointer, or reference) doesn't match what the loop actually
    extracts from the range.  Fix by matching the actual type better.
    
    This shouldn't cause any behavior/performance change, just be slightly
    clearer about what's actually happening when reading the code.
    
    Bug: chromium:1223264
    Change-Id: Ib8773fbbeb038609c54a52c7cd6ce5bd11fd99ba

--- src/compiler/serializer-for-background-compilation.cc
+++ src/compiler/serializer-for-background-compilation.cc
@@ -2798,7 +2798,7 @@ void SerializerForBackgroundCompilation::VisitSwitchOnSmiNoFeedback(
     interpreter::BytecodeArrayIterator* iterator) {
   interpreter::JumpTableTargetOffsets targets =
       iterator->GetJumpTableTargetOffsets();
-  for (const auto& target : targets) {
+  for (interpreter::JumpTableTargetOffset target : targets) {
     ContributeToJumpTargetEnvironment(target.target_offset);
   }
 }
