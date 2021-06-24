f11720394b88a5ce6ab88fdee8adf0ea895816a5
Maya Lekova <mslekova@chromium.org>
2021-06-24 15:17:54 +0200

    Migrate PerIsolateAssertScope storage to boolean[]
    This CL modifies the underlying storage of PerIsolateAssertScope from
    a bitfield to a boolean array. This slightly increases the space taken
    by the isolate, but allows for easier access to the individual fields,
    which is a prerequisite for implementing assertion scopes in TurboFan.
    
    Bug: chromium:1218898
    Change-Id: Ia5e43352ebba28be6f013376b75f13ec8d5dc972

--- src/common/assert-scope.cc
+++ src/common/assert-scope.cc
@@ -17,8 +17,6 @@ namespace {
 
 template <PerThreadAssertType kType>
 using PerThreadDataBit = base::BitField<bool, kType, 1>;
-template <PerIsolateAssertType kType>
-using PerIsolateDataBit = base::BitField<bool, kType, 1>;
 
 // Thread-local storage for assert data. Default all asserts to "allow".
 thread_local uint32_t current_per_thread_assert_data(~0);
@@ -50,45 +48,60 @@ bool PerThreadAssertScope<kType, kAllow>::IsAllowed() {
   return PerThreadDataBit<kType>::decode(current_per_thread_assert_data);
 }
 
-template <PerIsolateAssertType kType, bool kAllow>
-PerIsolateAssertScope<kType, kAllow>::PerIsolateAssertScope(Isolate* isolate)
-    : isolate_(isolate), old_data_(isolate->per_isolate_assert_data()) {
-  DCHECK_NOT_NULL(isolate);
-  isolate_->set_per_isolate_assert_data(
-      PerIsolateDataBit<kType>::update(old_data_, kAllow));
-}
+#define PER_ISOLATE_ASSERT_ENABLE_SCOPE_DEFINITION(EnableType, DisableType, \
+                                                   field)                   \
+  EnableType::EnableType(Isolate* isolate)                                  \
+      : isolate_(isolate), old_data_(isolate->field()) {                    \
+    DCHECK_NOT_NULL(isolate);                                               \
+    isolate_->set_##field(true);                                            \
+  }                                                                         \
+                                                                            \
+  EnableType::~EnableType() { isolate_->set_##field(old_data_); }           \
+                                                                            \
+  /* static */                                                              \
+  bool EnableType::IsAllowed(Isolate* isolate) { return isolate->field(); } \
+                                                                            \
+  /* static */                                                              \
+  void EnableType::Open(Isolate* isolate, bool* was_execution_allowed) {    \
+    DCHECK_NOT_NULL(isolate);                                               \
+    DCHECK_NOT_NULL(was_execution_allowed);                                 \
+    *was_execution_allowed = isolate->field();                              \
+    isolate->set_##field(true);                                             \
+  }                                                                         \
+  /* static */                                                              \
+  void EnableType::Close(Isolate* isolate, bool was_execution_allowed) {    \
+    DCHECK_NOT_NULL(isolate);                                               \
+    isolate->set_##field(was_execution_allowed);                            \
+  }
 
-template <PerIsolateAssertType kType, bool kAllow>
-PerIsolateAssertScope<kType, kAllow>::~PerIsolateAssertScope() {
-  isolate_->set_per_isolate_assert_data(old_data_);
-}
+#define PER_ISOLATE_ASSERT_DISABLE_SCOPE_DEFINITION(EnableType, DisableType, \
+                                                    field)                   \
+  DisableType::DisableType(Isolate* isolate)                                 \
+      : isolate_(isolate), old_data_(isolate->field()) {                     \
+    DCHECK_NOT_NULL(isolate);                                                \
+    isolate_->set_##field(false);                                            \
+  }                                                                          \
+                                                                             \
+  DisableType::~DisableType() { isolate_->set_##field(old_data_); }          \
+                                                                             \
+  /* static */                                                               \
+  bool DisableType::IsAllowed(Isolate* isolate) { return isolate->field(); } \
+                                                                             \
+  /* static */                                                               \
+  void DisableType::Open(Isolate* isolate, bool* was_execution_allowed) {    \
+    DCHECK_NOT_NULL(isolate);                                                \
+    DCHECK_NOT_NULL(was_execution_allowed);                                  \
+    *was_execution_allowed = isolate->field();                               \
+    isolate->set_##field(false);                                             \
+  }                                                                          \
+  /* static */                                                               \
+  void DisableType::Close(Isolate* isolate, bool was_execution_allowed) {    \
+    DCHECK_NOT_NULL(isolate);                                                \
+    isolate->set_##field(was_execution_allowed);                             \
+  }
 
-// static
-template <PerIsolateAssertType kType, bool kAllow>
-bool PerIsolateAssertScope<kType, kAllow>::IsAllowed(Isolate* isolate) {
-  return PerIsolateDataBit<kType>::decode(isolate->per_isolate_assert_data());
-}
-
-// static
-template <PerIsolateAssertType kType, bool kAllow>
-void PerIsolateAssertScope<kType, kAllow>::Open(Isolate* isolate,
-                                                bool* was_execution_allowed) {
-  DCHECK_NOT_NULL(isolate);
-  DCHECK_NOT_NULL(was_execution_allowed);
-  uint32_t old_data = isolate->per_isolate_assert_data();
-  *was_execution_allowed = PerIsolateDataBit<kType>::decode(old_data);
-  isolate->set_per_isolate_assert_data(
-      PerIsolateDataBit<kType>::update(old_data, kAllow));
-}
-// static
-template <PerIsolateAssertType kType, bool kAllow>
-void PerIsolateAssertScope<kType, kAllow>::Close(Isolate* isolate,
-                                                 bool was_execution_allowed) {
-  DCHECK_NOT_NULL(isolate);
-  uint32_t old_data = isolate->per_isolate_assert_data();
-  isolate->set_per_isolate_assert_data(
-      PerIsolateDataBit<kType>::update(old_data, was_execution_allowed));
-}
+PER_ISOLATE_ASSERT_TYPE(PER_ISOLATE_ASSERT_ENABLE_SCOPE_DEFINITION)
+PER_ISOLATE_ASSERT_TYPE(PER_ISOLATE_ASSERT_DISABLE_SCOPE_DEFINITION)
 
 // -----------------------------------------------------------------------------
 // Instantiations.
@@ -106,19 +119,6 @@ template class PerThreadAssertScope<CODE_DEPENDENCY_CHANGE_ASSERT, true>;
 template class PerThreadAssertScope<CODE_ALLOCATION_ASSERT, false>;
 template class PerThreadAssertScope<CODE_ALLOCATION_ASSERT, true>;
 template class PerThreadAssertScope<GC_MOLE, false>;
-
-template class PerIsolateAssertScope<JAVASCRIPT_EXECUTION_ASSERT, false>;
-template class PerIsolateAssertScope<JAVASCRIPT_EXECUTION_ASSERT, true>;
-template class PerIsolateAssertScope<JAVASCRIPT_EXECUTION_THROWS, false>;
-template class PerIsolateAssertScope<JAVASCRIPT_EXECUTION_THROWS, true>;
-template class PerIsolateAssertScope<JAVASCRIPT_EXECUTION_DUMP, false>;
-template class PerIsolateAssertScope<JAVASCRIPT_EXECUTION_DUMP, true>;
-template class PerIsolateAssertScope<DEOPTIMIZATION_ASSERT, false>;
-template class PerIsolateAssertScope<DEOPTIMIZATION_ASSERT, true>;
-template class PerIsolateAssertScope<COMPILATION_ASSERT, false>;
-template class PerIsolateAssertScope<COMPILATION_ASSERT, true>;
-template class PerIsolateAssertScope<NO_EXCEPTION_ASSERT, false>;
-template class PerIsolateAssertScope<NO_EXCEPTION_ASSERT, true>;
 
 }  // namespace internal
 }  // namespace v8
