aaab767a8d956bdafb5e074c3eb384ca433f631f
Jakob Gruber <jgruber@chromium.org>
2021-06-30 10:13:59 +0200

    [compiler] Concurrent JSGlobalObjectRef::GetPropertyCell
    .. and make JSGlobalObjectRef bg-serialized.
    
    GetPropertyCell was implemented as:
    
     LookupIterator it(holder, isolate, name, LookupIterator::OWN);
     it.TryLookupCachedProperty();
     if (it.state() == LookupIterator::DATA) it.GetPropertyCell();
    
    Due to concurrency requirements, we essentially have to reimplement
    this entire path for use in a concurrent setting:
    
     - Reads in some cases have to use relaxed or acquire semantics.
     - The IsPendingAllocation predicate must be called on some objects
       before reading into them.
     - Repeated reads of the same field must be avoided due to the
       possibility of concurrent modifications.
    
    This CL introduces two new methods:
    
    ConcurrentLookupIterator::TryGetPropertyCell implements the outer
    lookup logic, including the repeated lookup for accessors / cached
    property names.
    
    GlobalDictionary::TryFindPropertyCellForConcurrentLookupIterator is a
    slightly modified HashTable::FindEntry which follows the above rules.
    
    Bug: v8:7790
    Change-Id: Ic9a52da766afdfedce8efcbda92876845a17eed9

--- src/compiler/heap-refs.h
+++ src/compiler/heap-refs.h
@@ -84,7 +84,7 @@ enum class RefSerializationKind {
   V(JSBoundFunction, RefSerializationKind::kSerialized)                   \
   V(JSDataView, RefSerializationKind::kSerialized)                        \
   V(JSFunction, RefSerializationKind::kSerialized)                        \
-  V(JSGlobalObject, RefSerializationKind::kSerialized)                    \
+  V(JSGlobalObject, RefSerializationKind::kBackgroundSerialized)          \
   V(JSGlobalProxy, RefSerializationKind::kBackgroundSerialized)           \
   V(JSTypedArray, RefSerializationKind::kBackgroundSerialized)            \
   /* Subtypes of Context */                                               \
@@ -1023,13 +1023,7 @@ class JSGlobalObjectRef : public JSObjectRef {
 
   bool IsDetachedFrom(JSGlobalProxyRef const& proxy) const;
 
-  // If {serialize} is false:
-  //   If the property is known to exist as a property cell (on the global
-  //   object), return that property cell. Otherwise (not known to exist as a
-  //   property cell or known not to exist as a property cell) return nothing.
-  // If {serialize} is true:
-  //   Like above but potentially access the heap and serialize the necessary
-  //   information.
+  // Can be called even when there is no property cell for the given name.
   base::Optional<PropertyCellRef> GetPropertyCell(
       NameRef const& name, SerializationPolicy policy =
                                SerializationPolicy::kAssumeSerialized) const;
