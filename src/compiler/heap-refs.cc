46f8e3ec4f2b37c1250889a5cd48290b887ca37a
Mike Stanton <mvstanton@chromium.org>
2021-07-06 12:12:58 +0200

    [compiler] Representation mismatch detection missed a case
    In GetOwnFastDataPropertyFromHeap, we read a property value
    then compare it with the expected representation. We already
    had code to bail out of that particular optimization if there
    was a mismatch, however it missed the case of expected
    representation being a HeapObject, and when a Smi value was found.
    
    The fix is to use the excellent pre-existing method
    Object::FitsRepresentation() to make this check. Thusly, all
    cases are handled.
    
    Bug: chromium:1225607, v8:7790
    Change-Id: I7d9b1b7722d9691cf5427f8456a6deb466dda0d3

--- src/compiler/heap-refs.cc
+++ src/compiler/heap-refs.cc
@@ -592,8 +592,7 @@ base::Optional<ObjectRef> GetOwnFastDataPropertyFromHeap(
     // Since we don't have a guarantee that {value} is the correct value of the
     // property, we use the expected {representation} to weed out the most
     // egregious types  of wrong values.
-    if ((representation.IsSmi() && !value->IsSmi()) ||
-        (representation.IsDouble() && !value->IsHeapNumber())) {
+    if (!value->object()->FitsRepresentation(representation)) {
       TRACE_BROKER_MISSING(
           broker, "Mismatch between representation and value in " << holder);
       return {};
