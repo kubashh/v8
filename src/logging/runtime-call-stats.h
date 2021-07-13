882c06d8cf86bec5fd8faa03f2ee7b050acc5a85
Vicky Kontoura <vkont@google.com>
2021-07-13 11:49:53 +0000

    [web snapshot] Support classes
    Bug: v8:11525, v8:11706
    Change-Id: I16ca7cbc17b1811721081cda05124ce36073f9be

--- src/logging/runtime-call-stats.h
+++ src/logging/runtime-call-stats.h
@@ -475,6 +475,7 @@ class RuntimeCallTimer final {
   V(WebSnapshotDeserialize_Contexts)           \
   V(WebSnapshotDeserialize_Exports)            \
   V(WebSnapshotDeserialize_Functions)          \
+  V(WebSnapshotDeserialize_Classes)            \
   V(WebSnapshotDeserialize_Maps)               \
   V(WebSnapshotDeserialize_Objects)            \
   V(WebSnapshotDeserialize_Strings)
