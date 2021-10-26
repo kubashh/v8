e22e0c761f3b4c50b8b59cff9dc6c028eba0b1d6
Clemens Backes <clemensb@chromium.org>
2021-10-26 10:43:32 +0200

    [compiler] Compute dominator tree for tests
    The dominator tree is usually computed as part of scheduling (in
    {Scheduler::ComputeSchedule}). For tests it was missing, leading to
    DCHECK errors in the mid-tier register allocator, which uses the
    dominator tree.
    
    R=mslekova@chromium.org
    
    Bug: v8:12330
    Change-Id: I02bc8dee3aecb6a1613fa1d07d3aae85cd28de17

--- test/cctest/compiler/test-run-machops.cc
+++ test/cctest/compiler/test-run-machops.cc
@@ -18,6 +18,7 @@
 #include "test/cctest/cctest.h"
 #include "test/cctest/compiler/codegen-tester.h"
 #include "test/cctest/compiler/value-helper.h"
+#include "test/common/flag-utils.h"
 
 namespace v8 {
 namespace internal {
@@ -7295,6 +7296,16 @@ TEST(Regression738952) {
   m.Return(m.Load(MachineType::Int32(), m.PointerConstant(&sentinel),
                   m.TruncateFloat64ToWord32(m.Float64Constant(d))));
   CHECK_EQ(sentinel, m.Call());
+}
+
+TEST(Regression12330) {
+  FLAG_SCOPE(turbo_force_mid_tier_regalloc);
+
+  RawMachineAssemblerTester<int32_t> m(MachineType::Int64());
+  Node* add = m.Int64SubWithOverflow(m.Int64Constant(0), m.Parameter(0));
+  Node* ovf = m.Projection(1, add);
+  m.Return(ovf);
+  m.GenerateCode();
 }
 
 }  // namespace compiler
