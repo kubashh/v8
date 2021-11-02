03fb13a95dfe48fba65752e51b5d91dab7306546
Scott Violet <sky@chromium.org>
2021-11-01 17:05:39 -0700

    Adds counter for snapshot decompression time
    I'm in the process of evaluating impact of v8 snapshot and
    would like to understand the amount of time spend decompressing.
    
    BUG=chromium:1257321
    TEST=none
    
    Change-Id: I122a2151e48f61db3352da56763e3e66f7415efe

--- src/snapshot/snapshot.cc
+++ src/snapshot/snapshot.cc
@@ -11,6 +11,7 @@
 #include "src/execution/isolate-inl.h"
 #include "src/heap/safepoint.h"
 #include "src/init/bootstrapper.h"
+#include "src/logging/counters-scopes.h"
 #include "src/logging/runtime-call-stats-scope.h"
 #include "src/objects/code-kind.h"
 #include "src/objects/js-regexp-inl.h"
@@ -166,6 +167,12 @@ bool Snapshot::Initialize(Isolate* isolate) {
   if (!FLAG_skip_snapshot_checksum) {
     CHECK(VerifyChecksum(blob));
   }
+#ifdef V8_SNAPSHOT_COMPRESSION
+  base::Optional<NestedTimedHistogramScope> decompress_histogram;
+  if (base::TimeTicks::IsHighResolution()) {
+    decompress_histogram.emplace(isolate->counters()->snapshot_decompress());
+  }
+#endif
   base::Vector<const byte> startup_data =
       SnapshotImpl::ExtractStartupData(blob);
   base::Vector<const byte> read_only_data =
@@ -176,6 +183,9 @@ bool Snapshot::Initialize(Isolate* isolate) {
   SnapshotData startup_snapshot_data(MaybeDecompress(startup_data));
   SnapshotData read_only_snapshot_data(MaybeDecompress(read_only_data));
   SnapshotData shared_heap_snapshot_data(MaybeDecompress(shared_heap_data));
+#ifdef V8_SNAPSHOT_COMPRESSION
+  decompress_histogram.reset();
+#endif
 
   bool success = isolate->InitWithSnapshot(
       &startup_snapshot_data, &read_only_snapshot_data,
