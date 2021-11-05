f75f3f8574c684f720f1ca66aa7cace8535a3d93
Victor Gomes <victorgomes@chromium.org>
2021-11-05 19:16:33 +0100

    [heap] Support mprotect off thread
    Change-Id: Id18bcb9a44843b4ff747b1e4ac91913e80b74d80

--- src/heap/heap-inl.h
+++ src/heap/heap-inl.h
@@ -826,7 +826,6 @@ CodePageCollectionMemoryModificationScope::
     : heap_(heap) {
   if (heap_->write_protect_code_memory() &&
       !heap_->code_space_memory_modification_scope_depth()) {
-    heap_->EnableUnprotectedMemoryChunksRegistry();
     heap_->IncrementCodePageCollectionMemoryModificationScopeDepth();
   }
 }
@@ -835,10 +834,10 @@ CodePageCollectionMemoryModificationScope::
     ~CodePageCollectionMemoryModificationScope() {
   if (heap_->write_protect_code_memory() &&
       !heap_->code_space_memory_modification_scope_depth()) {
-    heap_->DecrementCodePageCollectionMemoryModificationScopeDepth();
-    if (heap_->code_page_collection_memory_modification_scope_depth() == 0) {
+    uintptr_t depth =
+        heap_->DecrementCodePageCollectionMemoryModificationScopeDepth();
+    if (depth == 0) {
       heap_->ProtectUnprotectedMemoryChunks();
-      heap_->DisableUnprotectedMemoryChunksRegistry();
     }
   }
 }
