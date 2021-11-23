e37c8d1ab22975a1b0b6c78b8a47a0ceeb76260a
Samuel Groß <saelo@chromium.org>
2021-11-23 19:06:14 +0100

    ArrayBuffer refactoring in preparation for CagedPointers
    The main changes of this CL are:
    
    It should no longer be assumed that an empty ArrayBuffer has a nullptr
    backing store. This is in preparation for the move to caged pointers,
    which cannot represent nullptr, and will instead likely provide a
    EmptyBackingStore constant pointing inside the virtual memory cage. For
    that reason, a new JSArrayBuffer::IsEmpty() helper is introduced, which
    should be used instead of checking against nullptr.
    
    CodeStubAssembler::GetTypedArrayBuffer now checks for on-heap
    TypedArrays instead of comparing the backing store pointer to nullptr.
    This is consistent with the implementation in JSTypedArray::GetBuffer.
    
    v8::ArrayBufferView::CopyContents now uses JSTypedArray::DataPtr instead
    of relying on nullptr backing stores to handle on-heap TypedArrays.
    
    The serializer and deserializer now check for is_empty() and use the
    kEmptyBackingStoreRefSentinel value to serialize empty backing stores.
    
    Empty ArrayBuffers allocated for on-heap TypedArrays now have a
    byte_length of zero. This allows removing the allocation_length() (and
    allocation_buffer()) methods, which were only (incorrectly, as they
    don't account for GSABs) used for memory measurements.
    
    Bug: chromium:1218005
    Change-Id: Ib889ccf855f68525f7a614f3963e46ea56865fa3

--- src/objects/js-array-buffer.cc
+++ src/objects/js-array-buffer.cc
@@ -125,22 +125,6 @@ void JSArrayBuffer::Detach(bool force_for_wasm_memory) {
   set_was_detached(true);
 }
 
-std::shared_ptr<BackingStore> JSArrayBuffer::GetBackingStore() const {
-  if (!extension()) return nullptr;
-  return extension()->backing_store();
-}
-
-size_t JSArrayBuffer::GetByteLength() const {
-  if V8_UNLIKELY (is_shared() && is_resizable()) {
-    // Invariant: byte_length for GSAB is 0 (it needs to be read from the
-    // BackingStore).
-    DCHECK_EQ(0, byte_length());
-
-    return GetBackingStore()->byte_length(std::memory_order_seq_cst);
-  }
-  return byte_length();
-}
-
 ArrayBufferExtension* JSArrayBuffer::EnsureExtension() {
   ArrayBufferExtension* extension = this->extension();
   if (extension != nullptr) return extension;
@@ -194,7 +178,7 @@ Handle<JSArrayBuffer> JSTypedArray::GetBuffer() {
   DCHECK(!array_buffer->is_resizable());
 
   // The existing array buffer should be empty.
-  DCHECK_NULL(array_buffer->backing_store());
+  DCHECK(array_buffer->IsEmpty());
 
   // Allocate a new backing store and attach it to the existing array buffer.
   size_t byte_length = self->byte_length();
