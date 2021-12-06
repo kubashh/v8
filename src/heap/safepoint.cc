2a496be60260e53fd2ee3aeec72a11585612bc10
Dominik Inführ <dinfuehr@chromium.org>
2021-12-06 12:57:33 +0100

    [heap] Make active_safepoint_scopes_ non-atomic
    Bug: v8:11708
    Change-Id: I5254aa5e655844739082144fc75ba9f0c13d1ba0

--- src/heap/safepoint.cc
+++ src/heap/safepoint.cc
@@ -51,7 +51,7 @@ void IsolateSafepoint::EnterGlobalSafepointScope(Isolate* initiator) {
     IgnoreLocalGCRequests ignore_gc_requests(initiator->heap());
     LockMutex(initiator->main_thread_local_heap());
   }
-  CHECK_EQ(active_safepoint_scopes_.exchange(1), 0);
+  CHECK_EQ(++active_safepoint_scopes_, 1);
 
   barrier_.Arm();
 
@@ -101,7 +101,7 @@ void IsolateSafepoint::LockMutex(LocalHeap* local_heap) {
 
 void IsolateSafepoint::LeaveGlobalSafepointScope(Isolate* initiator) {
   local_heaps_mutex_.AssertHeld();
-  CHECK_EQ(active_safepoint_scopes_.exchange(0), 1);
+  CHECK_EQ(--active_safepoint_scopes_, 0);
   ClearSafepointRequestedFlags(IncludeMainThreadUnlessInitiator(initiator));
   barrier_.Disarm();
   local_heaps_mutex_.Unlock();
