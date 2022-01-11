02c7af311ba899be4cbbca610c66ef509500f83d
Frank Tang <ftang@chromium.org>
2022-01-10 23:20:57 -0800

    [Temporal] Add Temporal.PlainDate.from
    Bug: v8:11544
    Change-Id: I781119561db5ec05b12b9bca31c98403355e35ac

--- src/builtins/builtins-definitions.h
+++ src/builtins/builtins-definitions.h
@@ -1640,6 +1640,7 @@ namespace internal {
   CPP(TemporalCalendarPrototypeToJSON)                                         \
                                                                                \
   /* "Private" (created but not exposed) Bulitins needed by Temporal */        \
+  TFJ(StringFixedArrayFromIterable, kJSArgcReceiverSlots, kIterable)           \
   TFJ(TemporalInstantFixedArrayFromIterable, kJSArgcReceiverSlots, kIterable)
 
 #define BUILTIN_LIST_BASE(CPP, TFJ, TFC, TFS, TFH, ASM) \
