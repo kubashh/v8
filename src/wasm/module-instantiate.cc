6a6d88e0d501b9b2a115e8756ce5d2216ebc6ab6
Andreas Haas <ahaas@chromium.org>
2022-01-12 10:08:11 +0100

    [wasm] Enter the native context when executing the start function
    The wpt test external/wpt/wasm/jsapi/functions/entry.html failed
    because the current context was entered when executing the start
    function instead of the native context. The test crashed because in
    GetEnteredOrMicrotaskContext a NativeContext is expected.
    
    Bug: chromium:1098844
    Change-Id: I52d50986c67a0a69c8d9e03756592dff670f83df

--- src/wasm/module-instantiate.cc
+++ src/wasm/module-instantiate.cc
@@ -857,7 +857,7 @@ bool InstanceBuilder::ExecuteStartFunction() {
   // v8::Context::Enter() and must happen in addition to the function call
   // sequence doing the compiled version of "isolate->set_context(...)".
   HandleScopeImplementer* hsi = isolate_->handle_scope_implementer();
-  hsi->EnterContext(start_function_->context());
+  hsi->EnterContext(start_function_->context().native_context());
 
   // Call the JS function.
   Handle<Object> undefined = isolate_->factory()->undefined_value();
