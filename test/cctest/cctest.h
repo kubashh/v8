51ccaac13df1fc2a3a0d0fcdcc1383c15f9fb769
Michael Lippautz <mlippautz@chromium.org>
2022-01-13 17:48:29 +0100

    Reland "cppgc-js,heap: Implement snapshots for embedder fields"
    This is a reland of 142dd775b49f77d4210774f51630007a1cf98440
    
    Original change's description:
    > cppgc-js,heap: Implement snapshots for embedder fields
    >
    > https://crrev.com/c/3293410 added concurrent processing of C++ objects
    > found through V8 embedder fields. The CL missed that those embedder
    > fields are not read atomically from JS objects. The problem is that
    > embedder fields are only aligned to kTaggedSize on builds with pointer
    > compression and are as such mis-aligned for atomic ops. This is not a
    > problem for on-heap values as the upper 32bits are anyways computed
    > from the cage. Is is a problem for generic C++ values though, as they
    > are used with Oilpan.
    >
    > This CL adds the standard marker snapshot protocol for embedder fields.
    >
    > Marker:
    > 1. Snapshot embedder fields
    > 2. Try to mark host object
    > 3. On success: process snapshot
    >
    > Main thread:
    > 1. On setting embedder fields mark the object black first
    > 2. Emit a write barrier for the embedder fields
    >
    > This will get simpler with the heap sandbox that uses a separate table
    > for embedder fields. Once the sandbox is the default configuration, we
    > 	can use it as dependency for the concurrent fast path.
    >
    > Bug: chromium:1285706
    > Change-Id: I6b975ea561be08cda840ef0dd27a11627de93900
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3380983
    > Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
    > Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#78604}
    
    Bug: chromium:1285706
    Change-Id: I024e50fc0757fbcd13cb9ffde027dff55f99d25c

--- test/cctest/cctest.h
+++ test/cctest/cctest.h
@@ -678,40 +678,16 @@ class StaticOneByteResource : public v8::String::ExternalOneByteStringResource {
 
 class V8_NODISCARD ManualGCScope {
  public:
-  ManualGCScope()
-      : flag_concurrent_marking_(i::FLAG_concurrent_marking),
-        flag_concurrent_sweeping_(i::FLAG_concurrent_sweeping),
-        flag_stress_concurrent_allocation_(
-            i::FLAG_stress_concurrent_allocation),
-        flag_stress_incremental_marking_(i::FLAG_stress_incremental_marking),
-        flag_parallel_marking_(i::FLAG_parallel_marking),
-        flag_detect_ineffective_gcs_near_heap_limit_(
-            i::FLAG_detect_ineffective_gcs_near_heap_limit) {
-    i::FLAG_concurrent_marking = false;
-    i::FLAG_concurrent_sweeping = false;
-    i::FLAG_stress_incremental_marking = false;
-    i::FLAG_stress_concurrent_allocation = false;
-    // Parallel marking has a dependency on concurrent marking.
-    i::FLAG_parallel_marking = false;
-    i::FLAG_detect_ineffective_gcs_near_heap_limit = false;
-  }
-  ~ManualGCScope() {
-    i::FLAG_concurrent_marking = flag_concurrent_marking_;
-    i::FLAG_concurrent_sweeping = flag_concurrent_sweeping_;
-    i::FLAG_stress_concurrent_allocation = flag_stress_concurrent_allocation_;
-    i::FLAG_stress_incremental_marking = flag_stress_incremental_marking_;
-    i::FLAG_parallel_marking = flag_parallel_marking_;
-    i::FLAG_detect_ineffective_gcs_near_heap_limit =
-        flag_detect_ineffective_gcs_near_heap_limit_;
-  }
+  ManualGCScope();
+  ~ManualGCScope();
 
  private:
-  bool flag_concurrent_marking_;
-  bool flag_concurrent_sweeping_;
-  bool flag_stress_concurrent_allocation_;
-  bool flag_stress_incremental_marking_;
-  bool flag_parallel_marking_;
-  bool flag_detect_ineffective_gcs_near_heap_limit_;
+  const bool flag_concurrent_marking_;
+  const bool flag_concurrent_sweeping_;
+  const bool flag_stress_concurrent_allocation_;
+  const bool flag_stress_incremental_marking_;
+  const bool flag_parallel_marking_;
+  const bool flag_detect_ineffective_gcs_near_heap_limit_;
 };
 
 // This is an abstract base class that can be overridden to implement a test
