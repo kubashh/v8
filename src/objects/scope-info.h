82941ff543117ea3cd050bc1bc2a18eb0e339bb5
Victor Gomes <victorgomes@chromium.org>
2022-01-17 11:53:13 +0100

    Reland "[runtime] Adds LocalNameIterator"
    This is a reland of f605d77822df392b83dd26c59d31dfdde188687c
    
    Original change's description:
    > [runtime] Adds LocalNameIterator
    >
    > ScopeInfo will contain either inlined (array) local names or
    > a hash table (names => index) containing the local names.
    >
    > We abstract iteration with LocalNameIterator and remove
    > ContextLocalName since accessing a local name by index in
    > the hash table would be expensive.
    >
    > This CL only implements the iterator for the array.
    >
    > Bug: v8:12315
    > Change-Id: I2c62802652fca1cf47815ce8768a3f7487f2c39f
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3386603
    > Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    > Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#78623}
    
    Bug: v8:12315
    Change-Id: I6288a08b9c342cd3a9cabcb621c40bb44c08c9c4

--- src/objects/scope-info.h
+++ src/objects/scope-info.h
@@ -143,8 +143,20 @@ class ScopeInfo : public TorqueGeneratedScopeInfo<ScopeInfo, HeapObject> {
   // Return true if the local names are inlined in the scope info object.
   inline bool HasInlinedLocalNames() const;
 
-  // Return the name of the given context local.
-  String ContextLocalName(int var) const;
+  template <typename ScopeInfoPtr>
+  class LocalNamesIterator;
+
+  // Returns a GC-safe local names iterator.
+  static inline LocalNamesIterator<Handle<ScopeInfo>> IterateLocalNames(
+      Handle<ScopeInfo> scope_info);
+
+  // The caller should not trigger GC while this is in scope.
+  static inline LocalNamesIterator<ScopeInfo*> UnsafeIterateLocalNames(
+      ScopeInfo* scope_info);
+
+  // Return the name of a given context local.
+  // It should only be used if inlined local names.
+  String ContextInlinedLocalName(int var) const;
 
   // Return the mode of the given context local.
   VariableMode ContextLocalMode(int var) const;
