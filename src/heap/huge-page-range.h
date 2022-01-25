c1cb38bae50ee05485817fa63f850e7f6128b376
JianxiaoLuIntel <jianxiao.lu@intel.com>
2022-01-26 05:49:24 +0800

    [heap] Enable THP, WIP.
    Change-Id: I7ff5c1b7939baaf6dbf316c65bb1b7a73320a19c

---
+++ src/heap/huge-page-range.h
@@ -0,0 +1,104 @@
+// Copyright 2021 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_HEAP_HUGE_PAGE_RANGE_H_
+#define V8_HEAP_HUGE_PAGE_RANGE_H_
+
+#include <list>
+
+#include "src/base/build_config.h"
+#include "src/heap/spaces.h"
+
+namespace v8 {
+namespace internal {
+class HugePageRange {
+ public:
+  static const size_t kHugeRangeSize = 1U << kHugePageSizeBits;
+
+  static const size_t kMaxPageNum = kHugeRangeSize / Page::kPageSize;
+
+  static HugePageRange* Initialize(Heap* heap, VirtualMemory reservation);
+
+  HugePageRange();
+
+  size_t page_num() { return page_num_; }
+
+  VirtualMemory* reserved_memory() { return &reserved_; }
+
+  Address address() { return reserved_.address(); }
+
+  size_t size() { return reserved_.size(); }
+
+  bool empty() { return page_num_ == 0; }
+
+  void Reset();
+
+  Address Allocate();
+
+  void Remove(BasicMemoryChunk* chunk);
+
+  inline bool Contains(size_t index) {
+    uint8_t mask = 1 << index;
+    return page_bitmap_ & mask;
+  }
+
+  inline void SetBitMap(size_t index, bool has_page) {
+    uint8_t mask = 1 << index;
+    if (has_page) {
+      page_bitmap_ |= mask;
+    } else {
+      mask = ~mask;
+      page_bitmap_ &= mask;
+    }
+  }
+
+  Heap* heap() { return heap_; }
+
+  size_t FirstAllocatableIndex();
+
+  inline Address AddressFromIndex(size_t index) {
+    return address() + index * Page::kPageSize;
+  }
+
+  size_t ChunkIndex(BasicMemoryChunk* chunk);
+
+ protected:
+  std::atomic<size_t> page_num_;
+  VirtualMemory reserved_;
+  Heap* heap_;
+  std::atomic<uint8_t> page_bitmap_;
+};
+
+class HugePageRangeManager {
+ public:
+  explicit HugePageRangeManager(Heap* heap) : heap_(heap), range_num_(0) {}
+
+  size_t HugePageRangeNum();
+
+  Page* TryAllocatePageInHugePageRange(Space* owner);
+
+  HugePageRange* TryFreePageInHugePageRange(MemoryChunk* chunk);
+
+  void RemoveHugePageRange(HugePageRange* range);
+
+ private:
+  HugePageRange* CreateHugePageRange();
+
+  // Fetch range with less hole first.
+  HugePageRange* FetchHugePageRange(AllocationSpace identity);
+
+#ifdef DEBUG
+  bool CheckRangeNum();
+#endif
+
+  Heap* const heap_;
+  base::Mutex mutex_;
+  std::list<HugePageRange*> ranges_[HugePageRange::kMaxPageNum];
+  std::atomic<size_t> range_num_;
+};
+
+}  // namespace internal
+}  // namespace v8
+
+#endif  // V8_HEAP_HUGE_PAGE_RANGE_H_
