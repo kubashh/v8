5de8dabe0fbc6fde123010356167cca0c9e207c3
Samuel Groß <saelo@chromium.org>
2022-02-01 10:53:25 +0100

    Use aligned reads/writes in SandboxedPointer accessors when possible
    Previously, when acessing SandboxedPointer fields with the sandbox
    disabled, we would always do a ReadUnalignedValue/WriteUnalignedValue.
    However, that is only necessary when pointer compression is enabled.
    Otherwise, the field will be properly aligned.
    
    Bug: chromium:1292669
    Change-Id: I2c1af187c5b2699101c3fee9cc551be788d3a845
    Cq-Include-Trybots: luci.v8.try:v8_linux64_heap_sandbox_dbg_ng,v8_linux_arm64_sim_heap_sandbox_dbg_ng

---
+++ src/common/ptr-compr.h
@@ -0,0 +1,51 @@
+// Copyright 2022 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_COMMON_PTR_COMPR_H_
+#define V8_COMMON_PTR_COMPR_H_
+
+#include "src/base/memory.h"
+#include "src/common/globals.h"
+
+namespace v8 {
+namespace internal {
+
+// Accessors for fields that may be unaligned if pointer compression is enabled
+
+template <typename V>
+static inline V ReadMaybeUnalignedValue(Address p) {
+  // Pointer compression causes types larger than kTaggedSize to be unaligned.
+#ifdef V8_COMPRESS_POINTERS
+  constexpr bool v8_pointer_compression_unaligned = sizeof(V) > kTaggedSize;
+#else
+  constexpr bool v8_pointer_compression_unaligned = false;
+#endif
+  if (std::is_same<V, double>::value || v8_pointer_compression_unaligned) {
+    // Bug(v8:8875) Double fields may be unaligned.
+    return base::ReadUnalignedValue<V>(p);
+  } else {
+    return base::Memory<V>(p);
+  }
+}
+
+template <typename V>
+static inline void WriteMaybeUnalignedValue(Address p, V value) {
+  // Pointer compression causes types larger than kTaggedSize to be unaligned.
+#ifdef V8_COMPRESS_POINTERS
+  constexpr bool v8_pointer_compression_unaligned = sizeof(V) > kTaggedSize;
+#else
+  constexpr bool v8_pointer_compression_unaligned = false;
+#endif
+  if (std::is_same<V, double>::value || v8_pointer_compression_unaligned) {
+    // Bug(v8:8875) Double fields may be unaligned.
+    base::WriteUnalignedValue<V>(p, value);
+  } else {
+    base::Memory<V>(p) = value;
+  }
+}
+
+}  // namespace internal
+}  // namespace v8
+
+#endif  // V8_COMMON_PTR_COMPR_H_
