cf43c830d90733f563bb9b72fc7e7cd2f7e65b95
Benedikt Meurer <bmeurer@chromium.org>
2022-02-21 16:52:54 +0100

    [inspector] Correctly report private accessor properties.
    When calling `Runtime.getProperties` with `accessorPropertiesOnly` we
    previously did not report any private fields at all, although it is
    possible to define private accessors.
    
    Bug: chromium:1296855
    Change-Id: I18b84bfc81449d224738ba3de1f0c41c234025b2

--- src/inspector/value-mirror.cc
+++ src/inspector/value-mirror.cc
@@ -893,7 +893,7 @@ void getPrivatePropertiesForPreview(
     int* nameLimit, bool* overflow,
     protocol::Array<PropertyPreview>* privateProperties) {
   std::vector<PrivatePropertyMirror> mirrors =
-      ValueMirror::getPrivateProperties(context, object);
+      ValueMirror::getPrivateProperties(context, object, false);
   for (auto& mirror : mirrors) {
     std::unique_ptr<PropertyPreview> propertyPreview;
     if (mirror.value) {
@@ -1429,7 +1429,8 @@ void ValueMirror::getInternalProperties(
 
 // static
 std::vector<PrivatePropertyMirror> ValueMirror::getPrivateProperties(
-    v8::Local<v8::Context> context, v8::Local<v8::Object> object) {
+    v8::Local<v8::Context> context, v8::Local<v8::Object> object,
+    bool accessorPropertiesOnly) {
   std::vector<PrivatePropertyMirror> mirrors;
   v8::Isolate* isolate = context->GetIsolate();
   v8::MicrotasksScope microtasksScope(isolate,
@@ -1462,6 +1463,8 @@ std::vector<PrivatePropertyMirror> ValueMirror::getPrivateProperties(
       if (!setter->IsNull()) {
         setterMirror = ValueMirror::create(context, setter);
       }
+    } else if (accessorPropertiesOnly) {
+      continue;
     } else {
       valueMirror = ValueMirror::create(context, value);
     }
