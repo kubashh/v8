807445751cd17dcc8279127a97da10fe288f03ea
Lu Yahan <yahan@iscas.ac.cn>
2022-03-01 16:58:57 +0800

    [riscv64][register-alloc] Implement vector register independently allocating
    vector register has different register file from float register in Risc64 rvv extension.
    So this cl add third FPalising kind INDEPENDENT to allocate independently simd register.
    
    Bug: v8:11976
    
    doc: https://docs.google.com/document/d/1UwmUwOI3eeIMYzZFRmeXmfyNXRFHNZAQ4BcN0ODdMmo/edit?usp=sharing
    
    Change-Id: I0fb8901294b4bc44b0bee55e630b60460e42bef2

--- src/compiler/backend/gap-resolver.cc
+++ src/compiler/backend/gap-resolver.cc
@@ -22,7 +22,7 @@ namespace {
 // aliasing, and makes swaps much easier to implement.
 MoveOperands* Split(MoveOperands* move, MachineRepresentation smaller_rep,
                     ParallelMove* moves) {
-  DCHECK(!kSimpleFPAliasing);
+  DCHECK(kFPAliasing == AliasingKind::COMBINE);
   // Splitting is only possible when the slot size is the same as float size.
   DCHECK_EQ(kSystemPointerSize, kFloatSize);
   const LocationOperand& src_loc = LocationOperand::cast(move->source());
@@ -104,7 +104,8 @@ void GapResolver::Resolve(ParallelMove* moves) {
     i++;
     source_kinds.Add(GetKind(move->source()));
     destination_kinds.Add(GetKind(move->destination()));
-    if (!kSimpleFPAliasing && move->destination().IsFPRegister()) {
+    if (kFPAliasing == AliasingKind::COMBINE &&
+        move->destination().IsFPRegister()) {
       fp_reps |= RepresentationBit(
           LocationOperand::cast(move->destination()).representation());
     }
@@ -119,7 +120,7 @@ void GapResolver::Resolve(ParallelMove* moves) {
     return;
   }
 
-  if (!kSimpleFPAliasing) {
+  if (kFPAliasing == AliasingKind::COMBINE) {
     if (fp_reps && !base::bits::IsPowerOfTwo(fp_reps)) {
       // Start with the smallest FP moves, so we never encounter smaller moves
       // in the middle of a cycle of larger moves.
@@ -167,7 +168,7 @@ void GapResolver::PerformMove(ParallelMove* moves, MoveOperands* move) {
 
   // We may need to split moves between FP locations differently.
   const bool is_fp_loc_move =
-      !kSimpleFPAliasing && destination.IsFPLocationOperand();
+      kFPAliasing == AliasingKind::COMBINE && destination.IsFPLocationOperand();
 
   // Perform a depth-first traversal of the move graph to resolve dependencies.
   // Any unperformed, unpending move with a source the same as this one's
