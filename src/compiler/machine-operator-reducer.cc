7ab9aa4d91a2f37446c31f521567b2a9d9bc7a74
Darius M <dmercadier@chromium.org>
2022-03-07 17:50:45 +0100

    [compiler] Improve code generated for patterns like "x >> 1 == 0"
    Change-Id: I79575ba61a3bdea93468f48d66a3cb3edd0e1442

--- src/compiler/machine-operator-reducer.cc
+++ src/compiler/machine-operator-reducer.cc
@@ -1370,6 +1370,40 @@ Reduction MachineOperatorReducer::ReduceWord32Comparisons(Node* node) {
       return Changed(node);
     }
   }
+  // Simplifying (x >> n) <= k into x <= (k << n), with "k << n" being being
+  // computed here at compile time.
+  if (m.right().HasResolvedValue() &&
+      m.left().op() == machine()->Word32SarShiftOutZeros()) {
+    uint32_t right = m.right().ResolvedValue();
+    Int32BinopMatcher mleft(m.left().node());
+    if (mleft.right().HasResolvedValue()) {
+      auto shift = mleft.right().ResolvedValue();
+      // Making sure that no significant bits will be shifted away.
+      if (static_cast<int32_t>(right) << shift >> shift ==
+          static_cast<int32_t>(right)) {
+        node->ReplaceInput(0, mleft.left().node());
+        node->ReplaceInput(1, Int32Constant(right << shift));
+        return Changed(node);
+      }
+    }
+  }
+  // Simplifying y <= (x >> k) into (k << n) <= x, with "k << n" being being
+  // computed here at compile time.
+  if (m.left().HasResolvedValue() &&
+      m.right().op() == machine()->Word32SarShiftOutZeros()) {
+    uint32_t left = m.left().ResolvedValue();
+    Int32BinopMatcher mright(m.right().node());
+    if (mright.right().HasResolvedValue()) {
+      auto shift = mright.right().ResolvedValue();
+      // Making sure that no significant bits will be shifted away.
+      if (static_cast<int32_t>(left) << shift >> shift ==
+          static_cast<int32_t>(left)) {
+        node->ReplaceInput(0, Int32Constant(left << shift));
+        node->ReplaceInput(1, mright.left().node());
+        return Changed(node);
+      }
+    }
+  }
   return NoChange();
 }
 
@@ -2322,6 +2356,20 @@ MachineOperatorReducer::ReduceWord32EqualForConstantRhs(Node* lhs,
           }
           return std::make_pair(Word32And(new_input, new_mask), new_rhs);
         }
+      }
+    }
+  }
+  // Replaces (x >> n) == k with x == k << n, with "k << n" being being computed
+  // here at compile time.
+  if (lhs->op() == machine()->Word32SarShiftOutZeros() &&
+      lhs->UseCount() == 1) {
+    typename WordNAdapter::UintNBinopMatcher mshift(lhs);
+    if (mshift.right().HasResolvedValue()) {
+      auto shift = mshift.right().ResolvedValue();
+      // Making sure that no significant bits will be shifted away.
+      if (static_cast<int32_t>(rhs) << shift >> shift ==
+          static_cast<int32_t>(rhs)) {
+        return std::make_pair(mshift.left().node(), rhs << shift);
       }
     }
   }
