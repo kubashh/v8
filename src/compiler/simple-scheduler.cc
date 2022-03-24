1ba85be5219caff42372444649215893886b1961
Hao Xu <hao.a.xu@intel.com>
2022-03-25 00:13:25 +0800

    [wasm][revec] Add SimpleScheduler
    Bug: v8:12716
    Change-Id: I20506f28a9126f881b7e4748f54b12551967ba76

---
+++ src/compiler/simple-scheduler.cc
@@ -0,0 +1,94 @@
+// Copyright 2013 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/compiler/simple-scheduler.h"
+
+#include "src/compiler/graph.h"
+#include "src/compiler/node-properties.h"
+#include "src/compiler/node.h"
+#include "src/zone/zone-containers.h"
+
+namespace v8 {
+namespace internal {
+namespace compiler {
+
+SimpleScheduler::SimpleScheduler(Zone* zone, Graph* graph)
+    : zone_(zone),
+      graph_(graph),
+      dominator_level_(zone),
+      immediate_dominator_(zone) {
+  ComputeDominatorsLevel();
+}
+
+void SimpleScheduler::ComputeDominatorsLevel() {
+  Node* start = graph_->start();
+  SetDominatorLevel(start, 0);
+
+  // Do BFS from the start node and compute the level of
+  // each dominator.
+  std::queue<Node*> queue({start});
+  while (!queue.empty()) {
+    Node* node = queue.front();
+    int level = GetDominatorLevel(node);
+    queue.pop();
+    for (Edge const edge : node->use_edges()) {
+      if (!NodeProperties::IsControlEdge(edge)) continue;
+      Node* use = edge.from();
+      if (dominator_level_.find(use) == dominator_level_.end() &&
+          use->opcode() != IrOpcode::kEnd) {
+        SetDominatorLevel(use, level + 1);
+        queue.push(use);
+      }
+    }
+  }
+}
+
+Node* SimpleScheduler::GetImmediateDominator(Node* node) {
+  DCHECK(!NodeProperties::IsControl(node));
+
+  auto it = immediate_dominator_.find(node);
+  if (it != immediate_dominator_.end()) return it->second;
+
+  Node* idom = nullptr;
+  // For phi node, the immediate dominator is its control node.
+  if (NodeProperties::IsPhi(node)) {
+    idom = NodeProperties::GetControlInput(node);
+  } else {
+    // For non-phi node, the immediate dominator is the immediate dominator
+    // of its input with maximal level.
+    int maximal_level = -1;
+    if (node->InputCount() == 0) {
+      idom = graph_->start();
+    }
+    for (int i = 0; i < node->InputCount(); ++i) {
+      Node* input = node->InputAt(i);
+      Node* dominator = nullptr;
+      if (NodeProperties::IsControl(input)) {
+        dominator = input;
+      } else {
+        dominator = GetImmediateDominator(input);
+      }
+      int level = GetDominatorLevel(dominator);
+      if (level > maximal_level) {
+        idom = dominator;
+      }
+    }
+  }
+
+  DCHECK(idom != nullptr);
+  SetImmediateDominator(node, idom);
+  return idom;
+}
+
+bool SimpleScheduler::SameBasicBlock(Node* node_a, Node* node_b) {
+  if (NodeProperties::IsControl(node_a)) {
+    return node_a == node_b;
+  } else {
+    return GetImmediateDominator(node_a) == GetImmediateDominator(node_b);
+  }
+}
+
+}  // namespace compiler
+}  // namespace internal
+}  // namespace v8
