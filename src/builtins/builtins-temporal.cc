3ff86b60089f281a11e8479a2562e6c7a98e9d1f
Frank Tang <ftang@chromium.org>
2022-03-26 20:03:33 -0700

    [Temporal] Add Calendar.prototype.dateUntil
    Also add AOs: ToLargestTemporalUnit, ToISODayOfYear, RegulateISODate,
    AddISODate, DifferenceISODate
    
    Spec Text:
    https://tc39.es/proposal-temporal/#sec-temporal.calendar.prototype.dateuntil
    https://tc39.es/proposal-temporal/#sec-temporal-tolargesttemporalunit
    https://tc39.es/proposal-temporal/#sec-temporal-toisodayofyear
    https://tc39.es/proposal-temporal/#sec-temporal-regulateisodate
    https://tc39.es/proposal-temporal/#sec-temporal-addisodate
    https://tc39.es/proposal-temporal/#sec-temporal-differenceisodate
    
    Bug: v8:11544
    Change-Id: I03a28bf07ddfae036491e49cb06278d050ddebf6

--- src/builtins/builtins-temporal.cc
+++ src/builtins/builtins-temporal.cc
@@ -314,8 +314,6 @@ TO_BE_IMPLEMENTED(TemporalCalendarPrototypeYearMonthFromFields)
 TO_BE_IMPLEMENTED(TemporalCalendarPrototypeMonthDayFromFields)
 /* Temporal #sec-temporal.calendar.prototype.dateadd */
 TO_BE_IMPLEMENTED(TemporalCalendarPrototypeDateAdd)
-/* Temporal #sec-temporal.calendar.prototype.dateuntil */
-TO_BE_IMPLEMENTED(TemporalCalendarPrototypeDateUntil)
 /* Temporal #sec-temporal.calendar.prototype.month */
 TO_BE_IMPLEMENTED(TemporalCalendarPrototypeMonth)
 /* Temporal #sec-temporal.calendar.prototype.monthcode */
@@ -800,8 +798,9 @@ TEMPORAL_GET_BIGINT_AFTER_DIVID(Instant, EpochMicroseconds, nanoseconds, 1000,
 // Calendar
 TEMPORAL_CONSTRUCTOR1(Calendar)
 TEMPORAL_ID_BY_TO_STRING(Calendar)
-TEMPORAL_PROTOTYPE_METHOD1(Calendar, Year, year)
+TEMPORAL_PROTOTYPE_METHOD3(Calendar, DateUntil, dateUntil)
 TEMPORAL_PROTOTYPE_METHOD1(Calendar, DaysInYear, daysInYear)
+TEMPORAL_PROTOTYPE_METHOD1(Calendar, Year, year)
 TEMPORAL_TO_STRING(Calendar)
 // #sec-temporal.calendar.from
 BUILTIN(TemporalCalendarFrom) {
