624f5cf0f5b343b0b92f4473c22134837ad364ca
Alexander Schulze <alexschulze@chromium.org>
2022-04-06 11:08:31 +0000

    Whitespace change
    Bug: v8:12743
    Change-Id: I4e1fe666125139567d9bf00f7f7b77a14107e76c

--- include/v8-inspector-protocol.h
+++ include/v8-inspector-protocol.h
@@ -1,6 +1,7 @@
 // Copyright 2016 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
 
 #ifndef V8_V8_INSPECTOR_PROTOCOL_H_
 #define V8_V8_INSPECTOR_PROTOCOL_H_
