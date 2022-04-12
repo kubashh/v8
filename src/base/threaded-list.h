7794f66472829f8fd8d69b0c49379bf1b6c7c5d6
Victor Gomes <victorgomes@chromium.org>
2022-04-12 14:40:54 +0200

    [base] Lazily ensure ThreadList invariant
    Since next() comes from ThreadedListTraits, the users of this class
    can modify the list by modifying the next pointer. This however breaks
    the invariant that `tail_` points to the last element of the list.
    
    We ensure this invariant lazily. This should be _almost_ no effect
    for users that do not manually modify the next pointer.
    
    Change-Id: If46283ab4fc5036a81f353b25823b0fd39b3e232

--- src/base/threaded-list.h
+++ src/base/threaded-list.h
@@ -33,6 +33,7 @@ class ThreadedListBase final : public BaseClass {
   ThreadedListBase& operator=(const ThreadedListBase&) = delete;
 
   void Add(T* v) {
+    EnsureValidTail();
     DCHECK_NULL(*tail_);
     DCHECK_NULL(*TLTraits::next(v));
     *tail_ = v;
@@ -70,6 +71,7 @@ class ThreadedListBase final : public BaseClass {
   void Append(ThreadedListBase&& list) {
     if (list.is_empty()) return;
 
+    EnsureValidTail();
     *tail_ = list.head_;
     tail_ = list.tail_;
     list.Clear();
@@ -78,6 +80,7 @@ class ThreadedListBase final : public BaseClass {
   void Prepend(ThreadedListBase&& list) {
     if (list.head_ == nullptr) return;
 
+    EnsureValidTail();
     T* new_head = list.head_;
     *list.tail_ = head_;
     if (head_ == nullptr) {
@@ -116,6 +119,7 @@ class ThreadedListBase final : public BaseClass {
       return true;
     }
 
+    EnsureValidTail();
     while (current != nullptr) {
       T* next = *TLTraits::next(current);
       if (next == v) {
@@ -213,10 +217,16 @@ class ThreadedListBase final : public BaseClass {
   };
 
   Iterator begin() { return Iterator(TLTraits::start(&head_)); }
-  Iterator end() { return Iterator(tail_); }
+  Iterator end() {
+    EnsureValidTail();
+    return Iterator(tail_);
+  }
 
   ConstIterator begin() const { return ConstIterator(TLTraits::start(&head_)); }
-  ConstIterator end() const { return ConstIterator(tail_); }
+  ConstIterator end() const {
+    EnsureValidTail();
+    return ConstIterator(tail_);
+  }
 
   // Rewinds the list's tail to the reset point, i.e., cutting of the rest of
   // the list, including the reset_point.
@@ -253,7 +263,7 @@ class ThreadedListBase final : public BaseClass {
     return *t;
   }
 
-  bool Verify() {
+  bool Verify() const {
     T* last = this->first();
     if (last == nullptr) {
       CHECK_EQ(&head_, tail_);
@@ -266,7 +276,13 @@ class ThreadedListBase final : public BaseClass {
     return true;
   }
 
-  void RevalidateTail() {
+  inline void EnsureValidTail() const {
+    if (V8_UNLIKELY(*tail_ != nullptr)) {
+      RevalidateTail();
+    }
+  }
+
+  void RevalidateTail() const {
     T* last = *tail_;
     if (last != nullptr) {
       while (*TLTraits::next(last) != nullptr) {
@@ -274,12 +290,12 @@ class ThreadedListBase final : public BaseClass {
       }
       tail_ = TLTraits::next(last);
     }
-    SLOW_DCHECK(Verify());
   }
 
  private:
   T* head_;
-  T** tail_;
+  mutable T** tail_;  // We need to ensure a valid `tail_` even when using a
+                      // const Iterator.
 };
 
 struct EmptyBase {};
