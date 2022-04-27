c4de9f2eebdf24f839c988026d842410d3cf8ca2
Camillo Bruni <cbruni@chromium.org>
2022-04-27 15:29:40 +0200

    [flags] Introduce --max-opt flag and replace --opt with --turbofan
    There are currently several flags to tune V8's optimisation level:
      --sparkplug, --maglev, --opt
    
    This CL tries to make this simpler by introducing yet another flag.
    --max-opt limits the maximum optimisation tier and avoids the common
    error to mistake --no-opt with no dynamic optimisations.
    
    Settings:
      --max-opt=999 Allow all optimisations, default configuration
      --max-opt=0   Allow only ignition
      --max-opt=1   Allow up to sparkplug
      --max-opt=2   Allow up to maglev
    
    
    Bug: v8:12825
    Change-Id: Iff9a0fcccdf05e9770168053a1430303613a7299

--- test/mjsunit/es6/super-ic-opt-no-turboprop.js
+++ test/mjsunit/es6/super-ic-opt-no-turboprop.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --allow-natives-syntax --super-ic --opt
+// Flags: --allow-natives-syntax --super-ic --turbofan
 // Flags: --no-always-opt --no-stress-opt --deopt-every-n-times=0
 
 // This file contains tests which are disabled for TurboProp. TurboProp deopts
--- test/mjsunit/optimized-array-some.js
+++ test/mjsunit/optimized-array-some.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --allow-natives-syntax --turbo-inline-array-builtins --opt
+// Flags: --allow-natives-syntax --turbo-inline-array-builtins --turbofan
 // Flags: --no-always-opt --no-lazy-feedback-allocation
 
 // TODO(v8:10195): Fix these tests s.t. we assert deoptimization occurs when
