41be5db0cf6b1a1736b39ff8e4111f009f3342e5
Hao Xu <hao.a.xu@intel.com>
2022-05-10 22:12:19 +0800

    [wasm][revec] Add LinearScheduler
    Add a simple, linear-time scheduler to check whether two nodes can be
    scheduled to a same basic block without actually building basic blocks.
    
    Bug: v8:12716
    Change-Id: I20506f28a9126f881b7e4748f54b12551967ba76

---
+++ src/compiler/linear-scheduler.cc
@@ -0,0 +1,91 @@
+// Copyright 2013 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/compiler/linear-scheduler.h"
+
+#include "src/compiler/graph.h"
+#include "src/compiler/node-properties.h"
+#include "src/compiler/node.h"
+#include "src/zone/zone-containers.h"
+
+namespace v8 {
+namespace internal {
+namespace compiler {
+
+LinearScheduler::LinearScheduler(Zone* zone, Graph* graph)
+    : graph_(graph), dominator_level_(zone), immediate_dominator_(zone) {
+  ComputeDominatorsLevel();
+}
+
+void LinearScheduler::ComputeDominatorsLevel() {
+  Node* start = graph_->start();
+  SetDominatorLevel(start, 0);
+
+  // Do BFS from the start node and compute the level of
+  // each dominator.
+  std::queue<Node*> queue({start});
+  while (!queue.empty()) {
+    Node* node = queue.front();
+    int level = GetDominatorLevel(node);
+    queue.pop();
+    for (Edge const edge : node->use_edges()) {
+      if (!NodeProperties::IsControlEdge(edge)) continue;
+      Node* use = edge.from();
+      if (dominator_level_.find(use) == dominator_level_.end() &&
+          use->opcode() != IrOpcode::kEnd) {
+        SetDominatorLevel(use, level + 1);
+        queue.push(use);
+      }
+    }
+  }
+}
+
+Node* LinearScheduler::GetImmediateDominator(Node* node) {
+  DCHECK(!NodeProperties::IsControl(node));
+
+  auto it = immediate_dominator_.find(node);
+  if (it != immediate_dominator_.end()) return it->second;
+
+  Node* idom = nullptr;
+  // For phi node, the immediate dominator is its control node.
+  if (NodeProperties::IsPhi(node)) {
+    idom = NodeProperties::GetControlInput(node);
+  } else {
+    // For non-phi node, the immediate dominator is the immediate dominator
+    // of its input with maximal level.
+    int maximal_level = -1;
+    if (node->InputCount() == 0) {
+      idom = graph_->start();
+    }
+    for (int i = 0; i < node->InputCount(); ++i) {
+      Node* input = node->InputAt(i);
+      Node* dominator = nullptr;
+      if (NodeProperties::IsControl(input)) {
+        dominator = input;
+      } else {
+        dominator = GetImmediateDominator(input);
+      }
+      int level = GetDominatorLevel(dominator);
+      if (level > maximal_level) {
+        idom = dominator;
+      }
+    }
+  }
+
+  DCHECK(idom != nullptr);
+  SetImmediateDominator(node, idom);
+  return idom;
+}
+
+bool LinearScheduler::SameBasicBlock(Node* node0, Node* node1) {
+  Node* idom0 =
+      NodeProperties::IsControl(node0) ? node0 : GetImmediateDominator(node0);
+  Node* idom1 =
+      NodeProperties::IsControl(node1) ? node1 : GetImmediateDominator(node1);
+  return idom0 == idom1;
+}
+
+}  // namespace compiler
+}  // namespace internal
+}  // namespace v8
