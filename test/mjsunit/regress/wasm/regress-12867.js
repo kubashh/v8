82facde19b869975ba29a7496518603e1c2763ee
Jakob Kummerow <jkummerow@chromium.org>
2022-05-12 14:28:48 +0200

    [wasm] Properly reject modules with unknown sections
    The IsValidSectionCode function shouldn't include internally-used
    numeric identifiers of well-known optional sections.
    
    Fixed: v8:12867
    Change-Id: I9d894ee57157455e92a17ddcde94f32f05fb038d

---
+++ test/mjsunit/regress/wasm/regress-12867.js
@@ -0,0 +1,16 @@
+// Copyright 2022 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+d8.file.execute("test/mjsunit/wasm/wasm-module-builder.js");
+
+for (let section = kLastKnownSectionCode + 1;
+     section <= kLastKnownSectionCode + 20; section++) {
+  let module = Uint8Array.from([0, 97, 115, 109, 1, 0, 0, 0, section, 0]);
+  //new WebAssembly.Instance(new WebAssembly.Module(module));
+  let hex = section.toString(16).padStart(2, '0');
+  let msg = `WebAssembly.Module(): unknown section code #0x${hex} @+10`;
+  assertThrows(
+    () => new WebAssembly.Instance(new WebAssembly.Module(module)),
+  WebAssembly.CompileError, msg);
+}
