081032638afd704afa8351018eb2c0aa788332ab
Dominik Inführ <dinfuehr@chromium.org>
2022-05-12 21:17:06 +0200

    [heap, deoptimizer] Do not invalidate slots in materialized objects
    We initialize the JSObject in the payload of a ByteArray, so we know
    that no slots were recorded there. This also means we don't need to
    remove any recorded slots and thus invalidation isn't required.
    
    Which this change only string use object slot invalidation on
    String::MakeExternal.
    
    Bug: v8:12578
    Change-Id: I009635c2a61ae8ff2b9e2480cb7d374451a8cc7d

--- src/deoptimizer/translated-state.cc
+++ src/deoptimizer/translated-state.cc
@@ -13,6 +13,7 @@
 #include "src/diagnostics/disasm.h"
 #include "src/execution/frames.h"
 #include "src/execution/isolate.h"
+#include "src/heap/heap.h"
 #include "src/numbers/conversions.h"
 #include "src/objects/arguments.h"
 #include "src/objects/heap-number-inl.h"
@@ -1850,7 +1851,17 @@ void TranslatedState::InitializeJSObjectAt(
   CHECK_GE(children_count, 2);
 
   // Notify the concurrent marker about the layout change.
-  isolate()->heap()->NotifyObjectLayoutChange(*object_storage, no_gc);
+  isolate()->heap()->NotifyObjectLayoutChange(*object_storage, no_gc,
+                                              InvalidateRecordedSlots::kNo);
+
+#if DEBUG
+  // No need to invalidate slots in object because no slot was recorded yet.
+  // Verify this here.
+  Address object_start = object_storage->address();
+  Address object_end = object_start + children_count * kTaggedSize;
+  isolate()->heap()->VerifySlotRangeHasNoRecordedSlots(object_start,
+                                                       object_end);
+#endif
 
   // Fill the property array field.
   {
@@ -1902,7 +1913,17 @@ void TranslatedState::InitializeObjectWithTaggedFieldsAt(
   }
 
   // Notify the concurrent marker about the layout change.
-  isolate()->heap()->NotifyObjectLayoutChange(*object_storage, no_gc);
+  isolate()->heap()->NotifyObjectLayoutChange(*object_storage, no_gc,
+                                              InvalidateRecordedSlots::kNo);
+
+#if DEBUG
+  // No need to invalidate slots in object because no slot was recorded yet.
+  // Verify this here.
+  Address object_start = object_storage->address();
+  Address object_end = object_start + children_count * kTaggedSize;
+  isolate()->heap()->VerifySlotRangeHasNoRecordedSlots(object_start,
+                                                       object_end);
+#endif
 
   // Write the fields to the object.
   for (int i = 1; i < children_count; i++) {
