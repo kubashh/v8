01822138063f8a9f108cbffb09d4a3287fd08ab6
Samuel Groß <saelo@chromium.org>
2022-05-17 17:58:01 +0200

    [sandbox] Add new Memory Corruption API
    When enabled, this API exposes a new global 'Memory' object which is a
    JSDataView with access to the entire sandbox address space. Further, the
    'Memory' object exposes two new functions: getAddressOf(obj) and
    getSizeOf(obj), which can be used respectively to obtain the address
    (relative to the base of the sandbox) and size of any HeapObject that
    can be accessed from JavaScript. The 'Memory' object therefore provides
    precise memory corruption primitives, in effect emulating typical V8
    exploits. It is useful for testing the sandbox, for example to
    facilitate developing PoC sandbox escapes or writing regression tests.
    In the future, it may also be used by custom V8 sandbox fuzzers.
    
    Bug: v8:12878
    Change-Id: I4e420b2ff28bd834b0693f1546942e51c71bfdda
    Cq-Include-Trybots: luci.v8.try:v8_linux64_heap_sandbox_dbg_ng,v8_linux_arm64_sim_heap_sandbox_dbg_ng

--- src/init/bootstrapper.cc
+++ src/init/bootstrapper.cc
@@ -24,6 +24,7 @@
 #include "src/logging/runtime-call-stats-scope.h"
 #include "src/objects/instance-type.h"
 #include "src/objects/objects.h"
+#include "src/sandbox/testing.h"
 #ifdef ENABLE_VTUNE_TRACEMARK
 #include "src/extensions/vtunedomain-support-extension.h"
 #endif  // ENABLE_VTUNE_TRACEMARK
@@ -5895,6 +5896,10 @@ bool Genesis::InstallSpecialObjects(Isolate* isolate,
     WasmJs::Install(isolate, false);
   }
 #endif  // V8_ENABLE_WEBASSEMBLY
+
+#ifdef V8_EXPOSE_MEMORY_CORRUPTION_API
+  MemoryCorruptionApi::Install(isolate);
+#endif  // V8_EXPOSE_MEMORY_CORRUPTION_API
 
   return true;
 }
