68e0f7feed4269759f727204ee8307aa60d9f6d3
Marja Hölttä <marja@chromium.org>
2022-05-17 12:03:47 +0200

    [web snapshot] Add support for derived constructors
    Required side fix: allowing functions from multiple scripts.
    
    Bug:v8:11525
    Change-Id: I8dc50b9144d17134a7c9b8fdbabe23f5e44f197f

--- src/web-snapshot/web-snapshot.h
+++ src/web-snapshot/web-snapshot.h
@@ -99,7 +99,7 @@ class WebSnapshotSerializerDeserializer {
 
   void IterateBuiltinObjects(std::function<void(String, HeapObject)> func);
 
-  static constexpr int kBuiltinObjectCount = 4;
+  static constexpr int kBuiltinObjectCount = 12;
 
   inline Factory* factory() const { return isolate_->factory(); }
 
@@ -220,8 +220,8 @@ class V8_EXPORT WebSnapshotSerializer
   void DiscoverObjectPropertiesWithDictionaryMap(T dict);
   void ConstructSource();
 
-  void SerializeFunctionInfo(ValueSerializer* serializer,
-                             Handle<JSFunction> function);
+  void SerializeFunctionInfo(Handle<JSFunction> function,
+                             ValueSerializer& serializer);
 
   void SerializeString(Handle<String> string, ValueSerializer& serializer);
   void SerializeSymbol(Handle<Symbol> symbol);
@@ -399,6 +399,7 @@ class V8_EXPORT WebSnapshotDeserializer
   void DeserializeExports(bool skip_exports);
   void DeserializeObjectPrototype(Handle<Map> map);
   Handle<Map> DeserializeObjectPrototypeAndCreateEmptyMap();
+  void DeserializeObjectPrototypeForFunction(Handle<JSFunction> function);
   void SetPrototype(Handle<Map> map, Handle<Object> prototype);
 
   template <typename T>
