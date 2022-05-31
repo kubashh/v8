eb1c567946193fb49d39cf90a5d3db5009af4f3b
Marja Hölttä <marja@chromium.org>
2022-05-31 13:27:42 +0200

    [rab/gsab] Tests for Array.p methods, part 1
    In this part: concat, includes.
    
    Bug:v8:11111
    Change-Id: I63eee835661310c21ad53992ae3b161cc3214dfc

---
+++ test/mjsunit/typedarray-resizablearraybuffer-array-methods.js
@@ -0,0 +1,39 @@
+// Copyright 2022 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --harmony-rab-gsab --allow-natives-syntax
+
+'use strict';
+
+d8.file.execute('test/mjsunit/typedarray-helpers.js');
+
+(function ArrayConcat() {
+  for (let ctor of ctors) {
+    const rab = CreateResizableArrayBuffer(4 * ctor.BYTES_PER_ELEMENT,
+                                           8 * ctor.BYTES_PER_ELEMENT);
+    const fixedLength = new ctor(rab, 0, 4);
+
+    const taWrite = new ctor(rab);
+    for (let i = 0; i < 4; ++i) {
+      WriteToTypedArray(taWrite, i, i + 1);
+    }
+
+    // Orig. array: [1, 2, 3, 4]
+    //              [1, 2, 3, 4] << fixedLength
+    //                    [3, 4] << fixedLengthWithOffset
+    //              [1, 2, 3, 4, ...] << lengthTracking
+    //                    [3, 4, ...] << lengthTrackingWithOffset
+
+    function helper(receiver, ...params) {
+      return ToNumbers(Array.prototype.concat.call(receiver, ...params));
+    }
+
+    // TypedArrays aren't concat spreadable.
+    assertEquals([fixedLength, 5, 6, 7], helper(fixedLength, [5, 6], [7]));
+
+    // OOBness doesn't matter since the TA is added as a single item.
+    rab.resize(0);
+    assertEquals([fixedLength, 5, 6, 7], helper(fixedLength, [5, 6], [7]));
+  }
+})();
