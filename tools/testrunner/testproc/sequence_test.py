48ce1247fddd389692bbb62bdc67f85a4f6f348b
Liviu Rau <liviurau@google.com>
2022-06-03 19:22:50 +0200

    [test] Refactor commands
    Bug: v8:12785
    Change-Id: I0ec2e9c6a81604a5cd1d4a80982dd3329c1b48db

--- tools/testrunner/testproc/sequence_test.py
+++ tools/testrunner/testproc/sequence_test.py
@@ -64,7 +64,7 @@ class TestSequenceProc(unittest.TestCase):
   def _test(self, tests, batch_size, max_heavy):
     # Set up a simple processing pipeline:
     # Loader -> observe results -> sequencer -> execution.
-    loader = LoadProc(iter(tests))
+    loader = LoadProc(iter(tests), batch_size)
     results = FakeResultObserver()
     sequence_proc = SequenceProc(max_heavy)
     execution = FakeExecutionProc()
@@ -74,7 +74,7 @@ class TestSequenceProc(unittest.TestCase):
 
     # Fill the execution queue (with the number of tests potentially
     # executed in parallel).
-    loader.load_initial_tests(batch_size)
+    loader.setup()
 
     # Simulate the execution test by test.
     while execution.tests:
