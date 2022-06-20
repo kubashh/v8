143ffb4c7c2c6c1f6df89ccbc6a357920bcb1479
Lu Yahan <yahan@iscas.ac.cn>
2022-06-20 19:38:11 +0800

    [riscv64] Optmize load float zero
    Change-Id: Ia651b26af419a2187217b8b0f2941ff61a17d247

--- src/codegen/riscv64/macro-assembler-riscv64.cc
+++ src/codegen/riscv64/macro-assembler-riscv64.cc
@@ -2406,6 +2406,7 @@ void TurboAssembler::InsertLowWordF64(FPURegister dst, Register src_low) {
 }
 
 void TurboAssembler::LoadFPRImmediate(FPURegister dst, uint32_t src) {
+  ASM_CODE_COMMENT(this);
   // Handle special values first.
   if (src == base::bit_cast<uint32_t>(0.0f) && has_single_zero_reg_set_) {
     if (dst != kDoubleRegZero) fmv_s(dst, kDoubleRegZero);
@@ -2413,7 +2414,7 @@ void TurboAssembler::LoadFPRImmediate(FPURegister dst, uint32_t src) {
              has_single_zero_reg_set_) {
     Neg_s(dst, kDoubleRegZero);
   } else {
-    if (dst == kDoubleRegZero) {
+    if (dst == kDoubleRegZero || src == base::bit_cast<uint32_t>(0.0f)) {
       DCHECK(src == base::bit_cast<uint32_t>(0.0f));
       fmv_w_x(dst, zero_reg);
       has_single_zero_reg_set_ = true;
@@ -2428,6 +2429,7 @@ void TurboAssembler::LoadFPRImmediate(FPURegister dst, uint32_t src) {
 }
 
 void TurboAssembler::LoadFPRImmediate(FPURegister dst, uint64_t src) {
+  ASM_CODE_COMMENT(this);
   // Handle special values first.
   if (src == base::bit_cast<uint64_t>(0.0) && has_double_zero_reg_set_) {
     if (dst != kDoubleRegZero) fmv_d(dst, kDoubleRegZero);
@@ -2435,7 +2437,7 @@ void TurboAssembler::LoadFPRImmediate(FPURegister dst, uint64_t src) {
              has_double_zero_reg_set_) {
     Neg_d(dst, kDoubleRegZero);
   } else {
-    if (dst == kDoubleRegZero) {
+    if (dst == kDoubleRegZero || src == base::bit_cast<uint64_t>(0.0)) {
       DCHECK(src == base::bit_cast<uint64_t>(0.0));
       fmv_d_x(dst, zero_reg);
       has_double_zero_reg_set_ = true;
@@ -3624,7 +3626,12 @@ void TurboAssembler::FPUCanonicalizeNaN(const DoubleRegister dst,
   // Subtracting 0.0 preserves all inputs except for signalling NaNs, which
   // become quiet NaNs. We use fsub rather than fadd because fsub preserves -0.0
   // inputs: -0.0 + 0.0 = 0.0, but -0.0 - 0.0 = -0.0.
-  fsub_d(dst, src, kDoubleRegZero);
+  if (IsDoubleZeroRegSet()) {
+    fsub_d(dst, src, kDoubleRegZero);
+  } else {
+    LoadFPRImmediate(kDoubleRegZero, 0.0f);
+    fsub_d(dst, src, kDoubleRegZero);
+  }
 }
 
 void TurboAssembler::MovFromFloatResult(const DoubleRegister dst) {
