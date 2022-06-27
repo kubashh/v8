5cce1a491639d2cb37cabdab984f334e90de1ede
Jakob Kummerow <jkummerow@chromium.org>
2022-06-27 23:34:16 +0200

    [wasm] Add disassembler implementation
    FunctionBodyDisassembler for function bodies,
    ModuleDisassembler for the rest of modules.
    
    Both are still unused as of this CL; users will follow.
    
    Bug: v8:12917
    Change-Id: I82658ea8a401834a5b3661068766bbdfec54d5a4

---
+++ src/wasm/wasm-disassembler-impl.h
@@ -0,0 +1,160 @@
+// Copyright 2022 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if !V8_ENABLE_WEBASSEMBLY
+#error This header should only be included if WebAssembly is enabled.
+#endif  // !V8_ENABLE_WEBASSEMBLY
+
+#ifndef V8_WASM_WASM_DISASSEMBLER_IMPL_H_
+#define V8_WASM_WASM_DISASSEMBLER_IMPL_H_
+
+#include <iomanip>
+
+#include "src/wasm/function-body-decoder-impl.h"
+#include "src/wasm/string-builder-multiline.h"
+#include "src/wasm/wasm-opcodes.h"
+
+namespace v8 {
+namespace internal {
+namespace wasm {
+
+template <Decoder::ValidateFlag validate>
+class ImmediatesPrinter;
+class NamesProvider;
+
+////////////////////////////////////////////////////////////////////////////////
+// Configuration flags for aspects of behavior where we might want to change
+// our minds. {true} is the legacy DevTools behavior.
+constexpr bool kSkipFunctionTypesInTypeSection = true;
+constexpr bool kIndicesAsComments = true;
+constexpr bool kSkipDataSegmentNames = true;
+
+////////////////////////////////////////////////////////////////////////////////
+// Helpers.
+
+class Indentation {
+ public:
+  Indentation(int current, int delta) : current_(current), delta_(delta) {
+    DCHECK_GE(current, 0);
+    DCHECK_GE(delta, 0);
+  }
+
+  Indentation Extra(int extra) { return {current_ + extra, delta_}; }
+
+  void increase() { current_ += delta_; }
+  void decrease() {
+    DCHECK_GE(current_, delta_);
+    current_ -= delta_;
+  }
+  int current() { return current_; }
+
+ private:
+  int current_;
+  int delta_;
+};
+
+inline StringBuilder& operator<<(StringBuilder& sb, Indentation indentation) {
+  char* ptr = sb.write(indentation.current());
+  memset(ptr, ' ', indentation.current());
+  return sb;
+}
+
+void PrintSignatureOneLine(StringBuilder& out, const FunctionSig* sig,
+                           uint32_t func_index, NamesProvider* names,
+                           bool param_names, bool indices_as_comments = false);
+
+////////////////////////////////////////////////////////////////////////////////
+// FunctionBodyDisassembler.
+
+class FunctionBodyDisassembler : public WasmDecoder<Decoder::kFullValidation> {
+ public:
+  static constexpr Decoder::ValidateFlag validate = Decoder::kFullValidation;
+
+  FunctionBodyDisassembler(Zone* zone, const WasmModule* module,
+                           uint32_t func_index, WasmFeatures* detected,
+                           const FunctionSig* sig, const byte* start,
+                           const byte* end, uint32_t offset,
+                           NamesProvider* names)
+      : WasmDecoder<validate>(zone, module, WasmFeatures::All(), detected, sig,
+                              start, end, offset),
+        func_index_(func_index),
+        names_(names) {}
+
+  void DecodeWat(MultiLineStringBuilder& out, Indentation indentation,
+                 bool include_header = true);
+
+  void DecodeGlobalInitializer(StringBuilder& out);
+
+  std::set<uint32_t>& used_types() { return used_types_; }
+
+ protected:
+  WasmOpcode GetOpcode();
+
+  uint32_t PrintImmediatesAndGetLength(StringBuilder& out);
+
+  void PrintHexNumber(StringBuilder& out, uint64_t number);
+
+  LabelInfo& label_info(int depth) {
+    return label_stack_[label_stack_.size() - 1 - depth];
+  }
+
+  friend class ImmediatesPrinter<validate>;
+  uint32_t func_index_;
+  WasmOpcode current_opcode_ = kExprUnreachable;
+  NamesProvider* names_;
+  std::set<uint32_t> used_types_;
+  std::vector<LabelInfo> label_stack_;
+  MultiLineStringBuilder* out_;
+  // Labels use two different indexing systems: for looking them up in the
+  // name section, their order of occurrence matters; for generating names
+  // like "$label0", the order in which they show up as targets of branch
+  // instructions matters.
+  // (This is legacy wasmparser behavior; we could change it.)
+  uint32_t label_occurrence_index_ = 0;
+  uint32_t label_generation_index_ = 0;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// ModuleDisassembler.
+
+class ModuleDisassembler {
+ public:
+  ModuleDisassembler(MultiLineStringBuilder& out, const WasmModule* module,
+                     NamesProvider* names, const ModuleWireBytes wire_bytes)
+      : out_(out),
+        module_(module),
+        names_(names),
+        wire_bytes_(wire_bytes),
+        start_(wire_bytes_.start()) {}
+
+  void PrintTypeDefinition(uint32_t type_index, Indentation indendation,
+                           bool index_as_comment);
+  void PrintModule(Zone* zone, Indentation indentation);
+
+ private:
+  void PrintImportName(const WasmImport& import);
+  void PrintExportName(ImportExportKindCode kind, uint32_t index);
+  void PrintMutableType(bool mutability, ValueType type);
+  void PrintTable(const WasmTable& table);
+  void PrintMemory();
+  void PrintGlobal(const WasmGlobal& global);
+  void PrintInitExpression(const ConstantExpression& init,
+                           ValueType expected_type, Zone* zone);
+  void PrintTagSignature(const FunctionSig* sig);
+  void PrintString(WireBytesRef ref);
+  void PrintStringAsJSON(WireBytesRef ref);
+  void LineBreakOrSpace(bool break_lines, Indentation indentation);
+
+  MultiLineStringBuilder& out_;
+  const WasmModule* module_;
+  NamesProvider* names_;
+  const ModuleWireBytes wire_bytes_;
+  const byte* start_;
+};
+
+}  // namespace wasm
+}  // namespace internal
+}  // namespace v8
+
+#endif  // V8_WASM_WASM_DISASSEMBLER_IMPL_H_
