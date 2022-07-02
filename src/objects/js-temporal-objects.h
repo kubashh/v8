c86c3285510393fe217c527143adb3f5f3d07960
Frank Tang <ftang@chromium.org>
2022-07-02 13:34:02 -0700

    [Temporal] Add since/until to PlainDate/PlainTime
    Change temporal::ToTemporalTime to use default argument value
    Fid negative float division bug in BalanceTime
    
    Bug: v8:11544
    Change-Id: Ic717c69f4e399bc0ed4aaaaff4578cb329223205

--- src/objects/js-temporal-objects.h
+++ src/objects/js-temporal-objects.h
@@ -362,6 +362,16 @@ class JSTemporalPlainDate
       Isolate* isolate, Handle<JSTemporalPlainDate> plain_date,
       Handle<Object> temporal_duration_like, Handle<Object> options);
 
+  // #sec-temporal.plaindate.prototype.until
+  V8_WARN_UNUSED_RESULT static MaybeHandle<JSTemporalDuration> Until(
+      Isolate* isolate, Handle<JSTemporalPlainDate> plain_date,
+      Handle<Object> other, Handle<Object> options);
+
+  // #sec-temporal.plaindate.prototype.since
+  V8_WARN_UNUSED_RESULT static MaybeHandle<JSTemporalDuration> Since(
+      Isolate* isolate, Handle<JSTemporalPlainDate> plain_date,
+      Handle<Object> other, Handle<Object> options);
+
   // #sec-temporal.plaindate.prototype.getisofields
   V8_WARN_UNUSED_RESULT static MaybeHandle<JSReceiver> GetISOFields(
       Isolate* isolate, Handle<JSTemporalPlainDate> plain_date);
@@ -624,6 +634,16 @@ class JSTemporalPlainTime
   V8_WARN_UNUSED_RESULT static MaybeHandle<JSTemporalPlainTime> Subtract(
       Isolate* isolate, Handle<JSTemporalPlainTime> plain_time,
       Handle<Object> temporal_duration_like);
+
+  // #sec-temporal.plaintime.prototype.until
+  V8_WARN_UNUSED_RESULT static MaybeHandle<JSTemporalDuration> Until(
+      Isolate* isolate, Handle<JSTemporalPlainTime> plain_time,
+      Handle<Object> other, Handle<Object> options);
+
+  // #sec-temporal.plaintime.prototype.since
+  V8_WARN_UNUSED_RESULT static MaybeHandle<JSTemporalDuration> Since(
+      Isolate* isolate, Handle<JSTemporalPlainTime> plain_time,
+      Handle<Object> other, Handle<Object> options);
 
   // #sec-temporal.plaintime.prototype.round
   V8_WARN_UNUSED_RESULT static MaybeHandle<JSTemporalPlainTime> Round(
