1d1489f1332544bebc22c1b2cf57c5ef21fd59d0
wenqin.yang <wenqin.yang@intel.com>
2022-07-04 12:13:21 +0800

    PKU support for JavaScript on x64
    We found that PKU has been enable for WASM, it can bring benefits for compilation JIT code by reducing time cost in permission switch. Now we try to port it to JavaScript.
    
    This CL is the first CL to port PKU implements from WASM to OS lib.
    
    Change-Id: I647f8c09bc41e5ef8a1d74b58a48a43e08454e0d
    
    Bug: v8:13023

--- src/base/platform/platform-posix.h
+++ src/base/platform/platform-posix.h
@@ -28,6 +28,53 @@ class PosixTimezoneCache : public TimezoneCache {
 int GetProtectionFromMemoryPermission(OS::MemoryPermission access);
 #endif
 
+#if V8_HAS_PKU_JIT_WRITE_PROTECT
+class V8_BASE_EXPORT PKU {
+ private:
+  PKU() = default;
+  ~PKU() { this->FreeMemoryProtectionKey(); }
+  PKU(const PKU&) = delete;
+  PKU& operator=(const PKU&) = delete;
+  int pku_key_;
+
+  using pkey_alloc_t = int (*)(unsigned, unsigned);
+  using pkey_free_t = int (*)(int);
+  using pkey_mprotect_t = int (*)(void*, size_t, int, int);
+  using pkey_get_t = int (*)(int);
+  using pkey_set_t = int (*)(int, unsigned);
+
+  pkey_alloc_t pkey_alloc = nullptr;
+  pkey_free_t pkey_free = nullptr;
+  pkey_mprotect_t pkey_mprotect = nullptr;
+  pkey_get_t pkey_get = nullptr;
+  pkey_set_t pkey_set = nullptr;
+
+  bool is_initialized_ = false;
+
+  bool is_support_pku_ = false;
+
+  void InitializeMemoryProtectionKeySupport();
+
+  int AllocateMemoryProtectionKey();
+
+  void FreeMemoryProtectionKey();
+
+ public:
+  static PKU& getInstance();
+
+  static bool SetPermissionsAndMemoryProtectionKey(
+      void* address, size_t size, OS::MemoryPermission permissions);
+
+  static void SetWriteableForMemoryProtectionKey(bool writeable);
+
+  static int GetPermissionsProtectionKey();
+
+  static OS::MemoryProtectionKeyPermission GetMemoryProtectionKeyPermission();
+
+  static bool HasMemoryProtectionKeySupport();
+};
+#endif  // V8_HAS_PKU_JIT_WRITE_PROTECT
+
 }  // namespace base
 }  // namespace v8
 
