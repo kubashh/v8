c30540b746822274e0e34994d42778f9c6fd4ecd
Danylo Boiko <danielboyko02@gmail.com>
2022-07-10 20:16:25 +0300

    [turbolizer] Turboshaft movable nodes layout
    Bug: v8:7327
    Change-Id: I9e141eb882ab0e22bd079b067e2229f5baa69433

--- tools/turbolizer/src/turboshaft-graph-layout.ts
+++ tools/turbolizer/src/turboshaft-graph-layout.ts
@@ -4,12 +4,12 @@
 
 import * as C from "./common/constants";
 import { TurboshaftGraph } from "./turboshaft-graph";
-import { GraphStateType } from "./phases/phase";
+import { GraphStateType, LayoutType } from "./phases/phase";
+import { LayoutOccupation } from "./layout-occupation";
 import {
   TurboshaftGraphBlock,
   TurboshaftGraphBlockType
 } from "./phases/turboshaft-graph-phase/turboshaft-graph-block";
-import { LayoutOccupation } from "./layout-occupation";
 
 export class TurboshaftGraphLayout {
   graph: TurboshaftGraph;
@@ -67,6 +67,7 @@ export class TurboshaftGraphLayout {
     // basis for bottom-down DFS to determine rank and block placement.
     const blocksHasNoInputs = new Array<boolean>();
     for (const block of this.graph.blocks()) {
+      block.collapsed = false;
       blocksHasNoInputs[block.id] = true;
     }
     for (const edge of this.graph.blocksEdges()) {
@@ -166,7 +167,7 @@ export class TurboshaftGraphLayout {
     const rankMaxBlockHeight = new Array<number>();
     for (const block of this.graph.blocks()) {
       rankMaxBlockHeight[block.rank] = Math.max(rankMaxBlockHeight[block.rank] ?? 0,
-        block.getHeight(showProperties));
+        block.getHeight(showProperties, this.graph.graphPhase.layoutType));
     }
 
     const rankSets = new Array<Array<TurboshaftGraphBlock>>();
@@ -201,6 +202,13 @@ export class TurboshaftGraphLayout {
       for (const block of rankSet) {
         if (block.visible) {
           block.x = this.layoutOccupation.occupy(block);
+          if (this.graph.graphPhase.layoutType == LayoutType.Movable) {
+            for (let i = 0; i < block.nodes.length; i++) {
+              block.nodes[i].x = C.TURBOSHAFT_NODE_X_INDENT;
+              block.nodes[i].y = i * (block.nodes[i].labelBox.height + C.TURBOSHAFT_NODE_SEPARATION)
+              + block.labelBox.height;
+            }
+          }
           const blockWidth = block.getWidth();
           this.trace(`Block ${block.id} is placed between [${block.x}, ${block.x + blockWidth})`);
           const staggeredFlooredI = Math.floor(placedCount++ % 3);
