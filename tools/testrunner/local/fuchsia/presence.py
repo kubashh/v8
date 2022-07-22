d6247f2a2aa786ca34222e50398f1dce880dcd43
Liviu Rau <liviurau@google.com>
2022-07-22 10:22:12 +0200

    [testrunner] Add support for running unittests in Fuchsia
    Change-Id: I80d9d7f0f98dc3d28425a959ba33067187655e80

---
+++ tools/testrunner/local/fuchsia/presence.py
@@ -0,0 +1,49 @@
+# Copyright 2022 the V8 project authors. All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of Google Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Contains context resolution function for Fuchsia. 
+
+By having this additional indirection we avoid import errors when Fuchsia
+conditional errors do not fetch Fuchsia dependencies (as is the case when
+we do not need to build Fuchsia).
+"""
+
+import os
+
+BASE_DIR = os.path.normpath(
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), '..', '..', '..', '..'))
+
+FUCHSIA_DIR = os.path.join(BASE_DIR, 'build', 'fuchsia')
+
+
+def getFuchsiaContextClass():
+  if not os.path.exists(FUCHSIA_DIR):
+    return None
+  from .context import FuchsiaOSContext
+  return FuchsiaOSContext
