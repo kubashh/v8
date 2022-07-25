1a09b898d0312520c9fdd7855594ce53e11a484a
Danylo Boiko <danielboyko02@gmail.com>
2022-07-26 02:02:52 +0300

    [turbolizer] TurboFan nodes history improvements
    Bug: v8:7327
    Change-Id: I7ecfdbef2c1bf9534c76f8ac253e846beeea8cb3

--- tools/turbolizer/src/views/history-view.ts
+++ tools/turbolizer/src/views/history-view.ts
@@ -125,7 +125,9 @@ export class HistoryView extends View {
     let recordY = 0;
 
     for (const [phaseId, phaseHistory] of this.phaseIdToHistory.entries()) {
+      if (!phaseHistory.hasChanges()) continue;
       const phaseName = this.sourceResolver.getPhaseNameById(phaseId);
+
       this.historyList
         .append("text")
         .classed("history-item", true)
@@ -267,6 +269,7 @@ export class HistoryView extends View {
     const uniqueAncestors = new Set<string>();
     const coefficient = this.getCoefficient("history-item-tspan-font-size");
     let prevNode = null;
+    let first = true;
     for (let i = 0; i < this.sourceResolver.phases.length; i++) {
       const phase = this.sourceResolver.getPhase(i);
       if (!(phase instanceof GraphPhase)) continue;
@@ -303,6 +306,11 @@ export class HistoryView extends View {
 
       if (node.nodeLabel.inplaceUpdatePhase && node.nodeLabel.inplaceUpdatePhase == phase.name) {
         this.addToHistory(i, node, HistoryChange.InplaceUpdated);
+      }
+
+      if (first) {
+        this.addToHistory(i, node, HistoryChange.Emersioned);
+        first = false;
       }
 
       this.addToHistory(i, node, HistoryChange.Current);
@@ -395,18 +403,19 @@ export class HistoryView extends View {
   }
 
   private getHeight(): number {
-    const clientRect = document.body.getBoundingClientRect();
-    return clientRect.height * C.HISTORY_DEFAULT_HEIGHT_PERCENT;
+    return window.screen.availHeight * C.HISTORY_DEFAULT_HEIGHT_PERCENT;
   }
 
   private getHistoryChangeColor(historyChange: HistoryChange): string {
     switch (historyChange) {
       case HistoryChange.Current:
         return "rgb(255, 167, 0)";
+      case HistoryChange.Emersioned:
+        return "rgb(129,8,248)";
       case HistoryChange.Lowered:
         return "rgb(0, 255, 0)";
       case HistoryChange.InplaceUpdated:
-        return "rgb(0, 0, 255)";
+        return "rgb(25,25,203)";
       case HistoryChange.Removed:
         return "rgb(255, 0, 0)";
       case HistoryChange.Survived:
@@ -444,6 +453,13 @@ export class PhaseHistory {
     }
     this.nodeIdToRecord.get(key).addChange(change);
   }
+
+  public hasChanges(): boolean {
+    for (const record of this.nodeIdToRecord.values()) {
+      if (record.hasChanges()) return true;
+    }
+    return false;
+  }
 }
 
 export class HistoryRecord {
@@ -458,10 +474,15 @@ export class HistoryRecord {
   public addChange(change: HistoryChange): void {
     this.changes.add(change);
   }
+
+  public hasChanges(): boolean {
+    return this.changes.size > 1 || (this.changes.size == 1 && !this.changes.has(HistoryChange.Current));
+  }
 }
 
 export enum HistoryChange {
   Current,
+  Emersioned,
   Lowered,
   InplaceUpdated,
   Removed,
