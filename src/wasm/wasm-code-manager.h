1d24df0394d8638f3018ac6c9af5f566bc67dd36
Andreas Haas <ahaas@chromium.org>
2022-08-05 13:17:12 +0200

    [wasm] Add metrics for lazy compilation
    This CL adds three metrics for lazy compilation: the number of functions
    compiled lazily, the total time spent on compiling functions lazily,
    and the maximum time spent on compiling a single function. All three
    metrics get recorded twice, once 5 seconds after instantiation, and once
    20 seconds after instantiation.
    
    R=clemensb@chromium.org
    
    Bug: v8:12852
    Change-Id: Ib9e5e12921fb1ec7aefd53af604cbb389bee79b9

--- src/wasm/wasm-code-manager.h
+++ src/wasm/wasm-code-manager.h
@@ -781,6 +781,22 @@ class V8_EXPORT_PRIVATE NativeModule final {
   }
   size_t tier_up_cpu_duration() const { return tier_up_cpu_duration_.load(); }
 
+  void AddLazyCompilationTimeSample(int64_t sample);
+
+  int num_lazy_compilations() const { return num_lazy_compilations_.load(); }
+
+  int64_t sum_lazy_compilation_time() const {
+    return sum_lazy_compilation_time_.load();
+  }
+
+  int64_t max_lazy_compilation_time() const {
+    return max_lazy_compilation_time_.load();
+  }
+
+  bool ShouldLazyCompilationMetricsBeReported() {
+    return should_metrics_be_reported_.exchange(false);
+  }
+
   bool HasWireBytes() const {
     auto wire_bytes = std::atomic_load(&wire_bytes_);
     return wire_bytes && !wire_bytes->empty();
@@ -1016,6 +1032,12 @@ class V8_EXPORT_PRIVATE NativeModule final {
   std::atomic<size_t> turbofan_code_size_{0};
   std::atomic<size_t> baseline_compilation_cpu_duration_{0};
   std::atomic<size_t> tier_up_cpu_duration_{0};
+
+  // Metrics for lazy compilation.
+  std::atomic<int> num_lazy_compilations_{0};
+  std::atomic<int64_t> sum_lazy_compilation_time_{0};
+  std::atomic<int64_t> max_lazy_compilation_time_{0};
+  std::atomic<bool> should_metrics_be_reported_{true};
 };
 
 class V8_EXPORT_PRIVATE WasmCodeManager final {
