6eeb40d869876f6ea10e7b8cc3c3b4e815b52f19
Marja Hölttä <marja@chromium.org>
2022-08-08 14:15:58 +0200

    [interpreter] Omit calling default ctors
    If we see a default ctor, walk up the constructors until we find a non-
    default one.
    
    Default ctors can only be skipped if there are no class fields / private
    brands.
    
    Bug:v8:13091
    Change-Id: Ie8ca8aedb01bd4b13adf1063332a5cdf41ab358a

--- src/interpreter/bytecodes.h
+++ src/interpreter/bytecodes.h
@@ -228,6 +228,8 @@ namespace interpreter {
   /* GetSuperConstructor operator */                                           \
   V(GetSuperConstructor, ImplicitRegisterUse::kReadAccumulator,                \
     OperandType::kRegOut)                                                      \
+  V(FindNonDefaultConstructor, ImplicitRegisterUse::kWriteAccumulator,         \
+    OperandType::kReg, OperandType::kReg, OperandType::kRegOut)                \
                                                                                \
   /* Call operations */                                                        \
   V(CallAnyReceiver, ImplicitRegisterUse::kWriteAccumulator,                   \
