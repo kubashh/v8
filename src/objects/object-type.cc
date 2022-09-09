7e6490cf366aa8037742ac6493219d259519cb90
Hao Xu <hao.a.xu@intel.com>
2022-09-09 19:28:24 +0800

    [csa][codegen] Optimize IsStrong/IsWeakOrCleared
    The way to determine whether a MaybeObject is a strong or weak
    reference to the heap object is to check its lowest two bits.
    However, if the MaybeObject is known to not be a smi, that is, the
    lowest bit is known to be 1, we can check one bit instead. This
    allows Turbofan to select better instructions:
    
    x64:
    
      Before:
        movl r9,r11
        andl r9,0x3
        cmpb r9l,0x1
    
      After:
        testb r11,0x2
    
    arm64:
    
      Before:
        and w8, w7, #0x3
        cmp w8, #0x1 (1)
        b.ne #+0x320
    
      After:
        tbnz w7, #1, #+0x320
    
    Change-Id: I03623183406ad7d920c96a752651e0116a22832e

--- src/objects/object-type.cc
+++ src/objects/object-type.cc
@@ -14,11 +14,49 @@ namespace internal {
 Address CheckObjectType(Address raw_value, Address raw_type,
                         Address raw_location) {
 #ifdef DEBUG
-  Object value(raw_value);
-  Smi type(raw_type);
+  ObjectType type = static_cast<ObjectType>(Smi(raw_type).value());
   String location = String::cast(Object(raw_location));
   const char* expected;
-  switch (static_cast<ObjectType>(type.value())) {
+
+  if (HAS_WEAK_HEAP_OBJECT_TAG(raw_value)) {
+    if (type == ObjectType::kHeapObjectReference) return Smi::FromInt(0).ptr();
+    // Casts of weak references are not allowed, one should use
+    // GetHeapObjectIfStrong / GetHeapObjectAssumeWeak first.
+    MaybeObject value(raw_value);
+    switch (type) {
+#define TYPE_CASE(Name)     \
+  case ObjectType::k##Name: \
+    expected = #Name;       \
+    break;
+#define TYPE_STRUCT_CASE(NAME, Name, name) \
+  case ObjectType::k##Name:                \
+    expected = #Name;                      \
+    break;
+
+      TYPE_CASE(Object)
+      TYPE_CASE(Smi)
+      TYPE_CASE(TaggedIndex)
+      TYPE_CASE(HeapObject)
+      TYPE_CASE(HeapObjectReference)
+      OBJECT_TYPE_LIST(TYPE_CASE)
+      HEAP_OBJECT_TYPE_LIST(TYPE_CASE)
+      STRUCT_LIST(TYPE_STRUCT_CASE)
+#undef TYPE_CASE
+#undef TYPE_STRUCT_CASE
+    }
+    std::stringstream value_description;
+    value.Print(value_description);
+    FATAL(
+        "Type cast failed in %s\n"
+        "  Expected %s but found %s",
+        location.ToAsciiArray(), expected, value_description.str().c_str());
+  } else {
+    Object value(raw_value);
+    switch (type) {
+      case ObjectType::kHeapObjectReference:
+        if (!value.IsSmi()) return Smi::FromInt(0).ptr();
+        expected = "HeapObjectReference";
+        break;
 #define TYPE_CASE(Name)                                 \
   case ObjectType::k##Name:                             \
     if (value.Is##Name()) return Smi::FromInt(0).ptr(); \
@@ -39,13 +77,14 @@ Address CheckObjectType(Address raw_value, Address raw_type,
     STRUCT_LIST(TYPE_STRUCT_CASE)
 #undef TYPE_CASE
 #undef TYPE_STRUCT_CASE
+    }
+    std::stringstream value_description;
+    value.Print(value_description);
+    FATAL(
+        "Type cast failed in %s\n"
+        "  Expected %s but found %s",
+        location.ToAsciiArray(), expected, value_description.str().c_str());
   }
-  std::stringstream value_description;
-  value.Print(value_description);
-  FATAL(
-      "Type cast failed in %s\n"
-      "  Expected %s but found %s",
-      location.ToAsciiArray(), expected, value_description.str().c_str());
 #else
   UNREACHABLE();
 #endif
