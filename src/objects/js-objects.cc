13983399874e2f2848fdc81c90f46a5e6b87ffc4
Matthias Liedtke <mliedtke@chromium.org>
2022-09-15 18:45:13 +0200

    [wasm-gc] Basic JS interop handling for wasm objects
    This change tests all JavaScript language constructs and builtins in
    combination with the unwrapped Wasm objects.
    For JavaScript, excluding some basic introspection (e.g.
    `Object.isExtensible`) WebAssembly GC objects are treated opaque.
    They can be passed around freely but don't allow any access to
    properties, elements etc.
    
    This behavior is currently exposed only if the `wasm-gc-js-interop`
    flag is set.
    
    Bug: v8:7748
    Change-Id: If0dc368f99d4097e3eaf53edde4e244e3081e334

--- src/objects/js-objects.cc
+++ src/objects/js-objects.cc
@@ -149,10 +149,12 @@ Handle<Object> JSReceiver::GetDataProperty(LookupIterator* it,
                                            AllocationPolicy allocation_policy) {
   for (; it->IsFound(); it->Next()) {
     switch (it->state()) {
+      case LookupIterator::WASM_OBJECT:
+        it->NotFound();
+        return it->isolate()->factory()->undefined_value();
       case LookupIterator::INTERCEPTOR:
       case LookupIterator::NOT_FOUND:
       case LookupIterator::TRANSITION:
-      case LookupIterator::WASM_OBJECT:
         UNREACHABLE();
       case LookupIterator::ACCESS_CHECK:
         // Support calling this method without an active context, but refuse
@@ -2312,6 +2314,13 @@ Maybe<bool> JSReceiver::SetPrototype(Isolate* isolate,
                                      Handle<JSReceiver> object,
                                      Handle<Object> value, bool from_javascript,
                                      ShouldThrow should_throw) {
+#if V8_ENABLE_WEBASSEMBLY
+  if (object->IsWasmObject()) {
+    RETURN_FAILURE(isolate, should_throw,
+                   NewTypeError(MessageTemplate::kWasmObjectsAreOpaque));
+  }
+#endif
+
   if (object->IsJSProxy()) {
     return JSProxy::SetPrototype(isolate, Handle<JSProxy>::cast(object), value,
                                  from_javascript, should_throw);
@@ -5149,6 +5158,13 @@ Maybe<bool> JSObject::SetPrototype(Isolate* isolate, Handle<JSObject> object,
       }
     }
   }
+
+#if V8_ENABLE_WEBASSEMBLY
+  if (value->IsWasmObject()) {
+    RETURN_FAILURE(isolate, should_throw,
+                   NewTypeError(MessageTemplate::kWasmObjectsAreOpaque));
+  }
+#endif
 
   // Set the new prototype of the object.
 
