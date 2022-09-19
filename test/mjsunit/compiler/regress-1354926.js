be93a52ef8b6531a94751aaa9c94486e095fd081
Maya Lekova <mslekova@chromium.org>
2022-09-19 17:19:31 +0200

    [test] Guard a CHECK against --fuzzing
    Bug: v8:1342305, v8:1354926
    Change-Id: I5ddedea227669908710b83f808cfb7c4718f0699

---
+++ test/mjsunit/compiler/regress-1354926.js
@@ -0,0 +1,12 @@
+// Copyright 2022 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// These tests exercise WebIDL annotations support in the fast API.
+
+// Flags: --turbo-fast-api-calls --expose-fast-api --turbofan
+// Flags: --fuzzing
+
+const fast_c_api = new d8.test.FastCAPI();
+
+fast_c_api.enforce_range_compare_i32(true, {}, -undefined );
