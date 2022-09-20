6c0dfb59879fcc8c22093616980df4bfc1e0369f
Darius M <dmercadier@chromium.org>
2022-09-20 09:43:55 +0200

    [compiler] Implement fast string builder in TF
    Change-Id: Ifcc1b32c7fdbb237d19b5f6ab451a999d34efb3f

--- src/compiler/access-builder.cc
+++ src/compiler/access-builder.cc
@@ -702,6 +702,15 @@ FieldAccess AccessBuilder::ForNameRawHashField() {
 }
 
 // static
+FieldAccess AccessBuilder::ForFreeSpaceSize() {
+  FieldAccess access = {kTaggedBase,         FreeSpace::kSizeOffset,
+                        MaybeHandle<Name>(), MaybeHandle<Map>(),
+                        Type::SignedSmall(), MachineType::TaggedSigned(),
+                        kNoWriteBarrier};
+  return access;
+}
+
+// static
 FieldAccess AccessBuilder::ForStringLength() {
   FieldAccess access = {kTaggedBase,
                         String::kLengthOffset,
