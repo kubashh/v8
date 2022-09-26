125913f219e61eccb6a6c21dca76e8d0827dbd62
Adam Klein <adamk@chromium.org>
2022-09-26 14:52:17 -0700

    [wasm] Make stack-switching (JSPI) flag imply type reflection flag
    Since JSPI doesn't work without type reflection, this ensures that
    passing --experimental-wasm-stack-switching alone doesn't leave
    developers (or users) in a broken state.
    
    Bug: v8:12191
    Change-Id: Idfabc39b7c9352dd20009924fda07504c4e5087b

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -1093,6 +1093,9 @@ FOREACH_WASM_FEATURE_FLAG(DECL_WASM_FLAG)
 
 DEFINE_IMPLICATION(experimental_wasm_gc, experimental_wasm_typed_funcref)
 
+DEFINE_IMPLICATION(experimental_wasm_stack_switching,
+                   experimental_wasm_type_reflection)
+
 DEFINE_BOOL(wasm_gc_js_interop, false, "experimental WasmGC-JS interop")
 
 DEFINE_BOOL(wasm_staging, false, "enable staged wasm features")
