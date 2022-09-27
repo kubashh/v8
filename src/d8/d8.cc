be3702b2003115e5546b50118835bf0f60e91797
Maya Lekova <mslekova@chromium.org>
2022-09-27 15:44:30 +0200

    [test] Make a test callback crash
    ... rather than throw an exception, as such is already
    thrown at an earlier point of the call chain.
    
    Change-Id: Iad28438c3b6b0d0fdc178d95701908338500eaa9

--- src/d8/d8.cc
+++ src/d8/d8.cc
@@ -3048,12 +3048,8 @@ void Shell::NodeTypeCallback(const v8::FunctionCallbackInfo<v8::Value>& args) {
 
   // HasInstance does a slow prototype chain lookup, and this function is used
   // for micro benchmarks too.
-#ifdef DEBUG
   PerIsolateData* data = PerIsolateData::Get(isolate);
-  if (!data->GetDomNodeCtor()->HasInstance(args.This())) {
-    isolate->ThrowError("Calling .nodeType on wrong instance type.");
-  }
-#endif
+  CHECK(data->GetDomNodeCtor()->HasInstance(args.This()));
 
   args.GetReturnValue().Set(v8::Number::New(isolate, 1));
 }
