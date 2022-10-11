deb45401abbc14e44faac39c7a2ce464efd8aec1
Anton Bikineev <bikineev@chromium.org>
2022-10-11 15:47:32 +0200

    cppgc: Add gdb printers for compressed pointers
    This CL adds the following gdb commands:
     cpcp <compressed-pointer>  # prints compressed pointer
     cpm <member>  # prints member value
    
    Bug: chromium:1373391
    Change-Id: If5cc121883afbe64f4b95965a48b52647179b9f8

--- src/heap/cppgc/member-storage.cc
+++ src/heap/cppgc/member-storage.cc
@@ -4,12 +4,34 @@
 
 #include "include/cppgc/internal/member-storage.h"
 
+#include "include/cppgc/garbage-collected.h"
+#include "include/cppgc/member.h"
+#include "src/base/compiler-specific.h"
+#include "src/base/macros.h"
+
 namespace cppgc {
 namespace internal {
 
 #if defined(CPPGC_POINTER_COMPRESSION)
 uintptr_t CageBaseGlobal::g_base_ = CageBaseGlobal::kLowerHalfWordMask;
 #endif  // defined(CPPGC_POINTER_COMPRESSION)
+
+// Debugging helpers.
+class V8_EXPORT_PRIVATE GCedDummy : public GarbageCollected<GCedDummy> {
+  void Trace(Visitor*) const {}
+};
+
+#if defined(CPPGC_POINTER_COMPRESSION)
+extern "C" V8_DONT_STRIP_SYMBOL V8_EXPORT_PRIVATE void*
+_cppgc_internal_Decompress_Compressed_Pointer(uint32_t cmprsd) {
+  return MemberStorage::Decompress(cmprsd);
+}
+#endif  // !defined(CPPGC_POINTER_COMPRESSION)
+
+extern "C" V8_DONT_STRIP_SYMBOL V8_EXPORT_PRIVATE void*
+_cppgc_internal_Print_Member(Member<GCedDummy>* m) {
+  return m->Get();
+}
 
 }  // namespace internal
 }  // namespace cppgc
