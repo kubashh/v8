c2580e7f4c25ef6a7a7bf24db3d07789139a360b
Marja Hölttä <marja@chromium.org>
2022-10-25 13:29:43 +0200

    [rab/gsab] Fix RAB/GSABness in %TypedArray%.of
    Bug:v8:11111,chromium:1377840
    Change-Id: I0a3d86b9f160c0daf28f45b9ec7a37f0a88be614

---
+++ test/mjsunit/regress/regress-crbug-1377840.js
@@ -0,0 +1,15 @@
+// Copyright 2022 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --harmony-rab-gsab
+
+const rab = new ArrayBuffer(3782, {maxByteLength: 4096});
+const u16a = new Int16Array(rab);
+rab.resize(0);
+
+function foo() {
+  return u16a;
+}
+
+assertThrows(() => { Float64Array.of.call(foo, 1); }, TypeError);
