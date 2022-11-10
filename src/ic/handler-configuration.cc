e060e24213320dab395bbf1f51a57a471cf980f1
Marja Hölttä <marja@chromium.org>
2022-11-10 15:40:38 +0100

    [ic] Hardening for the holder != lookup start object case
    Change-Id: Idd396b3d648a3933789eea506426c569848b61bf

--- src/ic/handler-configuration.cc
+++ src/ic/handler-configuration.cc
@@ -344,6 +344,14 @@ Handle<Object> StoreHandler::StoreProxy(Isolate* isolate,
                                MaybeObjectHandle::Weak(proxy));
 }
 
+bool LoadHandler::CanHandleHolderNotLookupStart(Object handler) {
+  if (handler.IsSmi()) {
+    auto kind = LoadHandler::KindBits::decode(handler.ToSmi().value());
+    return kind == LoadHandler::Kind::kSlow;
+  }
+  return handler.IsLoadHandler();
+}
+
 #if defined(OBJECT_PRINT)
 namespace {
 void PrintSmiLoadHandler(int raw_handler, std::ostream& os) {
