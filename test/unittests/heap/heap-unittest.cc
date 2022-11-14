b4f6e0556d0f9d4c3ee8b82d283ff14d6db74a96
Igor Sheludko <ishell@chromium.org>
2022-11-14 18:49:16 +0100

    WIP: don't require code range alignment
    change compression scheme for Code pointers
    
    
    introducing ExternalCodeCompressionScheme
    
    Bug: v8:7703, v8:11880
    Change-Id: Ia62bedd318f88c2147534ff000ab9fad354777f3

--- test/unittests/heap/heap-unittest.cc
+++ test/unittests/heap/heap-unittest.cc
@@ -151,7 +151,11 @@ TEST_F(HeapTest, HeapLayout) {
   EXPECT_TRUE(IsAligned(cage_base, size_t{4} * GB));
 
   Address code_cage_base = i_isolate()->code_cage_base();
-  EXPECT_TRUE(IsAligned(code_cage_base, size_t{4} * GB));
+  // Compression scheme for external code space doesn't require alignment of
+  // the cage base.
+  if (!V8_EXTERNAL_CODE_SPACE_BOOL) {
+    EXPECT_TRUE(IsAligned(code_cage_base, size_t{4} * GB));
+  }
 
 #ifdef V8_COMPRESS_POINTERS_IN_ISOLATE_CAGE
   Address isolate_root = i_isolate()->isolate_root();
