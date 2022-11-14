28dee8a5ed18e5c98a4c3ca8d54dfaa222cd5c5e
Igor Sheludko <ishell@chromium.org>
2022-11-14 17:04:45 +0100

    WIP: don't require code range alignment
    change compression scheme for Code pointers
    
    
    introducing ExternalCodeCompressionScheme
    
    Bug: v8:7703, v8:11880
    Change-Id: Ia62bedd318f88c2147534ff000ab9fad354777f3

--- src/heap/mark-compact.cc
+++ src/heap/mark-compact.cc
@@ -3690,6 +3690,14 @@ MaybeObject MakeSlotValue<FullMaybeObjectSlot, HeapObjectReferenceType::STRONG>(
   return HeapObjectReference::Strong(heap_object);
 }
 
+#ifdef V8_EXTERNAL_CODE_SPACE
+template <>
+Object MakeSlotValue<CodeObjectSlot, HeapObjectReferenceType::STRONG>(
+    HeapObject heap_object) {
+  return heap_object;
+}
+#endif  // V8_EXTERNAL_CODE_SPACE
+
 // The following specialization
 //   MakeSlotValue<FullMaybeObjectSlot, HeapObjectReferenceType::WEAK>()
 // is not used.
@@ -3704,9 +3712,10 @@ static inline void UpdateSlot(PtrComprCageBase cage_base, TSlot slot,
                     std::is_same<TSlot, ObjectSlot>::value ||
                     std::is_same<TSlot, FullMaybeObjectSlot>::value ||
                     std::is_same<TSlot, MaybeObjectSlot>::value ||
-                    std::is_same<TSlot, OffHeapObjectSlot>::value,
-                "Only [Full|OffHeap]ObjectSlot and [Full]MaybeObjectSlot are "
-                "expected here");
+                    std::is_same<TSlot, OffHeapObjectSlot>::value ||
+                    std::is_same<TSlot, CodeObjectSlot>::value,
+                "Only [Full|OffHeap]ObjectSlot, [Full]MaybeObjectSlot "
+                "or CodeObjectSlot are expected here");
   MapWord map_word = heap_obj.map_word(cage_base, kRelaxedLoad);
   if (map_word.IsForwardingAddress()) {
     DCHECK_IMPLIES((!v8_flags.minor_mc && !Heap::InFromPage(heap_obj)),
@@ -3725,6 +3734,11 @@ static inline void UpdateSlot(PtrComprCageBase cage_base, TSlot slot,
     }
     DCHECK(!Heap::InFromPage(target));
     DCHECK(!MarkCompactCollector::IsOnEvacuationCandidate(target));
+    if (V8_EXTERNAL_CODE_SPACE_BOOL &&
+        std::is_same<TSlot, CodeObjectSlot>::value) {
+      // base::OS::DebugBreak();
+      DCHECK(IsCodeSpaceObject(HeapObject::cast(Object(target.ptr()))));
+    }
   } else {
     DCHECK(MarkCompactCollector::IsMapOrForwarded(map_word.ToMap()));
   }
