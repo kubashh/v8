107c538c7a1c3d6c65a5f6d45b72d5a02c780149
Olivier Flückiger <olivf@chromium.org>
2022-12-19 17:24:29 +0000

    [static-roots] Ensure only initialized memory is dumped to r/o snapshot
    Eliminate 3 sources of uninitialized memory ending up in the memory
    snapshot. (1) padding of sequential strings (2) unused heap space (3)
    filler objects for alignment.
    
    Bug:v8:13466
    Change-Id: I139a5ebdda32b454096137e0f3bee5a6696194be

--- src/objects/string.cc
+++ src/objects/string.cc
@@ -1824,6 +1824,7 @@ Handle<String> SeqString::Truncate(Isolate* isolate, Handle<SeqString> string,
   // We are storing the new length using release store after creating a filler
   // for the left-over space to avoid races with the sweeper thread.
   string->set_length(new_length, kReleaseStore);
+  string->clear_padding();
 
   return string;
 }
@@ -1847,6 +1848,13 @@ SeqString::DataAndPaddingSizes SeqTwoByteString::GetDataAndPaddingSizes()
   int data_size = SeqString::kHeaderSize + length() * base::kUC16Size;
   int padding_size = SizeFor(length()) - data_size;
   return DataAndPaddingSizes{data_size, padding_size};
+}
+
+void SeqString::clear_padding() {
+  auto sz = GetDataAndPaddingSizes();
+  DCHECK_EQ(address() + sz.data_size + sz.padding_size, address() + Size());
+  if (sz.padding_size == 0) return;
+  memset(reinterpret_cast<void*>(address() + sz.data_size), 0, sz.padding_size);
 }
 
 uint16_t ConsString::Get(
