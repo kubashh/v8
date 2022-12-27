ec390cc626bdb3318290078f1df414071e33997b
Michael Achenbach <machenbach@chromium.org>
2022-12-27 15:58:52 +0100

    [js-fuzzer] Add optimization template for Maglev
    Bug: v8:7700
    Change-Id: I7727ea27fd956efab6fbee2b4a090213d1d7ff05

---
+++ tools/clusterfuzz/js_fuzzer/test_data/mutate_function_call_maglev_expected.js
@@ -0,0 +1,9 @@
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Original: mutate_function_call.js
+__f_0(1);
+
+a = __f_0(1);
+foo(1, __f_0());
