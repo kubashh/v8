0841a134ad28aa2f299c03d7563c9ca57e0e8e36
Leszek Swirski <leszeks@chromium.org>
2023-02-23 12:59:03 +0100

    [maglev] Preserve Float64Constant NaN bits
    Key and store Float64Constants with their bit representation, so that
    NaN bits are preserved. This in particular fixes issues with fast holey
    double array initialisations storing NaN instead of the hole.
    
    Bug: v8:7700
    Change-Id: I9ccabcce7785d493efd0ed5e1e7c14288fdbfbc5
    Fixed: v8:13763

--- src/maglev/arm64/maglev-assembler-arm64-inl.h
+++ src/maglev/arm64/maglev-assembler-arm64-inl.h
@@ -490,6 +490,9 @@ inline void MaglevAssembler::Move(Register dst, int32_t i) {
 inline void MaglevAssembler::Move(DoubleRegister dst, double n) {
   Fmov(dst, n);
 }
+inline void MaglevAssembler::Move(DoubleRegister dst, Float64 n) {
+  Fmov(dst, n.get_scalar());
+}
 inline void MaglevAssembler::Move(Register dst, Handle<HeapObject> obj) {
   Mov(dst, Operand(obj));
 }
