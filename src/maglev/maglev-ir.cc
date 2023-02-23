dd0ea12fea0e69c0a512c3c0454b7756cdaa2090
Victor Gomes <victorgomes@chromium.org>
2023-02-23 10:15:14 +0100

    [maglev][inline] Do not load undef for NewTarget if not needed
    Bug: v8:7700
    Change-Id: I555ef851b337d8d5b6bef64c3e0e05746cc01d8c

--- src/maglev/maglev-ir.cc
+++ src/maglev/maglev-ir.cc
@@ -2845,11 +2845,18 @@ void CallKnownJSFunction::GenerateCode(MaglevAssembler* masm,
   compiler::JSHeapBroker* broker = masm->compilation_info()->broker();
   __ Move(kContextRegister, function_.context(broker).object());
   __ Move(kJavaScriptCallTargetRegister, function_.object());
-  __ LoadRoot(kJavaScriptCallNewTargetRegister, RootIndex::kUndefinedValue);
   __ Move(kJavaScriptCallArgCountRegister, actual_parameter_count);
   if (shared_function_info(broker).HasBuiltinId()) {
+    __ LoadRoot(kJavaScriptCallNewTargetRegister, RootIndex::kUndefinedValue);
     __ CallBuiltin(shared_function_info(broker).builtin_id());
   } else {
+    compiler::SharedFunctionInfoRef shared = shared_function_info(broker);
+    if (!shared.HasBytecodeArray() ||
+        shared.GetBytecodeArray(broker)
+            .incoming_new_target_or_generator_register()
+            .is_valid()) {
+      __ LoadRoot(kJavaScriptCallNewTargetRegister, RootIndex::kUndefinedValue);
+    }
     __ AssertCallableFunction(kJavaScriptCallTargetRegister);
     __ LoadTaggedField(kJavaScriptCallCodeStartRegister,
                        FieldMemOperand(kJavaScriptCallTargetRegister,
