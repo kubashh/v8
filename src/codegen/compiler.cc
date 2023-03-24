c5d0a9604ca98d66b0ab44bbc2f715129f49b91a
Toon Verwaest <verwaest@chromium.org>
2023-03-24 14:33:54 +0100

    [tiering] Back off optimizing if we haven't seen enough invocations yet
    We might have marked the function for optimization due to a hot loop
    (which we might have OSRd into as well). Code before or after the loop
    won't have feedback yet, so it makes sense to back off compilation until
    we've seen enough calls. OSR'd code can handle the loop in the meantime.
    
    Change-Id: Ieb215d45cfb072d49c19f5c98d8fb70a26648a4b

--- src/codegen/compiler.cc
+++ src/codegen/compiler.cc
@@ -1275,6 +1275,13 @@ MaybeHandle<Code> GetOrCompileOptimized(
   // re-optimize.
   if (!IsOSR(osr_offset)) {
     ResetTieringState(*function, osr_offset);
+    int invocation_count =
+        function->feedback_vector().invocation_count(kRelaxedLoad);
+    if (invocation_count < v8_flags.minimum_invocations_before_optimization) {
+      function->feedback_vector().set_invocation_count(invocation_count + 1,
+                                                       kRelaxedStore);
+      return {};
+    }
   }
 
   // TODO(v8:7700): Distinguish between Maglev and Turbofan.
