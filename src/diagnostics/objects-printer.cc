cf404bd36112811357e745a1b0721e25e0938c59
JianxiaoLuIntel <jianxiao.lu@intel.com>
2023-06-06 15:31:04 +0800

    [x64] enable jump table switch for builtin
    Change-Id: I2f2ef10d739f9e4419e506ae0e4c164d0cf51309

--- src/diagnostics/objects-printer.cc
+++ src/diagnostics/objects-printer.cc
@@ -1930,6 +1930,8 @@ void Code::CodePrint(std::ostream& os, const char* name, Address current_pc) {
     os << "\n - constant_pool_offset: " << constant_pool_offset();
   }
   os << "\n - code_comments_offset: " << code_comments_offset();
+  os << "\n - builtin_jump_table_info_offset: "
+     << builtin_jump_table_info_offset();
 
   // Then, InstructionStream:
   if (has_instruction_stream()) {
