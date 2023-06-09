5dc22255ad61505c2c6ef931e4245570400ed0eb
Debadree Chatterjee <debadree333@gmail.com>
2023-06-09 16:51:34 +0530

    [compiler] inline String.prototype.endsWith
    we optimise the endsWith() function similar to how startsWith() is
    optimised using three conditions:
    
    - If search_element is definitely not a string, we make no change.
    - If search_element is definitely a string and its length is less
    or equal than max inline matching sequence threshold, we could
    inline the entire matching sequence.
    - Else we try to inline, and have a runtime deopt if search_element
    is not a string.
    
    Bug: v8:13940
    Change-Id: Id6c95b60707c8840e2634f2dbb78b59cd139c979

---
+++ test/mjsunit/compiler/string-endswith.js
@@ -0,0 +1,121 @@
+// Copyright 2019 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --allow-natives-syntax --turbofan
+
+(function() {
+  function foo(string) { return string.endsWith('a'); }
+
+  %PrepareFunctionForOptimization(foo);
+  assertEquals(false, foo(''));
+  assertEquals(true, foo('a'));
+  assertEquals(false, foo('ab'));
+  assertEquals(true, foo('cba'));
+  %OptimizeFunctionOnNextCall(foo);
+  assertEquals(false, foo(''));
+  assertEquals(true, foo('a'));
+  assertEquals(false, foo('ab'));
+  assertEquals(true, foo('cba'));
+  assertOptimized(foo);
+})();
+
+(function() {
+  function foo(string) { return string.endsWith('aaaaa'); }
+
+  %PrepareFunctionForOptimization(foo);
+  assertEquals(false, foo(''));
+  assertEquals(true, foo('aaabaaaaa'));
+  assertEquals(false, foo('ababababa'));
+  assertEquals(true, foo('cbaaaaaa'));
+  %OptimizeFunctionOnNextCall(foo);
+  assertEquals(false, foo(''));
+  assertEquals(true, foo('aaabaaaaa'));
+  assertEquals(false, foo('ababababa'));
+  assertEquals(true, foo('cbaaaaaa'));
+  assertOptimized(foo);
+})();
+
+(function() {
+  function f() { return "abc".endsWith(); }
+
+  %PrepareFunctionForOptimization(f);
+  assertEquals(false, f());
+  assertEquals(false, f());
+  %OptimizeFunctionOnNextCall(f);
+  assertEquals(false, f());
+  assertOptimized(f);
+})();
+
+(function() {
+  function g(n) { return "cba".endsWith("a", n); }
+  %PrepareFunctionForOptimization(g);
+  assertEquals(false, g(-1));
+  assertEquals(false, g(0));
+  assertEquals(false, g(1));
+  assertEquals(false, g(2));
+  assertEquals(true, g(3));
+  assertEquals(true, g(4));
+  %OptimizeFunctionOnNextCall(g);
+  assertEquals(false, g(-1));
+  assertEquals(false, g(0));
+  assertEquals(false, g(1));
+  assertEquals(false, g(2));
+  assertEquals(true, g(3));
+  assertEquals(true, g(4));
+  assertOptimized(g);
+})();
+
+(function() {
+  function g(n) { return "cbaaaaaaa".endsWith("aaaa", n); }
+  %PrepareFunctionForOptimization(g);
+  assertEquals(false, g(-1));
+  assertEquals(false, g(0));
+  assertEquals(false, g(1));
+  assertEquals(false, g(2));
+  assertEquals(false, g(3));
+  assertEquals(false, g(4));
+  assertEquals(false, g(5));
+  assertEquals(true, g(6));
+  assertEquals(true, g(7));
+  assertEquals(true, g(8));
+  assertEquals(true, g(9));
+  %OptimizeFunctionOnNextCall(g);
+  assertEquals(false, g(-1));
+  assertEquals(false, g(0));
+  assertEquals(false, g(1));
+  assertEquals(false, g(2));
+  assertEquals(false, g(3));
+  assertEquals(false, g(4));
+  assertEquals(false, g(5));
+  assertEquals(true, g(6));
+  assertEquals(true, g(7));
+  assertEquals(true, g(8));
+  assertEquals(true, g(9));
+  assertOptimized(g);
+})();
+
+(function() {
+  function f(n) { return "cba".endsWith("a", n); }
+  %PrepareFunctionForOptimization(f);
+  assertEquals(true, f());
+  assertEquals(true, f());
+  %OptimizeFunctionOnNextCall(f);
+  assertEquals(true, f(1073741824));
+  assertEquals(true, f(1073741824));
+  assertEquals(true, f(1073741824));
+  assertOptimized(f);
+})();
+
+(function() {
+  function f(str) {
+    return str.endsWith('');
+  }
+
+  %PrepareFunctionForOptimization(f);
+  f('foo');
+  f('');
+  %OptimizeFunctionOnNextCall(f);
+  assertEquals(f('foo'), true);
+  assertEquals(f(''), true);
+})();
