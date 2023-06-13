3ba38d4b370ab7acf0523cd893a21abeb166282f
JianxiaoLuIntel <jianxiao.lu@intel.com>
2023-06-13 13:05:45 +0800

    [x64] Enable jump table switch for builtin
    In V8 compiler, there are two methods to implement a 'switch' statement: Table Switch and Binary Search Switch. If the amount of cases is large and the values are contiguous, the Table Switch is usually better choice.
    
    Currently, the jump table will store absolute address of the jump target, it's position dependent, so it can not be used in builtins functions because their code positions may be changed.
    
    To solve this problem, when compile builtins, we replace the values stored in the table from `target_address` to `target_address - table_address` and it will become position independent, then we can enable Table Switch for builtins as well.
    
    Bug: v8:13710
    
    Change-Id: I2f2ef10d739f9e4419e506ae0e4c164d0cf51309

--- src/diagnostics/disassembler.cc
+++ src/diagnostics/disassembler.cc
@@ -5,11 +5,16 @@
 #include "src/diagnostics/disassembler.h"
 
 #include <algorithm>
+#include <cstdint>
 #include <iomanip>
 #include <memory>
 #include <sstream>
 #include <unordered_map>
 #include <vector>
+
+#ifdef V8_TARGET_ARCH_X64
+#include "src/codegen/x64/builtin-jump-table-info.h"
+#endif  // V8_TARGET_ARCH_X64
 
 #include "src/base/memory.h"
 #include "src/base/strings.h"
@@ -290,6 +295,15 @@ static int DecodeIt(Isolate* isolate, ExternalReferenceEncoder* ref_encoder,
                          disasm::Disassembler::kContinueOnUnimplementedOpcode);
   RelocIterator rit(code);
   CodeCommentsIterator cit(code.code_comments(), code.code_comments_size());
+
+#ifdef V8_TARGET_ARCH_X64
+  std::unique_ptr<BuiltinJumpTableInfoIterator> table_info_it = nullptr;
+  if (code.is_code() && code.as_code()->has_builtin_jump_table_info()) {
+    table_info_it = std::make_unique<BuiltinJumpTableInfoIterator>(
+        code.builtin_jump_table_info(), code.builtin_jump_table_info_size());
+  }
+#endif  // V8_TARGET_ARCH_X64
+
   int constants = -1;  // no constants being decoded at the start
 
   while (pc < end) {
@@ -321,6 +335,18 @@ static int DecodeIt(Isolate* isolate, ExternalReferenceEncoder* ref_encoder,
                  reinterpret_cast<intptr_t>(ptr),
                  static_cast<size_t>(ptr - begin));
         pc += sizeof(ptr);
+#ifdef V8_TARGET_ARCH_X64
+      } else if (table_info_it && table_info_it->HasCurrent() &&
+                 table_info_it->GetPCOffset() ==
+                     static_cast<uint32_t>(pc - begin)) {
+        int32_t target_pc_offset = table_info_it->GetTarget();
+        SNPrintF(decode_buffer, "jump table entry %08" V8PRIxPTR,
+                 static_cast<size_t>(target_pc_offset));
+        DCHECK_EQ(sizeof(target_pc_offset),
+                  sizeof(BuiltinJumpTableInfoEntry::target));
+        pc += sizeof(target_pc_offset);
+        table_info_it->Next();
+#endif  // V8_TARGET_ARCH_X64
       } else {
         decode_buffer[0] = '\0';
         pc += d.InstructionDecode(decode_buffer, pc);
