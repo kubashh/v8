2e88ca1ec4a50bf706beae4e264ad66a90de3a0b
Victor Gomes <victorgomes@chromium.org>
2023-06-27 17:20:13 +0200

    [maglev][arm] Implement Int32ModulusWithOverflow and Float64Modulus
    Drive-by: avoid a compare instruction in arm64 implementation
    
    Bug: v8:7700
    Change-Id: I758f1d5bfac47a6263736d16bf93577ae75e0b24

--- src/maglev/arm64/maglev-ir-arm64.cc
+++ src/maglev/arm64/maglev-ir-arm64.cc
@@ -475,8 +475,8 @@ void Int32ModulusWithOverflow::GenerateCode(MaglevAssembler* masm,
       [](MaglevAssembler* masm, ZoneLabelRef rhs_checked, Register rhs,
          Int32ModulusWithOverflow* node) {
         __ Negs(rhs, rhs);
-        __ EmitEagerDeoptIf(eq, deopt_reason, node);
-        __ Jump(*rhs_checked);
+        __ B(*rhs_checked, ne);
+        __ EmitEagerDeopt(node, deopt_reason);
       },
       rhs_checked, rhs, this);
   __ Bind(*rhs_checked);
@@ -491,12 +491,11 @@ void Int32ModulusWithOverflow::GenerateCode(MaglevAssembler* masm,
         __ Neg(lhs, lhs);
         __ Udiv(res, lhs, rhs);
         __ Msub(out, res, rhs, lhs);
-        __ Cmp(out, Immediate(0));
+        __ Negs(out, out);
+        __ B(*done, ne);
         // TODO(victorgomes): This ideally should be kMinusZero, but Maglev
         // only allows one deopt reason per IR.
-        __ EmitEagerDeoptIf(eq, deopt_reason, node);
-        __ Neg(out, out);
-        __ B(*done);
+        __ EmitEagerDeopt(node, deopt_reason);
       },
       done, lhs, rhs, out, this);
 
