089826f5d0463a4c5da72fb98c94775d7a75d754
Matthias Liedtke <mliedtke@chromium.org>
2023-06-29 18:33:55 +0200

    [js] JSON.parse: Show line and column on errors with position
    This adds a more helpful "file" location to JSON.parse error messages
    that already have a position. Some error messages do not show a
    position but instead show the full string input or the string input
    with ellipses like
    "Unexpected token '/', ..."8, 9, 10, /]" is not valid JSON".
    These errors are left unchanged.
    
    Given the example:
    > JSON.parse(`{"ab `);
    Previous error message:
    "Unterminated string in JSON at position 5"
    New error message:
    "Unterminated string in JSON at position 5 (line 1 column 6)"
    
    Spidermonkey for comparison:
    "JSON.parse: unterminated string at line 1 column 6 of the JSON data"
    
    It is a bit confusing with the position starting at 0 but I guess
    removing the position completely might be risky as there might be
    code extracting the position with a regex.
    
    Change-Id: Ibd1a27e8f78bec9f926e19870fcce5126273a57b

--- test/mjsunit/json-errors.js
+++ test/mjsunit/json-errors.js
@@ -12,19 +12,29 @@ function TryParse(s, message) {
 }
 
 var s = `{"a\\\\b `;
-TryParse(s, "Unterminated string in JSON at position 7");
+TryParse(s, "Unterminated string in JSON at position 7 (line 1 column 8)");
 
 var s = `{"a\\\\\u03A9 `;
-TryParse(s, "Unterminated string in JSON at position 7");
+TryParse(s, "Unterminated string in JSON at position 7 (line 1 column 8)");
 
 var s = `{"ab `;
-TryParse(s, "Unterminated string in JSON at position 5");
+TryParse(s, "Unterminated string in JSON at position 5 (line 1 column 6)");
 
 var s = `{"a\u03A9 `;
-TryParse(s, "Unterminated string in JSON at position 5");
+TryParse(s, "Unterminated string in JSON at position 5 (line 1 column 6)");
 
 var s = `{"a\nb":"b"}`;
-TryParse(s, "Bad control character in string literal in JSON at position 3");
+TryParse(s, "Bad control character in string literal in JSON " +
+            "at position 3 (line 1 column 4)");
 
 var s = `{"a\nb":"b\u03A9"}`;
-TryParse(s, "Bad control character in string literal in JSON at position 3");
+TryParse(s, "Bad control character in string literal in JSON " +
+            "at position 3 (line 1 column 4)");
+
+var s = `{\n7:1}`;
+TryParse(s, "Expected property name or '}' in JSON " +
+            "at position 2 (line 2 column 1)");
+
+var s = `\n  { \n\n"12345": 5,,}`;
+TryParse(s, "Expected double-quoted property name in JSON " +
+            "at position 18 (line 4 column 12)");
