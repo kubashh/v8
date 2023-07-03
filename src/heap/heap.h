4d8f2418b39f1785f7a1e8db17e910772583fe05
Omer Katz <omerkatz@chromium.org>
2023-07-03 17:52:02 +0200

    [heap] Remove MarkCompactCollectorBase
    Bug: v8:12612
    Change-Id: I700766445268b5273b420a785c79f76260351bb3

--- src/heap/heap.h
+++ src/heap/heap.h
@@ -2430,7 +2430,6 @@ class Heap final {
   template <typename ConcreteVisitor, typename MarkingState>
   friend class MarkingVisitorBase;
   friend class MarkCompactCollector;
-  friend class MarkCompactCollectorBase;
   friend class MinorGCJob;
   friend class MinorGCTaskObserver;
   friend class MinorMarkCompactCollector;
