5e41be93357a5670b2878b8d0f76321c7b1d75f3
Leszek Swirski <leszeks@chromium.org>
2023-07-04 10:14:39 +0200

    [maglev] Clear deopt literals map earlier
    Clear the deopt literals IdentityMap early, immediately after moving its
    data into the DeoptimizationLiteralArray, so that it is cleared while
    the local isolate is active. This ensures that we don't remove the
    identity map from the strong roots racily.
    
    Also, add some DCHECKs that IdentityMaps are cleared safely, which
    discovered another IdentityMap not being cleared correctly during
    finalization (namely, not cleared when finalization was skipped).
    
    Bug: v8:7700
    Change-Id: I33cde743d5abbd4bce6b74686b83a5e4a5c53d24
    Fixed: v8:14128

--- src/heap/heap.cc
+++ src/heap/heap.cc
@@ -6447,6 +6447,10 @@ size_t Heap::OldArrayBufferBytes() {
 StrongRootsEntry* Heap::RegisterStrongRoots(const char* label,
                                             FullObjectSlot start,
                                             FullObjectSlot end) {
+  // We're either on the main thread, or in a background thread with an active
+  // local heap.
+  DCHECK(isolate()->CurrentLocalHeap()->IsRunning());
+
   base::MutexGuard guard(&strong_roots_mutex_);
 
   StrongRootsEntry* entry = new StrongRootsEntry(label);
@@ -6471,6 +6475,10 @@ void Heap::UpdateStrongRoots(StrongRootsEntry* entry, FullObjectSlot start,
 }
 
 void Heap::UnregisterStrongRoots(StrongRootsEntry* entry) {
+  // We're either on the main thread, or in a background thread with an active
+  // local heap.
+  DCHECK(isolate()->CurrentLocalHeap()->IsRunning());
+
   base::MutexGuard guard(&strong_roots_mutex_);
 
   StrongRootsEntry* prev = entry->prev;
