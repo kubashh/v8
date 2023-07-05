d76ab2dd04ccb3e476ab1717c2d1a60cd940d42e
Stephen Roettger <sroettger@google.com>
2023-07-05 16:20:27 +0200

    WIP: emit endbr64 in JIT code
    DO NOT SUBMIT
    
    Bug: v8:13355
    Change-Id: Ifd350a566b2cb1aa9e5976b797dc658106df826c

--- src/compiler/backend/x64/code-generator-x64.cc
+++ src/compiler/backend/x64/code-generator-x64.cc
@@ -6704,11 +6704,11 @@ void CodeGenerator::AssembleArchTableSwitch(Instruction* instr) {
     // target = table + (target - table)
     __ addq(input, kScratchRegister);
     // Jump to the target.
-    __ jmp(input);
+    __ jmp(input, /*notrack=*/true);
   } else {
     // For non builtins, the value in the table is 'target_address' (8 bytes)
     // jmp [table + index*8]
-    __ jmp(Operand(kScratchRegister, input, times_8, 0));
+    __ jmp(Operand(kScratchRegister, input, times_8, 0), /*notrack=*/true);
   }
 }
 
