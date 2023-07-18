81a460a38e13e92234987a18e53a26add9637b7e
Daniel Lehmann <dlehmann@chromium.org>
2023-07-18 15:37:34 +0200

    [regalloc] Limit quadratic merging of SpillRanges
    Even after previous speed-ups to `AssignSpillSlots` (see v8:14133),
    the merging of all non-empty `SpillRange`s gives still fundamentally
    quadratic compile time in the number of `SpillRange`s.
    
    This CL limits that to linear above a certain threshold and
    probabilistically limits the downsides of that (see code comment).
    
    For v8:14133, this results in the exact same number of `SpillRange`s
    after merging (namely 989, i.e., the quality of the generated code
    should be the same for this example) and reduces compile time in the
    `V8.TFAssignSpillSlots` phase from ~1600ms to ~255ms (-84%).
    
    Bug: v8:14082, v8:14133
    Change-Id: I912f833c65e48ee8727c498fd40507956a68fed9

--- src/compiler/backend/register-allocator.cc
+++ src/compiler/backend/register-allocator.cc
@@ -5,6 +5,7 @@
 #include "src/compiler/backend/register-allocator.h"
 
 #include <iomanip>
+#include <random>
 
 #include "src/base/iterator.h"
 #include "src/base/small-vector.h"
@@ -1247,7 +1248,6 @@ TopTierRegisterAllocationData::TopTierRegisterAllocationData(
                                     this->config()->num_double_registers(),
                                 nullptr, allocation_zone()),
       fixed_simd128_live_ranges_(allocation_zone()),
-      spill_ranges_(code->VirtualRegisterCount(), nullptr, allocation_zone()),
       delayed_references_(allocation_zone()),
       assigned_registers_(nullptr),
       assigned_double_registers_(nullptr),
@@ -1418,7 +1418,6 @@ SpillRange* TopTierRegisterAllocationData::AssignSpillRangeToLiveRange(
     range->set_spill_type(SpillType::kSpillRange);
   }
 
-  spill_ranges()[range->vreg()] = spill_range;
   return spill_range;
 }
 
@@ -4625,30 +4624,73 @@ void OperandAssigner::DecideSpillingMode() {
 }
 
 void OperandAssigner::AssignSpillSlots() {
-  for (auto range : data()->live_ranges()) {
+  ZoneVector<SpillRange*> spill_ranges(data()->allocation_zone());
+  for (const TopLevelLiveRange* range : data()->live_ranges()) {
+    if (range != nullptr && range->HasSpillRange()) {
+      DCHECK_NOT_NULL(range->GetSpillRange());
+      spill_ranges.push_back(range->GetSpillRange());
+    }
+  }
+  // At this point, the `SpillRange`s for all `TopLevelLiveRange`s should be
+  // unique, since none have been merged yet.
+  DCHECK_EQ(spill_ranges.size(),
+            std::set(spill_ranges.begin(), spill_ranges.end()).size());
+
+  // Merge all `SpillRange`s that belong to the same `LiveRangeBundle`.
+  for (const TopLevelLiveRange* range : data()->live_ranges()) {
     data()->tick_counter()->TickAndMaybeEnterSafepoint();
     if (range != nullptr && range->get_bundle() != nullptr) {
       range->get_bundle()->MergeSpillRangesAndClear();
     }
   }
-  ZoneVector<SpillRange*>& spill_ranges = data()->spill_ranges();
-  // Merge disjoint spill ranges
-  for (size_t i = 0; i < spill_ranges.size(); ++i) {
+  // This may have left `SpillRange`s empty, so remove those.
+  spill_ranges.erase(
+      std::remove_if(spill_ranges.begin(), spill_ranges.end(),
+                     [](const SpillRange* range) { return range->IsEmpty(); }),
+      spill_ranges.end());
+
+  // Now merge *all* disjoint `SpillRange`s.
+  // In principle this merging is O(n^2) in the number of `SpillRange`s.
+  // However it then dominates compile time (>40%) for some pathological cases,
+  // e.g., https://crbug.com/v8/14133.
+  // The following is a probabilistic solution to the problem.
+  // We do the full quadratic merging below a certain number of `SpillRange`s.
+  // If there are more `SpillRange`s, the inner loop below tries at most
+  // `retries` times to merge the next `SpillRange` into the current one.
+  // This limits merging to O(n), at the cost of giving up if too many merges
+  // were unsuccessful.
+  // However, we can increase the probability of merges being successful
+  // by (pseudo-)randomly shuffling the `SpillRange`s, such that it is more
+  // likely that two adjacent `SpillRange`s are not intersecting.
+  constexpr size_t kMaxQuadraticMergeSize = 10000;
+  if (spill_ranges.size() > kMaxQuadraticMergeSize) {
+    std::shuffle(spill_ranges.begin(), spill_ranges.end(), std::minstd_rand());
+  }
+  SpillRange** end_nonempty = spill_ranges.end();
+  for (SpillRange** range_it = spill_ranges.begin(); range_it < end_nonempty;
+       ++range_it) {
     data()->tick_counter()->TickAndMaybeEnterSafepoint();
-    SpillRange* range = spill_ranges[i];
-    if (range == nullptr) continue;
-    if (range->IsEmpty()) continue;
-    for (size_t j = i + 1; j < spill_ranges.size(); ++j) {
-      SpillRange* other = spill_ranges[j];
-      if (other != nullptr && !other->IsEmpty()) {
-        range->TryMerge(other);
+    SpillRange* range = *range_it;
+    DCHECK(!range->IsEmpty());
+    size_t retries = kMaxQuadraticMergeSize;
+    for (SpillRange** other_it = range_it + 1; other_it < end_nonempty;
+         ++other_it) {
+      SpillRange* other = *other_it;
+      DCHECK(!other->IsEmpty());
+      if (range->TryMerge(other)) {
+        DCHECK(other->IsEmpty());
+        std::iter_swap(other_it, --end_nonempty);
+      } else if (--retries == 0) {
+        break;
       }
     }
   }
+  spill_ranges.erase(end_nonempty, spill_ranges.end());
+
   // Allocate slots for the merged spill ranges.
   for (SpillRange* range : spill_ranges) {
     data()->tick_counter()->TickAndMaybeEnterSafepoint();
-    if (range == nullptr || range->IsEmpty()) continue;
+    DCHECK(!range->IsEmpty());
     if (!range->HasSlot()) {
       // Allocate a new operand referring to the spill slot, aligned to the
       // operand size.
