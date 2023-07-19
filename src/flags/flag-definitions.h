dd50cd8a8b0024c0d321e1da00bc27e85401db00
Omer Katz <omerkatz@chromium.org>
2023-07-19 09:34:40 +0200

    [DO NOT REVIEW] Enable concurrent MinorMS by default
    Bug: v8:13012
    Change-Id: I9e11652c51d2d04004d460578f70b9bf89fde8c4

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -2355,7 +2355,11 @@ DEFINE_BOOL(minor_ms, false, "perform young generation mark sweep GCs")
 DEFINE_IMPLICATION(minor_ms, separate_gc_phases)
 DEFINE_IMPLICATION(minor_ms, page_promotion)
 
+<<<<<<< PATCH SET (b701e1 [DO NOT REVIEW] Enable concurrent MinorMS by default)
+DEFINE_BOOL(concurrent_minor_ms_marking, true,
+=======
 DEFINE_BOOL(concurrent_minor_ms_marking, false,
+>>>>>>> BASE      (f86dac BodyDescriptors: Remove IsValidSlot)
             "perform young generation marking concurrently")
 DEFINE_NEG_NEG_IMPLICATION(concurrent_marking, concurrent_minor_ms_marking)
 DEFINE_IMPLICATION(concurrent_minor_ms_marking, minor_ms)
