3332acc3370d63b7f5401d5ccbd89d49bb8a27ed
Omer Katz <omerkatz@chromium.org>
2023-07-31 14:01:42 +0200

    [heap] Merge MinorMS marking loops
    Bug: v8:13012
    Change-Id: Ibd6d1803d6adf06adc945ac473d6881fd20b08e5

--- src/heap/minor-mark-sweep.h
+++ src/heap/minor-mark-sweep.h
@@ -184,7 +184,6 @@ class MinorMarkSweepCollector final {
   void MarkLiveObjects();
   void MarkRoots(YoungGenerationRootMarkingVisitor& root_visitor,
                  bool was_marked_incrementally);
-  void DoParallelMarking();
   void DrainMarkingWorklist();
   void MarkRootsFromTracedHandles(
       YoungGenerationRootMarkingVisitor& root_visitor);
