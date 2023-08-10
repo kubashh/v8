5a36cc54dc04dc6c93436f1cf9d8883d5d551160
Darius M <dmercadier@chromium.org>
2023-08-10 09:49:03 +0200

    [turboshaft] Fix LoadElimination bug with Typed Arrays
    Turboshaft's LateLoadElimination assumed that Loads and Store at
    different offsets couldn't overlap. However, this doesn't hold for
    TypedArrays:
    
        function f(u32s, u8s) {
          u32s[0] = 0xffffffff;
          u8s[1] = 0;
          return u32s[0];
        }
    
    In this example, the 2 stores could overlap despite having different
    offsets.
    
    In practice, it was already safe before this CL, because Loads/Stores
    from/to TypedArrays always require some computation of the base, which
    prevents Load Elimination (GVN would have to run in parallel of Load
    Elimination for this to work). Still, relying on the fact that GVN
    never runs during Load Elimination sounds like bugs waiting to happen.
    
    This CL fixes this issue by invalidating more offsets when a Store in
    a TypedArray is performed. TypedArray stores/loads can only overlap
    with other TypedArray stores/loads, which will all have a
    tagged_base=false (unlike regular stores/loads). So, we keep 2
    separate MemoryContentTable: one for tagged_base and one for
    untagged_base. When invalidating at offset `x` in the untagged_base
    one, we actually invalidate from offset `x-7` up to `x+7`.
    
    Bug: v8:12783
    Change-Id: I8c2959e770e18c8d176c47a7a90505eb60df8ae5

---
+++ test/mjsunit/compiler/load-elimination-typed-arrays.js
@@ -0,0 +1,22 @@
+// Copyright 2023 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --allow-natives-syntax --turboshaft-load-elimination
+
+function f(u32s, u8s) {
+  u32s[0] = 0xffffffff;
+  // The following store will overlap with the 1st store, and should thus
+  // invalidate the Load Elimination state associated with the 1st store.
+  u8s[1] = 0;
+  // The following load should not be eliminated.
+  return u32s[0];
+}
+
+const u32s = Uint32Array.of(3, 8);
+const u8s = new Uint8Array(u32s.buffer);
+
+%PrepareFunctionForOptimization(f);
+assertEquals(f(u32s, u8s), 0xffff00ff);
+%OptimizeFunctionOnNextCall(f);
+assertEquals(f(u32s, u8s), 0xffff00ff);
