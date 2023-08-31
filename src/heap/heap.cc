79513da4f2c9653da55a1707d2adbc0ba38f71ff
Dominik Inführ <dinfuehr@chromium.org>
2023-08-31 17:11:35 +0200

    try to get rid of CanExpandOldGeneration
    Bug: v8:13842
    Change-Id: Ia0ff998836ab53ece9e384ebc20263858facdbff

--- src/heap/heap.cc
+++ src/heap/heap.cc
@@ -485,11 +485,7 @@ size_t Heap::Available() {
 
 bool Heap::CanExpandOldGeneration(size_t size) const {
   if (force_oom_ || force_gc_on_next_allocation_) return false;
-  if (!IsOldGenerationExpansionAllowed(size)) return false;
-  // The OldGenerationCapacity does not account compaction spaces used
-  // during evacuation. Ensure that expanding the old generation does push
-  // the total allocated memory size over the maximum heap size.
-  return memory_allocator()->Size() + size <= MaxReserved();
+  return IsOldGenerationExpansionAllowed(size);
 }
 
 bool Heap::IsOldGenerationExpansionAllowed(size_t size) const {
@@ -501,17 +497,6 @@ bool IsIsolateDeserializationActive(LocalHeap* local_heap) {
   return local_heap && !local_heap->heap()->deserialization_complete();
 }
 }  // anonymous namespace
-
-bool Heap::CanExpandOldGenerationBackground(LocalHeap* local_heap,
-                                            size_t size) {
-  if (force_oom_) return false;
-
-  // When the heap is tearing down, then GC requests from background threads
-  // are not served and the threads are allowed to expand the heap to avoid OOM.
-  return gc_state() == TEAR_DOWN || IsMainThreadParked(local_heap) ||
-         IsIsolateDeserializationActive(local_heap) ||
-         memory_allocator()->Size() + size <= MaxReserved();
-}
 
 bool Heap::CanPromoteYoungAndExpandOldGeneration(size_t size) const {
   size_t new_space_capacity = NewSpaceTargetCapacity();
