e44d2419ced7b7686d55fa1591843b4aea1a524a
José Dapena Paz <jdapena@igalia.com>
2023-09-05 10:04:15 +0200

    [d8] New command line switch to dump system memory usage
    Add new command line switch --dump-system-memory-stats for displaying
    memory usage statistics on finishing d8 execution. Right now it only
    shows the peak memory usage. In POSIX this is calculated using
    getrusage. In Windows, it uses GetProcessMemoryInfo peak working set
    size.
    
    Bug: v8:14298
    Change-Id: I30713034de5b7089545aca25421d8846de72ff0e

--- src/base/platform/platform-posix.cc
+++ src/base/platform/platform-posix.cc
@@ -849,6 +849,24 @@ int OS::GetUserTime(uint32_t* secs, uint32_t* usecs) {
 }
 #endif
 
+int OS::GetPeakMemoryUsageKb() {
+#if defined(V8_OS_FUCHSIA)
+  // Fuchsia does not implement getrusage()
+  return -1;
+#else
+  struct rusage usage;
+  if (getrusage(RUSAGE_SELF, &usage) < 0) return -1;
+
+#if defined(V8_OS_MACOS)
+  // MacOS ru_maxrss uses bytes
+  return static_cast<int>(usage.ru_maxrss / 1024);
+#else
+  // Most other cases (at least Linux, IOS, return kilobytes)
+  return static_cast<int>(usage.ru_maxrss);
+#endif  // defined(V8_OS_MACOS)
+#endif  // defined(V8_OS_FUCHSIA)
+}
+
 double OS::TimeCurrentMillis() {
   return Time::Now().ToJsTime();
 }
