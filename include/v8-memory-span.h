5b18bd18259987050751bb5c1dc41cbf184c5fe3
Nikolaos Papaspyrou <nikolaos@chromium.org>
2023-09-15 17:35:00 +0200

    [api] Deprecate vector<v8::Local>, part 1
    According to the V8's public API documentation, local handles (i.e.,
    objects of type v8::Local<T>) "should never be allocated on the heap".
    This disallows heap-allocated data structures containing instances of
    v8::Local, like std::vector<v8::Local<v8::String>>. It is unfortunate
    that the V8 API itself requires the usage of such data structures.
    
    This CL starts a deprecation of such data structures from V8's public
    API. It replaces the std::vector<Local<String>>, used as parameter to
    Module::CreateSyntheticModule, with a MemorySpan<const Local<String>>.
    This stands for a (contiguously allocated) array of local handles to
    strings, which will not be modified. The size of the array is known,
    as part of the MemorySpan object. The CL also replaces some vectors
    of locals with arrays of locals in API tests.
    
    Moreover, the CL adds some functionality to the existing v8::MemorySpan
    class, so that it behaves more like the upcoming (in C++20) std::span.
    In particular, it makes spans subscriptable and iterable, and allows
    implicit conversions from data structures such as std::array and
    std::vector. It also introduces a helper function template
    v8::to_array<T> (the upcoming std::to_array in C++20), to better
    support automatic deduction of array size without omitting the element
    type.
    
    Bug: chromium:1454114
    Change-Id: Iaa36da8199428a9120259aa2a3a4543e261fbc60

--- include/v8-memory-span.h
+++ include/v8-memory-span.h
@@ -7,12 +7,16 @@
 
 #include <stddef.h>
 
+#include <array>
+#include <iterator>
+#include <type_traits>
+
 #include "v8config.h"  // NOLINT(build/include_directory)
 
 namespace v8 {
 
 /**
- * Points to an unowned continous buffer holding a known number of elements.
+ * Points to an unowned contiguous buffer holding a known number of elements.
  *
  * This is similar to std::span (under consideration for C++20), but does not
  * require advanced C++ support. In the (far) future, this may be replaced with
@@ -26,18 +30,108 @@ class V8_EXPORT MemorySpan {
  public:
   /** The default constructor creates an empty span. */
   constexpr MemorySpan() = default;
+  constexpr MemorySpan(const MemorySpan& ms) = default;
 
   constexpr MemorySpan(T* data, size_t size) : data_(data), size_(size) {}
+
+  // Implicit conversion from standard containers supporting size() and
+  // data(), e.g., std::array and std::vector.
+  template <
+      typename C,
+      typename = std::enable_if_t<std::conjunction_v<
+          std::is_convertible<decltype(std::declval<C>().data()), T*>,
+          std::is_convertible<decltype(std::declval<C>().size()), size_t>>>>
+  MemorySpan(C& c)  // NOLINT(runtime/explicit)
+      : data_(c.data()), size_(c.size()) {}
 
   /** Returns a pointer to the beginning of the buffer. */
   constexpr T* data() const { return data_; }
   /** Returns the number of elements that the buffer holds. */
   constexpr size_t size() const { return size_; }
 
+  constexpr T& operator[](size_t i) const { return data_[i]; }
+
+  MemorySpan& operator=(const MemorySpan& ms) = default;
+
+  class Iterator {
+   public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = T;
+    using difference_type = std::ptrdiff_t;
+    using pointer = value_type*;
+    using reference = value_type&;
+
+    Iterator() = default;
+    Iterator(const Iterator& it) = default;
+
+    T& operator*() const { return *ptr_; }
+    T* operator->() const { return ptr_; }
+
+    bool operator==(Iterator other) const { return ptr_ == other.ptr_; }
+    bool operator!=(Iterator other) const { return !(*this == other); }
+
+    Iterator& operator=(const Iterator& it) = default;
+
+    Iterator& operator++() {
+      ++ptr_;
+      return *this;
+    }
+
+    Iterator operator++(int) {
+      Iterator temp(*this);
+      ++(*this);
+      return temp;
+    }
+
+   private:
+    explicit Iterator(T* ptr) : ptr_(ptr) {}
+
+    T* ptr_ = nullptr;
+  };
+
+  Iterator begin() const { return Iterator(data_); }
+  Iterator end() const { return Iterator(data_ + size_); }
+
  private:
   T* data_ = nullptr;
   size_t size_ = 0;
 };
+
+/**
+ * Helper function template to create an array of fixed length, initialized by
+ * the provided initializer list, without explicitly specifying the array size,
+ * e.g.
+ *
+ *   auto arr = v8::to_array<Local<String>>({v8_str("one"), v8_str("two")});
+ *
+ * In the (far) future, this may be replaced with or aliased to std::to_array
+ * (under consideration for C++20).
+ */
+
+namespace detail {
+template <class T, std::size_t N, std::size_t... I>
+constexpr std::array<std::remove_cv_t<T>, N> __to_array_lvalue_impl(
+    T (&a)[N], std::index_sequence<I...>) {
+  return {{a[I]...}};
+}
+
+template <class T, std::size_t N, std::size_t... I>
+constexpr std::array<std::remove_cv_t<T>, N> __to_array_rvalue_impl(
+    T (&&a)[N], std::index_sequence<I...>) {
+  return {{std::move(a[I])...}};
+}
+}  // namespace detail
+
+template <class T, std::size_t N>
+constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&a)[N]) {
+  return detail::__to_array_lvalue_impl(a, std::make_index_sequence<N>{});
+}
+
+template <class T, std::size_t N>
+constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&&a)[N]) {
+  return detail::__to_array_rvalue_impl(std::move(a),
+                                        std::make_index_sequence<N>{});
+}
 
 }  // namespace v8
 #endif  // INCLUDE_V8_MEMORY_SPAN_H_
