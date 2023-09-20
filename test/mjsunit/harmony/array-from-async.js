66f4c9ad0b2717c721788a9e0293b5e72c5381da
Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
2023-09-20 23:50:17 +0000

    [array-from-async] Add Array-like path
    This CL adds the array-like path to Array.fromasync implementation.
    
    Bug: v8:13321
    Change-Id: I4d3a3c0422c8a5cd9458e73e663ead6017d07cc6

--- test/mjsunit/harmony/array-from-async.js
+++ test/mjsunit/harmony/array-from-async.js
@@ -19,6 +19,11 @@ function* gen() {
   assertEquals([0, 2, 4, 6], arr);
 })();
 
+(async function TestArrayFromAsyncWithMapping() {
+  const arr = await Array.fromAsync(asyncGen(4), v => v **2);
+  assertEquals([0, 4, 16, 36], arr);
+})();
+
 (async function TestArrayFromAsyncSyncIterator() {
   const arr = await Array.fromAsync(gen());
   assertEquals([42, 43, 44], arr);
@@ -37,4 +42,26 @@ function* gen() {
 (async function TestArrayFromAsyncSyncArrayWithMapping() {
   const arr = await Array.fromAsync([1, 2, 3], v => v + 4);
   assertEquals([5, 6, 7], arr);
+})();
+
+(async function TestArrayFromAsyncArrayLike() {
+  const arrayLike = {
+    length: 3,
+    0: 1,
+    1: 2,
+    2: 3
+  };
+  const arr = await Array.fromAsync(arrayLike);
+  assertEquals([1, 2, 3], arr);
+})();
+
+(async function TestArrayFromAsyncArrayLikeWithMapping() {
+  const arrayLike = {
+    length: 3,
+    0: 1,
+    1: 2,
+    2: 3
+  };
+  const arr = await Array.fromAsync(arrayLike, v => v **2);
+  assertEquals([1, 4, 9], arr);
 })();
