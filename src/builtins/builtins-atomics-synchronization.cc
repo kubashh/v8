084d6af5386cca058a061eda28c012dba208bc37
Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
2023-09-25 15:54:52 -0700

    [shared-struct] Add Atomics.Mutex.asyncLock
    Bug: v8:12547
    Change-Id: I95eaa3f78781211b2fb69dc89da98b0214dae892

--- src/builtins/builtins-atomics-synchronization.cc
+++ src/builtins/builtins-atomics-synchronization.cc
@@ -4,6 +4,7 @@
 
 #include "src/builtins/builtins-utils-inl.h"
 #include "src/objects/js-atomics-synchronization-inl.h"
+#include "src/objects/promise-inl.h"
 
 namespace v8 {
 namespace internal {
@@ -86,6 +87,46 @@ BUILTIN(AtomicsMutexTryLock) {
   }
 
   return ReadOnlyRoots(isolate).false_value();
+}
+
+BUILTIN(AtomicsMutexLockAsync) {
+  DCHECK(v8_flags.harmony_struct);
+  constexpr char method_name[] = "Atomics.Mutex.lockAsync";
+  HandleScope scope(isolate);
+
+  Handle<Object> js_mutex_obj = args.atOrUndefined(isolate, 1);
+  if (!IsJSAtomicsMutex(*js_mutex_obj)) {
+    THROW_NEW_ERROR_RETURN_FAILURE(
+        isolate, NewTypeError(MessageTemplate::kMethodInvokedOnWrongType,
+                              isolate->factory()->NewStringFromAsciiChecked(
+                                  method_name)));
+  }
+  Handle<JSAtomicsMutex> js_mutex = Handle<JSAtomicsMutex>::cast(js_mutex_obj);
+  Handle<JSObject> run_under_lock =
+      Handle<JSObject>::cast(args.atOrUndefined(isolate, 2));
+  if (!IsCallable(*run_under_lock)) {
+    THROW_NEW_ERROR_RETURN_FAILURE(
+        isolate, NewTypeError(MessageTemplate::kNotCallable, run_under_lock));
+  }
+
+  Handle<JSPromise> promise = isolate->factory()->NewJSPromise();
+  JSAtomicsMutex::RunOrQueueCallable(js_mutex, isolate, run_under_lock,
+                                     promise);
+
+  return *promise;
+}
+
+BUILTIN(AtomicsMutexAsyncUnlock) {
+  DCHECK(v8_flags.harmony_struct);
+  HandleScope scope(isolate);
+
+  Handle<Object> mutex;
+  ASSIGN_RETURN_FAILURE_ON_EXCEPTION(
+      isolate, mutex, JSReceiver::GetProperty(isolate, args.target(), "Lock"));
+  Handle<JSAtomicsMutex> js_mutex = Handle<JSAtomicsMutex>::cast(mutex);
+
+  js_mutex->Unlock(isolate);
+  return *mutex;
 }
 
 BUILTIN(AtomicsConditionConstructor) {
--- src/objects/js-atomics-synchronization.h
+++ src/objects/js-atomics-synchronization.h
@@ -22,6 +22,8 @@ namespace internal {
 
 namespace detail {
 class WaiterQueueNode;
+class AsyncWaiterQueueNode;
+class SyncWaiterQueueNode;
 }  // namespace detail
 
 // Base class for JSAtomicsMutex and JSAtomicsCondition
@@ -109,14 +111,47 @@ class JSAtomicsMutex
     bool locked_;
   };
 
+  class V8_NODISCARD AsyncLockGuard final {
+   public:
+    inline AsyncLockGuard(Isolate* isolate, Handle<JSAtomicsMutex> mutex);
+    AsyncLockGuard(const AsyncLockGuard&) = delete;
+    AsyncLockGuard& operator=(const AsyncLockGuard&) = delete;
+    inline ~AsyncLockGuard();
+    bool locked() const { return locked_; }
+
+   private:
+    Isolate* isolate_;
+    Handle<JSAtomicsMutex> mutex_;
+    bool locked_;
+  };
+
   DECL_CAST(JSAtomicsMutex)
   DECL_PRINTER(JSAtomicsMutex)
   EXPORT_DECL_VERIFIER(JSAtomicsMutex)
 
   // Lock the mutex, blocking if it's currently owned by another thread.
   static inline void Lock(Isolate* requester, Handle<JSAtomicsMutex> mutex);
+  // static inline detail::AsyncWaiterQueueNode* AsyncLock(Isolate* requester,
+  // Handle<JSAtomicsMutex> mutex);
 
   V8_WARN_UNUSED_RESULT inline bool TryLock();
+  inline bool TryLockExplicitAndSetOwner();
+  static bool TryLockExplicit(std::atomic<StateT>* state, StateT& expected);
+  static bool SpinLockSetOwner(Handle<JSAtomicsMutex> mutex);
+  static MaybeHandle<Object> RunCallable(Handle<JSAtomicsMutex> mutex,
+                                         Isolate* isolate,
+                                         Handle<JSObject> run_under_lock,
+                                         Handle<JSPromise> promise);
+  inline static void RunOrQueueCallable(Handle<JSAtomicsMutex> mutex,
+                                        Isolate* isolate,
+                                        Handle<JSObject> run_under_lock,
+                                        Handle<JSPromise> promise);
+  static void RunOrQueueCallableSlowPath(Handle<JSAtomicsMutex> mutex,
+                                         Isolate* isolate,
+                                         Handle<JSObject> run_under_lock,
+                                         Handle<JSPromise> promise,
+                                         std::atomic<StateT>* state);
+  static void HandleAsyncNotify(detail::AsyncWaiterQueueNode* node);
 
   inline void Unlock(Isolate* requester);
 
@@ -128,6 +163,8 @@ class JSAtomicsMutex
  private:
   friend class Factory;
   friend class detail::WaiterQueueNode;
+  friend class detail::SyncWaiterQueueNode;
+  friend class detail::AsyncWaiterQueueNode;
 
   // There are 2 lock bits: whether the lock itself is locked, and whether the
   // associated waiter queue is locked.
@@ -146,7 +183,6 @@ class JSAtomicsMutex
 
   inline std::atomic<int32_t>* AtomicOwnerThreadIdPtr();
 
-  static bool TryLockExplicit(std::atomic<StateT>* state, StateT& expected);
   static bool TryLockWaiterQueueExplicit(std::atomic<StateT>* state,
                                          StateT& expected);
 
@@ -155,6 +191,18 @@ class JSAtomicsMutex
                                              std::atomic<StateT>* state);
   V8_EXPORT_PRIVATE void UnlockSlowPath(Isolate* requester,
                                         std::atomic<StateT>* state);
+  // V8_EXPORT_PRIVATE static detail::AsyncWaiterQueueNode*
+  // AsyncLockSlowPath(Isolate* requester,
+  //                                            Handle<JSAtomicsMutex> mutex,
+  //                                            std::atomic<StateT>* state);
+
+  V8_EXPORT_PRIVATE static bool SpinningMutexTryLock(
+      Isolate* requester, Handle<JSAtomicsMutex> mutex,
+      std::atomic<StateT>* state);
+
+  V8_EXPORT_PRIVATE static bool MaybeEnqueueNode(
+      Isolate* requester, Handle<JSAtomicsMutex> mutex,
+      std::atomic<StateT>* state, detail::WaiterQueueNode* this_waiter);
 
   using TorqueGeneratedJSAtomicsMutex<
       JSAtomicsMutex, JSSynchronizationPrimitive>::owner_thread_id;
