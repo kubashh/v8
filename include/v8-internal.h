ab6b731f54eea8a5536754ca61490a163147767e
Nikolaos Papaspyrou <nikolaos@chromium.org>
2023-10-02 20:47:38 +0200

    [api] Deprecate vector<v8::Local>, part 2
    According to V8's public API documentation, local handles (i.e.,
    objects of type v8::Local<T>) "should never be allocated on the heap".
    This disallows heap-allocated data structures containing instances of
    v8::Local, like std::vector<v8::Local<v8::String>>. It is unfortunate
    that the V8 API itself requires the usage of such data structures.
    
    This CL continues the deprecation of such data structures from V8's
    public API:
    
    1. It replaces the out parameter of type std::vector<Local<Object>> of
       Context::DeepFreezeDelegate::FreezeEmbedderObjectAndGetChildren
       with a LocalVector<Object>.
    2. It replaces the std::vector<std::pair<Local<Context>, size_t>> of
       MeasureMemoryDelegate with two MemorySpan objects.
    
    Bug: chromium:1454114
    Change-Id: Ifa7634a37849dee0a3d80d00355a2f1fbfe378ab

--- include/v8-internal.h
+++ include/v8-internal.h
@@ -1039,15 +1039,21 @@ class V8_EXPORT StrongRootAllocatorBase {
 // and internal::StrongRootAllocator<v8::Local<T>> register the allocated range
 // as strong roots.
 template <typename T>
-class StrongRootAllocator : public StrongRootAllocatorBase,
-                            public std::allocator<T> {
+class StrongRootAllocator : public StrongRootAllocatorBase {
  public:
+  using value_type = T;
+
   explicit StrongRootAllocator(Heap* heap) : StrongRootAllocatorBase(heap) {}
   explicit StrongRootAllocator(v8::Isolate* isolate)
       : StrongRootAllocatorBase(isolate) {}
   template <typename U>
   StrongRootAllocator(const StrongRootAllocator<U>& other) noexcept
       : StrongRootAllocatorBase(other) {}
+
+  value_type* allocate(size_t n) { return std::allocator<T>().allocate(n); }
+  void deallocate(value_type* p, size_t n) noexcept {
+    return std::allocator<T>().deallocate(p, n);
+  }
 };
 
 }  // namespace internal
