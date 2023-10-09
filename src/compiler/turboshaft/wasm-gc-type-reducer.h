8e3856e515e7c4b5730560409fa34139c2a756c8
Matthias Liedtke <mliedtke@chromium.org>
2023-10-09 17:14:41 +0200

    [wasm][turboshaft] WasmGCTypeReducer: Propagate types on phi operations
    This CL excludes phis in loop headers as they require revisiting the
    loop body and making sure to visit blocks in the "right" order.
    (See https://crrev.com/c/4919173)
    
    Bug: v8:14108
    Change-Id: I43224527ebdbc8af68c478b88e53de4ad7530e19

--- src/compiler/turboshaft/wasm-gc-type-reducer.h
+++ src/compiler/turboshaft/wasm-gc-type-reducer.h
@@ -72,6 +72,7 @@ class WasmGCTypeAnalyzer {
   void ProcessGlobalGet(const GlobalGetOp& global_get);
   void ProcessRefFunc(const WasmRefFuncOp& ref_func);
   void ProcessAllocateArray(const WasmAllocateArrayOp& allocate_array);
+  void ProcessPhi(const PhiOp& phi);
 
   void CreateMergeSnapshot(const Block& block);
 
@@ -95,6 +96,7 @@ class WasmGCTypeAnalyzer {
   // For any operation that could potentially refined, this map stores an entry
   // to the inferred input type based on the analysis.
   ZoneUnorderedMap<OpIndex, wasm::ValueType> input_type_map_{phase_zone_};
+  bool is_in_loop_header_ = false;
 };
 
 #include "src/compiler/turboshaft/define-assembler-macros.inc"
