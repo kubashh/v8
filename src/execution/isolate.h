1d44c7fe76587a37459b429119ba7e90b28bb703
Clemens Backes <clemensb@chromium.org>
2023-10-10 19:34:52 +0200

    [wasm] Add use counter for module compilation
    In addition to the feature-specific use counters, this adds a use
    counter that is incremented for every compiled module. This will allow
    us to better estimate the percentage of modules that have certain
    features enabled.
    
    Since setting a single use counter has some overhead (like creating a
    HandleScope) we also change the API to allow to set multiple use
    counters at once.
    
    R=mlippautz@chromium.org, jkummerow@chromium.org
    
    Change-Id: I69afa2bbec8fe8bed1ee1037fec39ec5f773a04b

--- src/execution/isolate.h
+++ src/execution/isolate.h
@@ -1694,7 +1694,9 @@ class V8_EXPORT_PRIVATE Isolate final : private HiddenFactory {
 
   void SetUseCounterCallback(v8::Isolate::UseCounterCallback callback);
   void CountUsage(v8::Isolate::UseCounterFeature feature);
-  void CountUsage(v8::Isolate::UseCounterFeature feature, int count);
+  // Count multiple usages at once; cheaper than calling the {CountUsage}
+  // separately for each feature.
+  void CountUsage(base::Vector<const v8::Isolate::UseCounterFeature> features);
 
   static std::string GetTurboCfgFileName(Isolate* isolate);
 
