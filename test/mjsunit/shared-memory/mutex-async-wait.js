67e8042a47ffb183e7d7166c63a2b863620aa897
Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
2023-10-17 13:48:34 -0700

    [shared-struct] Add Atomics.Mutex.asyncLock
    Bug: v8:12547
    Change-Id: I95eaa3f78781211b2fb69dc89da98b0214dae892

---
+++ test/mjsunit/shared-memory/mutex-async-wait.js
@@ -0,0 +1,38 @@
+// Copyright 2023 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Flags: --harmony-struct
+
+(async function TestAsyncWait() {
+
+  let mutex = new Atomics.Mutex;
+  let cv = new Atomics.Condition;
+  let notified = false;
+
+  Atomics.Mutex.lockAsync(mutex, async function() {
+    await Atomics.Condition.waitAsync(cv, mutex);
+    notified = true;
+  });
+
+  assertEquals(notified, false);
+  Atomics.Condition.notify(cv);
+  setTimeout(() => {assertEquals(notified, true);}, 1000);
+})();
+
+
+(async function TestAsyncWaitTimeout() {
+
+  let mutex = new Atomics.Mutex;
+  let cv = new Atomics.Condition;
+  let notified = false;
+
+  Atomics.Mutex.lockAsync(mutex, async function() {
+    await Atomics.Condition.waitAsync(cv, mutex, 1);
+    notified = true;
+    console.log('executing');
+  });
+
+  assertEquals(notified, false);
+  setTimeout(() => {assertEquals(notified, true);}, 100);
+})();
