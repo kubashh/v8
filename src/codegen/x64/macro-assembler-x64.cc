56ad70487809482a5c640f1b0d03a0ee59d8ff88
Stephen Roettger <sroettger@google.com>
2023-10-19 11:40:43 +0200

    WIP: emit endbr64 in JIT code
    DO NOT SUBMIT
    
    Bug: v8:13355
    Change-Id: Ifd350a566b2cb1aa9e5976b797dc658106df826c

--- src/codegen/x64/macro-assembler-x64.cc
+++ src/codegen/x64/macro-assembler-x64.cc
@@ -51,6 +51,8 @@ Operand StackArgumentsAccessor::GetArgumentOperand(int index) const {
   return Operand(rsp, kPCOnStackSize + index * kSystemPointerSize);
 }
 
+void MacroAssembler::CodeEntry() { endbr64(); }
+
 void MacroAssembler::Load(Register destination, ExternalReference source) {
   if (root_array_available_ && options().enable_root_relative_access) {
     intptr_t delta = RootRegisterOffsetForExternalReference(isolate(), source);
@@ -2145,7 +2147,7 @@ void MacroAssembler::Switch(Register scratch, Register reg, int case_value_base,
   cmpq(reg, Immediate(num_labels));
   j(above_equal, &fallthrough);
   leaq(table, MemOperand(&jump_table));
-  jmp(MemOperand(table, reg, times_8, 0));
+  jmp(MemOperand(table, reg, times_8, 0), /*notrack=*/true);
   // Emit the jump table inline, under the assumption that it's not too big.
   Align(kSystemPointerSize);
   bind(&jump_table);
