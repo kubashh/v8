4c29e6fcb6ce69d041fec21bdb1cc85c207f7071
Anton Bikineev <bikineev@chromium.org>
2023-11-13 13:58:01 +0100

    cppgc: Globalize normal page pool across all Oilpan instances
    Currently the page pool works per Oilpan heap, which can be suboptimal
    for workloads with multiple workers, since the pooled pages are not
    shared between Oilpan instances.
    
    Bug: v8:14390
    Change-Id: Ia63863e799221afbd1d60460e095d0932b8492bb

--- src/snapshot/mksnapshot.cc
+++ src/snapshot/mksnapshot.cc
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
+#include <sys/resource.h>
 
 #include <iomanip>
 
@@ -221,6 +222,23 @@ void MaybeSetCounterFunction(v8::Isolate* isolate) {
 }  // namespace
 
 int main(int argc, char** argv) {
+  {
+    const rlim_t kStackSize = 1 * 1024L * 1024L;  // min stack size = 64 Mb
+    struct rlimit rl;
+    int result;
+
+    result = getrlimit(RLIMIT_STACK, &rl);
+    if (result == 0) {
+      if (rl.rlim_cur < kStackSize) {
+        rl.rlim_cur = kStackSize;
+        result = setrlimit(RLIMIT_STACK, &rl);
+        if (result != 0) {
+          fprintf(stderr, "setrlimit returned result = %d\n", result);
+        }
+      }
+    }
+  }
+
   v8::base::EnsureConsoleOutput();
 
   // Make mksnapshot runs predictable to create reproducible snapshots.
