e21598d4016bed6b3c0e13c9b200a86a4499e7b3
Liviu Rau <liviurau@chromium.org>
2023-12-04 10:06:24 +0100

    [test262] Self approve exports
    Chromium/WPT exports are self approved via a web hook (http://screen/BYL5fPchGhjxcmu) with self approve functionality from http://shortn/_A8n7jtXYcp installed @ http://shortn/_Jc3u72WPMZ  .
    
    We will mimic the same behavior by sending an approve request just before we attempt a second pass merge.
    
    Bug: v8:14002
    Change-Id: I632b254147029649d41d9d4816ddfc8a2c05f4cb

---
+++ test/test262/tools/v8_exporter.py
@@ -0,0 +1,42 @@
+# Copyright 2023 the V8 project authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from requests.exceptions import HTTPError
+
+from blinkpy.w3c.test_exporter import TestExporter
+from blinkpy.w3c.wpt_github import GitHubError
+
+class V8TestExporter(TestExporter):
+
+    def merge_pull_request(self, pull_request):
+        self.approve(pull_request)
+        super().merge_pull_request(pull_request)
+
+
+    def approve(self, pr_number):
+        """Approves a PR.
+        Chromium/WPT exports are self approved via a web hook
+        (http://screen/BYL5fPchGhjxcmu) with self approve functionality from
+        http://shortn/_A8n7jtXYcp installed @ http://shortn/_Jc3u72WPMZ .
+
+        We will mimic the same behavior by sending an approve request just
+        before we attempt a second pass merge.
+        """
+        path = '/repos/%s/%s/pulls/%d/reviews'  % (
+            self.github.gh_org,
+            self.github.gh_repo_name,
+            pr_number)
+
+        body = {
+            'body': ('The review process for this patch is being conducted in '
+                'the V8 project.'),
+            'event': 'APPROVE',
+            'comments': [],
+        }
+
+        response = self.request(path, method='POST', body=body)
+
+        if response.status_code != 200:
+            raise GitHubError(200, response.status_code,
+                              'ap[rove PR %d' % pr_number)
