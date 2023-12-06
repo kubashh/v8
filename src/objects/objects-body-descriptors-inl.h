ea2698e1e6c118f9407d756205fe66890d6d61a0
Samuel Groß <saelo@chromium.org>
2023-12-06 12:25:00 +0000

    [sandbox] Move BytecodeArray::handler_table out of the sandbox
    We now use the newly added TrustedByteArray class for a BytecodeArrays'
    handler_table field. The handler table should be considered trusted as
    it contains indices into its associated BytecodeArray and would
    therefore allow executing attacker-controlled bytecode if manipulated.
    
    Bug: chromium:1472728
    Change-Id: I0addeb6f56f076228370c1f44ba04f106bf65f15

--- src/objects/objects-body-descriptors-inl.h
+++ src/objects/objects-body-descriptors-inl.h
@@ -501,8 +501,9 @@ class BytecodeArray::BodyDescriptor final : public BodyDescriptorBase {
   static inline void IterateBody(Tagged<Map> map, Tagged<HeapObject> obj,
                                  int object_size, ObjectVisitor* v) {
     IterateSelfIndirectPointer(obj, kBytecodeArrayIndirectPointerTag, v);
+    IterateTrustedTaggedPointer(TrustedObject::cast(obj), kHandlerTableOffset,
+                                v);
     IteratePointer(obj, kConstantPoolOffset, v);
-    IteratePointer(obj, kHandlerTableOffset, v);
     IteratePointer(obj, kWrapperOffset, v);
     IteratePointer(obj, kSourcePositionTableOffset, v);
   }
