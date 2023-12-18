6825fa6e1c409a87eab04d410c2a9d7a0e98c885
Olivier Flückiger <olivf@chromium.org>
2023-12-18 10:58:19 +0100

    [multimodal] Fuzzer coverage for changing modes
    Add some smoke tests and more importantly ensure the fuzzers can change
    modes at runtime.
    
    Bug: v8:14515
    Change-Id: Ia44eca680e0e4da7b314b6cc076e17805e3fcf11

---
+++ test/mjsunit/compiler/efficiency-mode-limits-tiering.js
@@ -0,0 +1,34 @@
+// Copyright 2023 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Flags: --allow-natives-syntax --efficiency-mode-disable-turbofan
+
+function f(x) {
+  var y = 0;
+  for (var i = 0; i < x; i++) {
+    y = 1;
+  }
+  return y;
+}
+
+function g(iter) {
+  if (%IsTurbofanEnabled()) {
+    while (!%ActiveTierIsTurbofan(f) && --iter) f(10);
+  }
+}
+%NeverOptimizeFunction(g);
+
+%NotifyIsolateBackground();
+// There is no hard guarantee that this does enable efficiency mode...
+if (%IsEfficiencyModeEnabled()) {
+  g(1000000);
+  assertFalse(%ActiveTierIsTurbofan(f));
+  %NotifyIsolateForeground();
+  if (!%IsEfficiencyModeEnabled()) {
+    g(1000000);
+    if (%IsTurbofanEnabled()) {
+      assertTrue(%ActiveTierIsTurbofan(f));
+    }
+  }
+}
