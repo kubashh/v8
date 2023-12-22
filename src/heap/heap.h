294704a1738554be8cc5443d6b0bc82bc5951c75
Omer Katz <omerkatz@chromium.org>
2023-12-22 14:22:39 +0000

    Revert "[heap] Check allocation limit overshot when expending new space"
    This reverts commit 8c29cfe6bdd432f6dfbcda6a8fa5b830b8502d40.
    
    Reason for revert: Suspected for pixel6 jetstream2 timeouts
    
    Original change's description:
    > [heap] Check allocation limit overshot when expending new space
    >
    > Don't let new space allocate more pages during full GC incremental
    > marking if the allocation limit is overshot by large margin.
    >
    > Drive-by: refactor the conditions for allocating another page to
    > 	  Heap::ShouldExpandYoungGenerationOnSlowAllocation.
    >
    > Bug: v8:12612
    > Change-Id: I984473b860176796337b95d23cef7b8276538915
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5116541
    > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#91515}
    
    Bug: v8:12612
    Change-Id: Id322cfa02f7dd2327ceee95cc60ed8fb7b22255e

--- src/heap/heap.h
+++ src/heap/heap.h
@@ -1952,7 +1952,6 @@ class Heap final {
 
   bool ShouldExpandOldGenerationOnSlowAllocation(LocalHeap* local_heap,
                                                  AllocationOrigin origin);
-  bool ShouldExpandYoungGenerationOnSlowAllocation();
   bool IsRetryOfFailedAllocation(LocalHeap* local_heap);
   bool IsMainThreadParked(LocalHeap* local_heap);
   bool IsMajorMarkingComplete(LocalHeap* local_heap);
@@ -2436,7 +2435,6 @@ class Heap final {
   friend class NewSpace;
   friend class ObjectStatsCollector;
   friend class Page;
-  friend class PagedNewSpaceAllocatorPolicy;
   friend class PagedSpaceAllocatorPolicy;
   friend class PagedSpaceBase;
   friend class PagedSpaceForNewSpace;
