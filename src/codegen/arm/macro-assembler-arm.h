4420176fd2c4ba574288cee67c08f4d86f4fcce4
Dominik Inführ <dinfuehr@chromium.org>
2024-02-12 17:27:40 +0100

    [compiler] Always emit safepoint for fast C calls
    Bug: chromium:1523607
    Change-Id: I73c458b590e2dad938f505308066bfe3c07e9e6e

--- src/codegen/arm/macro-assembler-arm.h
+++ src/codegen/arm/macro-assembler-arm.h
@@ -252,17 +252,17 @@ class V8_EXPORT_PRIVATE MacroAssembler : public MacroAssemblerBase {
   // garbage collection, since that might move the code and invalidate the
   // return address (unless this is somehow accounted for by the called
   // function).
-  void CallCFunction(
+  int CallCFunction(
       ExternalReference function, int num_arguments,
       SetIsolateDataSlots set_isolate_data_slots = SetIsolateDataSlots::kYes);
-  void CallCFunction(
+  int CallCFunction(
       Register function, int num_arguments,
       SetIsolateDataSlots set_isolate_data_slots = SetIsolateDataSlots::kYes);
-  void CallCFunction(
+  int CallCFunction(
       ExternalReference function, int num_reg_arguments,
       int num_double_arguments,
       SetIsolateDataSlots set_isolate_data_slots = SetIsolateDataSlots::kYes);
-  void CallCFunction(
+  int CallCFunction(
       Register function, int num_reg_arguments, int num_double_arguments,
       SetIsolateDataSlots set_isolate_data_slots = SetIsolateDataSlots::kYes);
 
