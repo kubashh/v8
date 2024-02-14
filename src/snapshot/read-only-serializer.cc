b825eaa3884570d9856fcb20572b4fd93c331b08
Samuel Groß <saelo@chromium.org>
2024-02-14 15:27:31 +0000

    [sandbox] Refactor Code::source_position_table
    In preparation for moving source position tables out of the sandbox,
    this Cl refactors how they are used. In particular:
    
    * The Code::source_position_table field can now be Smi::zero() in case
      of builtin Code in RO space. This is because we cannot currently have
      RO -> Trusted Space references.
    * The CodeBuilder's handling of source position tables is refactored to
      make it explicit when a source position table is expected and when it
      may be missing.
    * We introduce helper methods like CodeKindUsesBytecodeOffsetTable.
    
    Bug: b/40927183
    Change-Id: I5d40236afb6a5e8968335be315bd93dc39883f5a

--- src/snapshot/read-only-serializer.cc
+++ src/snapshot/read-only-serializer.cc
@@ -77,6 +77,9 @@ class ObjectPreProcessor final {
   }
   void PreProcessCode(Tagged<Code> o) {
     o->ClearInstructionStartForSerialization(isolate_);
+    // Embedded code should not have deopt data or source positions.
+    DCHECK(!o->has_source_position_table_or_bytecode_offset_table());
+    DCHECK(!o->has_deoptimization_data_or_interpreter_data());
   }
 
   Isolate* const isolate_;
