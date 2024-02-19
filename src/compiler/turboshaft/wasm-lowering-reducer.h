37b7da2e43ee7fe89c94815b9020f82fd3472755
Milad Fa <mfarazma@redhat.com>
2024-02-19 09:04:35 -0500

    [wasm][turboshaft] fix for big endian platforms
    Root pointers do not get compressed, reading 4 bytes will
    load the wrong half on big endian platofrms.
    
    Change-Id: I4eb8add616a6c47509fd42bdfef6f10d2387ac8e

--- src/compiler/turboshaft/wasm-lowering-reducer.h
+++ src/compiler/turboshaft/wasm-lowering-reducer.h
@@ -923,8 +923,16 @@ class WasmLoweringReducer : public Next {
                 wasm::IsSubtypeOf(type, wasm::kWasmExnRef, module_)
             ? RootIndex::kNullValue
             : RootIndex::kWasmNull;
+    // We load WasmNull as a pointer here and not as a TaggedPointer because
+    // WasmNull
+    // is stored uncompressed in the IsolateData, and a load of a TaggedPointer
+    // loads compressed pointers. We do not bitcast the WasmNull to Tagged at
+    // the moment because it would increase graph size, which may affect
+    // optimizations negatively. These regressions would be worth it if there
+    // was any benefit of the bitcast. However, graph validation based on types
+    // does not exist currently.
     return __ Load(roots, LoadOp::Kind::RawAligned().Immutable(),
-                   MemoryRepresentation::TaggedPointer(),
+                   MemoryRepresentation::UintPtr(),
                    IsolateData::root_slot_offset(index));
   }
 
