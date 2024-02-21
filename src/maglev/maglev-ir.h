49c147ebd70a5db425605264909d3c8d424603f5
Victor Gomes <victorgomes@chromium.org>
2024-02-21 16:33:15 +0100

    Reland "[maglev] Cache elements array"
    This is a reland of commit 94b71ec6f3c71a2f38ce740d53e51290ae038951
    
    Original change's description:
    > [maglev] Cache elements array
    >
    > Drive-by: also record JSArray::kLength in FastObjects
    >
    > Bug: v8:7700
    > Change-Id: I0440f5682ada55cf9ee7a34a6f31c0895c8c3428
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5306756
    > Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    > Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    > Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#92422}
    
    Bug: v8:7700
    Change-Id: I137ea524a2fd42d2f0071224ab5072bd3ba145d5

--- src/maglev/maglev-ir.h
+++ src/maglev/maglev-ir.h
@@ -6191,8 +6191,9 @@ class EnsureWritableFastElements
  public:
   explicit EnsureWritableFastElements(uint64_t bitfield) : Base(bitfield) {}
 
-  static constexpr OpProperties kProperties =
-      OpProperties::CanAllocate() | OpProperties::DeferredCall();
+  static constexpr OpProperties kProperties = OpProperties::CanAllocate() |
+                                              OpProperties::DeferredCall() |
+                                              OpProperties::CanWrite();
   static constexpr typename Base::InputTypes kInputTypes{
       ValueRepresentation::kTagged, ValueRepresentation::kTagged};
 
@@ -6216,9 +6217,9 @@ class MaybeGrowAndEnsureWritableFastElements
                                                   ElementsKind elements_kind)
       : Base(bitfield), elements_kind_(elements_kind) {}
 
-  static constexpr OpProperties kProperties = OpProperties::CanAllocate() |
-                                              OpProperties::DeferredCall() |
-                                              OpProperties::EagerDeopt();
+  static constexpr OpProperties kProperties =
+      OpProperties::CanAllocate() | OpProperties::DeferredCall() |
+      OpProperties::CanWrite() | OpProperties::EagerDeopt();
   static constexpr typename Base::InputTypes kInputTypes{
       ValueRepresentation::kTagged, ValueRepresentation::kTagged,
       ValueRepresentation::kInt32, ValueRepresentation::kInt32};
