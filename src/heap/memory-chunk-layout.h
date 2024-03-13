9ff09d8b17dc5d279bcc9e160db34b19055dbcd5
Stephen Roettger <sroettger@google.com>
2024-03-13 18:35:34 +0100

    [heap] Move Metadata out of the MemoryChunk
    Allocate the MutablePageMetadata objects on the C++ heap, only the
    ReadOnlyMetadata stays inline. This will allow us to:
    * get rid of the CodePageHeaderModificationScope on Apple silicon
    * make our code space compatible with memory sealing
    * fix sandbox bypasses once we sandboxify the metadata pointer
    
    Change-Id: If956b09dba4d1de623618eec03ddfd0054ad166b
    Bug: v8:13355, chromium:40849120

--- src/heap/memory-chunk-layout.h
+++ src/heap/memory-chunk-layout.h
@@ -44,9 +44,10 @@ class V8_EXPORT_PRIVATE MemoryChunkLayout {
 #define FIELD(Type, Name) \
   k##Name##Offset, k##Name##End = k##Name##Offset + sizeof(Type) - 1
   enum Header {
-    // MemoryChunk fields:
     FIELD(uintptr_t, Flags),
     FIELD(MemoryChunkMetadata*, Metadata),
+  };
+  enum Metadata {
     // MemoryChunkMetadata fields:
     FIELD(size_t, Size),
     FIELD(Heap*, Heap),
@@ -55,8 +56,8 @@ class V8_EXPORT_PRIVATE MemoryChunkLayout {
     FIELD(size_t, AllocatedBytes),
     FIELD(size_t, WastedMemory),
     FIELD(std::atomic<intptr_t>, HighWaterMark),
-    FIELD(Address, Owner),
     FIELD(VirtualMemory, Reservation),
+    FIELD(Address, Owner),
     // MutablePageMetadata fields:
     FIELD(SlotSet* [kNumSets], SlotSet),
     FIELD(TypedSlotsSet* [kNumSets], TypedSlotSet),
