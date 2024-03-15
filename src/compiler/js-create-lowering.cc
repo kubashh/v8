9e75022428a835c3f978ab78faefe52d1c925c98
Michael Lippautz <mlippautz@chromium.org>
2024-03-15 10:55:24 +0100

    Introduce CppHeapWrapper field for types that can hold external pointers
    This introduces a new field `cpp_heap_wrapper` for holding references
    to a CppHeap object. The field is put on the two class hierarchies
    that can hold embedder fields:
    - JSObjectWithEmbedderSlots
    - JSSpecialObject
    
    The class hierarchies are separate due to instance type range
    constraints.
    
    Missing support:
    - Actually read/write of fields
    - Triggering setup through FunctionTemplateInfo
    - Context serialization/deserialization
    
    Change-Id: Idd30c8540b5205c74eb50a7d588d9976cbc5afc8
    Bug: b/328117814

--- src/compiler/js-create-lowering.cc
+++ src/compiler/js-create-lowering.cc
@@ -1084,7 +1084,13 @@ Reduction JSCreateLowering::ReduceJSCreatePromise(Node* node) {
   static_assert(v8::Promise::kPending == 0);
   a.Store(AccessBuilder::ForJSObjectOffset(JSPromise::kFlagsOffset),
           jsgraph()->ZeroConstant());
-  static_assert(JSPromise::kHeaderSize == 5 * kTaggedSize);
+  a.Store(AccessBuilder::ForJSObjectWithEmbedderDataSlotsCppHeapWrappable(),
+          jsgraph()->ZeroConstant());
+#ifdef V8_ENABLE_SANDBOX
+  static_assert(JSPromise::kHeaderSize == 6 * kTaggedSize);
+#else
+  static_assert(JSPromise::kHeaderSize == 7 * kTaggedSize);
+#endif
   for (int offset = JSPromise::kHeaderSize;
        offset < JSPromise::kSizeWithEmbedderFields; offset += kTaggedSize) {
     a.Store(AccessBuilder::ForJSObjectOffset(offset),
