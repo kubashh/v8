400c7c7127d3ec6023fcc1f89f97f4663ebfc55f
Jakob Kummerow <jkummerow@chromium.org>
2024-03-18 12:46:41 +0100

    Fix building x64 with GCC-12
    - Mostly making unreachable code paths easier for the compiler
      to detect
    - Dropping one unused field from a struct
    - Dropping one unnecessary std::move preventing copy elision
    
    Other target platforms than x64 may need additional fixes.
    The build takes a long time but it does finish eventually.
    
    Change-Id: I314b1ca41959d7433d83bc2065facdcada91cc52

--- src/d8/d8.cc
+++ src/d8/d8.cc
@@ -3970,6 +3970,7 @@ V8_NOINLINE void FuzzerMonitor::UndefinedBehavior() {
 
 V8_NOINLINE void FuzzerMonitor::UseAfterFree() {
   // Use-after-free caught by ASAN.
+#if defined(__clang__)  // GCC-12 detects this at compile time!
   std::vector<bool>* storage = new std::vector<bool>(3);
   delete storage;
 #if defined(__clang__)
