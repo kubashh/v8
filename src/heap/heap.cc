f820ea0681ce42a0d6c4a80f3c3dde151429a9f7
Michael Lippautz <mlippautz@chromium.org>
2024-04-15 16:18:08 +0200

    Context disposal: Major GC
    Change-Id: I5efff4db65878b0c2de6a8442ebb6812d1a4f5b1

--- src/heap/heap.cc
+++ src/heap/heap.cc
@@ -1973,6 +1973,24 @@ void Heap::CollectGarbage(AllocationSpace space,
   }
 }
 
+namespace {
+
+class IdleTaskOnContextDispose final : public CancelableIdleTask {
+ public:
+  explicit IdleTaskOnContextDispose(Isolate* isolate)
+      : CancelableIdleTask(isolate), isolate_(isolate) {}
+
+  void RunInternal(double deadline_in_seconds) override {
+    auto* heap = isolate_->heap();
+    heap->CollectGarbage(OLD_SPACE, GarbageCollectionReason::kTesting);
+  }
+
+ private:
+  Isolate* isolate_;
+};
+
+}  // namespace
+
 int Heap::NotifyContextDisposed(bool has_dependent_context) {
   if (!has_dependent_context) {
     tracer()->ResetSurvivalEvents();
@@ -1980,6 +1998,9 @@ int Heap::NotifyContextDisposed(bool has_dependent_context) {
     if (memory_reducer_) {
       memory_reducer_->NotifyPossibleGarbage();
     }
+  } else {
+    GetForegroundTaskRunner()->PostIdleTask(
+        std::make_unique<IdleTaskOnContextDispose>(isolate()));
   }
   isolate()->AbortConcurrentOptimization(BlockingBehavior::kDontBlock);
   if (!isolate()->context().is_null()) {
