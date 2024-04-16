c032402dc2c86fa70ba1e6a7328599f629639e08
Lu Yahan <yahan@iscas.ac.cn>
2024-04-16 14:20:29 +0800

    [riscv][codegen] Introduce MemoryRepresentation::kProtectedPointer
    Port commit a9083da44ed0a330f9020197683d569bde21c438
    
    Port commit e7c147b65c4f5c1013a0b45cf25fa291d176d111
    [fastcall] Support throwing exceptions
    
    Bug: v8:14564
    
    Change-Id: I518020cef404236c5d6d237c8bd1d80c659f13a6

--- src/compiler/backend/riscv/instruction-scheduler-riscv.cc
+++ src/compiler/backend/riscv/instruction-scheduler-riscv.cc
@@ -337,6 +337,7 @@ int InstructionScheduler::GetTargetInstructionFlags(
     case kRiscvAtomicLoadDecompressTaggedSigned:
     case kRiscvAtomicLoadDecompressTagged:
     case kRiscvAtomicStoreCompressTagged:
+    case kRiscvLoadDecompressProtected:
 #elif V8_TARGET_ARCH_RISCV32
     case kRiscvWord32AtomicPairLoad:
 #endif
@@ -1580,6 +1581,8 @@ int InstructionScheduler::GetInstructionLatency(const Instruction* instr) {
              BranchShortLatency() + 1;
     case kRiscvAssertEqual:
       return AssertLatency();
+    case kRiscvLoadDecompressProtected:
+      return 11;
     default:
       return 1;
   }
