d0e0b413a50ab0a19d594fe412f28fbe2b815083
Samuel Groß <saelo@chromium.org>
2024-04-29 18:00:10 +0000

    [sandbox] Use Code::parameter_count in more places in the deoptimizer
    As the SharedFunctionInfo::internal_formal_parameter_count() is
    deprecated, this CL switches a few more places in the deoptimizer to use
    the parameter count from the Code object instead. It also now adds a
    code comment to SharedFunctionInfo::internal_formal_parameter_count() to
    note that the field is deprecated and should no longer be used.
    
    Bug: chromium:40931165
    Change-Id: Ic5cdc1370d2b8d673289b9b110cf1aa3539efa9c

--- src/objects/shared-function-info.h
+++ src/objects/shared-function-info.h
@@ -305,6 +305,8 @@ class SharedFunctionInfo
   // For subclass constructors, also includes new.target.
   // The size of function's frame is
   // internal_formal_parameter_count_with_receiver.
+  // DEPRECATED: please obtain the parameter count from the Code/BytecodeArray
+  // or another trusted source instead. See also crbug.com/40931165
   inline void set_internal_formal_parameter_count(int value);
   inline uint16_t internal_formal_parameter_count_with_receiver() const;
   inline uint16_t internal_formal_parameter_count_without_receiver() const;
