d146e3fd87be6e4cd39cf8b0060db40d2ce97c7a
Eva Herencsarova <evih@chromium.org>
2024-05-03 18:32:50 +0200

    [wasm][code flushing] Flush Liftoff code on memory pressure
    Change-Id: I960dabcb358f524d4d95b9e81fae513b39504895

--- src/objects/backing-store.cc
+++ src/objects/backing-store.cc
@@ -336,7 +336,10 @@ std::unique_ptr<BackingStore> BackingStore::TryAllocateAndPartiallyCommitMemory(
   auto gc_retry = [&](const std::function<bool()>& fn) {
     for (int i = 0; i < 3; i++) {
       if (fn()) return true;
-      // Collect garbage and retry.
+        // Flush out wasm code, collect garbage and retry.
+#if V8_ENABLE_WEBASSEMBLY
+      internal::wasm::GetWasmEngine()->FlushCode();
+#endif  // V8_ENABLE_WEBASSEMBLY
       did_retry = true;
       // TODO(wasm): try Heap::EagerlyFreeExternalMemory() first?
       if (isolate != nullptr) {
