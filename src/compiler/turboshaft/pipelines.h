e176bfae8db7245be4d0d29b1cf58d3245068e76
Nico Hartmann <nicohartmann@chromium.org>
2024-06-06 15:42:20 +0200

    Port the BigIntEqual builtin to Turboshaft
    EXPERIMENTAL! Do not commit!
    
    Change-Id: Iea9f7a2efd7b8904481b8fa0fd6fa96e633887cf

--- src/compiler/turboshaft/pipelines.h
+++ src/compiler/turboshaft/pipelines.h
@@ -33,7 +33,7 @@ namespace v8::internal::compiler::turboshaft {
 
 inline constexpr char kTempZoneName[] = "temp-zone";
 
-class Pipeline final {
+class Pipeline {
  public:
   explicit Pipeline(PipelineData* data) : data_(data) {}
 
@@ -209,7 +209,8 @@ class Pipeline final {
 
   void PrepareForInstructionSelection(
       const ProfileDataFromFile* profile = nullptr) {
-    if (data()->pipeline_kind() == TurboshaftPipelineKind::kCSA) {
+    if (data()->pipeline_kind() == TurboshaftPipelineKind::kCSA ||
+        data()->pipeline_kind() == TurboshaftPipelineKind::kTSABuiltin) {
       if (profile) {
         Run<ProfileApplicationPhase>(profile);
       }
@@ -489,6 +490,11 @@ class Pipeline final {
     return FinalizeCode();
   }
 
+  MaybeHandle<Code> GenerateCode(
+      Linkage* linkage, std::shared_ptr<OsrHelper> osr_helper = {},
+      JumpOptimizationInfo* jump_optimization_info = nullptr,
+      const ProfileDataFromFile* profile = nullptr, int initial_graph_hash = 0);
+
   void RecreateTurbofanGraph(compiler::TFPipelineData* turbofan_data,
                              Linkage* linkage);
 
@@ -564,6 +570,13 @@ class Pipeline final {
 
  private:
   PipelineData* data_;
+};
+
+class BuiltinPipeline : public Pipeline {
+ public:
+  explicit BuiltinPipeline(PipelineData* data) : Pipeline(data) {}
+
+  void OptimizeBuiltin();
 };
 
 }  // namespace v8::internal::compiler::turboshaft
