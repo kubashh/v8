13fb23dbad6cfa92ee10913fbd789eec1e94e455
Victor Gomes <victorgomes@chromium.org>
2024-06-10 16:05:58 +0200

    [maglev] Fix clobbered register in StringCodePointAt
    The {string} input and the result could register, which make the
    StringLength calculation buggy.
    
    If we are calculating the CodePointAt of a two byte string, we need
    a second temporary (or we would need to support adding constrains
    like input cannot alias with result in the register allocator).
    
    Drive-by: Make BuiltinStringPrototypeCharCodeOrCodePointAt
    arch-agnostic.
    
    Fixed: 345547970
    Change-Id: Id42fc7f244a0307699ee39707e7a5b892a4cedf2

---
+++ test/mjsunit/maglev/regress-345547970.js
@@ -0,0 +1,18 @@
+// Copyright 2024 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Flags: --allow-natives-syntax --fuzzing --no-testing-d8-test-runner
+// Flags: --no-lazy-feedback-allocation --single-threaded
+
+const v0 = [-303801024,-54947];
+const v1 = %OptimizeOsr();
+for (const v2 in v0) {
+    const v5 = String.fromCharCode(55598);
+    let v6 = v5.codePointAt();
+    const v7 = v6 >>> v6;
+    try {
+        const v10 = eval(v2);
+    } catch(e15) {
+    }
+}
