a346aa20d47ca8e64e87cab331d6404d66e72ee7
Giovanni Del Valle <t-delvalleg@microsoft.com>
2024-06-13 17:00:20 -0700

    Create Simplified Heap Iteration for Detached Elements
    Initial Commit: Get Detached Elements from V8
    
    
    Version 12.8.52
    
    Change-Id: I5dd543b961a19fd3e3252eba2385b52127927be7
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5630257
    Bot-Commit: v8-ci-autoroll-builder <v8-ci-autoroll-builder@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/12.8.52@{#1}
    Cr-Branched-From: d2f2649a7bb008d188bf6bc614d3e756c3c47df5-refs/heads/main@{#94443}

--- src/api/api.cc
+++ src/api/api.cc
@@ -11408,6 +11408,10 @@ const HeapSnapshot* HeapProfiler::TakeHeapSnapshot(ActivityControl* control,
   return TakeHeapSnapshot(options);
 }
 
+std::vector<v8::Local<v8::Data>> HeapProfiler::GetDetachedJSWrapperObjects() {
+  return reinterpret_cast<i::HeapProfiler*>(this)->GetDetachedJSWrapperObjects();
+}
+
 void HeapProfiler::StartTrackingHeapObjects(bool track_allocations) {
   reinterpret_cast<i::HeapProfiler*>(this)->StartHeapObjectsTracking(
       track_allocations);
