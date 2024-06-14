74e4daadbe21aa0b37abe6cd00c0f01562b5d5f4
Michael Achenbach <machenbach@chromium.org>
2024-06-14 23:03:42 +0200

    [fuzztest] Generalize fuzztest wrappers for more executables
    Change-Id: Iaeda6b6e2a7005e8ec1a61258828c3eb645cc854

--- test/common/fuzztest/gen_fuzztest_configs.py
+++ test/common/fuzztest/gen_fuzztest_configs.py
@@ -5,12 +5,13 @@
 # found in the LICENSE file.
 """List all available fuzz tests and create wrapper scripts for each.
 
-Invoked by GN depending on the unit-tests target. Expected to be called
-from the root out dir of GN.
+Invoked by GN depending on the executable targets containing fuzztests.
+Expected to be called from the root out dir of GN.
 
-Writes the wrappers and 'fuzztests.stamp' into the ./fuzztests/ directory.
+Writes the wrappers and stamp files into the ./fuzztests/ directory.
 """
 
+import argparse
 import os
 import re
 import shutil
@@ -21,14 +22,14 @@ import sys
 from pathlib import Path, PurePath
 
 # Set up path to be able to import action_helpers
-BASE_DIR = Path(__file__).absolute().parent.parent.parent
+BASE_DIR = Path(__file__).absolute().parent.parent.parent.parent
 sys.path.append(str(BASE_DIR / 'build'))
 import action_helpers
 
 CENTIPEDE = 'centipede'
-EXECUTABLE = 'v8_unittests'
 FUZZ_TEST_DIR = 'fuzztests'
-FUZZ_TEST_STAMP = 'fuzztests.stamp'
+FUZZ_TEST_STAMP = 'fuzztests_dir.stamp'
+FUZZ_TEST_WRAPPER_STAMP = '%s.stamp'
 
 # When this script is enabled, we expect to find at least the demo tests.
 MIN_FUZZTESTS = 2
@@ -50,12 +51,12 @@ exec $BINARY_DIR/centipede $@
 FUZZTEST_WRAPPER = WRAPPER_HEADER + """
 # Normal fuzzing.
 if [ "$#" -eq  "0" ]; then
-   exec $BINARY_DIR/v8_unittests --fuzz={test} --corpus_database=""
+   exec $BINARY_DIR/{executable} --fuzz={test} --corpus_database=""
 fi
 # Fuzztest replay.
 if [ "$#" -eq  "1" ]; then
    unset CENTIPEDE_RUNNER_FLAGS
-   FUZZTEST_REPLAY=$1 exec $BINARY_DIR/v8_unittests --fuzz={test} --corpus_database=""
+   FUZZTEST_REPLAY=$1 exec $BINARY_DIR/{executable} --fuzz={test} --corpus_database=""
 fi
 """
 
@@ -86,9 +87,9 @@ def fuzz_test_to_file_name(test):
   return 'v8_' + '_'.join(splitted) + '_fuzztest'
 
 
-def create_wrapper(file_name, template, test=''):
+def create_wrapper(file_name, template, executable='', test=''):
   with action_helpers.atomic_output(file_name) as f:
-    wrapper = template.format(test=test)
+    wrapper = template.format(executable=executable, test=test)
     f.write(wrapper.encode('utf-8'))
 
   # Make the wrapper world-executable.
@@ -97,10 +98,12 @@ def create_wrapper(file_name, template, test=''):
   os.chmod(file_name, m | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
 
-def setup_fuzztests_dir(cwd):
+def setup_fuzztests_dir(options):
+  cwd = Path(os.getcwd())
+  fuzz_test_dir = cwd / FUZZ_TEST_DIR
+
   # This script owns the fuzztests subdirectory. Purge everything
   # to enable consistent incremental builds.
-  fuzz_test_dir = cwd / FUZZ_TEST_DIR
   shutil.rmtree(fuzz_test_dir, ignore_errors=True)
   fuzz_test_dir.mkdir(exist_ok=True)
 
@@ -109,33 +112,52 @@ def setup_fuzztests_dir(cwd):
   # keep loading.
   create_wrapper(fuzz_test_dir / CENTIPEDE, CENTIPEDE_WRAPPER)
 
-  return fuzz_test_dir
+   # This is a place holder telling GN that we're done.
+  with action_helpers.atomic_output(fuzz_test_dir / FUZZ_TEST_STAMP) as f:
+    f.write('Initialized directory'.encode('utf-8'))
 
-
-def create_fuzztest_wrapper(fuzz_test_dir, test_name):
+def create_fuzztest_wrapper(fuzz_test_dir, executable, test_name):
   fuzztest_path = fuzz_test_dir / fuzz_test_to_file_name(test_name)
-  create_wrapper(fuzztest_path, FUZZTEST_WRAPPER, test_name)
+  create_wrapper(fuzztest_path, FUZZTEST_WRAPPER, executable, test_name)
 
 
-def main():
-  # The CWD is expected to be the root out dir of GN.
+def gen_wrappers(options):
   cwd = Path(os.getcwd())
+  fuzz_test_dir = cwd / FUZZ_TEST_DIR
 
   # We expect the unit-test executable present in the root dir.
-  executable = cwd / EXECUTABLE
+  executable = cwd / options.executable
   assert executable.exists()
 
-  fuzz_test_dir = setup_fuzztests_dir(cwd)
   fuzz_tests = list_fuzz_tests(executable)
   assert MIN_FUZZTESTS <= len(fuzz_tests) <= MAX_FUZZTESTS
 
   for test_name in fuzz_tests:
-    create_fuzztest_wrapper(fuzz_test_dir, test_name)
+    create_fuzztest_wrapper(fuzz_test_dir, options.executable, test_name)
 
   # This is a place holder telling GN that we're done.
-  with action_helpers.atomic_output(fuzz_test_dir / FUZZ_TEST_STAMP) as f:
+  stamp_file = FUZZ_TEST_WRAPPER_STAMP % options.executable
+  with action_helpers.atomic_output(fuzz_test_dir / stamp_file) as f:
     f.write('\n'.join(fuzz_tests).encode('utf-8'))
 
+
+def main(argv):
+  parser = argparse.ArgumentParser()
+  subps = parser.add_subparsers()
+
+  subp = subps.add_parser(
+      'init', description="Purge and set up fuzztests directory.")
+  subp.set_defaults(func=setup_fuzztests_dir)
+
+  subp = subps.add_parser(
+      'gen', description="Generate fuzztest wrappers for one executable.")
+  subp.set_defaults(func=gen_wrappers)
+
+  subp.add_argument(
+      '--executable', required=True, help='Executable file name.')
+
+  options = parser.parse_args(argv)
+  options.func(options)
 
 if __name__ == '__main__':
   main()
