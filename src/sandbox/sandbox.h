b088a18c1adf7776aa10399ae1bc5f8e7ed759c5
Stephen Roettger <sroettger@google.com>
2024-07-01 15:33:48 +0200

    [sandbox] Block sandbox access during SBXCHECK macros
    to avoid race conditions. These checks are only enabled in debug builds,
    but that should be enough to find potential issues.
    
    Change-Id: I11a07ac984122e53ba0c261bced7a7486b7858a2

--- src/sandbox/sandbox.h
+++ src/sandbox/sandbox.h
@@ -10,6 +10,7 @@
 #include "include/v8config.h"
 #include "src/base/bounds.h"
 #include "src/common/globals.h"
+#include "src/sandbox/hardware-support.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"  // nogncheck
 
 namespace v8 {
@@ -283,5 +284,55 @@ V8_INLINE void* EmptyBackingStoreBuffer() {
 
 }  // namespace internal
 }  // namespace v8
+
+// When the sandbox is enabled, a SBXCHECK behaves exactly like a CHECK, but
+// indicates that the check is required for the sandbox, i.e. prevents a
+// sandbox bypass. When the sandbox is off, it becomes a DCHECK.
+//
+// As an example, consider a scenario where an in-sandbox object stores an
+// index into an out-of-sandbox array (or a similar data structure). While
+// under normal circumstances it can be guaranteed that the index will always
+// be in bounds, with the sandbox attacker model, we have to assume that the
+// in-sandbox object can be corrupted by an attacker and so the access can go
+// out-of-bounds. In that case, a SBXCHECK can be used to both prevent memory
+// corruption outside of the sandbox and document that there is a
+// security-critical invariant that may be violated when an attacker can
+// corrupt memory inside the sandbox, but otherwise holds true.
+#ifdef V8_ENABLE_SANDBOX
+
+#ifdef DEBUG
+#define BLOCK_SANDBOX_ACCESS_IN_DEBUG_MODE \
+  auto block_access = SandboxHardwareSupport::MaybeBlockAccess()
+#else
+#define BLOCK_SANDBOX_ACCESS_IN_DEBUG_MODE
+#endif
+
+#define SBXCHECK(condition)             \
+  do {                                  \
+    BLOCK_SANDBOX_ACCESS_IN_DEBUG_MODE; \
+    CHECK(condition);                   \
+  } while (false)
+
+#define SBXCHECK_WRAPPED(CONDITION, lhs, rhs) \
+  do {                                        \
+    BLOCK_SANDBOX_ACCESS_IN_DEBUG_MODE;       \
+    CHECK_##CONDITION(lhs, rhs);              \
+  } while (false)
+
+#define SBXCHECK_EQ(lhs, rhs) SBXCHECK_WRAPPED(EQ, lhs, rhs)
+#define SBXCHECK_NE(lhs, rhs) SBXCHECK_WRAPPED(NE, lhs, rhs)
+#define SBXCHECK_GT(lhs, rhs) SBXCHECK_WRAPPED(GT, lhs, rhs)
+#define SBXCHECK_GE(lhs, rhs) SBXCHECK_WRAPPED(GE, lhs, rhs)
+#define SBXCHECK_LT(lhs, rhs) SBXCHECK_WRAPPED(LT, lhs, rhs)
+#define SBXCHECK_LE(lhs, rhs) SBXCHECK_WRAPPED(LE, lhs, rhs)
+#else
+#define SBXCHECK(condition) DCHECK(condition)
+#define SBXCHECK_EQ(lhs, rhs) DCHECK_EQ(lhs, rhs)
+#define SBXCHECK_NE(lhs, rhs) DCHECK_NE(lhs, rhs)
+#define SBXCHECK_GT(lhs, rhs) DCHECK_GT(lhs, rhs)
+#define SBXCHECK_GE(lhs, rhs) DCHECK_GE(lhs, rhs)
+#define SBXCHECK_LT(lhs, rhs) DCHECK_LT(lhs, rhs)
+#define SBXCHECK_LE(lhs, rhs) DCHECK_LE(lhs, rhs)
+#endif
 
 #endif  // V8_SANDBOX_SANDBOX_H_
