76f6cffd8aa3011e4b89509b7e24b9cde19c85e8
Marja Hölttä <marja@chromium.org>
2024-08-02 15:50:44 +0200

    [maglev] Printouts for inlinted functions' bytecodes and feedback
    When setting --print-maglev-graph, we print out the bytecode and
    the feedback for the function we're compiling. This CL extends the
    printout so that we print the same things for functions we're going
    to inline, too.
    
    Bug: v8:7700
    Change-Id: Ifd7d55dc84e3261dcc2ba9274055236704d8520a

--- src/maglev/maglev-graph-builder.cc
+++ src/maglev/maglev-graph-builder.cc
@@ -7103,6 +7103,17 @@ ReduceResult MaglevGraphBuilder::TryBuildInlinedCall(
   }
 
   compiler::BytecodeArrayRef bytecode = shared.GetBytecodeArray(broker());
+
+  if (v8_flags.print_maglev_code || v8_flags.print_maglev_graph ||
+      v8_flags.print_maglev_graphs || v8_flags.trace_maglev_graph_building ||
+      v8_flags.trace_maglev_phi_untagging || v8_flags.trace_maglev_regalloc) {
+    std::cout << "== Inlining " << Brief(*shared.object()) << std::endl;
+    BytecodeArray::Disassemble(bytecode.object(), std::cout);
+    if (v8_flags.maglev_print_feedback) {
+      i::Print(*feedback_vector->object(), std::cout);
+    }
+  }
+
   graph()->inlined_functions().push_back(
       OptimizedCompilationInfo::InlinedFunctionHolder(
           shared.object(), bytecode.object(), current_source_position_));
