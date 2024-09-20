f1838a9eccb1c68ffda0f9464dbfd9d9541e7c48
Etienne Pierre-doray <etiennep@chromium.org>
2024-09-20 19:17:09 +0000

    Add logging
    Change-Id: I4e040b8739c97b45b0a98d6f93720e20b0ad7ea9

--- src/heap/array-buffer-sweeper.cc
+++ src/heap/array-buffer-sweeper.cc
@@ -74,6 +74,7 @@ size_t ArrayBufferList::BytesSlow() const {
 bool ArrayBufferList::IsEmpty() const {
   DCHECK_IMPLIES(head_, tail_);
   DCHECK_IMPLIES(!head_, bytes_ == 0);
+  PrintF("IsEmpty %zu \n", bytes_);
   return head_ == nullptr;
 }
 
@@ -99,6 +100,9 @@ class ArrayBufferSweeper::SweepingState final {
               0);
     DCHECK_GE(
         old_bytes_ + sweeper->old_bytes_adjustment_ + sweeper->old_.bytes_, 0);
+    PrintF("MergeTo young %zu %zu\n", new_young_.bytes_,
+           sweeper->young_.bytes_);
+    PrintF("MergeTo old %zu %zu\n", new_old_.bytes_, sweeper->old_.bytes_);
     new_young_.bytes_ = young_bytes_;
     new_old_.bytes_ = old_bytes_;
     sweeper->young_.Append(new_young_);
@@ -141,6 +145,7 @@ class ArrayBufferSweeper::SweepingState::SweepingJob final : public JobTask {
         treat_all_young_as_promoted_(treat_all_young_as_promoted),
         trace_id_(trace_id),
         local_sweeper_(heap_->sweeper()) {
+    PrintF("Prepare %zu %zu\n", young_.bytes_, old_.bytes_);
     state_.young_bytes_ = young_.bytes_;
     state_.old_bytes_ = old_.bytes_;
   }
@@ -333,6 +338,8 @@ void ArrayBufferSweeper::Append(Tagged<JSArrayBuffer> object,
 
   FinishIfDone();
 
+  PrintF("Append %p %zu \n", extension, bytes);
+
   // `Heap::InYoungGeneration` during full GC with sticky markbits is generally
   // inaccurate. However, a full GC will sweep both lists and promote all to
   // old, so it doesn't matter which list initially holds the extension.
@@ -353,6 +360,8 @@ void ArrayBufferSweeper::Resize(ArrayBufferExtension* extension,
 
   ArrayBufferExtension::AccountingValue previous_value =
       extension->UpdateAccountingLength(delta);
+
+  PrintF("Resize %p %zu\n", extension, previous_value.accounting_length());
 
   switch (previous_value.age()) {
     case ArrayBufferExtension::Age::kYoung:
@@ -386,6 +395,8 @@ void ArrayBufferSweeper::Detach(ArrayBufferExtension* extension) {
 
   ArrayBufferExtension::AccountingValue previous_value =
       extension->ClearAccountingLength();
+
+  PrintF("Detach %p %zu \n", extension, previous_value.accounting_length());
 
   // We cannot free the extension eagerly here, since extensions are tracked in
   // a singly linked list. The next GC will remove it automatically.
