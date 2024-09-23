2ee329dcfb90375e236532354cacf27a3e14cff0
Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
2024-09-23 23:34:39 +0000

    [explicit-resource-management] Remove casting null to JSReceiver
    Bug:369010108
    Change-Id: I158410c4565e933a696fd04fbd15d37626408c5b

---
+++ test/mjsunit/harmony/regress/regress-369010108.js
@@ -0,0 +1,26 @@
+// Copyright 2024 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --fuzzing --disable-abortjs --disable-in-process-stack-traces
+// --assert-types --maglev --turboshaft --harmony-struct
+// --optimize-on-next-call-optimizes-to-maglev --no-lazy-feedback-allocation
+// --turbo-stress-instruction-scheduling --fuzzing --turbofan
+// --no-always-turbofan --js-explicit-resource-management
+
+function loop() {
+  try {
+    loop();
+  } catch (e) {
+  }
+  async_function();
+}
+loop();
+async function dispose_function() {
+  let stack = new AsyncDisposableStack();
+  stack.use();
+  await stack.disposeAsync();
+}
+async function async_function() {
+  await dispose_function();
+}
