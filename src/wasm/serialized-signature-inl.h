eb08647182c0c3bba440b7f55905e44af04b3efa
Clemens Backes <clemensb@chromium.org>
2024-09-25 14:46:48 +0200

    [wasm] Store canonicalized signature pointers on imports
    And use them in the generic wasm-to-js wrapper instead of the on-heap
    serialized signature, which can be manipulated.
    
    Overall we now use canonicalized signatures a lot more. This often
    allows to drop a {WasmModule*} which was only used to make sense of
    module-specific types and signatures.
    
    We now need to translate between canonical type IDs and canonical
    signatures in several places. I left a few TODOs to clean this up in
    follow-up CLs.
    
    R=jkummerow@chromium.org
    
    Bug: 354408144
    Change-Id: I1e7b86845f1f03f26a1404004c847bf1d4ef2bff

--- src/wasm/serialized-signature-inl.h
+++ src/wasm/serialized-signature-inl.h
@@ -20,6 +20,8 @@ namespace v8::internal::wasm {
 // wasm::FunctionSig into a PodArray<wasm::ValueType> and back. The serialized
 // format in the PodArray starts with the return count, followed by the return
 // types array and the parameter types array.
+// TODO(clemensb): Fully remove this; we never want to store signatures in
+// serialized form any more.
 class SerializedSignatureHelper {
  public:
   // Allocates a PodArray for the serialized signature and sets the return
@@ -35,51 +37,8 @@ class SerializedSignatureHelper {
     return result;
   }
 
-  static inline Handle<PodArray<wasm::ValueType>> SerializeSignature(
-      Isolate* isolate, const wasm::FunctionSig* sig) {
-    Handle<PodArray<wasm::ValueType>> result = NewEmptyPodArrayForSignature(
-        isolate, sig->return_count(), sig->parameter_count());
-    if (!sig->all().empty()) {
-      result->copy_in(1, sig->all().begin(),
-                      static_cast<int>(sig->all().size()));
-    }
-    return result;
-  }
-
-  static inline wasm::FunctionSig* DeserializeSignature(
-      Zone* zone, Tagged<PodArray<wasm::ValueType>> sig) {
-    int sig_size = sig->length() - 1;
-    int return_count = ReturnCount(sig);
-    int param_count = sig_size - return_count;
-    wasm::ValueType* types = zone->AllocateArray<wasm::ValueType>(sig_size);
-    if (sig_size > 0) {
-      sig->copy_out(1, types, sig_size);
-    }
-    return zone->New<wasm::FunctionSig>(return_count, param_count, types);
-  }
-
-  // Deserializes a PodArray signature into a wasm::FunctionSig. The memory
-  // allocated for the reps array is set in {out_reps} so that the caller has
-  // ownership of the allocated data.
-  static inline wasm::FunctionSig DeserializeSignature(
-      Tagged<PodArray<wasm::ValueType>> sig,
-      std::unique_ptr<wasm::ValueType[]>* out_reps) {
-    int sig_size = sig->length() - 1;
-    int return_count = ReturnCount(sig);
-    int param_count = sig_size - return_count;
-    out_reps->reset(new wasm::ValueType[sig_size]);
-    if (sig_size > 0) {
-      sig->copy_out(1, out_reps->get(), sig_size);
-    }
-    return wasm::FunctionSig(return_count, param_count, out_reps->get());
-  }
-
   static inline int ReturnCount(Tagged<PodArray<wasm::ValueType>> sig) {
     return sig->get(0).raw_bit_field();
-  }
-
-  static inline int ParamCount(Tagged<PodArray<wasm::ValueType>> sig) {
-    return sig->length() - ReturnCount(sig) - 1;
   }
 
   static inline void SetReturn(Tagged<PodArray<wasm::ValueType>> sig,
@@ -90,16 +49,6 @@ class SerializedSignatureHelper {
   static inline void SetParam(Tagged<PodArray<wasm::ValueType>> sig,
                               size_t index, wasm::ValueType type) {
     sig->set(static_cast<int>(1 + index + ReturnCount(sig)), type);
-  }
-
-  static inline wasm::ValueType GetReturn(Tagged<PodArray<wasm::ValueType>> sig,
-                                          size_t index) {
-    return sig->get(static_cast<int>(1 + index));
-  }
-
-  static inline wasm::ValueType GetParam(Tagged<PodArray<wasm::ValueType>> sig,
-                                         size_t index) {
-    return sig->get(static_cast<int>(1 + index + ReturnCount(sig)));
   }
 };
 
