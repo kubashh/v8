575b46fe3ac451f6c8e1d0b47d5d24d0fe2f92f9
Andrey Kosyakov <caseq@chromium.org>
2024-10-21 21:17:10 -0700

    Fix a couple of bugs in ArrayBufferView::GetContents()
    - detached array buffers should produce an empty span
    
    - properly handle typed arrays backed by resizeable array buffers, byte_length() is always 0 with those and GetByteLength() should be used instead [1]
    
    BUG: 356158110
    Change-Id: I70d23d8c9c979944eb1aaeff647d4be29fbcd8a1
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:v8/src/objects/js-array-buffer-inl.h;l=67;drc=e6fc2038d73ef96ff47deda3146d94d25530e13b;bpv=1;bpt=1

--- test/cctest/test-api-array-buffer.cc
+++ test/cctest/test-api-array-buffer.cc
@@ -1059,6 +1059,18 @@ TEST(ArrayBufferView_GetContentsSmallResizableDataView) {
   TestArrayBufferViewGetContent(source, expected);
 }
 
+TEST(ArrayBufferView_GetContentsResizableTypedArray) {
+  const char* source =
+      "let rsab = new ArrayBuffer(8, {maxByteLength: 8});"
+      "let array = new Uint8Array(rsab);"
+      "for (let i = 0; i < 8; ++i) {"
+      "  array[i] = i;"
+      "};"
+      "array";
+  uint8_t expected[]{0, 1, 2, 3, 4, 5, 6, 7};
+  TestArrayBufferViewGetContent(source, expected);
+}
+
 TEST(ArrayBufferView_GetContentsLargeResizableDataView) {
   const char* source =
       "let rsab = new ArrayBuffer(100, {maxByteLength: 200});"
@@ -1084,4 +1096,16 @@ TEST(ArrayBufferView_GetContentsResizableDataViewWithOffset) {
       "new DataView(rsab, 70, 9)";
   uint8_t expected[]{70, 71, 72, 73, 74, 75, 76, 77, 78, 79};
   TestArrayBufferViewGetContent(source, expected);
+}
+
+TEST(ArrayBufferView_GetContentsDetached) {
+  const char* source =
+      "let array = new Uint8Array(100);"
+      "for (let i = 0; i < 100; ++i) {"
+      "  array[i] = i;"
+      "}"
+      "const data_view = new DataView(array.buffer);"
+      "let buffer = array.buffer.transfer();"
+      "data_view";
+  TestArrayBufferViewGetContent(source, nullptr);
 }
